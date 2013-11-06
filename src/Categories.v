6e022019f7684be9af6692ca47557569408c23db
Daniel R. Grayson <dan@math.uiuc.edu>
2013-11-06 16:16:56 -0500

    Lemma isaprop_isBinaryProduct

--- src/Categories.v
+++ src/Categories.v
@@ -7,8 +7,9 @@ Local Notation "g 'o' f" := (precategories.compose f g) (at level 50).
 Module Products.
   Module Finite.
     Definition isInitial {C:precategory} (a:ob C) : Type := forall (x:ob C), iscontr (a --> x).
+
     Lemma isaprop_isInitial {C:precategory} (a:ob C) : isaprop (isInitial a).
-      unfold isInitial, isaprop.
+    Proof.
       apply impred.
       intro x.
       apply isapropiscontr.
@@ -20,10 +21,23 @@ Module Products.
       forall (p' : ob C) (f' : p' --> a) (g' : p' --> b),
         iscontr ( total2 ( fun h : p' --> p => dirprod (f o h = f') (g o h = g') )).
 
+    Lemma isaprop_isBinaryProduct {C:precategory} {a b p : ob C} (f : p --> a) (g : p --> b) :
+      isaprop (isBinaryProduct f g).
+    Proof.
+      apply impred.
+      intro p'.
+      apply impred.
+      intro f'.
+      apply impred.
+      intro g'.
+      apply isapropiscontr.
+    Defined.
 
-
-    (* Definition hasBinaryProducts (C:precategory) := forall a b : ob C, ... *)
-
+    Definition hasBinaryProducts (C:precategory) := 
+      forall a b, 
+        total2 (fun p : ob C => 
+                  total2 (fun f : p --> a => 
+                            total2 (fun g : p --> b => isBinaryProduct f g))).
 
   End Finite.
 End Products.
