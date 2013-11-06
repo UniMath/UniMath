2df06b6925e8a311f94df5dee9ec60205d4957e3
Daniel R. Grayson <dan@math.uiuc.edu>
2013-11-06 15:03:05 -0500

    start on finite products, part 2

--- src/Categories.v
+++ src/Categories.v
@@ -1,18 +1,29 @@
 Require Import RezkCompletion.precategories.
 Require Import Foundations.hlevel2.hSet.
 
-Local Notation "a --> b" := (precategory_morphisms a b)(at level 50).
+Local Notation "a --> b" := (precategory_morphisms a b) (at level 50).
+Local Notation "g 'o' f" := (precategories.compose f g) (at level 50).
 
 Module Products.
   Module Finite.
-    Definition isInitial (C:precategory) (a:ob C) : Type := forall (x:ob C), iscontr (a --> x).
-    Lemma isaprop_isInitial (C:precategory) (a:ob C) : isaprop (isInitial C a).
+    Definition isInitial {C:precategory} (a:ob C) : Type := forall (x:ob C), iscontr (a --> x).
+    Lemma isaprop_isInitial {C:precategory} (a:ob C) : isaprop (isInitial a).
       unfold isInitial, isaprop.
       apply impred.
       intro x.
       apply isapropiscontr.
     Defined.
 
-    Definition hasInitial (C:precategory) := total2 (isInitial C).
+    Definition hasInitial (C:precategory) := total2 (@isInitial C).
+
+    Definition isBinaryProduct {C:precategory} {a b p : ob C} (f : p --> a) (g : p --> b) :=
+      forall (p' : ob C) (f' : p' --> a) (g' : p' --> b),
+        iscontr ( total2 ( fun h : p' --> p => dirprod (f o h = f') (g o h = g') )).
+
+
+
+    (* Definition hasBinaryProducts (C:precategory) := forall a b : ob C, ... *)
+
+
   End Finite.
 End Products.
