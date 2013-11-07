92d8bf81c72087db26723730e5d506ecc4f6b5ca
Daniel R. Grayson <dan@math.uiuc.edu>
2013-11-07 14:08:09 -0500

    simplify

--- src/Categories.v
+++ src/Categories.v
@@ -10,41 +10,33 @@ Ltac prop_logic :=
   try (apply isapropishinh);
   try (apply isapropiscontr).
 
+Ltac prop p := apply (hProppair p); prop_logic.
+
 Module Products.
   Module Finite.
 
     Definition isInitial {C:precategory} (a:C) : hProp.
-    Proof.
-      set (prop := forall (x:C), iscontr (a --> x)).
-      apply (hProppair prop).
-      prop_logic.
+      prop (forall (x:C), iscontr (a --> x)).
     Defined.
 
     Definition hasInitial (C:precategory) : hProp.
-    Proof.
-      set (prop := ishinh (total2 (fun a:C => isInitial a))).
-      apply (hProppair prop).
-      prop_logic.
+      prop (ishinh (total2 (fun a:C => isInitial a))).
     Defined.
 
     Definition isBinaryProduct {C:precategory} {a b p : C} (f : p --> a) (g : p --> b) : hProp.
-    Proof.
-      set (prop := forall p' (f' : p' --> a) (g' : p' --> b),
-          iscontr ( total2 ( fun h => dirprod (f o h = f') (g o h = g')))).
-      apply (hProppair prop).
-      prop_logic.
+      prop (
+          forall p' (f' : p' --> a) (g' : p' --> b),
+            iscontr ( total2 ( fun h => dirprod (f o h = f') (g o h = g')))).
     Defined.
 
     Definition hasBinaryProducts (C:precategory) : hProp.
-    Proof.
-      apply (hProppair (
+      prop (
         forall a b : C, 
           ishinh (
               total2 (fun p => 
               total2 (fun f : p --> a => 
               total2 (fun g : p --> b => 
-                        isBinaryProduct f g)))))).
-      prop_logic.
+                        isBinaryProduct f g))))).
     Defined.
 
   End Finite.
