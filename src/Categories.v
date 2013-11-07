58f16d865374ac235f7cd652627761171e015778
Daniel R. Grayson <dan@math.uiuc.edu>
2013-11-07 14:01:59 -0500

    simplify

--- src/Categories.v
+++ src/Categories.v
@@ -5,7 +5,9 @@ Local Notation "a --> b" := (precategory_morphisms a b) (at level 50).
 Local Notation "g 'o' f" := (precategories.compose f g) (at level 50).
 
 Ltac prop_logic := 
+  simpl;
   repeat (try (apply isapropishinh); apply impred ; intro); 
+  try (apply isapropishinh);
   try (apply isapropiscontr).
 
 Module Products.
@@ -13,24 +15,23 @@ Module Products.
 
     Definition isInitial {C:precategory} (a:C) : hProp.
     Proof.
-      apply (hProppair (
-        forall (x:C), iscontr (a --> x))).
+      set (prop := forall (x:C), iscontr (a --> x)).
+      apply (hProppair prop).
       prop_logic.
     Defined.
 
     Definition hasInitial (C:precategory) : hProp.
     Proof.
-      apply (hProppair (
-        ishinh_UU (
-          total2 (fun a:C => isInitial a)))).
+      set (prop := ishinh (total2 (fun a:C => isInitial a))).
+      apply (hProppair prop).
       prop_logic.
     Defined.
 
     Definition isBinaryProduct {C:precategory} {a b p : C} (f : p --> a) (g : p --> b) : hProp.
     Proof.
-      apply (hProppair (
-        forall p' (f' : p' --> a) (g' : p' --> b),
-          iscontr ( total2 ( fun h => dirprod (f o h = f') (g o h = g'))))).
+      set (prop := forall p' (f' : p' --> a) (g' : p' --> b),
+          iscontr ( total2 ( fun h => dirprod (f o h = f') (g o h = g')))).
+      apply (hProppair prop).
       prop_logic.
     Defined.
 
@@ -38,7 +39,7 @@ Module Products.
     Proof.
       apply (hProppair (
         forall a b : C, 
-          ishinh_UU (
+          ishinh (
               total2 (fun p => 
               total2 (fun f : p --> a => 
               total2 (fun g : p --> b => 
