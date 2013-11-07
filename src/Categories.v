222473c7766e9f8b0d14e79d6a95713e84971d2e
Daniel R. Grayson <dan@math.uiuc.edu>
2013-11-07 14:13:37 -0500

    add terminal objects and binary coproducts

--- src/Categories.v
+++ src/Categories.v
@@ -2,7 +2,9 @@ Require Import RezkCompletion.precategories.
 Require Import Foundations.hlevel2.hSet.
 
 Local Notation "a --> b" := (precategory_morphisms a b) (at level 50).
+Local Notation "b <-- a" := (precategory_morphisms a b) (at level 50).
 Local Notation "g 'o' f" := (precategories.compose f g) (at level 50).
+Local Notation "f ;;  g" := (precategories.compose f g) (at level 50).
 
 Ltac prop_logic := 
   simpl;
@@ -19,14 +21,28 @@ Module Products.
       prop (forall (x:C), iscontr (a --> x)).
     Defined.
 
+    Definition isTerminal {C:precategory} (a:C) : hProp.
+      prop (forall (x:C), iscontr (a <-- x)).
+    Defined.
+
     Definition hasInitial (C:precategory) : hProp.
       prop (ishinh (total2 (fun a:C => isInitial a))).
+    Defined.
+
+    Definition hasTerminal (C:precategory) : hProp.
+      prop (ishinh (total2 (fun a:C => isTerminal a))).
     Defined.
 
     Definition isBinaryProduct {C:precategory} {a b p : C} (f : p --> a) (g : p --> b) : hProp.
       prop (
           forall p' (f' : p' --> a) (g' : p' --> b),
             iscontr ( total2 ( fun h => dirprod (f o h = f') (g o h = g')))).
+    Defined.
+
+    Definition isBinaryCoproduct {C:precategory} {a b p : C} (f : p <-- a) (g : p <-- b) : hProp.
+      prop (
+          forall p' (f' : p' <-- a) (g' : p' <-- b),
+            iscontr ( total2 ( fun h => dirprod (f ;; h = f') (g ;; h = g')))).
     Defined.
 
     Definition hasBinaryProducts (C:precategory) : hProp.
@@ -37,6 +53,16 @@ Module Products.
               total2 (fun f : p --> a => 
               total2 (fun g : p --> b => 
                         isBinaryProduct f g))))).
+    Defined.
+
+    Definition hasBinaryCoproducts (C:precategory) : hProp.
+      prop (
+        forall a b : C, 
+          ishinh (
+              total2 (fun p => 
+              total2 (fun f : p <-- a => 
+              total2 (fun g : p <-- b => 
+                        isBinaryCoproduct f g))))).
     Defined.
 
   End Finite.
