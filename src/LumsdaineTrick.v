5ab82229ec42932ab7766c3ff588c8ef4469e8fc
Daniel R. Grayson <dan@math.uiuc.edu>
2013-12-10 22:09:45 -0500

    working on eta expansion

--- src/LumsdaineTrick.v
+++ src/LumsdaineTrick.v
@@ -26,8 +26,20 @@ Definition etaExpandE : V' -> V'.
   exact  h.
 Defined.
 
+Definition type_equality_to_function {X Y:UU} : (X==Y) -> X -> Y.
+  intros ? ? e x. destruct e. exact x.
+Defined.
+
+Definition transport {X:UU} (P:X->UU) {x x':X} (e:x==x'): P x -> P x'.
+  (* this is a replacement for transportf in uu0.v that pushes the path
+     forward to the universe before destroying it, to make it more likely
+     we can prove it's trivial *)
+  intros ? ? ? ? e p.
+  exact (type_equality_to_function (maponpaths P e) p).
+Defined.
+
 Lemma pair_path {X:UU} {P:X->UU} {x x':X} {p: P x} {p' : P x'} 
-      (e : x == x') (e' : transportf _ e p == p') : 
+      (e : x == x') (e' : transport _ e p == p') : 
   tpair _ x p == tpair _ x' p'.
   (* compare with functtransportf in uu0.v *)
 Proof. intros. destruct e. destruct e'. apply idpath. Defined.
@@ -35,7 +47,7 @@ Proof. intros. destruct e. destruct e'. apply idpath. Defined.
 Lemma pair_path_pr1 {X:UU} {P:X->UU} {xp xp' : total2 P} (h : xp == xp') : pr1 xp == pr1 xp'.
 Proof. intros. apply (maponpaths pr1 h). Defined.
 
-Lemma pair_path_pr2 {X:UU} {P:X->UU} {xp xp' : total2 P} (h : xp == xp') : transportf P (pair_path_pr1 h) (pr2 xp) == pr2 xp'.
+Lemma pair_path_pr2 {X:UU} {P:X->UU} {xp xp' : total2 P} (h : xp == xp') : transport P (pair_path_pr1 h) (pr2 xp) == pr2 xp'.
 Proof. intros. destruct h. apply idpath. Defined.
 
 Module Attempt1.
@@ -44,6 +56,7 @@ Module Attempt1.
   Proof.
     intros [g h].    
     apply (pair_path (!etacorrection _ _ g)).
+    unfold transport, type_equality_to_function; simpl.
     (* now we're stuck with a transport over an eta correction path *)
   Abort.
 
@@ -79,23 +92,34 @@ Module Attempt3.
 
   Definition h2 : homot C' (fun f => C' (etaExpand B f)).
     intro f.
-    apply (maponpaths C' (etacorrection _ _ f)).
+    apply (maponpaths C' (!etacorrection _ _ f)).
   Defined.
 
-  Lemma H : homot2 h1 h2.
+  Lemma comprule (f : sections B) :
+    maponpaths (fun g : sections B => C (etaExpand B g)) (!etacorrection A B f) == idpath (C (fun t : A => f t)).
+  Proof.
+    admit.                      (* we take it as an axiom for now *)
+  Defined.
+
+  Lemma H : homot2 h2 h1.
   Proof.
     intro f.
     unfold h1, h2, C'.
     (* we're stuck, because we need a computation rule for etaExpand *)
-    admit.                      (* so we assume it *)
+    (* so we use the axiom we introduced above *)
+    exact (comprule f).
   Defined.
 
   (* let's see why the computation rule would have helped *)
 
   Lemma foo1 : forall v, etaExpandE v == v.
   Proof.
-    set (z := fpmaphomothomot h1 h2 H).
-
+    intros v.
+    assert (z := fpmaphomothomot h2 h1 H v).
+    destruct v as [g h].
+    apply (pair_path (!etacorrection A B g)).
+    unfold fpmaphomothomot, fpmaphomotfun,h1 in z.
+  (* it should be possible to complete this *)
   Abort.
 
 End Attempt3.
@@ -116,18 +140,19 @@ Module Attempt4.
     intros [g h]. apply idpath. 
   Defined.
 
-  Lemma transport_along_eta (g:sections B) (h:C' g) : transportf C' (!etacorrection A B g) h == h.
+  Lemma transport_along_eta (g:sections B) (h:C' g) : transport C' (!etacorrection A B g) h == h.
   Proof.
     intros.
     set (v := tpair C' g h).
     set (p := etaExpandE v).
     unfold etaExpandE, v in p.
-    set (q := pair_path_pr2 (!pair_path (!etacorrection A B g) (idpath (transportf C' (!etacorrection A B g) h))
-            @
-          (foo1 v)
+    set (q := pair_path_pr2 (
+                  !pair_path (!etacorrection A B g) (idpath (transport C' (!etacorrection A B g) h))
+                   @
+                  foo1 v
           )).
     unfold v, pr2 in q; simpl.
-    Check (q).
+    (* Check (q). *)
   (* it should be possible to complete this proof *)
   Abort.
 
