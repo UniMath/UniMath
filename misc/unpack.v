e30c8f011f9f6dd3aa4ca4c4ae5b8fb3ed525f9e
Daniel R. Grayson <dan@math.uiuc.edu>
2014-02-07 16:13:10 -0500

    experimenting with unpacking records

--- misc/unpack.v
+++ misc/unpack.v
@@ -1,32 +1,3 @@
-Module Unpack.
-  Require Import Foundations.Generalities.uu0.
-  Notation "a == b" := (paths a b) (at level 70, no associativity).
-  Notation "x ,, y" := (tpair _ x y) (at level 69, right associativity).
-  Variable A:Type.
-  Variable B:A->Type.
-  Variable C:forall a,B a->Type.
-  Variable D:forall a b, C a b->Type.
-  Record foo := make { a:A; b:B a; c:C a b; d:D a b c }.
-  Definition bar := total2 (fun 
-         a:A => total2 ( fun
-         b:B a => total2 ( fun
-         c:C a b => 
-           D a b c))).
-  Definition pack : bar -> foo
-    := fun X => make (pr1 X) (pr1 (pr2 X)) (pr1 (pr2 (pr2 X))) (pr2 (pr2 (pr2 X))).
-  Definition unpack : foo -> bar
-    := fun Y => (a Y,,(b Y,,(c Y,,d Y))).
-  Definition h (X:bar) : unpack (pack X) == X
-    := match X as t return (unpack (pack t) == t) 
-       with (a,,(b,,(c,,d))) => idpath (a,,(b,,(c,,d))) end.
-  Definition k (Y:foo) : pack (unpack Y) == Y
-    := match Y as i return (pack (unpack i) == i) 
-       with make a b c d => idpath _ end.
-  Lemma pack_weq : weq bar foo.
-  Proof. intros. exists pack. intros Y. exists (unpack Y,,k Y). intros [X m].
-         destruct m. assert (H := h X). destruct H. reflexivity. Qed.
-End Unpack.
-
 Module Unpack4.
   Inductive paths {A : Type} (a : A) : A -> Type := idpath : paths a a.
   Arguments idpath {A a} , [A] a.
@@ -62,15 +33,8 @@ Module Unpack4.
     equiv_isequiv : IsEquiv equiv_fun
   }.
   Notation "A <~> B" := (Equiv A B) (at level 85).
-  Definition ap' {A B} (f:A -> B) {x y:A} (p:x == y) : f x == f y
-    := transport (fun z => f x == f z) p (idpath (f x)).
-  Lemma apeq {A B} (f:A -> B) {x y:A} (p:x == y) : ap f p == ap' f p.
-    destruct p. reflexivity. Defined.
   Definition proj2 {X} {P:X->Type} {w w':sigT P} (p:w==w') :
     transport P (ap (@pr1 X P) p) (pr2 w) == pr2 w'.
-  Proof. destruct p. reflexivity. Defined.
-  Definition proj2' {X} {P:X->Type} {w w':sigT P} (p:w==w') :
-    transport P (ap' (@pr1 X P) p) (pr2 w) == pr2 w'.
   Proof. destruct p. reflexivity. Defined.
   Definition transport_hfiber {X Y} (f:X->Y) x (w:hfiber f (f x)) (p: (x;idpath) == w) :
     transport (fun z : X => f z == f x)
@@ -86,29 +50,25 @@ Module Unpack4.
     set (g := fun y => pr1 (pr1 (r y))). 
     set (p := fun y => pr2 (pr1 (r y)) : f (g y) == y). simpl in p.
     set (L := fun x => pr2 (r (f x)) (x; idpath (f x))).
-    set (q := fun x => ap' (@pr1 _ _) (L x) : g (f x) == x); simpl in q.
-    exact (BuildEquiv _ _ f 
-            (BuildIsEquiv f g p q 
-              (fun x => 
-                 reverse (proj2 (reverse (L x))) 
-                 @ 
-                 transport_hfiber f x (pr1 (r (f x))) (reverse (L x))
-                 @
-                 ap (ap f) 
-                    (reverse_ap (fun w : {x0 : X & f x0 == f x} => pr1 w) (L x) )))).
+    set (q := fun x => ap (@pr1 _ _) (L x) : g (f x) == x); simpl in q.
+    exact (BuildEquiv _ _ f (BuildIsEquiv f g p q 
+            (fun x => 
+               reverse (proj2 (reverse (L x))) @ 
+               transport_hfiber f x (pr1 (r (f x))) (reverse (L x)) @
+               ap (ap f) (reverse_ap (fun w => pr1 w) (L x))))).
   Defined.
   Definition IsEquiv_sig {A B : Type} (f : A -> B) :=
     { g:B->A & { r:Sect g f & { s:Sect f g & forall x : A, r (f x) == ap f (s x) }}}.
-  Definition pack {A B : Type} (f : A -> B) : (IsEquiv_sig f) -> (IsEquiv f)
+  Definition pack {A B : Type} (f : A -> B) : IsEquiv_sig f -> IsEquiv f
     := fun X => BuildIsEquiv f (pr1 X) (pr1 (pr2 X)) (pr1 (pr2 (pr2 X))) (pr2 (pr2 (pr2 X))).
   Definition unpack {A B : Type} (f : A -> B) : IsEquiv f -> IsEquiv_sig f
     := fun Y => (equiv_inv f Y;(eisretr f Y;(eissect f Y;eisadj f Y))).
-  Definition h {A B : Type} (f : A -> B) (X:(IsEquiv_sig f)) : unpack f (pack f X) == X
+  Definition h {A B : Type} (f : A -> B) (X:IsEquiv_sig f) : unpack f (pack f X) == X
     := match X as t return (unpack f (pack f t) == t) 
        with (a;(b;(c;d))) => idpath (a;(b;(c;d))) end.
-  Definition k {A B : Type} (f : A -> B) (Y:(IsEquiv f)) : pack f (unpack f Y) == Y
+  Definition k {A B : Type} (f : A -> B) (Y:IsEquiv f) : pack f (unpack f Y) == Y
     := match Y as i return (pack f (unpack f i) == i) 
-       with BuildIsEquiv equiv_inv eisretr eissect eisadj => idpath _ end.
+       with BuildIsEquiv _ equiv_inv eisretr eissect eisadj => idpath _ end.
   Lemma pack_weq {A B : Type} (f : A -> B) : weq (IsEquiv_sig f) (IsEquiv f).
   Proof. intros. exists (pack f). intros Y. exists (unpack f Y;k f Y). intros [X m].
          destruct m. assert (H := h f X). destruct H. reflexivity. Qed.
@@ -117,4 +77,41 @@ Module Unpack4.
   Proof.
     exact (weq_to_AdjointEquivalence _ _ (pack_weq f)).
   Defined.
+
+  Definition ap' {A B} (f:A -> B) {x y:A} (p:x == y) : f x == f y
+    := transport (fun z => f x == f z) p (idpath (f x)).
+  Lemma apeq {A B} (f:A -> B) {x y:A} (p:x == y) : ap f p == ap' f p.
+    destruct p. reflexivity. Defined.
+  Definition proj2' {X} {P:X->Type} {w w':sigT P} (p:w==w') :
+    transport P (ap' (@pr1 X P) p) (pr2 w) == pr2 w'.
+  Proof. destruct p. reflexivity. Defined.
 End Unpack4.
+
+Module Unpack.
+  Require Import Foundations.Generalities.uu0. (* Global Set Asymmetric Patterns. will persist *)
+  Notation "a == b" := (paths a b) (at level 70, no associativity).
+  Notation "x ,, y" := (tpair _ x y) (at level 69, right associativity).
+  Variable A:Type.
+  Variable B:A->Type.
+  Variable C:forall a,B a->Type.
+  Variable D:forall a b, C a b->Type.
+  Record foo := make { a:A; b:B a; c:C a b; d:D a b c }.
+  Definition bar := total2 (fun
+         a:A => total2 ( fun
+         b:B a => total2 ( fun
+         c:C a b =>
+           D a b c))).
+  Definition pack : bar -> foo
+    := fun X => make (pr1 X) (pr1 (pr2 X)) (pr1 (pr2 (pr2 X))) (pr2 (pr2 (pr2 X))).
+  Definition unpack : foo -> bar
+    := fun Y => (a Y,,(b Y,,(c Y,,d Y))).
+  Definition h (X:bar) : unpack (pack X) == X
+    := match X as t return (unpack (pack t) == t)
+       with (a,,(b,,(c,,d))) => idpath (a,,(b,,(c,,d))) end.
+  Definition k (Y:foo) : pack (unpack Y) == Y
+    := match Y as i return (pack (unpack i) == i)
+       with make a b c d => idpath _ end.
+  Lemma pack_weq : weq bar foo.
+  Proof. intros. exists pack. intros Y. exists (unpack Y,,k Y). intros [X m].
+         destruct m. assert (H := h X). destruct H. reflexivity. Qed.
+End Unpack.
