4a016ab61da4854992f4d11ecbbc5721395a0708
Daniel R. Grayson <dan@math.uiuc.edu>
2014-02-21 03:06:04 -0500

    clean up

--- src/Utilities.v
+++ src/Utilities.v
@@ -9,27 +9,12 @@ Require Import Foundations.hlevel2.hSet.
 Set Default Timeout 50.
 
 Definition two_cases {X Y T} : coprod X Y -> (X->T) -> (Y->T) -> T.
-  intros ? ? ? [x|y] f g. exact (f x). exact (g y). Defined.
+  exact (fun X Y T xy f g => sum_rect (fun _ => T) f g xy). Defined.
 
 Ltac exact_op x := (* from Jason Gross: same as "exact", but with unification the opposite way *)
   let T := type of x in
   let G := match goal with |- ?G => constr:(G) end in
   exact ((@id G : T -> G) x).
-
-Definition type_equality_to_function {X Y:UU} : (X==Y) -> X -> Y.
-  intros ? ? e x. destruct e. exact x.
-Defined.
-
-Definition transport {X:UU} (P:X->UU) {x x':X} (e:x==x'): P x -> P x'.
-  (* this is a replacement for transportf in uu0.v that pushes the path
-     forward to the universe before destroying it, to make it more likely
-     we can prove it's trivial *)
-  intros ? ? ? ? e p.
-  exact (type_equality_to_function (maponpaths P e) p).
-Defined.
-
-Lemma transport_idpath {X:UU} (P:X->UU) {x:X} (p:P x) : transport P (idpath x) p == p.
-Proof. intros. reflexivity. Defined.
 
 Module Import Notations.
     Notation ap := maponpaths.
@@ -40,8 +25,15 @@ Module Import Notations.
     Notation "x ,, y" := (tpair _ x y) (at level 69, right associativity).
     (* funcomp' is like funcomp, but with the arguments in the other order *)
     Definition funcomp' { X Y Z : UU } ( g : Y -> Z ) ( f : X -> Y ) := fun x : X => g ( f x ) . 
+    Notation transport := transportf.
     Notation "p # x" := (transport _ p x) (right associativity, at level 65, only parsing).
 End Notations.
+
+Lemma transport_idpath {X:UU} (P:X->UU) {x y:X} (p:x==y) (u:P x) : 
+  transport P p u == transport (idfun _) (ap P p) u.
+(* same as HoTT.PathGroupoids.transport_idmap_ap *)
+Proof. intros. destruct p. reflexivity. Defined.
+
 Definition sections {T:UU} (P:T->UU) := forall t:T, P t.
 Definition evalat {T:UU} {U:UU} (t:T) (f:T->U) := f t.
 Definition apevalat {T:UU} {U:UU} (t:T) {f g:T->U}
@@ -276,11 +268,6 @@ Proof.
   apply impred; intro.
   apply isapropiscontr.
 Defined.
-
-(* compare this to functtransportf in uu0 *)
-Lemma functtransportf_universal {X:UU} {P:X->UU} {x x':X } (e:x==x') (p:P x) :
-  type_equality_to_function (ap P e) p == e # p.
-Proof.  intros. destruct e. apply idpath. Defined.   
 
 (* from Vladimir, two lemmas, possibly useful for eta-correction: *)
 Definition fpmaphomotfun {X: UU} {P Q: X -> UU} (h: homot P Q) (xp: total2 P): total2 Q.
