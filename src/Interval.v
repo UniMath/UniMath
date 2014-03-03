3592fd57024cdfb6fb335b41df1d198eeae6598b
Daniel R. Grayson <dan@math.uiuc.edu>
2014-03-02 21:47:27 -0500

    clean up

--- src/Interval.v
+++ src/Interval.v
@@ -1,4 +1,8 @@
-(** * use propositional truncation to construct an interval *)
+(** * Use propositional truncation to construct an interval
+
+Martin Escardo's version in Agda is 
+%\href{http://www.cs.bham.ac.uk/~mhe/truncation-and-extensionality/hsetfunext.html#6060}{here}.%
+#<a href="http://www.cs.bham.ac.uk/~mhe/truncation-and-extensionality/hsetfunext.html##6060">here</a>.# *)
 
 Unset Automatic Introduction.
 Require Import Foundations.hlevel1.hProp.
@@ -7,12 +11,15 @@ Notation "! p " := (pathsinv0 p) (at level 50).
 
 Definition bool_map {Y} := bool_rect (fun _ => Y) : Y -> Y -> bool -> Y.
 
+(** ** The definition of the interval 
+
+We define the interval as the propositional truncation of [bool]. *)
+
 Definition interval := ishinh bool.
 Definition left := hinhpr _ true.
 Definition right := hinhpr _ false.
 Definition interval_path : left == right.
 Proof. apply (pr2 (ishinh _)). Defined.
-
 Definition interval_map Y (f : bool -> Y) : f true == f false -> interval -> Y.
 Proof. 
   intros ? ? e h.
@@ -24,16 +31,12 @@ Proof.
                         (bool_rect (funcomp f q) e (idpath _) v)))).
 Defined.
 
-(* verify a computation is definitional *)
+(** We verify that a computation is definitional. *)
 Goal forall Y (f : bool -> Y) (e:f true == f false) (v:bool), 
        interval_map Y f e (hinhpr _ v) == f v.
   reflexivity. Qed.
 
-(** ** use the interval to prove functional extensionality for sections
-
-       Notice that [ishinh] depends on [funextfunax],
-       so we don't get something for nothing, but this is
-       a quick way of deducing [funextsec] from [funextfunax]. *)
+(** ** Functional extensionality for sections using the interval *)
 
 Definition funextsec2 X (Y:X->Type) (f g:forall x,Y x) :
            (forall x, f x==g x) -> f == g.
@@ -41,3 +44,7 @@ Proof. intros ? ? ? ? e.
        exact (maponpaths
            (fun h x => interval_map (Y x) (bool_map (f x) (g x)) (e x) h)
            interval_path). Defined.
+
+(**    Notice that [ishinh] depends on [funextfunax],
+       so we don't get something for nothing, but this is
+       a quick way of deducing [funextsec] from [funextfunax]. *)
