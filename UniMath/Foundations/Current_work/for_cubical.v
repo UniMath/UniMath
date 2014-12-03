94cf86f1e21a58b787576d60573ea305763eb7bb
Vladimir Voevodsky <vladimir@ias.edu>
2014-12-03 10:05:19 -0500

    Added uu0_no_use_of_match, sSet and for_cubical to the git tree

---
+++ UniMath/Foundations/Current_work/for_cubical.v
@@ -0,0 +1,14 @@
+(* Vladimir Voevodsky, started Nov. 27, 2014 *)
+
+Unset Automatic Introduction.
+
+Require Export Foundations.hlevel2.finitesets.
+Require Export RezkCompletion.precategories. 
+Require Export RezkCompletion.category_hset.
+Require Export RezkCompletion.functors_transformations.
+
+
+
+
+
+(* End of the file for_cubical.v *)
