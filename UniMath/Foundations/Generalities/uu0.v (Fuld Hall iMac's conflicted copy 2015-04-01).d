5cb1d9618f596384bf81d0a9d43597a3f3acfd73
Vladimir Voevodsky <vladimir@ias.edu>
2015-05-26 18:05:25 +0200

    Delete uu0.v (Fuld Hall iMac's conflicted copy 2015-04-01).d
    Cleaning up

--- UniMath/Foundations/Generalities/uu0.v (Fuld Hall iMac's conflicted copy 2015-04-01).d
+++
@@ -1,2 +0,0 @@
-UniMath/Foundations/Generalities/uu0.vo UniMath/Foundations/Generalities/uu0.glob UniMath/Foundations/Generalities/uu0.v.beautified: UniMath/Foundations/Generalities/uu0.v UniMath/Foundations/Generalities/uuu.vo
-UniMath/Foundations/Generalities/uu0.vi: UniMath/Foundations/Generalities/uu0.v UniMath/Foundations/Generalities/uuu.vi
