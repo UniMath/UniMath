70471e2169c641257e83c9e24adf877a24b1f3e7
Ralph Matthes <Ralph.Matthes@irit.fr>
2015-06-24 10:30:05 +0200

    theorem completed

--- GenMendlerIteration.v
+++ GenMendlerIteration.v
@@ -66,6 +66,8 @@ Let η : nat_trans _ _ := eta_from_left_adjoint is_left_adj_L.
 Let ε : nat_trans _ _ := eps_from_left_adjoint is_left_adj_L.
 Let φ_natural_precomp := @SubstSystems.AdjunctionHomTypesWeq.φ_natural_precomp _ _ _ is_left_adj_L.
 Let φ_inv_natural_precomp := @SubstSystems.AdjunctionHomTypesWeq.φ_inv_natural_precomp _ _ _ is_left_adj_L.
+Let φ_after_φ_inv := @AdjunctionHomTypesWeq.φ_after_φ_inv _ _ _ is_left_adj_L.
+Let φ_inv_after_φ := @AdjunctionHomTypesWeq.φ_inv_after_φ _ _ _ is_left_adj_L.
 
 
 Arguments φ {_ _} _ .
@@ -82,13 +84,14 @@ Variable ψ : ψ_source ⟶ ψ_target.
 
 Definition It : L μF ⇒ X := φ_inv (iter (φ (ψ (R X) (ε X)))).
 
-(* what is the usual name of the following lemma? *)
+(* what is the usual name of the following lemma? it is toforallpaths
 Lemma aux0 (A B: UU)(f g: A -> B)(a: A): f = g -> f a = g a.
 Proof.
   intro Hyp.
   rewrite Hyp.
   apply idpath.
 Qed.
+*)
    
 Lemma ψ_naturality (A B: C)(h: B ⇒ A)(f: L A ⇒ X): ψ B (#L h;; f) = #L (#F h);; ψ A f.
 Proof.
@@ -96,7 +99,8 @@ Proof.
   simpl.
   red in ψ0_is_nat.
   assert (ψ0_is_nat_inst1 := ψ0_is_nat _ _ h).
-  assert (ψ0_is_nat_inst2 := aux0 _ _ _ _ f ψ0_is_nat_inst1).
+  (* assert (ψ0_is_nat_inst2 := aux0 _ _ _ _ f ψ0_is_nat_inst1). *)
+  assert (ψ0_is_nat_inst2 := toforallpaths _ _ _ ψ0_is_nat_inst1 f).
   apply ψ0_is_nat_inst2.
 Qed.
 
@@ -121,14 +125,14 @@ Focus 2.
   rewrite id_right.
   apply pathsinv0.
   change (φ_inv(φ h) = h).
-  apply AdjunctionHomTypesWeq.φ_inv_after_φ.
+  apply φ_inv_after_φ.
 Qed.
 
 Lemma cancel_φ {A: C}{B: C'} (f g : L A ⇒ B): φ f = φ g -> f = g.
 Proof.
   intro Hyp.
-  rewrite <- (AdjunctionHomTypesWeq.φ_inv_after_φ _ _ _ is_left_adj_L f).
-  rewrite <- (AdjunctionHomTypesWeq.φ_inv_after_φ _ _ _ is_left_adj_L g).
+  rewrite <- (φ_inv_after_φ _ _ f).
+  rewrite <- (φ_inv_after_φ _ _ g).
   apply maponpaths.
   exact Hyp.
 Qed.
@@ -142,9 +146,34 @@ Proof.
     rewrite φ_ψ_μF_eq.
     rewrite φ_natural_precomp.
     unfold It.
-    do 2 rewrite (AdjunctionHomTypesWeq.φ_after_φ_inv _ _ _ is_left_adj_L).
-    assert (iter_eq := algebra_mor_commutes _ _ _ _ (InitialArrow _ μF_Initial ⟨_,φ (ψ (R X) (ε X))⟩)).   exact iter_eq.
-  - admit.
-Admitted.
+    do 2 rewrite φ_after_φ_inv.
+    assert (iter_eq := algebra_mor_commutes _ _ _ _ (InitialArrow _ μF_Initial ⟨_,φ (ψ (R X) (ε X))⟩)).
+    exact iter_eq.
+  - intros [h h_rec_eq]; simpl.
+    assert (same: h = It).
+Focus 2.
+    apply (total2_paths_second_isaprop).
+    + simpl.
+    apply hsC'.
+Focus 2.
+    simpl.
+    exact same.
+
+    apply cancel_φ.
+    unfold It.
+    rewrite φ_after_φ_inv.
+    assert (iter_uniq := pr2 (pr2 μF_Initial ⟨_,φ (ψ (R X) (ε X))⟩)).
+    simpl in iter_uniq.
+    assert(φh_is_alg_mor: inF ;; φ h = #F(φ h) ;; φ (ψ (R X) (ε X))).
+      (* remark: I am missing a definition of the algebra morphism property in UniMath.RezkCompletion.FunctorAlgebras *)
+    + rewrite <- φ_ψ_μF_eq. 
+      rewrite <- φ_natural_precomp.
+      apply maponpaths.
+      exact h_rec_eq.
+    + set(φh_alg_mor := tpair _ _ φh_is_alg_mor : pr1 μF_Initial ⇒ ⟨ R X, φ (ψ (R X) (ε X)) ⟩).
+      assert (iter_uniq_inst := iter_uniq φh_alg_mor). clear iter_uniq.
+      apply (maponpaths pr1) in iter_uniq_inst.
+      exact iter_uniq_inst.
+Qed.
 
 End GenMenIt.
