08a149254bfd6f31b28896595d9d002dae5e9c63
Ralph Matthes <matthes@irit.fr>
2015-07-10 22:44:19 +0200

    new lemma eq_ptd_mor_precat with maybe too complicated proof (already used in previous commit!)

--- PointedFunctors.v
+++ PointedFunctors.v
@@ -97,6 +97,14 @@ Proof.
   exact (nat_trans_id _ ).
 Defined.
 
+Lemma eq_ptd_mor_precat {F G : precategory_Ptd} (a b : F ⇒ G) 
+  : a = b ≃ (a : ptd_mor F G) = b.
+Proof.
+  refine (tpair _ _ _).
+  intro H.
+  exact H.
+  apply idisweq.
+Qed.
 
 (** Forgetful functor to functor category *)
 
