e46df9950f91415c43b5e634a0d7fe3540fd9dc9
cathlelay <clelay@ias.edu>
2015-10-06 14:44:20 -0400

    Results about hq

--- UniMath/Dedekind/Complements.v
+++ UniMath/Dedekind/Complements.v
@@ -59,28 +59,33 @@ Definition lePoset {X : Poset} : hrel X :=
   end.
 Definition subset (X : hSet) : hSet := hSetpair _ (isasethsubtypes X).
 
-Definition isUpperBound {X : Poset} (E : subset X) (ub : X) :=
-  forall x : X, E x -> lePoset x ub.
-Definition isSmallerThanUpperBounds {X : Poset} (E : subset X) (lub : X) :=
-  forall ub : X, isUpperBound E ub -> lePoset lub ub.
+Definition isUpperBound {X : UU} (le : po X) (E : X -> hProp) (ub : X) :=
+  forall x : X, E x -> le x ub.
+Definition isSmallerThanUpperBounds {X : UU} le E (lub : X) :=
+  forall ub : X, isUpperBound le E ub -> le lub ub.
 
-Definition isLeastUpperBound {X : Poset} (E : subset X) (lub : X) :=
-  dirprod (isUpperBound E lub) (isSmallerThanUpperBounds E lub).
-Definition LeastUpperBound {X : Poset} (E : subset X) :=
-  total2 (isLeastUpperBound E).
-Definition pairLeastUpperBound {X : Poset} (E : subset X) (lub : X)
-           (is : isLeastUpperBound E lub) : LeastUpperBound E :=
-  tpair (isLeastUpperBound E) lub is.
-Definition pr1LeastUpperBound {X : Poset} {E : X -> hProp} :
-  LeastUpperBound E -> X := pr1.
+Definition isLeastUpperBound {X : UU} le E (lub : X) :=
+  dirprod (isUpperBound le E lub) (isSmallerThanUpperBounds le E lub).
+Definition LeastUpperBound {X : UU} (le : po X) E :=
+  total2 (isLeastUpperBound le E).
+Definition pairLeastUpperBound {X : UU} le E (lub : X)
+           (is : isLeastUpperBound le E lub) : LeastUpperBound le E :=
+  tpair (isLeastUpperBound le E) lub is.
+Definition pr1LeastUpperBound {X : UU} {le : po X} {E : X -> hProp} :
+  LeastUpperBound le E -> X := pr1.
 
-Definition isCompleteSet (X : Poset) :=
-  forall E : subset X,
-    hexists (isUpperBound E) -> hexists E -> LeastUpperBound E.
-Definition CompleteSet :=
-  total2 (fun X : Poset => isCompleteSet X).
-Definition pr1CompletePartialOrder : CompleteSet -> Poset := pr1.
-Coercion pr1CompletePartialOrder : CompleteSet >-> Poset.
+Definition isCompleteOrder {X : UU} (le : po X) :=
+  forall E,
+    hexists (isUpperBound le E) -> hexists E -> LeastUpperBound le E.
+Definition CompleteOrder (X : UU) :=
+  total2 (fun le : po X => isCompleteOrder le).
+Definition pr1CompleteOrder {X : UU} : CompleteOrder X -> hrel X := pr1.
+Coercion pr1CompleteOrder : CompleteOrder >-> hrel.
+
+Definition isCompleteSet (X : UU) := CompleteOrder X.
+Definition CompleteSet := total2 (fun X : UU => isCompleteSet X).
+Definition pr1CompleteSet : CompleteSet -> UU := pr1.
+Coercion pr1CompleteSet : CompleteSet >-> UU.
 
 (** ** for RationalNumbers.v *)
 
@@ -92,6 +97,96 @@ Notation "x <= y" := (hqleh x y) : hq_scope.
 Notation "x >= y" := (hqgeh x y) : hq_scope.
 Notation "x < y" := (hqlth x y) : hq_scope.
 Notation "x > y" := (hqgth x y) : hq_scope.
+Notation "/ x" := (hqmultinv x) : hq_scope.
+Notation "x / y" := (hqdiv x y) : hq_scope.
+Notation "2" := (hztohq (nattohz 2)) : hq_scope.
+
+Lemma hztohq_0 :
+  hztohq 0%hz = 0.
+Proof.
+  now apply isunital1funtofldfrac.
+Qed.
+Lemma hztohq_1 :
+  hztohq 1%hz = 1.
+Proof.
+  now apply isunital2funtofldfrac.
+Qed.
+
+Lemma hq2eq1plus1 :
+  2 = 1 + 1.
+Proof.
+  rewrite <- hztohq_1, <- nattohzand1.
+  now rewrite <- hztohqandplus, <- nattohzandplus.
+Qed.
+
+Lemma hq2_gt0 : 2 > 0.
+Proof.
+  rewrite <- hztohq_0, <- nattohzand0.
+  now apply hztohqandgth, nattohzandgth.
+Qed.
+Lemma hq1_gt0 : 1 > 0.
+Proof.
+  rewrite <- hztohq_0, <- hztohq_1.
+  rewrite <- nattohzand1, <- nattohzand0.
+  now apply hztohqandgth, nattohzandgth.
+Qed.
+Lemma hqgth_hqneq :
+  forall x y : hq, x > y -> hqneq x y.
+Proof.
+  intros x y Hlt Heq.
+  rewrite Heq in Hlt.
+  now apply isirreflhqgth with y.
+Qed.
+
+Lemma hqldistr :
+  forall x y z, x * (y + z) = x * y + x * z.
+Proof.
+  intros x y z.
+  now apply rngldistr.
+Qed.
+
+Lemma hqmult2r :
+  forall x : hq, x * 2 = x + x.
+Proof.
+  intros x.
+  now rewrite hq2eq1plus1, hqldistr, (hqmultr1 x).
+Qed.
+
+Lemma hqlth_between :
+  forall x y : hq, x < y -> total2 (fun z => dirprod (x < z) (z < y)).
+Proof.
+  assert (H : forall x : hq, x = (x + x) / 2).
+  { intros x.
+    apply hqmultrcan with 2.
+    now apply hqgth_hqneq, hq2_gt0.
+    unfold hqdiv.
+    rewrite hqmultassoc.
+    rewrite (hqislinvmultinv 2).
+    2: now apply hqgth_hqneq, hq2_gt0.
+    rewrite (hqmultr1 (x + x)).
+    apply hqmult2r.
+  }
+  assert (H0 : / 2 > 0).
+  { apply hqgthandmultlinv with 2.
+    apply hq2_gt0.
+    rewrite hqisrinvmultinv, hqmultx0.
+    now apply hq1_gt0.
+    now apply hqgth_hqneq, hq2_gt0.
+  }
+  intros x y Hlt.
+  exists ((x + y) / 2).
+  split.
+  - rewrite (H x).
+    apply (hqlthandmultr _ _ (/ 2)).
+    exact H0.
+    rewrite <- (H x).
+    now apply (hqlthandplusl _ _ x Hlt).
+  - rewrite (H y).
+    apply (hqlthandmultr _ _ (/ 2)).
+    exact H0.
+    rewrite <- (H y).
+    now apply (hqlthandplusr _ _ y Hlt).
+Qed.
 
 Lemma hq0lehandplus:
   forall n m : hq,
