a58de071edec84140cd20a7b9b2e0b877d0dd948
Anders Mörtberg <andersmortberg@gmail.com>
2015-10-11 22:29:02 -0400

    Move and clean colimits.v

--- UniMath/CategoryTheory/colimits/colimits.v
+++ UniMath/CategoryTheory/colimits/colimits.v
@@ -13,10 +13,6 @@ Require Import UniMath.CategoryTheory.UnicodeNotations.
 
 Local Notation "# F" := (functor_on_morphisms F)(at level 3).
 (* Local Notation "F ⟶ G" := (nat_trans F G) (at level 39). *)
-(* Local Notation "G □ F" := (functor_composite _ _ _ F G) (at level 35). *)
-
-Ltac pathvia b := (apply (@pathscomp0 _ _ b _ )).
-
 Local Notation "C ⟦ a , b ⟧" := (precategory_morphisms (C:=C) a b) (at level 50).
 
 Section move_upstream.
@@ -81,25 +77,14 @@ Definition isColimitCocone {g : graph} (d : diagram g C) (c0 : C)
     (Hc : ∀ (u v : vertex g) (e : edge u v), dmor d e ;; fc v = fc u),
   iscontr (Σ x : C⟦c0,c⟧, ∀ (v : vertex g), f v ;; x = fc v).
 
-(* Definition isCoproductCocone (a b co : C) (ia : a ⇒ co) (ib : b ⇒ co) :=  *)
-(*   ∀ (c : C) (f : a ⇒ c) (g : b ⇒ c), *)
-(*     iscontr (Σ fg : co ⇒ c, (ia ;; fg = f) × (ib ;; fg = g)). *)
-
 Definition ColimitCocone {g : graph} (d : diagram g C) : UU :=
   Σ (A : Σ c0f : (Σ c0 : C, ∀ (v : vertex g), C⟦dob d v,c0⟧),
    ∀ (u v : vertex g) (e : edge u v), dmor d e ;; (pr2 c0f) v = (pr2 c0f) u),
     isColimitCocone d (pr1 (pr1 A)) (pr2 (pr1 A)) (pr2 A).
 
-(* Definition CoproductCocone (a b : C) :=  *)
-(*    Σ coiaib : (Σ co : C, a ⇒ co × b ⇒ co), *)
-(*           isCoproductCocone a b (pr1 coiaib) (pr1 (pr2 coiaib)) (pr2 (pr2 coiaib)). *)
-
 Definition Colimits : UU := ∀ {g : graph} (d : diagram g C), ColimitCocone d.
 Definition hasColimits : UU  :=
   ∀ {g : graph} (d : diagram g C), ishinh (ColimitCocone d).
-
-(* Definition Coproducts := ∀ (a b : C), CoproductCocone a b. *)
-(* Definition hasCoproducts := ishinh Coproducts. *)
 
 Definition ColimitBottom {g : graph} {d : diagram g C}
   (CC : ColimitCocone d) : C := pr1 (pr1 (pr1 CC)).
@@ -112,12 +97,6 @@ Definition ColimitInCommutes {g : graph} {d : diagram g C}
   (CC : ColimitCocone d) : ∀ (u v : vertex g) (e : edge u v),
    dmor d e ;; ColimitIn CC v = ColimitIn CC u :=
      pr2 (pr1 CC).
-
-(* Definition CoproductObject {a b : C} (CC : CoproductCocone a b) : C := pr1 (pr1 CC). *)
-(* Definition CoproductIn1 {a b : C} (CC : CoproductCocone a b): a ⇒ CoproductObject CC := *)
-(*   pr1 (pr2 (pr1 CC)). *)
-(* Definition CoproductIn2 {a b : C} (CC : CoproductCocone a b) : b ⇒ CoproductObject CC := *)
-(*    pr2 (pr2 (pr1 CC)). *)
 
 Definition isColimitCocone_ColimitCocone {g : graph} {d : diagram g C}
   (CC : ColimitCocone d) :
@@ -132,18 +111,6 @@ Proof.
 exact (pr1 (pr1 (isColimitCocone_ColimitCocone CC c fc Hc))).
 Defined.
 
-(* Definition isCoproductCocone_CoproductCocone {a b : C} (CC : CoproductCocone a b) :  *)
-(*    isCoproductCocone a b  (CoproductObject CC) (CoproductIn1 CC) (CoproductIn2 CC). *)
-(* Proof. *)
-(*   exact (pr2 CC). *)
-(* Defined. *)
-
-(* Definition CoproductArrow {a b : C} (CC : CoproductCocone a b) {c : C} (f : a ⇒ c) (g : b ⇒ c) :  *)
-(*       CoproductObject CC ⇒ c. *)
-(* Proof. *)
-(*   exact (pr1 (pr1 (isCoproductCocone_CoproductCocone CC _ f g))). *)
-(* Defined. *)
-
 Lemma ColimitArrowCommutes {g : graph} {d : diagram g C} (CC : ColimitCocone d)
   (c : C) (fc : ∀ (v : vertex g), C⟦dob d v,c⟧)
   (Hc : ∀ (u v : vertex g) (e : edge u v), dmor d e ;; fc v = fc u)
@@ -152,20 +119,6 @@ Lemma ColimitArrowCommutes {g : graph} {d : diagram g C} (CC : ColimitCocone d)
 Proof.
 exact ((pr2 (pr1 (isColimitCocone_ColimitCocone CC _ fc Hc))) u).
 Qed.
-
-(* Lemma CoproductIn1Commutes (a b : C) (CC : CoproductCocone a b): *)
-(*      ∀ (c : C) (f : a ⇒ c) g, CoproductIn1 CC ;; CoproductArrow CC f g  = f. *)
-(* Proof. *)
-(*   intros c f g. *)
-(*   exact (pr1 (pr2 (pr1 (isCoproductCocone_CoproductCocone CC _ f g)))). *)
-(* Qed. *)
-
-(* Lemma CoproductIn2Commutes (a b : C) (CC : CoproductCocone a b): *)
-(*      ∀ (c : C) (f : a ⇒ c) g, CoproductIn2 CC ;; CoproductArrow CC f g = g. *)
-(* Proof. *)
-(*   intros c f g. *)
-(*   exact (pr2 (pr2 (pr1 (isCoproductCocone_CoproductCocone CC _ f g)))). *)
-(* Qed. *)
 
 Lemma ColimitArrowUnique {g : graph} {d : diagram g C} (CC : ColimitCocone d)
   (c : C) (fc : ∀ (v : vertex g), C⟦dob d v,c⟧)
@@ -176,17 +129,6 @@ Lemma ColimitArrowUnique {g : graph} {d : diagram g C} (CC : ColimitCocone d)
 Proof.
 now apply path_to_ctr, Hk.
 Qed.
-
-(* Lemma CoproductArrowUnique (a b : C) (CC : CoproductCocone a b) (x : C) *)
-(*     (f : a ⇒ x) (g : b ⇒ x) (k : CoproductObject CC ⇒ x) : *)
-(*     CoproductIn1 CC ;; k = f → CoproductIn2 CC ;; k = g → *)
-(*       k = CoproductArrow CC f g. *)
-(* Proof. *)
-(*   intros H1 H2. *)
-(*   set (H := tpair (λ h, dirprod _ _ ) k (dirprodpair H1 H2)). *)
-(*   set (H' := (pr2 (isCoproductCocone_CoproductCocone CC _ f g)) H). *)
-(*   apply (base_paths _ _ H'). *)
-(* Qed. *)
 
 Lemma Cocone_postcompose {g : graph} {d : diagram g C}
   (c : C) (fc : ∀ (v : vertex g), C⟦dob d v,c⟧)
@@ -205,14 +147,6 @@ Proof.
 apply ColimitArrowUnique.
 now intro u; apply idpath.
 Qed.
-
-(* Lemma CoproductArrowEta (a b : C) (CC : CoproductCocone a b) (x : C) *)
-(*     (f : CoproductObject CC ⇒ x) :  *)
-(*     f = CoproductArrow CC (CoproductIn1 CC ;; f) (CoproductIn2 CC ;; f). *)
-(* Proof. *)
-(*   apply CoproductArrowUnique; *)
-(*   apply idpath. *)
-(* Qed. *)
 
 Definition ColimitOfArrows {g : graph} {d1 d2 : diagram g C}
   (CC1 : ColimitCocone d1) (CC2 : ColimitCocone d2)
@@ -236,19 +170,6 @@ Proof.
 now unfold ColimitOfArrows; intro u; rewrite ColimitArrowCommutes.
 Qed.
 
-(* Definition CoproductOfArrows {a b : C} (CCab : CoproductCocone a b) {c d : C} *)
-(*     (CCcd : CoproductCocone c d) (f : a ⇒ c) (g : b ⇒ d) :  *)
-(*           CoproductObject CCab ⇒ CoproductObject CCcd := *)
-(*     CoproductArrow CCab (f ;; CoproductIn1 CCcd) (g ;; CoproductIn2 CCcd). *)
-
-(* Lemma CoproductOfArrowsIn1 {a b : C} (CCab : CoproductCocone a b) {c d : C} *)
-(*     (CCcd : CoproductCocone c d) (f : a ⇒ c) (g : b ⇒ d) :  *)
-(*     CoproductIn1 CCab ;; CoproductOfArrows CCab CCcd f g = f ;; CoproductIn1 CCcd. *)
-(* Proof. *)
-(*   unfold CoproductOfArrows. *)
-(*   apply CoproductIn1Commutes. *)
-(* Qed. *)
-
 Lemma preCompWithColimitOfArrows_subproof {g : graph} {d1 d2 : diagram g C}
   (CC1 : ColimitCocone d1) (CC2 : ColimitCocone d2)
   (f : ∀ (u : vertex g), C⟦dob d1 u,dob d2 u⟧)
@@ -262,21 +183,6 @@ Proof.
 intros u v e; simpl.
 now rewrite <- (Hx u v e), !assoc, fNat.
 Qed.
-
-(* Lemma precompWithCoproductArrow {a b : C} (CCab : CoproductCocone a b) {c d : C} *)
-(*     (CCcd : CoproductCocone c d) (f : a ⇒ c) (g : b ⇒ d)  *)
-(*     {x : C} (k : c ⇒ x) (h : d ⇒ x) :  *)
-(*         CoproductOfArrows CCab CCcd f g ;; CoproductArrow CCcd k h =  *)
-(*          CoproductArrow CCab (f ;; k) (g ;; h). *)
-(* Proof. *)
-(*   apply CoproductArrowUnique. *)
-(*   - rewrite assoc. rewrite CoproductOfArrowsIn1. *)
-(*     rewrite <- assoc, CoproductIn1Commutes. *)
-(*     apply idpath. *)
-(*   - rewrite assoc, CoproductOfArrowsIn2. *)
-(*     rewrite <- assoc, CoproductIn2Commutes. *)
-(*     apply idpath. *)
-(* Qed. *)
 
 Lemma precompWithColimitOfArrows {g : graph} (d1 d2 : diagram g C)
   (CC1 : ColimitCocone d1) (CC2 : ColimitCocone d2)
@@ -349,12 +255,6 @@ refine (ColimitOfArrows _ _ _ _ _).
 - now intro u; apply (# (pr1 (dob D u)) f).
 - abstract (now intros u v e; simpl; apply pathsinv0, (nat_trans_ax (dmor D e))).
 Defined.
-
-(*
-Definition coproduct_functor_mor (c c' : C) (f : c ⇒ c')
-  : coproduct_functor_ob c ⇒ coproduct_functor_ob c'
-  := CoproductOfArrows _ _ _ (#F f) (#G f).
- *)
 
 Definition ColimitFunctor_data : functor_data A C :=
   tpair _ _ ColimitFunctor_mor.
@@ -436,186 +336,7 @@ Proof.
 now intros g d; apply ColimitFunctorCocone.
 Qed.
 
-(* Lemma is_nat_trans_coproduct_nat_trans_in1_data  *)
-(*   : is_nat_trans _ _ coproduct_nat_trans_in1_data. *)
-(* Proof. *)
-(*   unfold is_nat_trans. *)
-(*   intros c c' f. *)
-(*   unfold coproduct_nat_trans_in1_data. *)
-(*   unfold coproduct_functor. simpl. *)
-(*   unfold coproduct_functor_mor. *)
-(*   assert (XX:= CoproductOfArrowsIn1).  *)
-(*   assert (X1 := XX _ (F c) (G c) (HD (F c) (G c))). *)
-(*   assert (X2 := X1 _ _ (HD (F c') (G c'))). *)
-(*   rewrite X2. *)
-(*   apply idpath. *)
-(* Qed.   *)
-
-(* Definition coproduct_nat_trans_in1 : nat_trans _ _  *)
-(*   := tpair _ _ is_nat_trans_coproduct_nat_trans_in1_data. *)
-
-(* Definition coproduct_nat_trans_in2_data : ∀ c, G c ⇒ coproduct_functor c *)
-(*   := λ c : C, CoproductIn2 _ (HD (F c) (G c)). *)
-
-(* Lemma is_nat_trans_coproduct_nat_trans_in2_data  *)
-(*   : is_nat_trans _ _ coproduct_nat_trans_in2_data. *)
-(* Proof. *)
-(*   unfold is_nat_trans. *)
-(*   intros c c' f. *)
-(*   unfold coproduct_nat_trans_in2_data. *)
-(*   unfold coproduct_functor. simpl. *)
-(*   unfold coproduct_functor_mor. *)
-(*   assert (XX:= CoproductOfArrowsIn2).  *)
-(*   assert (X1 := XX _ (F c) (G c) (HD (F c) (G c))). *)
-(*   assert (X2 := X1 _ _ (HD (F c') (G c'))). *)
-(*   rewrite X2. *)
-(*   apply idpath. *)
-(* Qed.   *)
-
-(* Definition coproduct_nat_trans_in2 : nat_trans _ _  *)
-(*   := tpair _ _ is_nat_trans_coproduct_nat_trans_in2_data. *)
-
-
-(* Section vertex. *)
-
-(* (** The coproduct morphism of a diagram with vertex [A] *) *)
-
-(* Variable A : functor C D. *)
-(* Variable f : F ⟶ A. *)
-(* Variable g : G ⟶ A. *)
-
-(* Definition coproduct_nat_trans_data : ∀ c, coproduct_functor c ⇒ A c. *)
-(* Proof. *)
-(*   intro c. *)
-(*   apply CoproductArrow. *)
-(*   - exact (f c). *)
-(*   - exact (g c). *)
-(* Defined. *)
-
-(* Lemma is_nat_trans_coproduct_nat_trans_data : is_nat_trans _ _ coproduct_nat_trans_data. *)
-(* Proof. *)
-(*   intros a b k. *)
-(*   simpl. *)
-(*   unfold coproduct_functor_mor. *)
-(*   unfold coproduct_nat_trans_data. *)
-(*   simpl. *)
-(*   set (XX:=precompWithCoproductArrow). *)
-(*   set (X1 := XX D _ _ (HD (F a) (G a))). *)
-(*   set (X2 := X1 _ _ (HD (F b) (G b))). *)
-(*   rewrite X2. *)
-(*   clear X2 X1 XX. *)
-(*   set (XX:=postcompWithCoproductArrow). *)
-(*   set (X1 := XX D _ _ (HD (F a) (G a))).  *)
-(*   rewrite X1. *)
-(*   rewrite (nat_trans_ax f). *)
-(*   rewrite (nat_trans_ax g). *)
-(*   apply idpath. *)
-(* Qed. *)
-
-(* Definition coproduct_nat_trans : nat_trans _ _  *)
-(*   := tpair _ _ is_nat_trans_coproduct_nat_trans_data. *)
-
-(* Lemma coproduct_nat_trans_In1Commutes :  *)
-(*   nat_trans_comp _ _ _ coproduct_nat_trans_in1 coproduct_nat_trans = f. *)
-(* Proof. *)
-(*   apply nat_trans_eq. *)
-(*   - apply hsD. *)
-(*   - intro c; simpl. *)
-(*     apply CoproductIn1Commutes. *)
-(* Qed.  *)
-
-(* Lemma coproduct_nat_trans_In2Commutes :  *)
-(*   nat_trans_comp _ _ _ coproduct_nat_trans_in2 coproduct_nat_trans = g. *)
-(* Proof. *)
-(*   apply nat_trans_eq. *)
-(*   - apply hsD. *)
-(*   - intro c; simpl. *)
-(*     apply CoproductIn2Commutes. *)
-(* Qed.  *)
-
-(* End vertex. *)
-
-
-(* Lemma coproduct_nat_trans_univ_prop (A : [C, D, hsD]) *)
-(*   (f : (F : [C,D,hsD]) ⇒ A) (g : (G : [C,D,hsD]) ⇒ A) : *)
-(*    ∀ *)
-(*    t : Σ fg : (coproduct_functor:[C,D,hsD]) ⇒ A, *)
-(*        (coproduct_nat_trans_in1 : (F:[C,D,hsD]) ⇒ coproduct_functor);; fg = f  *)
-(*       ×  *)
-(*        (coproduct_nat_trans_in2: (G : [C,D,hsD]) ⇒ coproduct_functor);; fg = g, *)
-(*    t = *)
-(*    tpair *)
-(*      (λ fg : (coproduct_functor:[C,D,hsD]) ⇒ A, *)
-(*       (coproduct_nat_trans_in1 : (F:[C,D,hsD]) ⇒ coproduct_functor);; fg = f  *)
-(*    ×  *)
-(*       (coproduct_nat_trans_in2 : (G:[C,D,hsD]) ⇒ coproduct_functor) ;; fg = g) *)
-(*      (coproduct_nat_trans A f g) *)
-(*      (dirprodpair (coproduct_nat_trans_In1Commutes A f g) *)
-(*         (coproduct_nat_trans_In2Commutes A f g)). *)
-(* Proof. *)
-(*   intro t. *)
-(*   simpl in *. *)
-(*   destruct t as [t1 [ta tb]]. *)
-(*   simpl in *. *)
-(*   apply (total2_paths_second_isaprop). *)
-(*   - apply isapropdirprod; *)
-(*     apply isaset_nat_trans; *)
-(*     apply hsD. *)
-(*   - simpl. *)
-(*     apply nat_trans_eq. *)
-(*     + apply hsD. *)
-(*     + intro c. *)
-(*       unfold coproduct_nat_trans. *)
-(*       simpl. *)
-(*       unfold coproduct_nat_trans_data. *)
-(*       simpl. *)
-(*       apply CoproductArrowUnique. *)
-(*       * apply (nat_trans_eq_pointwise _ _ _ _ _ _ ta). *)
-(*       * apply (nat_trans_eq_pointwise _ _ _ _ _ _ tb). *)
-(* Qed. *)
-
-
-(* Definition functor_precat_coproduct_cocone  *)
-(*   : CoproductCocone [C, D, hsD] F G. *)
-(* Proof. *)
-(*   exists (tpair _ coproduct_functor (dirprodpair coproduct_nat_trans_in1  *)
-(*                                                  coproduct_nat_trans_in2)). *)
-(*   intros A f g. *)
-(*   exists (tpair _ (coproduct_nat_trans A f g) *)
-(*              (dirprodpair (coproduct_nat_trans_In1Commutes _ _ _ ) *)
-(*                           (coproduct_nat_trans_In2Commutes _ _ _ ))). *)
-(*   simpl. *)
-(*   apply coproduct_nat_trans_univ_prop. *)
-(* Defined. *)
-
-(* End coproduct_functor. *)
-
-
-(* Definition Coproducts_functor_precat : Coproducts [C, D, hsD]. *)
-(* Proof. *)
-(*   intros F G. *)
-(*   apply functor_precat_coproduct_cocone. *)
-(* Defined. *)
-
-(* End def_functor_pointwise_coprod. *)
-
-
-
-
-
-
-(* Lemma postcompWithCoproductArrow {a b : C} (CCab : CoproductCocone a b) {c : C} *)
-(*     (f : a ⇒ c) (g : b ⇒ c) {x : C} (k : c ⇒ x)  :  *)
-(*        CoproductArrow CCab f g ;; k = CoproductArrow CCab (f ;; k) (g ;; k). *)
-(* Proof. *)
-(*   apply CoproductArrowUnique. *)
-(*   -  rewrite assoc, CoproductIn1Commutes; *)
-(*      apply idpath. *)
-(*   -  rewrite assoc, CoproductIn2Commutes; *)
-(*      apply idpath. *)
-(* Qed. *)
-
-
+(**** TODO: Adapt *)
 (** * Proof that coproducts are unique when the precategory [C] is a category *)
 
 (* Section coproduct_unique. *)
