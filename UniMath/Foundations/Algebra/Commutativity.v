cbe4153e17c6a9a989640798c1cba20a36859315
Daniel R. Grayson <dan@math.uiuc.edu>
2015-10-14 12:04:13 -0400

    finish first case of commutativity theorem
    ... the case where the permutation preserves the last term of the product

--- UniMath/Foundations/Algebra/Commutativity.v
+++ UniMath/Foundations/Algebra/Commutativity.v
@@ -23,12 +23,25 @@ Proof.
       apply (maponpaths (λ m, m * x(lastelement n))).
       set (f' := weqoncompl f (lastelement n)).
       set (g := eqweqmap (maponpaths (compl (stn (S n))) a)).
+      assert (pr1_g : ∀ i, pr1 (pr1 (g i)) = pr1 (pr1 i)).
+      { intros. induction a. reflexivity. }
       set (f'' := weqcomp f' g).
       set (wc := weqdnicompl n (lastelement n)).
+      assert (pr1_wc : ∀ i, pr1 (pr1 (wc i)) = pr1 i).
+      { induction i as [i b].
+        unfold wc; simpl.
+        unfold dni; simpl.
+        induction (natlthorgeh i n) as [p|p]. { reflexivity. } { contradicts b p. } }
+      assert (pr1_inv_wc : ∀ i, pr1 (invweq wc i) = pr1 (pr1 i)).
+      { intros.
+        set (k := homotweqinvweq wc i).
+        set (l := maponpaths pr1 (maponpaths pr1 k)).
+        rewrite pr1_wc in l.
+        exact l. }
       set (f''' := weqcomp (weqcomp wc f'') (invweq wc)).
       intermediate_path (sequenceProduct (n,, x ∘ dni_allButLast n ∘ f''')).
-      * apply IH.
-      * change ((x ∘ dni_allButLast n) ∘ f''') with (x ∘ (dni_allButLast n ∘ f''')).
+      { apply IH. }
+      { change ((x ∘ dni_allButLast n) ∘ f''') with (x ∘ (dni_allButLast n ∘ f''')).
         apply maponpaths.
         apply maponpaths.
         apply (maponpaths (λ g, x ∘ g)).
@@ -42,8 +55,6 @@ Proof.
         apply total2_paths_second_isaprop.
         { apply isasetbool. }
         rewrite pr1_dni_allButLast.
-        unfold wc at 2.
-        Set Printing Coercions.
         unfold weqdnicompl.
         change (pr1weq
                  (weqpair (dnitocompl n (lastelement n))
@@ -54,6 +65,22 @@ Proof.
                   (weqpair (maponcomplweq f (lastelement n))
                            (isweqmaponcompl f (lastelement n))))
         with (maponcomplweq f (lastelement n)).
+        rewrite pr1_inv_wc.
+        rewrite pr1_g.
+        assert (pp: ∀ j, pr1 (pr1 (maponcomplweq f (lastelement n) j)) = pr1 (f (pr1 j))).
+        { intros. unfold maponcomplweq. unfold maponcomplincl.
+          induction j as [j c]; simpl. reflexivity. }
+        rewrite pp.
+        apply maponpaths.
+        apply maponpaths.
+        apply total2_paths_second_isaprop.
+        { exact (pr2 (natlth _ _)). }
+        rewrite pr1_wc.
+        simpl.
+        reflexivity.
+        }
+    + 
+
 
 
 Abort.
