481a82f1638798f729a347041e788dac9d2775cd
cathlelay <clelay@ias.edu>
2015-10-16 14:46:59 -0400

    Revert "remove my-commit of master"
    This reverts commit 39d6a1b666f35ef6eeaef7607fa43e2bede35289.

---
+++ my-commit.sh
@@ -0,0 +1,32 @@
+git checkout dev
+git pull https://github.com/cathlelay/UniMath dev
+make
+if [ "$?" -eq 0 ]
+then
+    git checkout master
+    git pull
+    git pull upstream
+    git merge upstream/master
+    git gui
+    make
+    if [ "$?" -eq 0 ]
+    then
+	echo "Au travail"
+	git checkout dev
+	git merge master
+	git gui
+	make
+	emacs UniMath/Dedekind
+	
+	make -j -k && git gui
+    else
+	echo "erreur dans la mise a jour de master"
+	git gui
+	./my-commit.sh
+    fi
+else
+    echo "erreur dans la mise a jour de dev"
+    git gui
+    ./my-commit.sh
+fi
+
