27689f3e89dbe98d9dbb309ce2f98e098b2d9f84
Daniel R. Grayson <dan@math.uiuc.edu>
2015-10-19 19:13:58 -0400

    ordered sets ...

--- UniMath/Foundations/OrderedSets.v
+++ UniMath/Foundations/OrderedSets.v
@@ -1,48 +1,144 @@
 (* -*- coding: utf-8 -*- *)
 
+Require Import UniMath.Foundations.FiniteSets.
+Unset Automatic Introduction.
+
+(** preliminaries on posets, move upstream *)
+
+Definition PosetEquivalence (X Y:Poset) := Σ f:X≃Y, isaposetmorphism f.
+
+Definition identityPosetEquivalence (X:Poset) : PosetEquivalence X X.
+Proof. intros. exists (idweq X). intros x y le. exact le.
+Defined.
+
+Definition paths_to_poset_equivalences {X Y:Poset} : X=Y -> PosetEquivalence X Y.
+Proof. intros ? ? e. induction e. apply identityPosetEquivalence.
+Defined.
+
+Theorem Poset_univalence {X Y:Poset} :
+    isweq (@paths_to_poset_equivalences X Y).
+Proof.
+  intros.
+  set (f := total2_paths_equiv _ X Y).
+
+
+Abort.  
+
+
+
 (** * Ordered sets *)
 
 (** see Bourbaki, Set Theory, III.1, where they are called totally ordered sets *)
 
-Require Import UniMath.Foundations.FiniteSets.
 
-Unset Automatic Introduction.
+Definition isOrdered (X:Poset) := istotal (pr1 (pr2 X)) × isantisymm (pr1 (pr2 X)).
 
-Definition Oset := total2 (fun X : Poset => istotal (pr1 (pr2 X)) × isantisymm (pr1 (pr2 X))).
+Definition OrderedSet := Σ X:Poset, isOrdered X.
 
-Definition underlyingSet (X:Oset) : hSet := pr1 X.
-Coercion underlyingSet : Oset >-> hSet.
+Local Definition underlyingSet (X:OrderedSet) : hSet := pr1 X.
+Coercion underlyingSet : OrderedSet >-> hSet.
 
-Definition underlyingRelation (X:Oset) := pr1 (pr2 (pr1 X)).
+Local Definition underlyingRelation (X:OrderedSet) := pr1 (pr2 (pr1 X)).
 
+Delimit Scope oset_scope with oset. 
 Notation "m ≤ n" := (underlyingRelation _ m n) (no associativity, at level 70) : oset_scope.
+Notation "m < n" := (m ≤ n × m != n)%oset (only parsing) :oset_scope.
 Open Scope oset_scope.
-Notation "m ≥ n" := (n ≤ m) (no associativity, at level 70) : oset_scope.
-Notation "m < n" := (m ≤ n × m != n) : oset_scope.
-Notation "m > n" := (n < m) : oset_scope.
 
-Definition stnoset (n:nat) : Oset.
+Definition isOrderedMap {X Y:OrderedSet} (f:X->Y) := ∀ x x', x ≤ x' -> f x ≤ f x'.
+
+Local Lemma idfun_ordered {X:OrderedSet} : isOrderedMap (idfun X).
+Proof. intros ? x y le. exact le. Defined.
+
+Lemma isaprop_isOrderedMap {X Y:OrderedSet} (f:X->Y) : isaprop (isOrderedMap f).
+Proof.
+  intros. apply impredtwice; intros. apply impred; intros _. apply propproperty.
+Defined.
+
+Definition OrderedMap (X Y:OrderedSet) := Σ f:X->Y, isOrderedMap f.
+
+Definition identityOrderedMap (X:OrderedSet) : OrderedMap X X.
+Proof. intros. exists (idfun X). apply idfun_ordered. Defined.
+
+Definition OrderedEquivalence (X Y:OrderedSet) := Σ f:X≃Y, isOrderedMap f.
+
+Notation "X ≅ Y" := (OrderedEquivalence X Y) (at level 80, no associativity) : oset_scope.
+(* written \cong in Agda input method *) 
+
+Definition underlyingEquivalence {X Y} : X≅Y -> X≃Y := pr1.
+Coercion underlyingEquivalence : OrderedEquivalence >-> weq.
+
+Definition identityOrderedEquivalence (X:OrderedSet) : X ≅ X.
+Proof. intros. exists (idweq X). apply idfun_ordered. Defined.
+
+Definition paths_to_equivalences {X Y:OrderedSet} : X=Y -> X≅Y.
+Proof.
+  intros ? ? e.
+  induction e.
+  apply identityOrderedEquivalence.
+Defined.
+
+Theorem OrderedSet_univalence {X Y:OrderedSet} :
+    isweq (@paths_to_equivalences X Y).
 Proof.
   intros.
-  exists (stnposet n).
-  split.
-  { intros x y. apply istotalnatleh. }
-  { intros ? ? ? ?.
-    apply (invmaponpathsincl _ (isinclstntonat _)).
-    { apply isantisymmnatleh. assumption. assumption. }}
-Defined.
+  set (f := total2_paths_equiv _ X Y).
+  set (g := total2_paths_equiv _ (pr1 X) (pr1 Y)).
 
-Definition isordered {X Y:Oset} (f:X->Y) := forall (x x':X), x ≤ x' -> f x ≤ f x'.
+Abort.
 
-Lemma isapropisordered {X Y:Oset} (f:X->Y) : isaprop (isordered f).
+Definition isMinimal {X:OrderedSet} (x:X) := ∀ y, x≤y.
+Definition isMaximal {X:OrderedSet} (x:X) := ∀ y, y≤x.
+Definition consecutive {X:OrderedSet} (x y:X) := x<y × ∀ z, ¬ (x<z × z<y).
+
+Lemma isaprop_isMinimal {X:OrderedSet} (x:X) : isaprop (isMaximal x).
 Proof.
-  intros; apply impredtwice; intros ; apply impred; intros _.
-  apply propproperty.
+  intros. unfold isMaximal. apply impred; intros z. apply propproperty.
 Defined.
 
-Definition Map (X Y:Oset) := total2 (fun f:X->Y => isordered f).
+Lemma isaprop_isMaximal {X:OrderedSet} (x:X) : isaprop (isMaximal x).
+Proof.
+  intros. unfold isMaximal. apply impred; intros z. apply propproperty.
+Defined.
 
-Definition FiniteStructure (X:Oset) := total2 (fun n => total2 (fun f : weq (stnoset n) X => isordered (pr1 f))).
+Lemma isaprop_consecutive {X:OrderedSet} (x y:X) : isaprop (consecutive x y).
+Proof.
+  intros. unfold consecutive. apply isapropdirprod.
+  { apply isapropdirprod. { apply pr2. } simpl. apply isapropneg. }
+  apply impred; intro z. apply isapropneg.
+Defined.
+
+Lemma isMinimal_preserved {X Y:OrderedSet} {x:X} (is:isMinimal x) (f:X ≅ Y) : isMinimal (f x).
+Proof.
+  intros.
+
+
+Abort.
+  
+(* standard ordered sets *)
+
+Definition standardOrderedSet (n:nat) : OrderedSet.
+Proof.
+  intros. exists (stnposet n). split.
+  { intros x y. apply istotalnatleh. }
+  intros ? ? ? ?. apply isinjstntonat. now apply isantisymmnatleh.
+Defined.
+
+Local Notation "⟦ n ⟧" := (standardOrderedSet n) (at level 0). (* in agda-mode \[[ n \]] *)
+
+Definition FiniteStructure (X:OrderedSet) := Σ n, Σ f:standardOrderedSet n ≃ X, isOrderedMap f.
+
+Local Lemma std_auto n : iscontr (OrderedEquivalence ⟦ n ⟧ ⟦ n ⟧).
+Proof.
+  intros. exists (identityOrderedEquivalence _). intros f.
+  apply total2_paths_isaprop.
+  { intros g. apply isaprop_isOrderedMap. }
+  simpl. apply isinjpr1weq. simpl. apply funextfun. intros i.
+  induction i as [i l]; induction f as [f is]. induction i as [|i h].
+  { simpl. 
+    
+
+Abort.
 
 Lemma isapropFiniteStructure X : isaprop (FiniteStructure X).
 Proof.
@@ -66,7 +162,7 @@ Proof.
         
         
         admit. }
-      { apply isapropisweq. } }
-    { intros; apply impredtwice; intros; apply impred; intros. apply pr2. }
+      apply isapropisweq. }
+    intros; apply impredtwice; intros; apply impred; intros. apply pr2.
   }
 Abort.
