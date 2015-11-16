4e2e32f52cb197208a5b7430ad378b0b6c83bc37
Daniel R. Grayson <dan@math.uiuc.edu>
2015-11-16 12:39:40 -0500

    ... diagram colimits ...

--- UniMath/Ktheory/DiagramColimit.v
+++ UniMath/Ktheory/DiagramColimit.v
@@ -8,22 +8,34 @@ Require UniMath.Ktheory.Representation.
 
 Definition cocone_functor_data {C:precategory} (h: has_homsets C)
            {Γ: graph} (D: diagram Γ C) : functor_data C SET.
-
+Proof.
   intros. refine (_,,_).
   - intro c. exists (cocone D c). unfold cocone. apply (isofhleveltotal2 2).
     + apply impred_isaset; intro g. apply h.
     + intro φ. apply impred_isaset; intro g.
       apply impred_isaset; intro g'. apply impred_isaset; intro e.
       apply isasetaprop. apply h.
-  - intros c c' f φ; simpl.
-    exists (λ g, compose (coconeIn φ g) f).
+  - intros c c' f φ; simpl. exists (λ g, compose (coconeIn φ g) f).
     intros g g' e; simpl. rewrite assoc. now rewrite coconeInCommutes.
 Defined.
 
-(* Definition type (C:precategory) (hs: has_homsets C) *)
-(*            (D: diagram) *)
-(*            (c:I -> ob C) := *)
-(*   Representation.Data (HomFamily.precat C hs c). *)
+Definition cocone_functor {C:precategory} (h: has_homsets C)
+           {Γ: graph} (D: diagram Γ C) : C ==> SET.
+Proof.
+  intros. exists (cocone_functor_data h D). split.
+  - intros c. apply funextfun; intros f. refine (total2_paths _ _).
+    * apply funextsec; intro g. apply id_right.
+    * apply funextsec; intro g. apply funextsec; intro g'. apply funextsec; intro e. apply h.
+  - intros a b c p q. apply funextfun; intro r. refine (total2_paths _ _).
+    * apply funextsec; intro g. apply assoc.
+    * apply funextsec; intro g. apply funextsec; intro g'. apply funextsec; intro e. apply h.
+Defined.
+
+Definition type  {C:precategory} (h: has_homsets C) {Γ: graph} (D: diagram Γ C) :=
+  Representation.Data (cocone_functor h D).
+
+
+
 (* Definition Object {C:precategory} (hs: has_homsets C) {I} {c:I -> ob C} (r:type C hs c) *)
 (*            : ob C := Representation.Object r. *)
 (* Definition In {C:precategory} (hs: has_homsets C) {I} {b:I -> ob C} (B:type C hs b) i : *)
