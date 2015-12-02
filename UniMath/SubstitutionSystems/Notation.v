66235336248e9488a4ee78ebd2407c992fd866b1
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2015-12-02 17:53:50 -0500

    adapted notation for postwhiskering

--- UniMath/SubstitutionSystems/Notation.v
+++ UniMath/SubstitutionSystems/Notation.v
@@ -47,7 +47,7 @@ Notation "α ∙∙ β" := (hor_comp β α) (at level 20).
 Ltac pathvia b := (apply (@pathscomp0 _ _ b _ )).
 
 Notation "α 'ø' Z" := (pre_whisker Z α)  (at level 25).
-Notation "Z ∘ α" := (post_whisker _ _ _ _ α Z) (at level 50, left associativity).
+Notation "Z ∘ α" := (post_whisker α Z) (at level 50, left associativity).
 
 Notation "` T" := (alg_carrier _ T) (at level 3, format "` T").
 Notation "α •• Z" :=  (# (pre_composition_functor_data _ _ _ _ _ Z) α) (at level 25).
