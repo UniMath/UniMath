c61c283a6c640faa9aea052cbe131099f3155f71
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-05-03 20:26:26 -0400

    Add a variety of product category helpers from Lumsdaine's code

--- UniMath/CategoryTheory/ProductPrecategory.v
+++ UniMath/CategoryTheory/ProductPrecategory.v
@@ -35,6 +35,51 @@ Local Notation "G ∙ F" := (functor_composite _ _ _ F G) (at level 35).
 
 Ltac pathvia b := (apply (@pathscomp0 _ _ b _ )).
 
+(* Imported from Peter Lumsdaine's old bicategory code: *)
+
+Section Dirproduct_utils.
+(** * Utility functions on direct products of types. *)
+
+(** The next few are either aliases or very mild generalisations of existing functions from the UniMath libraries.  They differ generally in using projections instead of destructing, making them apply and/or reduce in more situations.  The aliases are included just to standardise local naming conventions. *)
+
+Notation "( x , y , .. , z )" := (dirprodpair .. (dirprodpair x y) .. z).
+
+(** Compare [pathsdirprod]. *)
+Definition dirproduct_paths {A B : Type} {p q : A × B}
+  : pr1 p = pr1 q -> pr2 p = pr2 q -> p = q.
+Proof.
+  destruct p as [a b], q as [a' b']; apply pathsdirprod.
+Defined.
+
+(** Compare [total2asstol]. *)
+Definition dirproduct_assoc {C0 C1 C2 : Type}
+  : (C0 × (C1 × C2)) -> ((C0 × C1) × C2).
+Proof.
+  intros c. exact ((pr1 c , (pr1 (pr2 c))) , pr2 (pr2 c)).
+Defined.
+
+Definition dirproduct_unassoc {C0 C1 C2 : Type}
+  : ((C0 × C1) × C2) -> (C0 × (C1 × C2)).
+Proof.
+  intros c. exact (pr1 (pr1 c) , ((pr2 (pr1 c)) , pr2 c)).
+Defined.
+
+(** Identical to [dirprodf]. *)
+Definition dirproduct_maps {A0 A1 B0 B1} (f0 : A0 -> B0) (f1 : A1 -> B1)
+  : A0 × A1 -> B0 × B1.
+Proof.
+  intros aa. exact (f0 (pr1 aa), f1 (pr2 aa)).
+Defined.
+
+(** Compare [prodtofuntoprod]. *)
+Definition dirproduct_pair_maps {A B0 B1} (f0 : A -> B0) (f1 : A -> B1)
+  : A -> B0 × B1.
+Proof.
+  intros a; exact (f0 a, f1 a).
+Defined.
+
+End Dirproduct_utils.
+
 Definition product_precategory_ob_mor (C D : precategory_ob_mor) : precategory_ob_mor.
 Proof.
   exists (C × D).
@@ -170,4 +215,115 @@ Definition prodcatmor {C D : precategory} {X X' : C} {Z Z' : D} (α : X ⇒ X') 
 Proof.
   exists α.
   exact β.
+Defined.
+
+(* Imported from Peter Lumsdaine's old bicategory code: *)
+
+Local Notation "C × D" := (product_precategory C D) (at level 75, right associativity) : precategory_scope.
+
+Open Scope precategory_scope.
+Delimit Scope precategory_scope with precat.
+
+Definition unit_precategory : precategory.
+Proof.
+  use tpair. use tpair.
+  (* ob, mor *) exists unit. intros; exact unit.
+  (* identity, comp *) split; intros; constructor.
+  (* id_left *) simpl; split; try split; intros; apply isconnectedunit.
+Defined.
+
+Definition unit_functor C : functor C unit_precategory.
+Proof.
+  use tpair. use tpair.
+  (* functor_on_objects *) intros; exact tt.
+  (* functor_on_morphisms *) intros F a b; apply identity.
+  split.
+  (* functor_id *) intros x; apply paths_refl.
+  (* functor_comp *) intros x y z w v; apply paths_refl.
+Defined.
+
+(* TODO: perhaps generalise to constant functors? *)
+Definition ob_as_functor_data {C : precategory_data} (c : C) : functor_data unit_precategory C.
+Proof.
+  use tpair.
+  (* functor_on_objects *) intros; exact c.
+  (* functor_on_morphisms *) intros F a b; apply identity.
+Defined.
+
+Definition ob_as_functor {C : precategory} (c : C) : functor unit_precategory C.
+Proof.
+  exists (ob_as_functor_data c).
+  split.
+  (* functor_id *) intros; constructor.
+  (* functor_comp *) intros x y z w v; simpl. apply pathsinv0, id_left.
+Defined.
+
+Definition product_precategory_assoc_data (C0 C1 C2 : precategory_data)
+  : functor_data (product_precategory_data C0 (product_precategory_data C1 C2))
+                 (product_precategory_data (product_precategory_data C0 C1) C2).
+Proof.
+  (* functor_on_objects *) exists dirproduct_assoc.
+  (* functor_on_morphisms *) intros a b; apply dirproduct_assoc.
+Defined.
+
+Definition product_precategory_assoc (C0 C1 C2 : precategory)
+  : functor (C0 × (C1 × C2)) ((C0 × C1) × C2).
+Proof.
+  exists (product_precategory_assoc_data _ _ _). split.
+  (* functor_id *) intros c. simpl; apply paths_refl.
+  (* functor_comp *) intros c0 c1 c2 f g. simpl; apply paths_refl.
+Defined.
+
+Definition product_precategory_unassoc_data (C0 C1 C2 : precategory_data)
+  : functor_data (product_precategory_data (product_precategory_data C0 C1) C2)
+                 (product_precategory_data C0 (product_precategory_data C1 C2)).
+Proof.
+  (* functor_on_objects *) exists dirproduct_unassoc.
+  (* functor_on_morphisms *) intros a b; apply dirproduct_unassoc.
+Defined.
+
+Definition product_precategory_unassoc (C0 C1 C2 : precategory)
+  : functor ((C0 × C1) × C2) (C0 × (C1 × C2)).
+Proof.
+  exists (product_precategory_unassoc_data _ _ _). split.
+  (* functor_id *) intros c. simpl; apply paths_refl.
+  (* functor_comp *) intros c0 c1 c2 f g. simpl; apply paths_refl.
+Defined.
+
+Definition product_functor_data {C0 C1 D0 D1 : precategory_data}
+  (F0 : functor_data C0 D0) (F1 : functor_data C1 D1)
+: functor_data (product_precategory_data C0 C1) (product_precategory_data D0 D1).
+Proof.
+  (* functor_on_objects *) exists (dirproduct_maps F0 F1).
+  (* functor_on_morphisms *) intros a b.
+    apply dirproduct_maps; apply functor_on_morphisms.
+Defined.
+
+Definition product_functor {C0 C1 D0 D1 : precategory}
+  (F0 : functor C0 D0) (F1 : functor C1 D1)
+: functor (C0 × C1) (D0 × D1).
+Proof.
+  exists (product_functor_data F0 F1); split.
+  (* functor_id *) intros c. apply dirproduct_paths; apply functor_id.
+  (* functor_comp *) intros c0 c1 c2 f g.
+    apply dirproduct_paths; apply functor_comp.
+Defined.
+
+Definition pair_functor_data {C D0 D1 : precategory_data}
+  (F0 : functor_data C D0) (F1 : functor_data C D1)
+: functor_data C (product_precategory_data D0 D1).
+Proof.
+  (* functor_on_objects *) exists (dirproduct_pair_maps F0 F1).
+  (* functor_on_morphisms *) intros a b.
+    apply dirproduct_pair_maps; apply functor_on_morphisms.
+Defined.
+
+Definition pair_functor {C D0 D1 : precategory}
+  (F0 : functor C D0) (F1 : functor C D1)
+: functor C (D0 × D1).
+Proof.
+  exists (pair_functor_data F0 F1); split.
+  (* functor_id *) intros c. apply dirproduct_paths; apply functor_id.
+  (* functor_comp *) intros c0 c1 c2 f g.
+    apply dirproduct_paths; apply functor_comp.
 Defined.
