3da022bfc0f8f77a44156df0a028fd45f48f9cdb
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-06-29 18:37:17 -0400

    Move whiskering lemmas to their own file, and create a file of bicategory notations

--- UniMath/CategoryTheory/bicategories/prebicategory.v
+++ UniMath/CategoryTheory/bicategories/prebicategory.v
@@ -66,9 +66,7 @@ Definition compose_2mor_horizontal {C : prebicategory_id_comp} {a b c : C}
   : ( f ;1; g ) -2-> ( f' ;1; g' ).
 Proof.
   apply functor_on_morphisms.
-  unfold precategory_morphisms.
-  simpl.
-  exact (dirprodpair alpha beta).
+  exact (prodcatmor alpha beta).
 Defined.
 
 Local Notation "alpha ;h; beta" := (compose_2mor_horizontal alpha beta) (at level 50, format "alpha ;h; beta").
@@ -318,135 +316,10 @@ Lemma interchange {C : prebicategory} {a b c : C}
   : (a1 ;v; a2) ;h; (b1 ;v; b2) = (a1 ;h; b1) ;v; (a2 ;h; b2).
 Proof.
   unfold compose_2mor_horizontal.
-  unfold dirprodpair.
-  assert (X : (prodcatmor a1 b1) ;v; (prodcatmor a2 b2) = (prodcatmor (a1;v;a2) (b1;v;b2))) by reflexivity.
-  unfold prodcatmor in X.
-  simpl in X. (* This has no visible effect on X, but rewrite doesn't work without it  *)
+
+  assert (X : (prodcatmor a1 b1) ;v; (prodcatmor a2 b2)
+            = (prodcatmor (a1;v;a2) (b1;v;b2))) by reflexivity.
   rewrite <- X.
+
   apply functor_comp.
 Qed.
-
-(******************************************************************************)
-(* Whiskering *)
-
-Definition whisker_left {C : prebicategory} {a b c : C}
-           (f : a -1-> b) {g h : b -1-> c} (alpha : g -2-> h)
-  : (f ;1; g) -2-> (f ;1; h)
-  := (identity_2mor f) ;h; alpha.
-
-Lemma whisker_left_id_1mor {C : prebicategory} {b c : C}
-           {g h : b -1-> c} (alpha : g -2-> h)
-  : whisker_left (identity_1mor _) alpha =
-    left_unitor _ ;v; alpha ;v; iso_inv_from_iso (left_unitor _).
-Proof.
-  unfold whisker_left.
-  apply id_2mor_left.
-Defined.
-
-Lemma whisker_left_id_2mor {C : prebicategory} {a b c : C}
-           (f : a -1-> b) (g : b -1-> c)
-  : whisker_left f (identity_2mor g) = identity_2mor (f ;1; g).
-Proof.
-  pathvia (functor_on_morphisms (compose_functor a b c)
-                                (identity (prodcatpair f g))).
-  reflexivity.
-  apply functor_id.
-Defined.
-
-Definition whisker_left_iso {C : prebicategory} {a b c : C}
-           (f : a -1-> b) {g h : b -1-> c} (alpha : iso g h)
-  : iso (f ;1; g) (f ;1; h)
-  := (identity_iso f) ;hi; alpha.
-
-Definition whisker_left_inv {C : prebicategory} {a b c : C}
-           (f : a -1-> b) {g h : b -1-> c} (alpha : iso g h)
-  : whisker_left_iso f (iso_inv_from_iso alpha)
-  = iso_inv_from_iso (whisker_left_iso f alpha).
-Proof.
-  unfold whisker_left_iso at 1.
-  rewrite <- iso_inv_of_iso_id.
-  apply inv_horizontal_comp.
-Defined.
-
-Lemma whisker_left_on_comp {C : prebicategory} {a b c : C}
-  (f : a -1-> b) {g h i : b -1-> c}
-  (alpha : g -2-> h) (alpha' : h -2-> i)
-  : whisker_left f (alpha ;v; alpha')
-  = whisker_left f alpha ;v; whisker_left f alpha'.
-Proof.
-  unfold whisker_left.
-  pathvia ((identity_2mor f;v; identity_2mor f);h;(alpha;v;alpha')).
-  - rewrite id_left.
-    reflexivity.
-  - apply interchange.
-Defined.
-
-Definition whisker_right {C : prebicategory} {a b c : C}
-  {f g : a -1-> b} (alpha : f -2-> g) (h : b -1-> c)
-  : (f ;1; h) -2-> (g ;1; h)
-  := alpha ;h; (identity_2mor h).
-
-Lemma whisker_right_id_1mor {C : prebicategory} {a b : C}
-           {f g : a -1-> b} (alpha : f -2-> g)
-  : whisker_right alpha (identity_1mor _) =
-    right_unitor _ ;v; alpha ;v; iso_inv_from_iso (right_unitor _).
-Proof.
-  unfold whisker_right.
-  apply id_2mor_right.
-Defined.
-
-Lemma whisker_right_id_2mor {C : prebicategory} {a b c : C}
-  (f : a -1-> b) (g : b -1-> c)
-  : whisker_right (identity_2mor f) g = identity_2mor (f ;1; g).
-Proof.
-  pathvia (functor_on_morphisms (compose_functor a b c)
-                                (identity (prodcatpair f g))).
-  reflexivity.
-  apply functor_id.
-Defined.
-
-Definition whisker_right_iso {C : prebicategory} {a b c : C}
-           {f g : a -1-> b} (alpha : iso f g) (h : b -1-> c)
-  : iso (f ;1; h) (g ;1; h)
-  := alpha ;hi; (identity_iso h).
-
-Definition whisker_right_inv {C : prebicategory} {a b c : C}
-           {f g : a -1-> b} (alpha : iso f g) (h : b -1-> c)
-  : whisker_right_iso (iso_inv_from_iso alpha) h
-  = iso_inv_from_iso (whisker_right_iso alpha h).
-Proof.
-  unfold whisker_right_iso at 1.
-  rewrite <- iso_inv_of_iso_id.
-  apply inv_horizontal_comp.
-Defined.
-
-Lemma whisker_right_on_comp {C : prebicategory} {a b c : C}
-  {f g h : a -1-> b} (alpha : f -2-> g) (alpha' : g -2-> h) (i : b -1-> c)
-  : whisker_right (alpha ;v; alpha') i
-  = whisker_right alpha i ;v; whisker_right alpha' i.
-Proof.
-  unfold whisker_right.
-  pathvia ((alpha;v;alpha');h;(identity_2mor i;v; identity_2mor i)).
-  - rewrite id_left.
-    reflexivity.
-  - apply interchange.
-Defined.
-
-Lemma twomor_naturality {C : prebicategory} {a : C}
-  {f g h i : a -1-> a}
-  (alpha : f -2-> g) (beta  : h -2-> i)
-  : (whisker_right beta f) ;v; (whisker_left i alpha)
-  = (whisker_left h alpha) ;v; (whisker_right beta g).
-Proof.
-  unfold whisker_left, whisker_right.
-
-  pathvia ((beta ;v; identity_2mor i);h;(identity_2mor f ;v; alpha)).
-    apply pathsinv0.
-    apply interchange.
-
-  pathvia ((identity_2mor h ;v; beta);h;(alpha ;v; identity_2mor g)).
-    rewrite !id_left, !id_right.
-    reflexivity.
-
-  apply interchange.
-Defined.
