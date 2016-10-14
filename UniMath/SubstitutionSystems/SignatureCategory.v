c4f1001fd65b45aa8eb39be908b1a24e515a5cd4
Anders Mörtberg <andersmortberg@gmail.com>
2016-10-14 00:02:03 -0400

    wip on binproducts in the category of signatures

--- UniMath/SubstitutionSystems/SignatureCategory.v
+++ UniMath/SubstitutionSystems/SignatureCategory.v
@@ -4,13 +4,16 @@ Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.UnicodeNotations.
 Require Import UniMath.CategoryTheory.whiskering.
-Require Import UniMath.CategoryTheory.PointedFunctors.
+Require Import UniMath.CategoryTheory.limits.binproducts.
 Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
+Require Import UniMath.CategoryTheory.PointedFunctors.
+Require Import UniMath.CategoryTheory.PointedFunctorsComposition.
 Require Import UniMath.CategoryTheory.HorizontalComposition.
 Require Import UniMath.CategoryTheory.EndofunctorsMonoidal.
 
 Require Import UniMath.SubstitutionSystems.Notation.
 Require Import UniMath.SubstitutionSystems.Signatures.
+Require Import UniMath.SubstitutionSystems.BinProductOfSignatures.
 
 Section SignatureCategory.
 
@@ -19,15 +22,15 @@ Variables (C : precategory) (hsC : has_homsets C).
 Local Notation "'U'" := (functor_ptd_forget C hsC).
 Local Notation "'Ptd'" := (precategory_Ptd C hsC).
 
-(* Define the commutative diagram used in the morphisms *)
+(** Define the commutative diagram used in the morphisms *)
 Section Signature_category_mor.
 
 Variables (Ht Ht' : Signature C hsC).
 
 Let H := Signature_Functor _ _ Ht.
 Let H' := Signature_Functor _ _ Ht'.
-Let θ : nat_trans (θ_source C hsC Ht) (θ_target C hsC Ht) := theta Ht.
-Let θ' : nat_trans (θ_source C hsC Ht') (θ_target C hsC Ht') := theta Ht'.
+Let θ : nat_trans (θ_source Ht) (θ_target Ht) := theta Ht.
+Let θ' : nat_trans (θ_source Ht') (θ_target Ht') := theta Ht'.
 
 Variables (α : nat_trans H H').
 Variables (X : [C,C,hsC]) (Y : Ptd).
@@ -104,4 +107,55 @@ Qed.
 Definition Signature_precategory : precategory :=
  (Signature_precategory_data,,is_precategory_Signature_precategory_data).
 
+Lemma has_homsets_Signature_precategory : has_homsets Signature_precategory.
+Admitted.
+
 End SignatureCategory.
+
+Section BinProducts.
+
+Variables (C : precategory) (hsC : has_homsets C) (BC : BinProducts C).
+
+Lemma BinProducts_Signature_precategory : BinProducts (Signature_precategory C hsC).
+Proof.
+intros A B.
+use mk_BinProductCone.
+- apply (BinProduct_of_Signatures _ _ BC A B).
+- simpl in *.
+mkpair.
++
+simpl.
+(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl. *)
+apply (binproduct_nat_trans_pr1 [C,C,hsC] [C,C,hsC]). (* Can we use BinProductPr1 instead? *)
++ abstract (
+destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl;
+intros X Y;
+apply (nat_trans_eq hsC); intro c; simpl;
+rewrite functor_id, id_right;
+apply BinProductOfArrowsPr1).
+- simpl in *.
+mkpair.
++
+(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl. *)
+apply (binproduct_nat_trans_pr2 [C,C,hsC] [C,C,hsC]). (* Can we use BinProductPr2 instead? *)
++ abstract (
+destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl;
+intros X Y;
+apply (nat_trans_eq hsC); intro c; simpl;
+rewrite functor_id, id_right;
+apply BinProductOfArrowsPr2).
+-
+(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]. *)
+use mk_isBinProductCone.
++ apply has_homsets_Signature_precategory.
++ simpl.
+  intros Ht F G.
+assert (temp : BinProducts [C,C,hsC]).
+  admit.
+assert (temp2 : has_homsets [C,C,hsC]).
+  admit.
+generalize (binproduct_nat_trans_univ_prop _ _ temp temp2 (pr1 A) (pr1 B) (pr1 Ht)).
+admit.
+Admitted.
+
+End BinProducts.
