ede7f48195a12efc3cf3b00790e2d48b91e3e653
Anders Mörtberg <andersmortberg@gmail.com>
2016-10-14 13:27:53 -0400

    finish construction of binary products in category of signatures

--- UniMath/SubstitutionSystems/SignatureCategory.v
+++ UniMath/SubstitutionSystems/SignatureCategory.v
@@ -116,46 +116,97 @@ Section BinProducts.
 
 Variables (C : precategory) (hsC : has_homsets C) (BC : BinProducts C).
 
+Let BCC : BinProducts [[C,C,hsC],[C,C,hsC],functor_category_has_homsets _ _ hsC].
+Proof.
+apply BinProducts_functor_precat.
+apply (BinProducts_functor_precat C _ BC hsC).
+Defined.
+
 Lemma BinProducts_Signature_precategory : BinProducts (Signature_precategory C hsC).
 Proof.
-intros A B.
+intros Ht1 Ht2.
 use mk_BinProductCone.
-- apply (BinProduct_of_Signatures _ _ BC A B).
+- apply (BinProduct_of_Signatures _ _ BC Ht1 Ht2).
 - simpl in *.
 mkpair.
 +
-simpl.
-(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl. *)
-apply (binproduct_nat_trans_pr1 [C,C,hsC] [C,C,hsC]). (* Can we use BinProductPr1 instead? *)
-+ abstract (
-destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl;
-intros X Y;
-apply (nat_trans_eq hsC); intro c; simpl;
-rewrite functor_id, id_right;
-apply BinProductOfArrowsPr1).
+apply (BinProductPr1 _ (BCC (pr1 Ht1) (pr1 Ht2))).
++
+abstract (intros X Y; apply (nat_trans_eq hsC); intro c; simpl;
+eapply pathscomp0; [apply BinProductOfArrowsPr1|];
+eapply pathscomp0; [|apply assoc]; apply maponpaths;
+apply pathsinv0; eapply pathscomp0; [apply cancel_postcomposition, functor_id|];
+apply id_left).
 - simpl in *.
 mkpair.
 +
-(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl. *)
-apply (binproduct_nat_trans_pr2 [C,C,hsC] [C,C,hsC]). (* Can we use BinProductPr2 instead? *)
-+ abstract (
-destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]; simpl;
+apply (BinProductPr2 _ (BCC (pr1 Ht1) (pr1 Ht2))).
++
+abstract (
 intros X Y;
 apply (nat_trans_eq hsC); intro c; simpl;
 rewrite functor_id, id_right;
 apply BinProductOfArrowsPr2).
 -
-(* destruct A as [A1 [A2 [A3 A4]]]; destruct B as [B1 [B2 [B3 B4]]]. *)
 use mk_isBinProductCone.
 + apply has_homsets_Signature_precategory.
 + simpl.
-  intros Ht F G.
-assert (temp : BinProducts [C,C,hsC]).
-  admit.
-assert (temp2 : has_homsets [C,C,hsC]).
-  admit.
-generalize (binproduct_nat_trans_univ_prop _ _ temp temp2 (pr1 A) (pr1 B) (pr1 Ht)).
-admit.
+  intros Ht3 F G.
+set (H := @BinProductArrow _ _ _ (BCC (pr1 Ht1) (pr1 Ht2)) (pr1 Ht3) (pr1 F) (pr1 G)).
+use unique_exists; simpl.
+* apply (tpair _ H).
+intros X Y.
+generalize (pr2 G X Y).
+generalize (pr2 F X Y).
+unfold Signature_category_mor_diagram.
+unfold H.
+intros HH1 HH2.
+apply (nat_trans_eq hsC); intro c.
+simpl.
+rewrite <- assoc.
+apply pathsinv0.
+eapply pathscomp0.
+eapply maponpaths.
+apply BinProductOfArrows_comp.
+rewrite !functor_id.
+rewrite !id_left.
+eapply pathscomp0.
+apply postcompWithBinProductArrow.
+apply pathsinv0.
+apply BinProductArrowUnique.
+eapply pathscomp0.
+rewrite <- assoc.
+apply maponpaths.
+apply BinProductPr1Commutes.
+eapply pathscomp0.
+apply (nat_trans_eq_pointwise HH1 c).
+simpl.
+rewrite functor_id.
+rewrite id_right.
+apply idpath.
+eapply pathscomp0.
+rewrite <- assoc.
+apply maponpaths.
+apply BinProductPr2Commutes.
+eapply pathscomp0.
+apply (nat_trans_eq_pointwise HH2 c).
+simpl.
+rewrite functor_id.
+rewrite id_right.
+apply idpath.
+* split.
+{ apply SignatureMor_eq, (BinProductPr1Commutes _ _ _ (BCC  _ _)). }
+{ apply SignatureMor_eq, (BinProductPr2Commutes _ _ _ (BCC  _ _)). }
+* intros X; simpl in *.
+apply isapropdirprod.
+apply has_homsets_Signature_precategory.
+apply has_homsets_Signature_precategory.
+* intros X.
+intros H1H2.
+apply SignatureMor_eq; simpl.
+apply (BinProductArrowUnique _ _ _ (BCC  _ _)).
+apply (maponpaths pr1 (pr1 H1H2)).
+apply (maponpaths pr1 (pr2 H1H2)).
 Admitted.
 
 End BinProducts.
