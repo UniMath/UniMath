a21bca71c6f42d3cc12b39c43873d5e33e93ea86
mweav <mzw@cs.princeton.edu>
2016-11-07 10:17:38 -0500

    More incomplete defns

--- UniMath/CategoryTheory/elems_slice_equiv.v
+++ UniMath/CategoryTheory/elems_slice_equiv.v
@@ -13,13 +13,74 @@ Section elems_slice_equiv.
   Local Open Scope cat.
 
   Local Definition el {C : Precategory} (P : C ==> SET) : Precategory := cat P.
-  Local Definition pshf (C : Precategory) : Precategory := [C^op, SET].
+  Local Definition pshf (C : Precategory) : Precategory := [C, SET]. (* Need to change to C^op, once I figure how to get proofs to go through w/ C^op *)
+  Local Definition slice (C : Precategory) (X : C) : Precategory :=
+    ((slice_precat C X (pr2 C)) ,, (has_homsets_slice_precat C (pr2 C) X)).
 
   Variable (C : Precategory).
   Variable (P : pshf C).
 
+  Print functor_data.
 
+  Definition pshf_to_slice_ob_funct_fun (F : pshf (el P)) : C → SET :=
+    fun X => (Σ(p : pr1 ((pr1 P) X)), pr1 ((pr1 F) (X ,, p)))
+               ,, (isaset_total2 _ (pr2 ((pr1 P) X)) (fun x => pr2 ((pr1 F) (X,, x)))).
 
+  Definition pshf_to_slice_ob_funct_mor (F : pshf (el P)) (X Y : C) (f : X --> Y) :
+    pshf_to_slice_ob_funct_fun F X --> pshf_to_slice_ob_funct_fun F Y :=
+    fun p => # (pr1 P) f (pr1 p) ,, pr2 (pr1 F) (X ,, (pr1 p)) (Y,, # (pr1 P) f (pr1 p)) (f ,, idpath (# (pr1 P) f (pr1 p))) (pr2 p).
 
+  Definition pshf_to_slice_ob_funct_data (F : pshf (el P)) : functor_data C SET :=
+    (pshf_to_slice_ob_funct_fun F) ,, (pshf_to_slice_ob_funct_mor F).
+
+  Definition pshf_to_slice_ob_is_funct (F : pshf (el P)) : is_functor (pshf_to_slice_ob_funct_data F).
+  Proof.
+    intro F. unfold is_functor. split.
+    + unfold functor_idax. simpl.
+      intro a.
+      unfold pshf_to_slice_ob_funct_mor.
+      unfold pshf_to_slice_ob_funct_fun.
+      simpl.
+      apply funextsec. intro p. Check ((pr1 (pr2 F)) (a ,, pr1 p)).
+      assert (H : pr1 p = # (pr1 P) (identity a) (pr1 p)).
+    - apply (transportb (fun f => pr1 p = f (pr1 p)) ((pr1 (pr2 P)) a) (idpath _)).
+    - unfold functor_on_morphisms. unfold functor_on_morphisms in H.
+      (*
+      Set Printing All. simpl.
+      unfold identity. simpl.
+      rewrite tppr.
+      apply (total2_paths2 (transportb (fun f => f (pr1 p) = pr1 p) ((pr1 (pr2 P)) a) (idpath _))).
+      unfold identity. simpl. rewrite ((pr1 (pr2 P)) a).
+
+              ((pr1 (pr2 P)) a)
+       *)
+  Admitted.
+
+  Definition presheaf_to_slice_ob : pshf (el P) → slice (pshf C) P.
+  Proof.
+    simpl.
+    intro F.
+    unfold slicecat_ob. simpl.
+  Admitted.
+
+  (* make/find helper functions to extract various pieces of stuff I want *)
+
+  Definition slice_to_presheaf_ob_ob (Q : slice (pshf C) P) : el P → SET :=
+    fun p =>
+      hfiber ((pr1 (pr2 Q)) (pr1 p)) (pr2 p) ,,
+             isaset_hfiber ((pr1 (pr2 Q)) (pr1 p)) (pr2 p) (pr2 (((pr1 (pr1 Q)) (pr1 p)))) (pr2 ((pr1 P) (pr1 p))).
+
+  Definition slice_to_presheaf_ob_mor (Q : slice (pshf C) P) (F G : el P) (f : F --> G) :
+    slice_to_presheaf_ob_ob Q F --> slice_to_presheaf_ob_ob Q G.
+    unfold slice_to_presheaf_ob_ob. unfold hfiber. simpl.
+    intros Q F G f s.
+    exists ((pr2 (pr1 (pr1 Q))) _ _ (pr1 f) (pr1 s)).
+    rewrite <- (pr2 f).
+    Check (pr2 s). Check (pr2 Q).
+
+  Definition slice_to_presheaf_ob : slice (pshf C) P → pshf (el P).
+  Proof.
+    simpl. unfold slicecat_ob. simpl.
+    intro Q. unfold "==>".
 
 End elems_slice_equiv.
