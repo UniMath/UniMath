3b8bc243aac75dfc1a9b02b9f68a8c99e1ebbcb6
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2016-11-14 18:22:29 +0100

    finished proof

--- UniMath/CategoryTheory/CocontFunctors.v
+++ UniMath/CategoryTheory/CocontFunctors.v
@@ -1572,7 +1572,16 @@ Proof.
       * intros a; simpl.
         apply identity.
       * intros a b f; rewrite id_left, id_right.
-        admit.
+        cbn.
+        unfold ColimFunctor_mor.
+        unfold colimOfArrows.
+        match goal with |[|- _ = colimArrow _ _ ?CCCCCC] =>
+                    set (CCC:= CCCCCC) end.
+        use (colimArrowUnique (bar a) _ CCC).
+        intro u. cbn.
+        assert (XR := nat_trans_ax (coconeIn ccG u)).
+        rewrite <- XR.
+        apply idpath.
     + split.
       * apply (nat_trans_eq hsC).
         simpl.
@@ -1590,7 +1599,8 @@ Proof.
         apply (colimArrowUnique (bar a)); intro u.
         simpl.
         now rewrite id_right.
-Admitted.
+Defined.
+
 
 (* First try *)
 (* Lemma key2 (H : Π a, isColimCocone _ _ (cocone_pointwise _ _ _ _ d G ccG a)) : *)
