a84ee440ee1fa5ae5730ee85794a23aea1a5081c
Anders Mörtberg <andersmortberg@gmail.com>
2016-11-30 11:54:13 +0100

    add a lot of comments

--- UniMath/SubstitutionSystems/FromBindingSigsToMonads_Summary.v
+++ UniMath/SubstitutionSystems/FromBindingSigsToMonads_Summary.v
@@ -1,3 +1,16 @@
+(**
+
+This file provides a stable interface to the formalization of the paper:
+
+  From binding signatures to monads in UniMath
+
+by Benedikt Ahrens, Ralph Matthes and Anders Mörtberg.
+
+
+PLEASE DO NOT RENAME THIS FILE - its name is referenced in the article
+about this formalization
+
+*)
 Require Import UniMath.Foundations.Basics.PartD.
 Require Import UniMath.Foundations.Basics.Propositions.
 Require Import UniMath.Foundations.Basics.Sets.
@@ -31,6 +44,91 @@ Require Import UniMath.SubstitutionSystems.Signatures.
 Require Import UniMath.SubstitutionSystems.SubstitutionSystems.
 Require Import UniMath.SubstitutionSystems.BindingSigToMonad.
 Require Import UniMath.SubstitutionSystems.LiftingInitial_alt.
+
+(** Definition 1: Artiy, Binding signature *)
+
+(** Example 2: Binding signature of untyped lambda calculus *)
+
+(** Example 3: Binding signature of presyntax of Martin-Löf type theory *)
+
+(** Definition 4: Signatures with strength *)
+
+(** Definition 5: Morphism of signatures with strength *)
+
+(** Definition 6: Coproduct of signatures with strength *)
+
+(** Definition 7: Binary product of signatures with strength *)
+
+(** Problem 8: Signatures with strength from binding signatures *)
+
+(** Definition 15: Graph *)
+
+(** Definition 16: Diagram *)
+
+(** Definition 18: Cocone *)
+
+(** Definition 19: Colimiting cocone *)
+
+(** Remark 20: Uniqueness of colimits *)
+
+(** Definition 21: Preservation of colimits *)
+
+(** Lemma 22: Invariance of cocontinuity under isomorphism *)
+
+(** Problem 23: Colimits in functor categories *)
+
+(** Problem 25: Initial algebras of ω-cocontinuous functors *)
+
+(** Lemma 26: Lambek's lemma *)
+
+(** Problem 28: Colimits in Set *)
+
+(** Definition 30: Smallest equivalence relation containing a relation *)
+
+(** Lemma 32: Left adjoints preserve colimits *)
+
+(** Lemma 33: Examples of preservation of colimits *)
+(** (i): Identity functor *)
+(** (ii): Constant functor *)
+(** (iii): Diagonal functor *)
+(** (iv): Coproduct functor *)
+
+(** Lemma 34: Examples of preservation of cocontinuity *)
+(** (i): Composition of functors *)
+(** (ii): Families of functors *)
+
+(** Definition 35: Exponentials *)
+
+(** Example 36: Exponentials in Set *)
+
+(** Lemma 37: Left and right product functors preserves colimits *)
+
+(** Theorem 38: Binary product functor is ω-cocontinuous *)
+
+(** Example 39: Lists of sets *)
+
+(** Theorem 41: Colimits of a specific shape in functor categories *)
+
+(** Theorem 42: Precomposition functor preserves colimits *)
+
+(** Theorem 44: Signature functor associated to a binding signature is ω-cocontinuous *)
+
+(** Construction 46: Datatypes specified by binding signatures (initial algebra of Id_H + H) *)
+
+(** Definition 47: Heterogeneous substitution systems *)
+
+(** Theorem 48: Construction of a substitution operation on an initial algebra *)
+
+(** Theorem 49: Construction of a monad from a substitution system *)
+
+(** Section 4.2: Binding signatures to monads *)
+
+(** Example 50: Untyped lambda calculus *)
+
+(** Example 51: Raw syntax of Martin-Löf type theory *)
+
+
+
 
 Definition Arity_to_Signature :
   Π (C : precategory) (hsC : has_homsets C),
