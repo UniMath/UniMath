dcd5f90ce4cbbe3459340d9b5748b1775870f9dc
mweav <mzw@cs.princeton.edu>
2016-12-08 17:29:04 -0500

    General progress on proof

--- UniMath/CategoryTheory/elems_slice_equiv.v
+++ UniMath/CategoryTheory/elems_slice_equiv.v
@@ -13,8 +13,6 @@ Require Import UniMath.Foundations.Basics.Sets
 Section elems_slice_equiv.
 
   Local Open Scope cat.
-  Local Open Scope set.
-  Local Open Scope type.
 
   Local Definition el {C : Precategory} (P : C^op ==> SET) : Precategory := @cat C P.
   Local Definition pshf (C : Precategory) : Precategory := [C^op, SET].
@@ -37,80 +35,150 @@ Section elems_slice_equiv.
 
   Definition pshf_to_slice_ob_is_funct (F : pshf (el P)) : is_functor (pshf_to_slice_ob_funct_data F).
   Proof.
-    intros [[ F Fmor ] [ Fid Fcomp ]].
+    intros [[Ffun Fmor] [Fid Fcomp]].
     split.
     + intro a. simpl.
-      unfold pshf_to_slice_ob_funct_mor.
-      unfold pshf_to_slice_ob_funct_fun.
-      simpl.
-      apply funextsec. intro p.
-      unfold identity. simpl.
-      SearchAbout tpair.
-
-      rewrite tppr.
-      apply (total2_paths2 (transportf (fun f => f (pr1 p) = pr1 p) (!(pr1 (pr2 P)) a) (idpath _))).
-
-      SearchAbout transportf.
-
-      (*
-      rewrite transport_idfun.
-      rewrite (transport_idfun ((λ f : pr1 ((pr1 P) a) → pr1 ((pr1 P) a), (f (pr1 p) = pr1 p)%type))). *)
-
+      unfold identity.
+      unfold pshf_to_slice_ob_funct_mor. simpl.
+      induction P as [[Pfun Pmor] [Pid Pcomp]]; simpl.
+      apply funextsec; intro p.
+      induction p as [p Fp]; simpl.
+      apply (total2_paths2 (apevalat p (Pid a))).
+      induction ((Pid a)).
+      unfold transportf.
+      unfold constr1. simpl.
+      unfold idfun.
+      admit.
+    + intros a b c f g.
+      unfold pshf_to_slice_ob_funct_data; simpl.
+      unfold pshf_to_slice_ob_funct_mor; simpl.
+      (* apply funextsec; intro p.
+      induction p as [p Fp]; simpl.
+      apply (total2_paths2 (apevalat p ((pr2 (pr2 P)) a b c f g))).
+      induction P as [[Pfun Pmor] [Pid Pcomp]]; simpl. *)
+      admit.
   Admitted.
 
-  Definition presheaf_to_slice_ob : pshf (el P) → slice (pshf C) P.
+  Definition pshf_to_slice_ob_funct (F : pshf (el P)) : pshf C :=
+    pshf_to_slice_ob_funct_data F ,, pshf_to_slice_ob_is_funct F.
+
+  Definition pshf_to_slice_ob_nat_fun (F : pshf (el P)) (x : C) : (Σ (Px : pr1 ((pr1 P) x)), pr1 ((pr1 F) (x,, Px))) → pr1 ((pr1 P) x) := pr1.
+
+  Definition pshf_to_slice_ob : pshf (el P) → slice (pshf C) P.
   Proof.
-    simpl.
     intro F.
-    unfold slicecat_ob. simpl.
-  Admitted.
+    exists (pshf_to_slice_ob_funct F).
+    exists (pshf_to_slice_ob_nat_fun F).
+    intros x x' f.
+    reflexivity.
+  Defined.
+
+  Definition pshf_to_slice_ob_nat (X Y : pshf (el P)) (f : X --> Y) (c : C)
+    : (Σ (Px : pr1 ((pr1 P) c)), pr1 ((pr1 X) (c,, Px))) → (Σ (Px : pr1 ((pr1 P) c)), pr1 ((pr1 Y) (c,, Px))) := (* rewrite w/ defn*)
+    fun p => pr1 p ,, (pr1 f) (c ,, (pr1 p)) (pr2 p).
+
+  Definition pshf_to_slice_ob_isnat (X Y : pshf (el P)) (f : X --> Y) :
+    is_nat_trans (pshf_to_slice_ob_funct_data X) (pshf_to_slice_ob_funct_data Y) (pshf_to_slice_ob_nat X Y f).
+    simpl.
+    intros X Y f c c' g.
+    apply funextsec; intro p.
+    apply pair_path_in2.
+    exact (apevalat (pr2 p) ((pr2 f) (c ,, pr1 p) (c',, # (pr1 P) g (pr1 p)) (g,, idpath (# (pr1 P) g (pr1 p))))).
+  Defined.
+
+  Definition pshf_to_slice_mor (X Y : pshf (el P)) (f : X --> Y) :
+    pshf_to_slice_ob X --> pshf_to_slice_ob Y.
+    simpl.
+    intros X Y f.
+    exists (pshf_to_slice_ob_nat X Y f ,, pshf_to_slice_ob_isnat X Y f).
+    apply (invmaponpathsincl pr1).
+    + apply isofhlevelfpr1.
+      intros ?.
+      apply (isaprop_is_nat_trans _ _ has_homsets_HSET).
+    + reflexivity.
+  Defined.
+
+  Definition pshf_to_slice_data : functor_data (pshf (el P)) (slice (pshf C) P) :=
+    pshf_to_slice_ob ,, pshf_to_slice_mor.
+
+  Definition pshf_to_slice_is_funct : is_functor pshf_to_slice_data.
+  Proof.
+    split; [intros X | intros X Y Z f g];
+      apply slice_precat_morphisms_pr1_eq;
+      apply (invmaponpathsincl pr1);
+      try (apply isofhlevelfpr1;
+           intros ?;
+                  apply (isaprop_is_nat_trans _ _ has_homsets_HSET));
+      simpl.
+    + unfold pshf_to_slice_ob_nat , pshf_to_slice_ob_funct_fun.
+      apply funextsec; intro c.
+      apply funextsec; intro p.
+      rewrite tppr.
+      reflexivity.
+    + unfold pshf_to_slice_ob_nat.
+      apply funextsec; intro c.
+      apply funextsec; intro p.
+      rewrite tppr. reflexivity.
+  Defined.
+
+  Definition pshf_to_slice : (pshf (el P)) ==> (slice (pshf C) P) :=
+    pshf_to_slice_data ,, pshf_to_slice_is_funct.
+
+  Print pshf_to_slice_is_funct.
 
   (* make/find helper functions to extract various pieces of stuff I want *)
   Local Definition sl_nat {C : Precategory} {P : pshf C} (Q : slice (pshf C) P) := pr1 (pr2 Q).
   Local Definition sl_funct {C : Precategory} {P : pshf C} (Q : slice (pshf C) P) := pr1 (pr1 Q).
 
-  Definition slice_to_presheaf_ob_ob (Q : slice (pshf C) P) : (el P)^op → SET :=
+  Definition slice_to_pshf_ob_ob (Q : slice (pshf C) P) : (el P)^op → SET :=
     fun p =>
       hfiber ((sl_nat Q) (pr1 p)) (pr2 p) ,,
              isaset_hfiber ((sl_nat Q) (pr1 p)) (pr2 p) (pr2 (((sl_funct Q) (pr1 p)))) (pr2 ((pr1 P) (pr1 p))).
 
-  Definition slice_to_presheaf_ob_mor (Q : slice (pshf C) P) (F G : (el P)^op) (f : F --> G) :
-    slice_to_presheaf_ob_ob Q F --> slice_to_presheaf_ob_ob Q G.
-    unfold slice_to_presheaf_ob_ob. simpl.
-    intros Q [x Px] [y Py] [f feq] s.
-    apply (hfibersgftog (# (sl_funct Q) f) (sl_nat Q y)).
+  Definition slice_to_pshf_ob_mor (Q : slice (pshf C) P) (F G : (el P)^op) (f : F --> G) :
+    slice_to_pshf_ob_ob Q F --> slice_to_pshf_ob_ob Q G.
+    intros [[[Q Qmor] [Qid Qcomp]] [Qnat Qisnat]] [x Px] [y Py] [f feq] s.
+    apply (hfibersgftog (Qmor _ _ f) (Qnat y)).
     exists (pr1 s).
-    rewrite (feq).
-    assert (H : funcomp (# (sl_funct Q) f) (sl_nat Q y) (pr1 s) = (sl_nat Q (pr1 (y,, Py)) ∘ # (sl_funct Q) f) (pr1 s)).
-    admit.
-    rewrite H.
-    unfold sl_nat , sl_funct.
-    set (K := (apevalat (pr1 s) ((pr2 (pr2 Q)) _ _ f))).
-    simpl in K. simpl.
+    rewrite feq.
+    destruct (pr2 s).
+    exact (apevalat (pr1 s) (Qisnat _ _ f)).
+  Defined.
 
+  Definition slice_to_pshf_ob_funct_data (Q : slice (pshf C) P) : functor_data ((el P)^op) SET :=
+    (slice_to_pshf_ob_ob Q) ,, (slice_to_pshf_ob_mor Q).
 
-(*
-    intros [[Q Qis] [Qnat Qisnat]] [x Px] [y Py] [f feq] s.
-    apply (hfibersgftog (# Q f) (Qnat y)).
-    exists (pr1 s).
-    rewrite (feq).
-    assert (H : funcomp (# Q f) (Qnat y) (pr1 s) = (Qnat (pr1 (y,, Py)) ∘ # Q f) (pr1 s)).
-    admit.
-    rewrite H.
-    Check (Qisnat _ _ f).
-    rewrite (apevalat (pr1 s) (Qisnat _ _ f)).
-*)
-
-  (* exists ((pr2 (pr1 (pr1 Q))) _ _ (pr1 f) (pr1 s)).
-     rewrite <- (pr2 f).
-     Check (pr2 s). Check (pr2 Q). *)
-  Admitted.
-
-  Definition slice_to_presheaf_ob : slice (pshf C) P → pshf (el P).
+  Definition slice_to_pshf_ob_is_funct (Q : slice (pshf C) P) : is_functor (slice_to_pshf_ob_funct_data Q).
   Proof.
-    simpl. unfold slicecat_ob. simpl.
-    intro Q. unfold "==>". unfold functor_data. simpl.
+    split;
+      [intros [x Px] | intros [x Px] [y Py] [z Pz] [f feq] [g geq]];
+      destruct Q as [[[Q Qmor] [Qid Qcomp]] [Qnat Qisnat]];
+      apply funextsec; intro p;
+      apply (invmaponpathsincl pr1);
+      try (apply isofhlevelfpr1;
+           intros ?; exact (pr2 (eqset _ _))).
+    + exact (apevalat (pr1 p) (Qid x)).
+    + exact (apevalat (pr1 p) (Qcomp x y z f g)).
+  Defined.
+
+  Definition slice_to_pshf_ob : slice (pshf C) P → pshf (el P) :=
+    fun Q =>  slice_to_pshf_ob_funct_data Q ,,  slice_to_pshf_ob_is_funct Q.
+
+  Definition slice_to_pshf_ob_nat (X Y : slice (pshf C) P) (f : X --> Y) (e : (el P)^op) :
+    (slice_to_pshf_ob_ob X) e --> (slice_to_pshf_ob_ob Y) e.
+  Proof.
+    intros [[[X Xmor] [Xid Xcomp]] [Xnat Xisnat]] [[[Y Ymor] [Yid Ycomp]] [Ynat Yisnat]] f e.
+    unfold slice_to_pshf_ob_ob. unfold sl_nat. simpl.
   Admitted.
+
+
+  Definition slice_to_pshf_mor (X Y : slice (pshf C) P) (f : X --> Y) :
+    slice_to_pshf_ob X --> slice_to_pshf_ob Y.
+  Proof.
+    intros X Y f.
+    exists (slice_to_pshf_ob_nat X Y f).
+  Admitted.
+
 
 
 End elems_slice_equiv.
