241072ddb1c2e16318a15b6546c3d8571bf66548
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2017-01-22 16:04:24 +0100

    almost finished construction of fibration from CwDM

--- DisplayedCatFromCwDM.v
+++ DisplayedCatFromCwDM.v
@@ -11,12 +11,14 @@ category over [C]. Objects over [c:C] are display maps into [c].
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.UnicodeNotations.
+Require Import UniMath.CategoryTheory.limits.pullbacks.
 
 Require Import TypeTheory.Auxiliary.Auxiliary.
 Require Import TypeTheory.Auxiliary.UnicodeNotations.
 
 Require Import TypeTheory.Displayed_Cats.Auxiliary.
 Require Import TypeTheory.Displayed_Cats.Core.
+Require Import TypeTheory.Displayed_Cats.Fibrations.
 
 Require Import TypeTheory.OtherDefs.DM.
 
@@ -39,6 +41,9 @@ the subcategory of the arrow category where objects are display maps
 Section DM_Disp.
 
 Context (C:Precategory).
+
+Section defining_the_displayed_cat.
+
 Context (H : dm_sub_struct C).
 
 Definition DM_disp_ob_mor : disp_precat_ob_mor C.
@@ -101,8 +106,81 @@ Proof.
     + intro. apply isasetaprop. apply homset_property.
 Qed.
 
-Definition cod_disp : disp_precat C
+Definition DM_disp : disp_precat C
   := (DM_disp_data ,, DM_disp_axioms).
+
+End defining_the_displayed_cat.
+
+Variable H : DM_structure C.
+
+Local Definition D : disp_precat C := DM_disp H.
+
+Lemma is_fibration_DM_disp : is_fibration D.
+Proof.
+  intros x y f d.
+  unfold cartesian_lift.
+  unfold D in d. cbn in d. unfold DM_over in d.
+  destruct d as [[d g] H1].
+  unfold is_cartesian.
+  cbn in H1.
+  use tpair. 
+  - cbn. unfold DM_over.
+    use tpair.
+    + use tpair.
+      * { use pb_ob_of_DM.
+          - apply H.
+          - exact d.
+          - exact x.
+          - exact (g,,H1).
+          - exact y.
+          - exact f.
+        } 
+      * cbn. 
+        set (XR:= @pb_mor_of_DM C H _ _ (g,,H1) _ f). exact XR.
+    + cbn.
+      apply (pr2 (pr2 (pr1 H) _ _ _ _ _ )).
+  - simpl.
+    use tpair.
+    + use tpair. 
+      * use (@pb_mor_of_mor _ H).
+      * abstract (cbn; apply pathsinv0; use (@sqr_comm_of_dm_sub_pb _ H) ).
+    + cbn. 
+      intros c'' g0 d'' hh.
+      destruct d'' as [[e k] H2]. cbn in *.
+      destruct hh as [h H3]. cbn in *.
+      { use unique_exists.
+        - use tpair.
+          use PullbackArrow.
+          + exact h.
+          + exact (k ;; g0).
+          + abstract (etrans; [apply (!H3) |]; apply assoc ) .
+          + abstract (
+                cbn;
+                match goal with [|- _ = PullbackArrow ?PP _ _ _ _ ;; _    ] => 
+                                set (P := PP) end;
+                apply pathsinv0;
+                apply (PullbackArrow_PullbackPr2 P)
+              ).
+        - cbn.
+          apply subtypeEquality.
+          { intro.  apply homset_property. }
+          cbn.
+          match goal with [|- PullbackArrow ?PP _ _ _ _  ;; _ = _ ] =>
+                                set (P := PP) end.
+          apply (PullbackArrow_PullbackPr1 P).
+        - intro y0.
+          simpl.
+          admit.
+        - cbn.
+          intros y0 H5.
+          apply subtypeEquality.
+          { intro. apply homset_property. }
+          destruct y0 as [p H6]. cbn in *.
+          use PullbackArrowUnique.
+          assert (H7 := maponpaths pr1 H5); cbn in H7.
+          apply H7.
+          apply (!H6).
+Abort.
 
 End DM_Disp.
 
