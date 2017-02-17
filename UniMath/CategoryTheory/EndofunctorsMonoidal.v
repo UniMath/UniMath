58dac8c33b19f8ddabaff12703836aa89118d1b6
Ralph Matthes <Ralph.Matthes@irit.fr>
2017-02-17 20:06:50 +0100

    added more comments to why these definitions are needed
    this uses comments of Vladimir to the PR #576

--- UniMath/CategoryTheory/EndofunctorsMonoidal.v
+++ UniMath/CategoryTheory/EndofunctorsMonoidal.v
@@ -28,14 +28,26 @@ Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.UnicodeNotations.
 
-(** There is a monoidal structure on endofunctors, given by composition.
-    While this is considered to be strict in set-theoretic category theory,
-    it ain't strict in type theory with respect to convertibility.
-    So we consider it to be a weak monoidal structure instead.
+(** There is a monoidal structure on endofunctors, given by composition.  While
+    this is considered to be strict in set-theoretic category theory, it ain't
+    strict in type theory with respect to convertibility.  So we consider it to
+    be a weak monoidal structure instead. However, pointwise, it suffices to
+    take the identity for all those natural transformations (the identity is
+    also behind the definition of nat_trans_functor_assoc).
+
+    To understand the need for this structure even better, notice that the
+    proofs of functor axioms for one composition in the unitality and
+    associativity properties are slightly different from the proofs for another
+    and because of it the composition of functors is not strictly unital or
+    associative. However, these proofs are not used in the definition of natural
+    transformations, to be precise only functor_data is used, and the
+    composition of functor_data is strictly unital and associative.
+
 *)
 Section Monoidal_Structure_on_Endofunctors.
 
-(** while this is normally used for endofunctors, it can be done more generally *)
+(** while this is normally used for endofunctors, it can be done more generally,
+    but already for endofunctors, this is crucial for the development of substitution systems *)
 
 Context {C D : precategory}.
 
