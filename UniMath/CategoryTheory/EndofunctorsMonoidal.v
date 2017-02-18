ec66b5bf58aaa5073f6a7294adf3f7935d7216e2
Ralph Matthes <matthes@irit.fr>
2017-02-18 15:35:36 +0100

    simplify the proof presentation and get rid of match constructs
    Thanks to the feedback by Anders, Vladimir and Dan.

--- UniMath/CategoryTheory/EndofunctorsMonoidal.v
+++ UniMath/CategoryTheory/EndofunctorsMonoidal.v
@@ -80,36 +80,21 @@ Definition α_functor_inv (X : functor C D)(Y : functor D E)(Z : functor E F) :
     of the three pairs of functors; the extra assumption on having homsets is only used in order
     to have simple proofs, it is not necessary, as shown in Section "functor_equalities" in
     functor_categories.v: Lemmas functor_identity_left, functor_identity_right and functor_assoc *)
-Lemma motivation_ρ_functor (hsD : has_homsets D)(X : functor C D) : functor_composite X (functor_identity D) = X.
+Local Lemma motivation_ρ_functor (hsD : has_homsets D)(X : functor C D) : functor_composite X (functor_identity D) = X.
 Proof.
-  apply functor_eq.
-  exact hsD.
-  simpl.
-  destruct X as [data laws].
-  destruct data as [onobs onmorphs].
-  apply idpath.
-Qed.
+  now apply (functor_eq _ _ hsD); induction X as [data laws]; induction data as [onobs onmorphs].
+Defined.
 
-Lemma motivation_λ_functor (hsD : has_homsets D)(X : functor C D) : functor_composite (functor_identity C) X = X.
+Local Lemma motivation_λ_functor (hsD : has_homsets D)(X : functor C D) : functor_composite (functor_identity C) X = X.
 Proof.
-  apply functor_eq.
-  exact hsD.
-  simpl.
-  destruct X as [data laws].
-  destruct data as [onobs onmorphs].
-  apply idpath.
-Qed.
+  now apply (functor_eq _ _ hsD); induction X as [data laws]; induction data as [onobs onmorphs].
+Defined.
 
-Lemma motivation_α_functor (hsF : has_homsets F)(X : functor C D)(Y : functor D E)(Z : functor E F) :
+Local Lemma motivation_α_functor (hsF : has_homsets F)(X : functor C D)(Y : functor D E)(Z : functor E F) :
   functor_composite (functor_composite X Y) Z = functor_composite X (functor_composite Y Z).
 Proof.
-  apply functor_eq.
-  exact hsF.
-  simpl.
-  destruct X as [data laws].
-  destruct data as [onobs onmorphs].
-  apply idpath.
-Qed.
+  now apply (functor_eq _ _ hsF); induction X as [data laws]; induction data as [onobs onmorphs].
+Defined.
 
 (** these laws do not help in type-checking definitions which is why the transformations further above are needed *)
 
