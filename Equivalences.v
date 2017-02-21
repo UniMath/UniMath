84071f73e3d8d980f2be516f6bb30115cf2f2596
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2017-02-21 21:59:12 +0100

    adapt to separate file on adjunctions in UniMath/UniMath

--- Equivalences.v
+++ Equivalences.v
@@ -4,6 +4,7 @@
 
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.CategoryTheory.precategories.
+Require Import UniMath.CategoryTheory.Adjunctions.
 Require Import UniMath.CategoryTheory.equivalences.
 Require Import UniMath.CategoryTheory.UnicodeNotations.
 
