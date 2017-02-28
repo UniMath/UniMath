645e75a9bd177f2137d64b3459e7f2dd5a5bd79c
Daniel R. Grayson <dan@math.uiuc.edu>
2017-02-28 14:05:19 -0500

    add file

---
+++ UniMath/Combinatorics/WellOrderedSets.v
@@ -0,0 +1,6 @@
+(* -*- coding: utf-8 -*- *)
+
+Require Import UniMath.Combinatorics.OrderedSets.
+Unset Automatic Introduction.
+Require Import UniMath.Foundations.UnivalenceAxiom.
+Local Open Scope poset.
