5f84ee3930858cd67bb85262a65edddb5653aa21
Anders Mörtberg <andersmortberg@gmail.com>
2017-03-16 20:38:56 +0100

    minor

--- UniMath/CategoryTheory/limits/coproducts.v
+++ UniMath/CategoryTheory/limits/coproducts.v
@@ -17,9 +17,10 @@ Require Import UniMath.Foundations.Sets.
 Require Import UniMath.CategoryTheory.total2_paths.
 Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.functor_categories.
-Local Open Scope cat.
 Require Import UniMath.CategoryTheory.ProductPrecategory.
 Require Import UniMath.CategoryTheory.limits.graphs.colimits.
+
+Local Open Scope cat.
 
 (** * Definition of indexed coproducts of objects in a precategory *)
 Section coproduct_def.
