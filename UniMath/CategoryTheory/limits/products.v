917b3fc64b802192167e5be40979616002051dc4
Anders Mörtberg <andersmortberg@gmail.com>
2017-03-17 16:58:30 +0100

    oops

--- UniMath/CategoryTheory/limits/products.v
+++ UniMath/CategoryTheory/limits/products.v
@@ -19,7 +19,7 @@ Require Import UniMath.Foundations.Sets.
 Require Import UniMath.MoreFoundations.Tactics.
 
 Require Import UniMath.CategoryTheory.total2_paths.
-Require Import UniMath.CategoryTheory.precategories
+Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.ProductPrecategory.
 Require Import UniMath.CategoryTheory.limits.graphs.colimits.
