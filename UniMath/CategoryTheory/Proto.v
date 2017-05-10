c9b3acdac0c0ce010c7f2b73c0c12646f4c71af1
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2017-05-10 16:20:44 +0200

    update Proto

--- UniMath/CategoryTheory/Proto.v
+++ UniMath/CategoryTheory/Proto.v
@@ -17,7 +17,7 @@ Require Import UniMath.Foundations.Sets.
 
 Require Import UniMath.MoreFoundations.Tactics.
 
-Require Import UniMath.CategoryTheory.precategories.
+Require Import UniMath.CategoryTheory.Categories.
 Require Import UniMath.CategoryTheory.functor_categories.
 
 Local Open Scope cat.
