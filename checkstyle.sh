8154ce4c81c4153432173d01cf3469bfa1e38d50
Auke Booij <auke@tulcod.com>
2017-12-02 12:55:55 +0000

    remove superfluous comments from checkstyle.sh

--- checkstyle.sh
+++ checkstyle.sh
@@ -138,25 +138,3 @@ case "$1" in
         *)                              cmd_check_files "$@" ;;
 esac
 exit 0
-
-
-# Do not use Admitted or introduce new axioms.
-# Do not use apply with a term that needs no additional arguments filled in, because using exact would be clearer.
-# Do not use Prop or Set, and ensure definitions don't produce elements of them.
-# Do not use Type, except in Foundations/Basics/Preamble.v. Use UU instead. If higher universes are needed, they should be added to Foundations/Basics/Preamble.v.
-# Do not use Inductive or Record, except in Foundations/Basics/Preamble.v.
-# Do not use Module or Structure.
-# Do not use Fixpoint.
-# Do not use destruct, match, case, square brackets with intros, or nested square brackets with induction. (The goal is to prevent generation of proof terms using match.)
-# Use do with a specific numerical count, rather than repeat, to make proofs easier to repair.
-# Use as to name all new variables introduced by induction or destruct, if the corresponding type is defined in a remote location, because different names might be used by Coq when the definition of the type is changed. Name all variables introduced by assert, if they are used by name later, with as or to the left of a colon.
-# Do not end a proof with Qed., except with Goal, for that may prevent later computations.
-# Start all proofs with Proof. on a separate line and end it with Defined. on a separate line, as this makes it possible for us to generate HTML with expansible/collapsible proofs.
-# Use Unicode notation freely, but make the parsing conventions uniform across files, and consider putting them into a scope.
-# Each line should be limited to at most 100 (unicode) characters. The makefile target enforce-max-line-length can be used to detect nonconforming files, and the target show-long-lines can be used to display the nonconforming lines.
-# Always use Coq's proof structuring syntax ( { } + - * ) to focus on a single goal immediately after a tactic creates additional goals.
-# Indentation should normally be that produced automatically by emacs' coq-mode.
-# Within the core Foundations package:
-#     Do not start lines with : or with :=.
-#     One should normally put an extra blank line between units. Exceptions may be made for closely related items.
-# When using abstract in a proof, it is unsound to refer later by name to the abstracted lemma (whose name typically ends with _subproof), because its type may vary from one version of Coq to another. Coq's current behavior is also unlikely to be duplicated precisely by a future proof assistant.
