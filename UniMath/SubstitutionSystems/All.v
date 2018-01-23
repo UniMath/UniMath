ee1ad84201041c999c809d398c7ca905f3a18bfb
Daniel R. Grayson <dan@math.uiuc.edu>
2018-01-22 23:10:24 -0500

    Add full export file for each package
    ... using the Makefile.
    
    This supplants pull request #821.

---
+++ UniMath/SubstitutionSystems/All.v
@@ -0,0 +1,27 @@
+(* This file has been auto-generated, do not edit it. *)
+Require Export UniMath.SubstitutionSystems.Notation.
+Require Export UniMath.SubstitutionSystems.Signatures.
+Require Export UniMath.SubstitutionSystems.BinSumOfSignatures.
+Require Export UniMath.SubstitutionSystems.SumOfSignatures.
+Require Export UniMath.SubstitutionSystems.BinProductOfSignatures.
+Require Export UniMath.SubstitutionSystems.SubstitutionSystems.
+Require Export UniMath.SubstitutionSystems.MonadsFromSubstitutionSystems.
+Require Export UniMath.SubstitutionSystems.GenMendlerIteration.
+Require Export UniMath.SubstitutionSystems.GenMendlerIteration_alt.
+Require Export UniMath.SubstitutionSystems.LiftingInitial.
+Require Export UniMath.SubstitutionSystems.LiftingInitial_alt.
+Require Export UniMath.SubstitutionSystems.ModulesFromSignatures.
+Require Export UniMath.SubstitutionSystems.LamSignature.
+Require Export UniMath.SubstitutionSystems.Lam.
+Require Export UniMath.SubstitutionSystems.SignatureExamples.
+Require Export UniMath.SubstitutionSystems.SignatureCategory.
+Require Export UniMath.SubstitutionSystems.SubstitutionSystems_Summary.
+Require Export UniMath.SubstitutionSystems.LamHSET.
+Require Export UniMath.SubstitutionSystems.BindingSigToMonad.
+Require Export UniMath.SubstitutionSystems.LamFromBindingSig.
+Require Export UniMath.SubstitutionSystems.MLTT79.
+Require Export UniMath.SubstitutionSystems.FromBindingSigsToMonads_Summary.
+Require Export UniMath.SubstitutionSystems.MonadsMultiSorted.
+Require Export UniMath.SubstitutionSystems.MultiSorted.
+Require Export UniMath.SubstitutionSystems.STLC.
+Require Export UniMath.SubstitutionSystems.CCS.
