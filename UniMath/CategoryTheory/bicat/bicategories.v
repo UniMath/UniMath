ebec67aa5b8faa048bce7e007ef2a6ce126fb552
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2018-02-17 18:14:28 +0000

    Towards bicats from bicategories.

--- UniMath/CategoryTheory/bicat/bicategories.v
+++ UniMath/CategoryTheory/bicat/bicategories.v
@@ -22,62 +22,70 @@ Open Scope cat.
 
 Section unfold_data.
 
-(* Projections. *)
-
-Variable C : prebicategory_data.
-
-Local Definition C_id_comp : prebicategory_id_comp := pr1 C.
-
-Local Definition C_ob_1mor_2mor : prebicategory_ob_1mor_2mor := pr1 C_id_comp.
-
-Local Definition C0 : UU := pr1 C_ob_1mor_2mor.
-
-Local Definition Chom : C0 → C0 → precategory := pr2 C_ob_1mor_2mor.
-
-Local Definition C_id1 : ∏ a : C_ob_1mor_2mor, homprecat a a :=
-  pr1 (pr2 C_id_comp).
-
-Local Definition C_comp1
-  : ∏ (a b c : C0),
-    precategory_binproduct (homprecat a b) (homprecat b c) ⟶ homprecat a c
-  := pr2 (pr2 C_id_comp).
-
-Local Definition C_lassociator
-  : ∏ a b c d : C0, associator_trans_type a b c d
-  := pr1 (pr2 C).
-
-Local Definition C_left_unitor
-  : ∏ a b : C0, left_unitor_trans_type a b
-  := pr1 (pr2 (pr2 C)).
-
-Local Definition C_right_unitor
-  : ∏ a b : C0, right_unitor_trans_type a b
-  := pr2 (pr2 (pr2 C)).
-
-(* Packing back into a bicat_data. *)
+Variable C : prebicategory.
 
 Definition bcat_precategory_ob_mor : precategory_ob_mor.
 Proof.
-  use (C0,, _). simpl.
-  intros a b.
-  exact (Chom a b).
+  exists C.
+  exact (λ a b, homprecat a b).
 Defined.
 
 Definition bcat_cell_struct : bicat_cell_struct bcat_precategory_ob_mor
-  := λ (a b : C0) (f g : Chom a b), (Chom a b) ⟦ f, g ⟧.
+  := λ (a b : C) (f g : homprecat a b), (homprecat a b) ⟦ f, g ⟧.
 
 Definition bcat_ob_mor_cells : ∑ (C : precategory_ob_mor), bicat_cell_struct C.
 Proof.
-  refine (bcat_precategory_ob_mor,, _).
+  exists bcat_precategory_ob_mor.
   exact bcat_cell_struct.
 Defined.
 
-(*
 Definition bcat_cells_1_id_comp : ∑ C : bicat_ob_mor_cells, precategory_id_comp C.
 Proof.
   refine (bcat_ob_mor_cells,, _).
   split.
-  - simpl. intros.
- *)
+  - simpl. intros. exact (identity_1mor _).
+  - simpl. intros a b c f g. exact (compose_1mor f g).
+Defined.
+
+Definition bcat_2_id_comp_struct : bicat_2_id_comp_struct bcat_cells_1_id_comp.
+Proof.
+  repeat split; simpl; unfold bcat_cell_struct.
+  - (* 2-unit *)
+    intros. exact (identity _).
+  - (* left unitor *)
+    intros.
+    set (p := left_unitor_trans a b).
+    unfold left_unitor_trans_type in p.
+    exact (p f).
+  - (* right unitor *)
+    intros.
+    set (p := right_unitor_trans a b).
+    unfold right_unitor_trans_type in p.
+    exact (p f).
+  - (* left inverse unitor *)
+    admit.
+  - (* right inverse unitor *)
+    admit.
+  - (* right associator *)
+    admit.
+  - (* left associator *)
+    intros.
+    exact (associator_2mor f g h).
+  - (* vertical composition *)
+    intros a b f g h x y.
+    exact (x · y).
+  - (* left whiskering *)
+    intros a b c f g1 g2 x.
+    exact (compose_2mor_horizontal (identity f) x).
+  - (* right whiskering *)
+    intros a b c f1 f2 g x.
+    exact (compose_2mor_horizontal x (identity g)).
+Admitted.
+
+Definition bcat_data : ∑ C, bicat_2_id_comp_struct C.
+Proof.
+  exists bcat_cells_1_id_comp.
+  exact bcat_2_id_comp_struct.
+Defined.
 
 End unfold_data.
