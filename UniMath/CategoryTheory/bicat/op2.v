51bc2e254b5cefe649757c41fe764c8a08d9a330
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2018-02-20 18:39:08 +0000

    finish op2 bicat

--- UniMath/CategoryTheory/bicat/op2.v
+++ UniMath/CategoryTheory/bicat/op2.v
@@ -45,11 +45,35 @@ Proof.
   - apply id2_rwhisker.
   - apply lwhisker_vcomp.
   - apply rwhisker_vcomp.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
+  - use inv_cell_to_cell_post; [ apply is_equivalence_linvunitor |].
+    cbn.
+    apply pathsinv0.
+    etrans. apply vassocr.
+    use cell_to_inv_cell_post; [ apply is_equivalence_linvunitor |].
+    cbn.
+    apply pathsinv0. apply vcomp_lunitor.
+  - use inv_cell_to_cell_post; [ apply is_equivalence_rinvunitor |].
+    cbn.
+    apply pathsinv0.
+    etrans. apply vassocr.
+    use cell_to_inv_cell_post; [ apply is_equivalence_rinvunitor |].
+    cbn.
+    apply pathsinv0. apply vcomp_runitor.
+  - apply lassociator_to_rassociator_pre.
+    apply pathsinv0.
+    etrans. apply (vassocr _ _ _ ).
+    apply lassociator_to_rassociator_post.
+    apply pathsinv0. apply lwhisker_lwhisker.
+  - apply lassociator_to_rassociator_pre.
+    apply pathsinv0.
+    etrans. apply (vassocr _ _ _ ).
+    apply lassociator_to_rassociator_post.
+    apply pathsinv0. apply rwhisker_lwhisker.
+  - apply pathsinv0, lassociator_to_rassociator_pre.
+    apply pathsinv0.
+    etrans. apply (vassocr _ _ _ ).
+    apply lassociator_to_rassociator_post.
+    apply rwhisker_rwhisker.
   - apply (!vcomp_whisker _ _  ).
   - apply lunitor_linvunitor.
   - apply linvunitor_lunitor.
@@ -57,7 +81,42 @@ Proof.
   - apply rinvunitor_runitor.
   - apply lassociator_rassociator.
   - apply rassociator_lassociator.
-  - admit.
-  - admit.
-Admitted.
+  - use inv_cell_to_cell_post.
+    { use is_equivalence_rwhisker.
+      apply is_equivalence_rinvunitor.
+    } cbn.
+    apply pathsinv0.
+    use cell_to_inv_cell_post.
+    { use is_equivalence_lwhisker.
+      apply is_equivalence_linvunitor.
+    } cbn.
+    apply pathsinv0.
+    apply lassociator_to_rassociator_pre.
+    apply pathsinv0, runitor_rwhisker.
+  - use inv_cell_to_cell_post.
+    { use is_equivalence_rwhisker.
+      apply is_equivalence_rassociator.
+    } cbn.
+    apply pathsinv0.
+    etrans. apply vassocr.
+    use cell_to_inv_cell_post.
+    { apply is_equivalence_rassociator.
+    } cbn.
+    use cell_to_inv_cell_post.
+    { apply is_equivalence_rassociator.
+    } cbn.
+    apply pathsinv0.
+    repeat rewrite <- vassocr.
+    use inv_cell_to_cell_post.
+    { apply is_equivalence_rassociator.
+    } cbn.
+    use inv_cell_to_cell_post.
+    { apply is_equivalence_lwhisker.
+      apply is_equivalence_rassociator.
+    } cbn.
+    apply pathsinv0.
+    rewrite vassocr.
+    apply lassociator_lassociator.
+Qed.
+
 End op2.
