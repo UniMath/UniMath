b306534b053c2ab690ab6ee15340a8d1444aa43b
Langston Barrett <langston.barrett@gmail.com>
2018-02-28 19:07:34 +0000

    new lemma: x = y → (x = z ≃ y = z)

--- UniMath/MoreFoundations/PartA.v
+++ UniMath/MoreFoundations/PartA.v
@@ -132,17 +132,35 @@ Proof.
   apply idpath.
 Defined.
 
+(** If x = y, then x = z if and only if y = z by transitivity. *)
+Definition transitive_paths_weq {X : UU} {x y z : X} :
+  x = y -> (x = z ≃ y = z).
+Proof.
+  intro xeqy.
+  use weq_iso.
+  - intro xeqz.
+    exact (!xeqy @ xeqz).
+  - intro yeqz.
+    exact (xeqy @ yeqz).
+  - intro xeqz.
+    refine (path_assoc _ _ _ @ _).
+    refine (maponpaths (λ p, p @ xeqz) (pathsinv0r xeqy) @ _).
+    reflexivity.
+  - intro yeqz.
+    refine (path_assoc _ _ _ @ _).
+    refine (maponpaths (λ p, p @ yeqz) (pathsinv0l xeqy) @ _).
+    reflexivity.
+Defined.
+
 (** A rewrite of [pathsdirprod] as an equivalence:
     Two pairs are equal if and only if both of their components are. *)
 Definition pathsdirprodweq {X Y : UU} {x1 x2 : X} {y1 y2 : Y} :
   (dirprodpair x1 y1 = dirprodpair x2 y2) ≃ (x1 = x2) × (y1 = y2).
 Proof.
   intermediate_weq (dirprodpair x1 y1 ╝ dirprodpair x2 y2).
-  - apply (total2_paths_equiv (λ x : X, Y)).
+  - apply total2_paths_equiv.
   - unfold PathPair; cbn.
-    intermediate_weq (∑ p : x1 = x2, y1 = y2).
-    + use weqfibtototal; intro p; cbn.
-      apply eqweqmap.
-      exact (maponpaths (λ x, x = y2) (eqtohomot (transportf_const p Y) y1)).
-    + exact (idfun _,, idisweq _).
+    use weqfibtototal; intro p; cbn.
+    apply transitive_paths_weq.
+    apply (toforallpaths _ _ _ (transportf_const p Y) y1).
 Defined.
