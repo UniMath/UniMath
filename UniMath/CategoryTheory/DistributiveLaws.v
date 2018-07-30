fa0d8263ef85be5a7971db900821b810d3c72c5d
Hichem Saghrouni <saghrounihichem@hotmail.fr>
2018-07-30 11:52:21 +0200

    Update CategoryTheory/All.v and beginiing of DistributiveLaws.v

--- UniMath/CategoryTheory/DistributiveLaws.v
+++ UniMath/CategoryTheory/DistributiveLaws.v
@@ -2,7 +2,16 @@
 Internship at IRIT, 2018
 Under the supervision of Ralph Matthes *)
 
-Require Import UniMath.CategoryTheory.All.
+Require Import UniMath.Foundations.Sets.
+Require Import UniMath.CategoryTheory.Categories.
+Require Import UniMath.CategoryTheory.functor_categories.
+Require Import UniMath.CategoryTheory.whiskering.
+Require Import UniMath.CategoryTheory.FunctorAlgebras.
+Require Import UniMath.CategoryTheory.EndofunctorsMonoidal.
+Require Import UniMath.CategoryTheory.limits.initial.
+Require Import UniMath.CategoryTheory.Adjunctions.
+
+Local Open Scope cat.
 
 Section DefDistrLaw.
 
@@ -14,12 +23,18 @@ Variable H : functor C' C.
 Variable K : functor D' D.
 *)
 
-  Definition DistrLaw {C C' D D' : precategory} (F : functor C D) (F' : functor C' D') (H : functor C' C) (K : functor D' D) : UU := nat_trans (functor_composite H F) (functor_composite F' K).
+   Definition DistrLaw {C C' D D' : precategory} (F : functor C D) (F' :
+functor C' D') (H : functor C' C) (K : functor D' D) : UU := nat_trans
+(functor_composite H F) (functor_composite F' K).
 
-Definition DistrLaw_data_type {C C' D D' : precategory} (F : functor C D) (F' : functor C' D') (H : functor C' C) (K : functor D' D) : UU :=  ∏ x : ob C', (functor_composite H F) x -->  (functor_composite F' K) x.
+Definition DistrLaw_data_type {C C' D D' : precategory} (F : functor C
+D) (F' : functor C' D') (H : functor C' C) (K : functor D' D) : UU :=  ∏
+x : ob C', (functor_composite H F) x --> (functor_composite F' K) x.
 
 
-Definition is_distr_law {C C' D D' : precategory} (F : functor C D) (F' : functor C' D') (H : functor C' C) (K : functor D' D) (t :  DistrLaw_data_type F F' H K) := is_nat_trans _ _ t.
+Definition is_distr_law {C C' D D' : precategory} (F : functor C D) (F'
+: functor C' D') (H : functor C' C) (K : functor D' D) (t :
+DistrLaw_data_type F F' H K) := is_nat_trans _ _ t.
 
 End DefDistrLaw.
 
@@ -27,24 +42,29 @@ Print DistrLaw.
 
 Section OperationsDistrLaws.
 
-  Definition comp_distr_laws {C C' C'' D D' D'' : precategory} {F : functor C D} {F' : functor C' D'}  {F'' : functor C'' D''} {H : functor C' C} {H' : functor C'' C'} {K : functor D' D} {K' : functor D'' D'} (lambda : DistrLaw F F' H K) (lambda' : DistrLaw F' F'' H' K') : DistrLaw F F'' (functor_composite H' H ) (functor_composite K' K).
-  Proof.
-    red.
-    apply (nat_trans_comp (α_functor _ _ _)).
-    use (nat_trans_comp _ (α_functor _ _ _)).
-    apply (nat_trans_comp (pre_whisker H' lambda)).
-    apply (nat_trans_comp (α_functor_inv _ _ _)).
-    exact (post_whisker lambda' K).
-    Defined.
+   Definition comp_distr_laws {C C' C'' D D' D'' : precategory} {F :
+functor C D} {F' : functor C' D'}  {F'' : functor C'' D''} {H : functor
+C' C} {H' : functor C'' C'} {K : functor D' D} {K' : functor D'' D'}
+(lambda : DistrLaw F F' H K) (lambda' : DistrLaw F' F'' H' K') :
+DistrLaw F F'' (functor_composite H' H ) (functor_composite K' K).
+   Proof.
+     red.
+     apply (nat_trans_comp _ _ _ (α_functor _ _ _)).
+     use (nat_trans_comp _ _ _ _ (α_functor _ _ _)).
+     apply (nat_trans_comp _ _ _ (pre_whisker H' lambda)).
+     apply (nat_trans_comp _ _ _ (α_functor_inv _ _ _)).
+     exact (post_whisker lambda' K).
+     Defined.
 
 
 
-  Definition id_distr_law  {C D : precategory} (F : functor C D) : DistrLaw F F (functor_identity C) (functor_identity D).
-  Proof.
-    red.
-    apply (nat_trans_comp (λ_functor _)).
-    apply ρ_functor_inv.
-  Defined.
+   Definition id_distr_law  {C D : precategory} (F : functor C D) :
+DistrLaw F F (functor_identity C) (functor_identity D).
+   Proof.
+     red.
+     apply (nat_trans_comp _ _ _ (λ_functor _)).
+     apply ρ_functor_inv.
+   Defined.
 
   Lemma comp_distr_laws_assoc {C C' C'' C''' D D' D'' D''' : precategory} {F : functor C D} {F' : functor C' D'}  {F'' : functor C'' D''} {F''' : functor C''' D'''} {H : functor C' C} {H' : functor C'' C'} {H'' : functor C''' C''} {K : functor D' D} {K' : functor D'' D'} {K'' : functor D''' D''} (hs : has_homsets D) (lambda : DistrLaw F F' H K) (lambda' : DistrLaw F' F'' H' K') (lambda'' : DistrLaw F'' F''' H'' K'') :
        comp_distr_laws (comp_distr_laws lambda lambda') lambda'' =
