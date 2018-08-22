2c03890a466167a3f3971392deb00a53e84f43e3
Auke Booij <auke@tulcod.com>
2018-08-22 21:25:02 +0200

    Module was renamed.

--- UniMath/Algebra/Modules/Quotient.v
+++ UniMath/Algebra/Modules/Quotient.v
@@ -5,7 +5,7 @@ Auke Booij, December 2017
 
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.MoreFoundations.All.
-Require Import UniMath.Algebra.Rigs_and_Rings.
+Require Import UniMath.Algebra.RigsAndRings.
 Require Import UniMath.Algebra.Monoids_and_Groups.
 Require Import UniMath.Algebra.Modules.Core.
 Require Import UniMath.Algebra.Modules.Submodule.
