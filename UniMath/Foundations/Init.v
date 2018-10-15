da5def9b6356befc20d2979b3c8cf6773a587672
Daniel R. Grayson <dan@math.uiuc.edu>
2018-10-15 14:31:14 +0200

    create a combined "All.v" file,
    ... which loads all files from all packages.
    
    This involved settling upon a uniform parsing precedence for the
    notation "A ⇒ B".

--- UniMath/Foundations/Init.v
+++ UniMath/Foundations/Init.v
@@ -110,13 +110,10 @@ Reserved Notation "F ◾ b"  (at level 40, left associativity).
 Reserved Notation "F ▭ f"  (at level 40, left associativity). (* \rew1 *)
 (* to input: type "\rew" or "\re" with Agda input method *)
 
-(* conflict:
-    Reserved Notation "A ⇒ B" (at level 95, no associativity).
-    Reserved Notation "c ⇒ X" (at level 50).
-    (* to input: type "\Rightarrow" or "\r=" or "\r" or "\Longrightarrow" or "\=>" with Agda input method *)
-*)
+Reserved Notation "A ⇒ B" (at level 95, no associativity).
+(* to input: type "\Rightarrow" or "\r=" or "\r" or "\Longrightarrow" or "\=>" with Agda input method *)
 
-Reserved Notation "X ⇐ c"   (at level 50, left associativity).
+Reserved Notation "X ⇐ c"   (at level 95, no associativity).
 (* to input: type "\Leftarrow" or "\Longleftarrow" or "\l=" or "\l" with Agda input method *)
 
 Reserved Notation "x ⟲ f"  (at level 50, left associativity).
