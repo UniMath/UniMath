18c7e31ecde2201bac74459bf07297a37d4eba91
Daniel R. Grayson <dan@math.uiuc.edu>
2018-10-15 19:45:39 +0200

    add declarations

--- UniMath/MoreFoundations/DoubleNegation.v
+++ UniMath/MoreFoundations/DoubleNegation.v
@@ -1,7 +1,7 @@
 Require Import UniMath.Foundations.All.
 
 (** When proving a negation, we may undo a double negation. *)
-Lemma wma_dneg {X} P : ¬¬ P -> (P -> ¬ X) -> ¬ X.
+Lemma wma_dneg {X:Type} (P:Type) : ¬¬ P -> (P -> ¬ X) -> ¬ X.
 Proof.
   intros dnp p.
   apply dnegnegtoneg.
@@ -11,7 +11,7 @@ Proof.
 Defined.
 
 (** It's not false that a type is decidable. *)
-Lemma dneg_decidable P : ¬¬ decidable P.
+Lemma dneg_decidable (P:Type) : ¬¬ decidable P.
 Proof.
   intros ndec.
   unfold decidable in ndec.
@@ -20,7 +20,7 @@ Proof.
 Defined.
 
 (** When proving a negation, we may assume a type is decidable. *)
-Lemma wma_decidable {X} P : (decidable P -> ¬ X) -> ¬ X.
+Lemma wma_decidable {X:Type} (P:Type) : (decidable P -> ¬ X) -> ¬ X.
 Proof.
   apply (wma_dneg (decidable P)).
   apply dneg_decidable.
@@ -29,7 +29,7 @@ Defined.
 Local Open Scope logic.
 
 (** Compare with [negforall_to_existsneg], which uses LEM instead. *)
-Lemma negforall_to_existsneg' {X} (P:X->Type) : (¬ ∏ x, ¬¬ (P x)) -> ¬¬ (∃ x, ¬ (P x)).
+Lemma negforall_to_existsneg' {X:Type} (P:X->Type) : (¬ ∏ x, ¬¬ (P x)) -> ¬¬ (∃ x, ¬ (P x)).
 Proof.
   intros nf c. use nf; clear nf. intro x.
   assert (q := neghexisttoforallneg _ c x); clear c; simpl in q.
