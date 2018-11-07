1d023e853285d0dded108aba5554e665d08d1e7d
Daniel R. Grayson <dan@math.uiuc.edu>
2018-11-07 18:49:46 +0100

    exact categories
    clean up

--- UniMath/CategoryTheory/Ktheory/GrothendieckGroup.v
+++ UniMath/CategoryTheory/Ktheory/GrothendieckGroup.v
@@ -6,25 +6,46 @@ Require Import UniMath.Algebra.Free_Monoids_and_Groups.
 Import AddNotation.
 Local Open Scope addmonoid.
 
+Section WeakEquivalences.
+  Goal ∏ (X Y Z:Type) (f:X≃Y) (g:Y≃Z), pr1weq (weqcomp f g) = pr1weq g ∘ pr1weq f.
+  Proof.
+    reflexivity.
+  Qed.
+  Goal ∏ (X Y Z:Type) (f:X≃Y) (g:Y≃Z), invmap (weqcomp f g) = invmap f ∘ invmap g.
+  Proof.
+    reflexivity.
+  Qed.
+  Lemma weqtotal2 {X Y:Type} {P:X->Type} {Q:Y->Type} (f : X ≃ Y) :
+    (∏ x, P x ≃ Q (f x)) -> (∑ x:X, P x) ≃ (∑ y:Y, Q y).
+  Proof.                          (* move upstream *)
+    intros e. exists (λ xp, (f(pr1 xp),,e (pr1 xp) (pr2 xp))).
+    exact (twooutof3c _ _ (isweqfibtototal P (Q ∘ f) e) (pr2 (weqfp f Q))).
+  Defined.
+End WeakEquivalences.
+
 Section setquot.
+  Lemma setquot_map_recovery {X : Type} (R : eqrel X) (Y : hSet) (h : setquot R → Y) :
+    ∏ w : setquot R,
+          setquotuniv R Y (h ∘ setquotpr R)
+                      (λ (x x' : X) (r : R x x'), maponpaths h (iscompsetquotpr R x x' r)) w =
+          h w.
+  Proof.
+    apply (surjectionisepitosets (setquotpr R)).
+    - apply issurjsetquotpr.
+    - apply setproperty.
+    - intros x. refine (@setquotunivcomm X R Y (h ∘ setquotpr R) _ _).
+  Qed.
   Definition setquot_universal_property (X:Type) (R:eqrel X) (Y:hSet) :
     (setquot R -> Y) ≃ (∑ f : X -> Y, iscomprelfun R f).
   Proof.
-    use weqpair.
+    use weq_iso.
     - intros h. exists (h ∘ setquotpr R).
       intros x x' r. unfold funcomp. apply maponpaths. apply iscompsetquotpr. exact r.
-    - intros f. apply iscontraprop1.
-      + apply isaproptotal2.
-        * intros h. apply isaset_total2.
-          { apply impred_isaset. intros x. apply setproperty. }
-          intros f'. apply isasetaprop. apply isapropiscomprelfun.
-        * intros h h' e e'. apply funextsec; intros w.
-          apply (surjectionisepitosets _ _ _ (issurjsetquotpr R)).
-          { apply setproperty. }
-          exact (λ x, maponpaths (adjev x) (maponpaths pr1 e @ ! maponpaths pr1 e')).
-      + exists (setquotuniv R Y (pr1 f) (pr2 f)). apply subtypeEquality.
-        * intros f'. apply isapropiscomprelfun.
-        * reflexivity.
+    - intros f. exact (setquotuniv R Y (pr1 f) (pr2 f)).
+    - intros h. apply funextsec. unfold pr1,pr2. intros w. apply setquot_map_recovery.
+    - intros f. cbn beta. apply subtypeEquality.
+      * intros f'. apply isapropiscomprelfun.
+      * cbn. reflexivity.
   Defined.
   Goal ∏ (X:Type) (R:eqrel X) (Y:hSet) (h : setquot R -> Y),
     pr1 (setquot_universal_property X R Y h) = h ∘ setquotpr R.
@@ -44,24 +65,19 @@ Section Pi0.
   Definition component {X:Type} : X -> π₀ X := setquotpr (pathseqrel X).
   Definition π₀_map {X Y:Type} : (X -> Y) -> (π₀ X -> π₀ Y)
     := λ f, setquotfun (pathseqrel X) (pathseqrel Y) f (λ x x', hinhfun (maponpaths f)).
-  Definition π₀_universal_property (X:Type) (Y:hSet) : (π₀ X -> Y) ≃ (X -> Y).
+  Definition π₀_universal_property {X:Type} {Y:hSet} : (π₀ X -> Y) ≃ (X -> Y).
   Proof.
-    exists (λ h, h ∘ component). intros f.
-    apply iscontraprop1.
+    exists (λ h, h ∘ component). intros f. apply iscontraprop1.
     - apply isaproptotal2.
       + intros h. use (_ : isaset _). apply impred_isaset. intros x. apply setproperty.
       + intros h h' e e'. apply funextsec. intro w.
         { apply (surjectionisepitosets component).
           - apply issurjsetquotpr.
           - apply setproperty.
-          - intros x.
-            exact (maponpaths (λ k, k x) (e @ ! e')). }
-    - use tpair.
-      + exact (setquotuniv _ _ f (λ x y e, squash_to_prop e (setproperty Y (f x) (f y)) (maponpaths f))).
-      + cbn beta. reflexivity.
+          - intros x. exact (maponpaths (λ k, k x) (e @ ! e')). }
+    - now exists (setquotuniv _ _ f (λ x y e, squash_to_prop e (setproperty Y (f x) (f y)) (maponpaths f))).
   Defined.
-  Definition π₀_universal_map {X:Type} {Y:hSet} : (X -> Y) -> (π₀ X -> Y)
-    := invmap (π₀_universal_property X Y).
+  Definition π₀_universal_map {X:Type} {Y:hSet} : (X -> Y) -> (π₀ X -> Y) := invmap π₀_universal_property.
   Lemma π₀_universal_map_eqn {X:Type} {Y:hSet} (f : X -> Y) :
     ∏ (x:X), π₀_universal_map f (component x) = f x.
   Proof.
@@ -102,27 +118,13 @@ Proof.
   apply iscompsetquotpr. apply generated_binopeqrel_intro. apply hinhpr.
   exists E. split; reflexivity.
 Defined.
-Goal ∏ (X Y Z:Type) (f:X≃Y) (g:Y≃Z), pr1weq (weqcomp f g) = pr1weq g ∘ pr1weq f.
-Proof.
-  reflexivity.
-Qed.
-Goal ∏ (X Y Z:Type) (f:X≃Y) (g:Y≃Z), invmap (weqcomp f g) = invmap f ∘ invmap g.
-Proof.
-  reflexivity.
-Qed.
-Lemma weqtotal2 {X Y:Type} {P:X->Type} {Q:Y->Type} (f : X ≃ Y) :
-  (∏ x, P x ≃ Q (f x)) -> (∑ x:X, P x) ≃ (∑ y:Y, Q y).
-Proof.                          (* move upstream *)
-  intros e. exists (λ xp, (f(pr1 xp),,e (pr1 xp) (pr2 xp))).
-  exact (twooutof3c _ _ (isweqfibtototal P (Q ∘ f) e) (pr2 (weqfp f Q))).
-Defined.
 Lemma K_0_map_universal_property {M:ExactCategory} {G:abgr} :
   monoidfun (K_0 M) G
   ≃
   ∑ f : ob M -> G, ∏ E:ShortExactSequence M, f(Ob2 E) = f(Ob1 E) + f(Ob3 E).
 Proof.
   apply (weqcomp (presented_abgr_universal_property (K_0_hrel M) G)).
-  apply (weqtotal2 (π₀_universal_property _ _)). intros h. apply weqiff.
+  apply (weqtotal2 π₀_universal_property). intros h. apply weqiff.
   + split.
     * intros i E. exact (i _ _ (K_0_related E)).
     * intros k w w' r. apply (squash_to_prop r (setproperty _ _ _)).
@@ -154,37 +156,3 @@ Proof.
   apply pathsinv0.              (* omit this and it is slow! *)
   reflexivity.
 Qed.
-
-Section Try.
-  (* maybe try a different way to speed it up: *)
-  Definition K_0_universal_map' {M:ExactCategory} {G:abgr} (f : ob M -> G) :
-    (∏ E:ShortExactSequence M, f(Ob2 E) = f(Ob1 E) + f(Ob3 E)) -> monoidfun (K_0 M) G.
-  Proof.
-    intros c. use presented_abgr_extend.
-    - exact (π₀_universal_map f).
-    - intros x y Rxy. apply (squash_to_prop Rxy).
-      + apply setproperty.
-      + clear Rxy; intros [E [r s]]. induction (!r), (!s); clear r s.
-        change (f (Ob2 E) = f (Ob1 E) + f (Ob3 E)). exact (c E).
-  Defined.
-  Goal ∏ (M:ExactCategory) (G:abgr) (f : ob M -> G)
-        (add : ∏ E:ShortExactSequence M, f(Ob2 E) = f(Ob1 E) + f(Ob3 E)) (A:M),
-    K_0_universal_map' f add (K_0_class A) = f A.
-  Proof.
-    reflexivity.
-  Qed.
-  Lemma K_0_map_universal_property' {M:ExactCategory} {G:abgr} :
-    monoidfun (K_0 M) G
-    ≃
-    ∑ f : ob M -> G, ∏ E:ShortExactSequence M, f(Ob2 E) = f(Ob1 E) + f(Ob3 E).
-  Proof.
-    use weq_iso.
-    - intros h. exists (h ∘ K_0_class). intros E. unfold funcomp.
-      intermediate_path (h (K_0_class (Ob1 E) + K_0_class (Ob3 E))).
-      + apply (maponpaths h). apply K_0_eqn.
-      + apply h.
-    - intros [f add]. exact (K_0_universal_map' f add).
-    - cbn beta. intros h. admit.
-    - cbn beta.
-  Abort.
-End Try.
