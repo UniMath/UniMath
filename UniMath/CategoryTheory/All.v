166cd2aa6c8a0b591474fdb511301baa22d07c26
Niels van der Weide <n.vanderweide@science.ru.nl>
2018-11-19 11:37:48 +0100

    Added some proofs in bicategory_laws_2

--- UniMath/CategoryTheory/All.v
+++ UniMath/CategoryTheory/All.v
@@ -148,6 +148,7 @@ Require Export UniMath.CategoryTheory.DisplayedCats.Adjunctions.
 Require Export UniMath.CategoryTheory.Bicategories.Bicat.
 Require Export UniMath.CategoryTheory.Bicategories.BicatAliases.
 Require Export UniMath.CategoryTheory.Bicategories.bicategory_laws.
+Require Export UniMath.CategoryTheory.Bicategories.bicategory_laws_2.
 Require Export UniMath.CategoryTheory.Bicategories.Examples.one_types.
 Require Export UniMath.CategoryTheory.Bicategories.StandardBicategories.
 Require Export UniMath.CategoryTheory.Bicategories.OpCellBicat.
