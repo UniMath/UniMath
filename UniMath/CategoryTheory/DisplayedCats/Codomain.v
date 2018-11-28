08f43ed32e7b5af2188f8b6c562138dcaf7be4e5
Daniel R. Grayson <dan@math.uiuc.edu>
2018-11-28 12:41:50 +0100

    remove a duplicate definition
    ... and do a little clean-up
    
    Resolves #1050

--- UniMath/CategoryTheory/DisplayedCats/Codomain.v
+++ UniMath/CategoryTheory/DisplayedCats/Codomain.v
@@ -69,14 +69,14 @@ Proof.
     etrans. apply id_left.
     destruct ff as [ff H].
     apply pathsinv0.
-    etrans. use (pr1_transportf (C⟦x,y⟧)).
+    etrans. use (pr1_transportf (A := C⟦x,y⟧)).
     cbn; apply (eqtohomot (transportf_const _ _)).
   - apply subtypeEquality.
     { intro. apply homset_property. }
     etrans. apply id_right.
     destruct ff as [ff H].
     apply pathsinv0.
-    etrans. use (pr1_transportf (C⟦x,y⟧)).
+    etrans. use (pr1_transportf (A := C⟦x,y⟧)).
     cbn; apply (eqtohomot (transportf_const _ _)).
   - apply subtypeEquality.
     { intro. apply homset_property. }
@@ -84,7 +84,7 @@ Proof.
     destruct ff as [ff H].
     apply pathsinv0.
     etrans. unfold mor_disp.
-    use (pr1_transportf (C⟦x,w⟧)).
+    use (pr1_transportf (A := C⟦x,w⟧)).
     cbn; apply (eqtohomot (transportf_const _ _)).
   - apply (isofhleveltotal2 2).
     + apply homset_property.
