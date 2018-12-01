427aca9dc0440456afb917afcf414a88dec5570b
Langston Barrett <langston.barrett@gmail.com>
2018-12-01 00:39:55 -0800

    Free/forgetful adjunction for monoids

--- UniMath/CategoryTheory/categories/monoids.v
+++ UniMath/CategoryTheory/categories/monoids.v
@@ -4,6 +4,7 @@
 - Category of monoids
 - Forgetful functor to [HSET]
 - Free functor from [HSET]
+- Free/forgetful adjunction
  *)
 
 Require Import UniMath.Foundations.PartD.
@@ -16,10 +17,13 @@ Require Import UniMath.Algebra.Monoids_and_Groups.
 
 Require Import UniMath.Algebra.Free_Monoids_and_Groups.
 Require Import UniMath.Combinatorics.Lists.
+Require Import UniMath.Algebra.IteratedBinaryOperations.
 
 Require Import UniMath.CategoryTheory.Categories.
 Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.categories.HSET.Core.
+Require Import UniMath.CategoryTheory.Adjunctions.
+
 Local Open Scope cat.
 
 (** ** Precategory of monoids *)
@@ -226,3 +230,82 @@ Proof.
       intros ? ? ? ? ?.
       abstract (apply monoidfun_paths, funextfun, (free_monoidfun_comp_homot f g)).
 Defined.
+
+(** ** Free/forgetful adjunction *)
+
+(** The unit of this adjunction is the singleton function [x ↦ x::nil] *)
+Definition monoid_free_forgetful_unit :
+  nat_trans (functor_identity _)
+            (functor_composite monoid_free_functor monoid_forgetful_functor).
+Proof.
+  use mk_nat_trans.
+  - intros ? x; exact (cons x Lists.nil).
+  - intros ? ? ?.
+    abstract (apply funextfun; intro; reflexivity).
+Defined.
+
+(** This amounts to naturality of the counit: mapping commutes with folding *)
+Lemma iterop_list_mon_map {m n : monoid} (f : monoidfun m n) :
+  ∏ l, ((iterop_list_mon ∘ map (pr1monoidfun m n f)) l =
+        (pr1monoidfun _ _ f ∘ iterop_list_mon) l)%functions.
+Proof.
+  apply list_ind.
+  - apply pathsinv0, monoidfununel.
+  - intros x xs H.
+    unfold funcomp in *.
+    refine (maponpaths iterop_list_mon (map_cons _ _ _) @ _).
+    refine (iterop_list_mon_step _ _ @ _).
+    refine (_ @ !maponpaths _ (iterop_list_mon_step _ _)).
+    refine (_ @ !binopfunisbinopfun f _ _).
+    apply maponpaths.
+    assumption.
+Qed.
+
+(** The counit of this adjunction is the "folding" function
+    [[a, b, …, z] ↦ a · b · ⋯ · z]
+
+    (This is known to Haskell programmers as [mconcat].) *)
+Definition monoid_free_forgetful_counit :
+  nat_trans (functor_composite monoid_forgetful_functor monoid_free_functor )
+            (functor_identity _).
+Proof.
+  use mk_nat_trans.
+  - intros ?.
+    use tpair.
+    + intro; apply iterop_list_mon; assumption.
+    + split.
+      * intros ? ?; apply iterop_list_mon_concatenate.
+      * reflexivity.
+  - intros ? ? f; apply monoidfun_paths.
+    apply funextfun; intro; simpl in *.
+    apply (iterop_list_mon_map f).
+Defined.
+
+Definition monoid_free_forgetful_adjunction_data :
+  adjunction_data HSET monoid_precategory .
+Proof.
+  use tpair; [|use tpair]. (* TODO: there should be a constructor for this *)
+  - exact monoid_free_functor.
+  - exact monoid_forgetful_functor.
+  - split.
+    + exact monoid_free_forgetful_unit.
+    + exact monoid_free_forgetful_counit.
+Defined.
+
+Lemma monoid_free_forgetful_adjunction :
+  form_adjunction' monoid_free_forgetful_adjunction_data.
+Proof.
+  split; intro.
+  - apply monoidfun_paths.
+    apply funextfun.
+    simpl; unfold funcomp.
+    unfold homot; apply list_ind; [reflexivity|].
+    intros x xs ?.
+    unfold funcomp.
+    rewrite map_cons.
+    (* For some reason, the unifier needs a lot of help here... *)
+    refine (iterop_list_mon_step ((cons _ _) : pr1hSet (free_monoid _))
+                                  (map (λ z, cons z Lists.nil) xs) @ _).
+    apply maponpaths; assumption.
+  - reflexivity.
+Qed.
