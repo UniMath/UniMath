1a98a662d00a8ac9da17e42ce2e7ba0a047521d3
Daniel R. Grayson <dan@math.uiuc.edu>
2018-12-03 13:46:35 -0600

    remove automatically manufactured files All.v from the repository

--- UniMath/Tactics/All.v
+++
@@ -1,6 +0,0 @@
-(* This file has been auto-generated, do not edit it. *)
-Require Export UniMath.Tactics.Utilities.
-Require Export UniMath.Tactics.Monoids_Tactics.
-Require Export UniMath.Tactics.Abmonoids_Tactics.
-Require Export UniMath.Tactics.Groups_Tactics.
-Require Export UniMath.Tactics.Nat_Tactics.
--- UniMath/Topology/All.v
+++
@@ -1,5 +0,0 @@
-(* This file has been auto-generated, do not edit it. *)
-Require Export UniMath.Topology.Prelim.
-Require Export UniMath.Topology.Filters.
-Require Export UniMath.Topology.Topology.
-Require Export UniMath.Topology.CategoryTop.
