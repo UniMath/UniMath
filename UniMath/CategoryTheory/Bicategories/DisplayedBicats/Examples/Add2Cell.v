32ccf3f8cb81198dbec6251e45c6a37b122fc7f7
Niels van der Weide <n.vanderweide@science.ru.nl>
2018-12-05 20:48:39 +0100

    Example of monads. Mostly finished. The transformations still needs to be finished

---
+++ UniMath/CategoryTheory/Bicategories/DisplayedBicats/Examples/Add2Cell.v
@@ -0,0 +1,184 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Categories.
+Require Import UniMath.CategoryTheory.functor_categories.
+Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Bicat. Import Bicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.BicategoryLaws.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Invertible_2cells.
+Require Import UniMath.CategoryTheory.Bicategories.PseudoFunctor. Import PseudoFunctor.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.LaxTransformation.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispBicat. Import DispBicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Unitors.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Adjunctions.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Univalence.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispAdjunctions.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispUnivalence.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Examples.DisplayedCatToBicat.
+Require Import UniMath.CategoryTheory.Bicategories.identity.
+
+Local Open Scope cat.
+
+Section Add2Cell.
+  Context {C : bicat}.
+  Variable (D : disp_bicat C).
+
+  Local Notation E := (total_bicat D).
+  Local Notation F := (pr1_psfunctor D).
+
+  Variable (S : psfunctor E E)
+           (l r : pstrans S (ps_id_functor E)).
+
+  Definition add_cell_disp_cat_data : disp_cat_ob_mor E.
+  Proof.
+    use tpair.
+    - exact (λ X, #F (l X) ==> #F (r X)).
+    - exact (λ X Y α β f,
+             ((psfunctor_comp F (l X) f)^-1)
+               • (α ▹ #F f)
+               • psfunctor_comp F (r X) f
+               • ##F (laxnaturality_of r f)
+             =
+             (##F (laxnaturality_of l f))
+               • (psfunctor_comp F (#S f) (l Y))^-1
+               • (#F(#S f) ◃ β)
+               • psfunctor_comp F (#S f) (r Y)).
+  Defined.
+
+  Definition add_cell_disp_cat_laws : disp_cat_id_comp E add_cell_disp_cat_data.
+  Proof.
+    split.
+    - intros x xx ; cbn.
+      rewrite !id2_left, !id2_right.
+      pose (maponpaths pr1 (laxtrans_id_alt l x)) as p.
+      cbn in p.
+      rewrite p ; clear p.
+      pose (maponpaths pr1 (laxtrans_id_alt r x)) as p.
+      cbn in p.
+      rewrite p ; clear p.
+      rewrite !lwhisker_id2, !id2_left.
+      rewrite !vassocr.
+      rewrite vcomp_runitor.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite !vassocr.
+      rewrite linvunitor_natural.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite <- lwhisker_hcomp.
+      rewrite vcomp_whisker.
+      reflexivity.
+    - intros x y z f g xx yy zz Hf Hg.
+      pose (maponpaths pr1 (laxtrans_comp_alt l f g)) as pl.
+      pose (maponpaths pr1 (laxtrans_comp_alt r f g)) as pr.
+      cbn in *.
+      rewrite pl, pr ; clear pl pr.
+      rewrite !id2_left, !id2_right in Hf.
+      rewrite !id2_left, !id2_right in Hg.
+      rewrite !id2_left, !id2_right.
+      rewrite !vassocr.
+      rewrite vcomp_whisker.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite vcomp_whisker.
+      rewrite !vassocr.
+      apply maponpaths_2.
+      rewrite <- rwhisker_rwhisker.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite <- lwhisker_lwhisker.
+      rewrite !vassocr.
+      apply maponpaths_2.
+      rewrite rwhisker_vcomp.
+      etrans.
+      {
+        apply maponpaths_2.
+        apply maponpaths_2.
+        apply maponpaths.
+        apply Hf.
+      }
+      rewrite <- rwhisker_vcomp.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite !vassocr.
+      rewrite <- rwhisker_lwhisker_rassociator.
+      rewrite !vassocl.
+      apply maponpaths.
+      rewrite lwhisker_vcomp.
+      etrans.
+      {
+        apply maponpaths.
+        apply Hg.
+      }
+      rewrite <- lwhisker_vcomp.
+      reflexivity.
+  Qed.
+
+  Definition add_cell_disp_cat : disp_bicat E.
+  Proof.
+    use disp_cell_unit_bicat.
+    use tpair.
+    - exact add_cell_disp_cat_data.
+    - exact add_cell_disp_cat_laws.
+  Defined.
+
+  Definition add_cell_disp_cat_locally_univalent
+    : disp_locally_univalent add_cell_disp_cat.
+  Proof.
+    apply disp_cell_unit_bicat_locally_univalent.
+    intros.
+    simpl.
+    apply C.
+  Defined.
+
+  Definition add_cell_disp_cat_univalent_2_0
+             (HC : is_univalent_2_1 C)
+             (HD : disp_locally_univalent D)
+    : disp_univalent_2_0 add_cell_disp_cat.
+  Proof.
+    use disp_cell_unit_bicat_univalent_2_0.
+    - intro ; intros.
+      apply isasetaprop.
+      apply isapropunit.
+    - apply total_is_locally_univalent.
+      + exact HC.
+      + exact HD.
+    - intros.
+      apply C.
+    - intros x xx yy.
+      use isweqimplimpl.
+      + intros p.
+        induction p as [p q].
+        cbn ; unfold idfun.
+        cbn in p, q.
+        rewrite !id2_left, id2_right in p.
+        rewrite (laxtrans_id_alt l), (laxtrans_id_alt r) in p.
+        cbn in p.
+        rewrite !lwhisker_id2 in p.
+        rewrite !id2_left, !id2_right in p.
+        rewrite !vassocr in p.
+        rewrite vcomp_runitor in p.
+        rewrite !vassocl in p.
+        pose (vcomp_lcancel _ (is_invertible_2cell_runitor _) p) as p'.
+        use (vcomp_rcancel (linvunitor (pr1 (r x)))).
+        { is_iso. }
+        use (vcomp_rcancel (pr1 (laxfunctor_id S x) ▹ pr1 (r x))).
+        { is_iso.
+          use tpair.
+          - apply (pr1(pr2 S) x).
+          - split.
+            + exact (base_paths _ _ (pr1(pr2(pr1(pr2 S) x)))).
+            + exact (base_paths _ _ (pr2(pr2(pr1(pr2 S) x)))).
+        }
+        rewrite !vassocl.
+        refine (p' @ _).
+        rewrite vcomp_whisker.
+        rewrite !vassocr.
+        apply maponpaths_2.
+        rewrite lwhisker_hcomp.
+        refine (!(linvunitor_natural _)).
+      + apply C.
+      + apply isapropdirprod ; apply C.
+  Defined.
+End Add2Cell.
---
+++ UniMath/CategoryTheory/Bicategories/DisplayedBicats/Examples/Monads.v
@@ -0,0 +1,284 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Categories.
+Require Import UniMath.CategoryTheory.functor_categories.
+Require Import UniMath.CategoryTheory.PrecategoryBinProduct.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Bicat. Import Bicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.BicategoryLaws.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Invertible_2cells.
+Require Import UniMath.CategoryTheory.Bicategories.PseudoFunctor. Import PseudoFunctor.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.identity.
+Require Import UniMath.CategoryTheory.Bicategories.LaxTransformation.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispBicat. Import DispBicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Unitors.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Adjunctions.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Univalence.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispAdjunctions.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispUnivalence.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Sigma.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Examples.Algebras.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Examples.Add2Cell.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Examples.Prod.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.Examples.FullSub.
+
+Definition TODO {A : UU} : A.
+Admitted.
+
+Section MonadBicategory.
+  Variable (C : bicat).
+
+  Definition plain_monad
+    : bicat
+    := bicat_algebra (ps_id_functor C).
+
+  Definition plain_monad_is_univalent_2_1
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_1 plain_monad.
+  Proof.
+    apply bicat_algebra_is_univalent_2_1.
+    exact HC_1.
+  Defined.
+
+  Definition plain_monad_is_univalent_2_0
+             (HC_0 : is_univalent_2_0 C)
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_0 plain_monad.
+  Proof.
+    apply bicat_algebra_is_univalent_2_0.
+    - exact HC_0.
+    - exact HC_1.
+  Defined.
+
+  Definition unit_left_data
+    : laxtrans_data (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use mk_laxtrans_data.
+    - intros X.
+      cbn in *.
+      exists (id₁ _).
+      exact (runitor _ • linvunitor _).
+    - intros X Y f
+      ; cbn in * ; unfold total_prebicat_cell_struct in * ; cbn in *
+      ; unfold alg_disp_cat_2cell in * ; cbn in *.
+      exists (lunitor _ • rinvunitor _).
+      apply TODO.
+  Defined.
+
+  Definition unit_left_laws
+    : is_laxtrans unit_left_data.
+  Proof.
+    apply TODO.
+  Qed.
+
+  Definition unit_left
+    : pstrans (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact unit_left_data.
+      + exact unit_left_laws.
+    - apply TODO.
+  Defined.
+
+  Definition unit_right_data
+    : laxtrans_data (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use mk_laxtrans_data.
+    - intros X.
+      cbn in * ; unfold total_prebicat_cell_struct in * ; cbn in *
+      ; unfold alg_disp_cat_2cell in * ; cbn in *.
+      exists (pr2 X).
+      exact (id₂ _).
+    - intros X Y f
+      ; cbn in * ; unfold total_prebicat_cell_struct in * ; cbn in *
+      ; unfold alg_disp_cat_2cell in * ; cbn in *.
+      apply TODO.
+  Defined.
+
+  Definition unit_right_laws
+    : is_laxtrans unit_right_data.
+  Proof.
+    apply TODO.
+  Qed.
+
+  Definition unit_right
+    : pstrans (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact unit_right_data.
+      + exact unit_right_laws.
+    - apply TODO.
+  Defined.
+
+  Definition add_unit
+    : disp_bicat plain_monad.
+  Proof.
+    use add_cell_disp_cat.
+    - exact (ps_id_functor _).
+    - exact unit_left.
+    - exact unit_right.
+  Defined.
+
+  Definition bind_left_data
+    : laxtrans_data (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use mk_laxtrans_data.
+    - intros X.
+      cbn in *.
+      exists (pr2 X · pr2 X).
+      exact (lassociator _ _ _).
+    - intros X Y f
+      ; cbn in * ; unfold total_prebicat_cell_struct in * ; cbn in *
+      ; unfold alg_disp_cat_2cell in * ; cbn in *.
+      apply TODO.
+  Defined.
+
+  Definition bind_left_laws
+    : is_laxtrans bind_left_data.
+  Proof.
+    apply TODO.
+  Qed.
+
+  Definition bind_left
+    : pstrans (ps_id_functor plain_monad) (ps_id_functor plain_monad).
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact bind_left_data.
+      + exact bind_left_laws.
+    - apply TODO.
+  Defined.
+
+  Definition add_bind
+    : disp_bicat plain_monad.
+  Proof.
+    use add_cell_disp_cat.
+    - exact (ps_id_functor _).
+    - exact bind_left.
+    - exact unit_right.
+  Defined.
+
+  Definition add_unit_bind
+    : disp_bicat plain_monad
+    := disp_dirprod_bicat add_unit add_bind.
+
+  Definition lawless_monad := total_bicat add_unit_bind.
+
+  Definition lawless_monad_is_univalent_2_1
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_1 lawless_monad.
+  Proof.
+    apply total_is_locally_univalent.
+    - exact (plain_monad_is_univalent_2_1 HC_1).
+    - apply is_univalent_2_1_dirprod_bicat.
+      * apply add_cell_disp_cat_locally_univalent.
+      * apply add_cell_disp_cat_locally_univalent.
+  Defined.
+
+  Definition lawless_monad_is_univalent_2_0
+             (HC_0 : is_univalent_2_0 C)
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_0 lawless_monad.
+  Proof.
+    apply total_is_univalent_2_0.
+    - exact (plain_monad_is_univalent_2_0 HC_0 HC_1).
+    - apply is_univalent_2_0_dirprod_bicat.
+      + exact (plain_monad_is_univalent_2_1 HC_1).
+      + apply add_cell_disp_cat_univalent_2_0.
+        * exact HC_1.
+        * apply disp_alg_bicat_locally_univalent.
+      + apply add_cell_disp_cat_univalent_2_0.
+        * exact HC_1.
+        * apply disp_alg_bicat_locally_univalent.
+      + apply add_cell_disp_cat_locally_univalent.
+      + apply add_cell_disp_cat_locally_univalent.
+  Defined.
+
+  Definition monad_obj : lawless_monad → C
+    := λ m, pr1 (pr1 m).
+
+  Definition monad_map : ∏ (m : lawless_monad), monad_obj m --> monad_obj m
+    := λ m, pr2(pr1 m).
+
+  Definition monad_unit : ∏ (m : lawless_monad), id₁ (monad_obj m) ==> monad_map m
+    := λ m, pr1(pr2 m).
+
+  Definition monad_bind
+    : ∏ (m : lawless_monad), monad_map m · monad_map m ==> monad_map m
+    := λ m, pr2(pr2 m).
+
+  Definition monad_laws (m : lawless_monad) : UU
+    := ((linvunitor (monad_map m))
+          • (monad_unit m ▹ monad_map m)
+          • monad_bind m
+        =
+        id₂ (monad_map m))
+       ×
+       ((rinvunitor (monad_map m))
+          • (monad_map m ◃ monad_unit m)
+          • monad_bind m
+        =
+        id₂ (monad_map m))
+       ×
+       ((monad_map m ◃ monad_bind m)
+          • monad_bind m
+        =
+        (lassociator (monad_map m) (monad_map m) (monad_map m))
+          • (monad_bind m ▹ monad_map m)
+          • monad_bind m).
+
+  Definition monad := fullsubbicat lawless_monad monad_laws.
+
+  Definition mk_monad
+             (X : C)
+             (f : C⟦X,X⟧)
+             (η : id₁ X ==> f)
+             (μ : f · f ==> f)
+             (ημ : linvunitor f • (η ▹ f) • μ
+                   =
+                   id₂ f)
+             (μη : rinvunitor f • (f ◃ η) • μ
+                   =
+                   id₂ f)
+             (μμ : (f ◃ μ) • μ
+                   =
+                   lassociator f f f • (μ ▹ f) • μ)
+    : monad.
+  Proof.
+    use tpair.
+    - use tpair.
+      + use tpair.
+        * exact X.
+        * exact f.
+      + split.
+        * exact η.
+        * exact μ.
+    - repeat split.
+      + exact ημ.
+      + exact μη.
+      + exact μμ.
+  Defined.
+
+  Definition monad_is_univalent_2_1
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_1 monad.
+  Proof.
+    apply is_univalent_2_1_fullsubbicat.
+    apply lawless_monad_is_univalent_2_1.
+    exact HC_1.
+  Defined.
+
+  Definition monad_is_univalent_2_0
+             (HC_0 : is_univalent_2_0 C)
+             (HC_1 : is_univalent_2_1 C)
+    : is_univalent_2_0 monad.
+  Proof.
+    apply is_univalent_2_0_fullsubbicat.
+    - exact (lawless_monad_is_univalent_2_0 HC_0 HC_1).
+    - exact (lawless_monad_is_univalent_2_1 HC_1).
+    - intro ; simpl.
+      repeat (apply isapropdirprod) ; apply C.
+  Defined.
+End MonadBicategory.
