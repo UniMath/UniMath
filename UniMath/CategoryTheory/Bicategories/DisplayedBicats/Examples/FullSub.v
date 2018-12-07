491bf9cae2b178027d353909bb9082b05ea1bde3
Niels van der Weide <n.vanderweide@science.ru.nl>
2018-12-07 13:39:27 +0100

    Adjoint equivalences and invertible 2cells in fulls ubbicategory

--- UniMath/CategoryTheory/Bicategories/DisplayedBicats/Examples/FullSub.v
+++ UniMath/CategoryTheory/Bicategories/DisplayedBicats/Examples/FullSub.v
@@ -3,10 +3,7 @@
     Benedikt Ahrens, Marco Maggesi
     February 2018
 
- Various basic constructions of displayed and non displayed bicategories:
- - Unit displayed bicategory of a displayed 1-category.
- - Full subbicategory of a bicategory.
- - Direct product of bicategories.
+ Full subbicategory of a bicategory and proof that it's univalent.
  ********************************************************************************* *)
 
 Require Import UniMath.Foundations.All.
@@ -59,6 +56,50 @@ Section FullSubBicat.
     : f ==> g
     := (α ,, tt).
 
+  Definition fullsub_is_invertible_2cell_to_bicat_is_invertible_2cell
+             {X Y : fullsubbicat}
+             {f g : X --> Y}
+             (α : f ==> g)
+    : is_invertible_2cell α → @is_invertible_2cell C (pr1 X) (pr1 Y) (pr1 f) (pr1 g) (pr1 α).
+  Proof.
+    intros Hα.
+    use tpair.
+    - apply Hα.
+    - split ; cbn.
+      + exact (base_paths _ _ (vcomp_rinv Hα)).
+      + exact (base_paths _ _ (vcomp_lid Hα)).
+  Defined.
+
+  Definition bicat_is_invertible_2cell_to_fullsub_is_invertible_2cell
+             {X Y : fullsubbicat}
+             {f g : X --> Y}
+             (α : f ==> g)
+    : @is_invertible_2cell C (pr1 X) (pr1 Y) (pr1 f) (pr1 g) (pr1 α) → is_invertible_2cell α.
+  Proof.
+    intros Hα.
+    use tpair.
+    - refine (_ ,, tt).
+      apply Hα.
+    - split ; use subtypeEquality.
+      + intro ; apply isapropunit.
+      + apply Hα.
+      + intro ; apply isapropunit.
+      + apply Hα.
+  Defined.
+
+  Definition bicat_is_invertible_2cell_is_fullsub_is_invertible_2cell
+             {X Y : fullsubbicat}
+             {f g : X --> Y}
+             (α : f ==> g)
+    : @is_invertible_2cell C (pr1 X) (pr1 Y) (pr1 f) (pr1 g) (pr1 α) ≃ is_invertible_2cell α.
+  Proof.
+    use weqimplimpl.
+    - exact (bicat_is_invertible_2cell_to_fullsub_is_invertible_2cell α).
+    - exact (fullsub_is_invertible_2cell_to_bicat_is_invertible_2cell α).
+    - apply isaprop_is_invertible_2cell.
+    - apply isaprop_is_invertible_2cell.
+  Defined.
+
   Definition disp_fullsubbicat_locally_univalent
     : disp_locally_univalent disp_fullsubbicat.
   Proof.
@@ -66,8 +107,7 @@ Section FullSubBicat.
     intros x y f xx yy ff gg.
     use isweqimplimpl.
     - intros.
-      induction ff, gg.
-      reflexivity.
+      apply isapropunit.
     - apply isasetaprop.
       exact isapropunit.
     - simple refine (isaprop_total2 (_ ,, _) (λ η , _ ,, _)).
@@ -84,6 +124,103 @@ Section FullSubBicat.
     apply total_is_locally_univalent.
     - exact HC.
     - exact disp_fullsubbicat_locally_univalent.
+  Defined.
+
+  Definition fullsub_left_adjoint_equivalence_to_bicat_left_adjoint_equivalence
+             {X Y : fullsubbicat}
+             (f : X --> Y)
+    : left_adjoint_equivalence f → @left_adjoint_equivalence C (pr1 X) (pr1 Y) (pr1 f).
+  Proof.
+    intros Hf.
+    use tpair.
+    - use tpair.
+      + exact (pr1 (left_adjoint_right_adjoint Hf)).
+      + split.
+        * exact (pr1 (left_adjoint_unit Hf)).
+        * exact (pr1 (left_adjoint_counit Hf)).
+    - split ; split.
+      + exact (base_paths _ _ (internal_triangle1 Hf)).
+      + exact (base_paths _ _ (internal_triangle2 Hf)).
+      + cbn.
+        apply (fullsub_is_invertible_2cell_to_bicat_is_invertible_2cell
+                 (left_adjoint_unit (pr1 Hf))).
+        apply Hf.
+      + cbn.
+        apply (fullsub_is_invertible_2cell_to_bicat_is_invertible_2cell
+                 (left_adjoint_counit (pr1 Hf))).
+        apply Hf.
+  Defined.
+
+  Definition bicat_left_adjoint_equivalence_to_fullsub_left_adjoint_equivalence
+             {X Y : fullsubbicat}
+             (f : X --> Y)
+    : @left_adjoint_equivalence C (pr1 X) (pr1 Y) (pr1 f) → left_adjoint_equivalence f.
+  Proof.
+    intros Hf.
+    use tpair.
+    - use tpair.
+      + exact (left_adjoint_right_adjoint Hf ,, tt).
+      + split.
+        * exact (left_adjoint_unit Hf ,, tt).
+        * exact (left_adjoint_counit Hf ,, tt).
+    - split ; split.
+      + use subtypeEquality.
+        * intro ; apply isapropunit.
+        * exact (internal_triangle1 Hf).
+      + use subtypeEquality.
+        * intro ; apply isapropunit.
+        * exact (internal_triangle2 Hf).
+      + apply bicat_is_invertible_2cell_to_fullsub_is_invertible_2cell.
+        apply Hf.
+      + apply bicat_is_invertible_2cell_to_fullsub_is_invertible_2cell.
+        apply Hf.
+  Defined.
+
+  Definition fullsub_left_adjoint_equivalence_is_bicat_left_adjoint_equivalence
+             {X Y : fullsubbicat}
+             (f : X --> Y)
+    : left_adjoint_equivalence f ≃ @left_adjoint_equivalence C (pr1 X) (pr1 Y) (pr1 f).
+  Proof.
+    use weqpair.
+    - exact (fullsub_left_adjoint_equivalence_to_bicat_left_adjoint_equivalence f).
+    - use isweq_iso.
+      + exact (bicat_left_adjoint_equivalence_to_fullsub_left_adjoint_equivalence f).
+      + intros x.
+        use subtypeEquality.
+        * intro.
+          do 2 apply isapropdirprod.
+          ** apply fullsubbicat.
+          ** apply fullsubbicat.
+          ** apply isaprop_is_invertible_2cell.
+          ** apply isaprop_is_invertible_2cell.
+        * induction x as [a hx].
+          induction a as [r uc].
+          induction uc as [η ε].
+          induction r as [r x].
+          induction x.
+          induction η as [η x].
+          induction x.
+          induction ε as [ε x].
+          induction x.
+          cbn in *.
+          reflexivity.
+      + intros x.
+        use subtypeEquality.
+        * intro.
+          do 2 apply isapropdirprod ; try (apply C) ; apply isaprop_is_invertible_2cell.
+        * reflexivity.
+  Defined.
+
+  Definition bicat_adjoint_equivalence_is_fullsub_adjoint_equivalence
+             (X Y : fullsubbicat)
+    : @adjoint_equivalence C (pr1 X) (pr1 Y) ≃ adjoint_equivalence X Y.
+  Proof.
+    apply invweq.
+    use weqtotal2.
+    - apply invweq.
+      apply weqtodirprodwithunit.
+    - intro ; cbn.
+      apply fullsub_left_adjoint_equivalence_is_bicat_left_adjoint_equivalence.
   Defined.
 
   Definition is_univalent_2_0_fullsubbicat
