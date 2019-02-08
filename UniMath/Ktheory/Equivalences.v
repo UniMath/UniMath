cf54c5e245661bc4474170c1a28604fd906ccf48
Daniel R. Grayson <dan@math.uiuc.edu>
2019-02-08 16:37:03 +0100

    clean up file on equivalences

--- UniMath/Ktheory/Equivalences.v
+++ UniMath/Ktheory/Equivalences.v
@@ -1,13 +1,33 @@
 (** * Equivalences *)
 
-Require Import UniMath.Foundations.PartD.
-Require Import UniMath.Foundations.UnivalenceAxiom.
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.Notations.
 Require Import UniMath.MoreFoundations.Tactics.
-Require Import UniMath.Ktheory.Tactics.
 
-Definition Equivalence X Y :=
-  ∑ (f:X->Y) (g:Y->X) (p:∏ y, f(g y) = y) (q:∏ x, g(f x) = x),
-      ∏ x, ap f (q x) = p(f x).
+Definition post_cat {X} {x y z:X} {p:y = z} : x = y -> x = z.
+Proof. intros q. exact (pathscomp0 q p). Defined.
+
+Definition pre_cat {X} {x y z:X} {p:x = y} : y = z -> x = z.
+Proof. intros q. exact (pathscomp0 p q). Defined.
+
+Ltac maponpaths_pre_post_cat :=
+  repeat rewrite path_assoc; repeat apply (maponpaths post_cat); repeat rewrite <- path_assoc;
+  repeat apply (maponpaths pre_cat); repeat rewrite path_assoc; repeat rewrite maponpathsinv0;
+  try reflexivity.
+
+Definition isEquivalence {X Y:Type} (f:X->Y) :=
+  ∑ (g:Y->X) (p:∏ y, f(g y) = y) (q:∏ x, g(f x) = x), ∏ x, maponpaths f (q x) = p(f x).
+
+Definition isEquivalence_toInverseFunction {X Y} {f:X->Y} : isEquivalence f -> Y->X := pr1.
+
+Definition isEquivalence_toTargetHomotopy {X Y} {f:X->Y} (i:isEquivalence f) : ∏ y, f (isEquivalence_toInverseFunction i y) = y := pr12 i.
+
+Definition isEquivalence_toSourceHomotopy {X Y} {f:X->Y} (i:isEquivalence f) : ∏ x, isEquivalence_toInverseFunction i (f x) = x := pr122 i.
+
+Definition isEquivalence_toAdjointness {X Y} {f:X->Y} (i:isEquivalence f)
+  : ∏ x, maponpaths f (isEquivalence_toSourceHomotopy i x) = isEquivalence_toTargetHomotopy i (f x) := pr222 i.
+
+Definition Equivalence X Y := ∑ (f:X->Y), isEquivalence f.
 
 Notation "X ≅ Y" := (Equivalence X Y) (at level 60, no associativity) : type_scope.
 
@@ -16,6 +36,8 @@ Definition makeEquivalence X Y f g p q h := (f,,g,,p,,q,,h) : X ≅ Y.
 Definition Equivalence_toFunction {X Y} : X≅Y -> X->Y := pr1.
 
 Coercion Equivalence_toFunction : Equivalence >-> Funclass.
+
+Definition Equivalence_to_isEquivalence {X Y} (w:X≅Y) : isEquivalence w := pr2 w.
 
 Definition Equivalence_toInverseFunction {X Y} : X≅Y -> Y->X.
 Proof. intros f. exact (pr1 (pr2 f)). Defined.
@@ -27,30 +49,35 @@ Definition Equivalence_toSourceHomotopy {X Y} (f:Equivalence X Y) : ∏ x, Equiv
   := pr1 (pr2 (pr2 (pr2 f))).
 
 Definition Equivalence_toAdjointness {X Y} (f:Equivalence X Y)
-  : ∏ x, ap f (Equivalence_toSourceHomotopy f x) = Equivalence_toTargetHomotopy f (f x)
+  : ∏ x, maponpaths f (Equivalence_toSourceHomotopy f x) = Equivalence_toTargetHomotopy f (f x)
   := pr2 (pr2 (pr2 (pr2 f))).
 
 Lemma transportf_fun_idpath {X Y} {f:X->Y} x x' (w:x = x') (t:f x = f x) :
               transportf (λ x', f x' = f x) w (idpath (f x)) = maponpaths f (!w).
 Proof. induction w. reflexivity. Qed.
 
+Definition isEquivalence_to_isweq {X Y} {f:X->Y} : isEquivalence f -> isweq f.
+Proof.
+  intros i.
+  set (g := isEquivalence_toInverseFunction i).
+  set (p := isEquivalence_toTargetHomotopy i).
+  set (q := isEquivalence_toSourceHomotopy i).
+  set (h := isEquivalence_toAdjointness i).
+  intro y.
+  exists (g y,,p y).
+  intros xe.
+  simple refine (hfibertriangle2 _ _ _ _ _).
+  - exact (! (q (pr1 xe)) @ maponpaths g (pr2 xe)).
+  - induction xe as [x e]; simpl. induction e; simpl.
+    rewrite pathscomp0rid.
+    rewrite maponpathsinv0.
+    rewrite h.
+    now rewrite pathsinv0l.
+Defined.
+
 Definition Equivalence_to_weq {X Y} : X ≅ Y -> X ≃ Y.
 Proof. intros w.
-       set (f := Equivalence_toFunction w).
-       set (g := Equivalence_toInverseFunction w).
-       set (p := Equivalence_toTargetHomotopy w).
-       set (q := Equivalence_toSourceHomotopy w).
-       set (h := Equivalence_toAdjointness w).
-       exists f. intro y.
-       exists (g y,,p y).
-       intros xe.
-       simple refine (hfibertriangle2 _ _ _ _ _).
-       - exact (! (q (pr1 xe)) @ maponpaths g (pr2 xe)).
-       - induction xe as [x e]; simpl. induction e; simpl.
-         rewrite pathscomp0rid.
-         rewrite maponpathsinv0.
-         rewrite h.
-         now rewrite pathsinv0l.
+       exact (weqpair (Equivalence_toFunction w) (isEquivalence_to_isweq (Equivalence_to_isEquivalence w))).
 Defined.
 
 Definition weq_to_Equivalence {X Y} : X ≃ Y -> X ≅ Y.
@@ -92,7 +119,7 @@ Definition weq_to_Equivalence' X Y : X ≃ Y -> Equivalence X Y.
   set (p := λ y, pr2 (pr1 (r y))).
   simpl in p.
   set (L := λ x, pr2 (r (f x)) (hfiberpair f x (idpath (f x)))).
-  set (q := λ x, ap pr1 (L x)).
+  set (q := λ x, maponpaths pr1 (L x)).
   set (q' := λ x, !q x).
   exact (makeEquivalence X Y f g p q'
              (λ x,
@@ -128,12 +155,12 @@ Proof. intros e. destruct q.
 
 Definition maponpaths_fun_fun_natl {X Y Z} {g g':X->Y} {f f':Y->Z}
            (q:homot g g') (p:homot f f') x :
-  ap f (q x) @ p (g' x) = p (g x) @ ap f' (q x).
+  maponpaths f (q x) @ p (g' x) = p (g x) @ maponpaths f' (q x).
 Proof. intros. destruct (q x). simpl. rewrite pathscomp0rid. reflexivity. Defined.
 
 Definition maponpaths_fun_fun_fun_natl {X Y Z W} {g g':X->Y}
            (q:homot g g') (h:Y->Z) {f f':Z->W} (p:homot f f') x :
-  ap f (ap h (q x)) @ p (h (g' x)) = p (h (g x)) @ ap f' (ap h (q x)).
+  maponpaths f (maponpaths h (q x)) @ p (h (g' x)) = p (h (g x)) @ maponpaths f' (maponpaths h (q x)).
 Proof. intros. destruct (q x). simpl. rewrite pathscomp0rid. reflexivity. Defined.
 
 
@@ -147,45 +174,45 @@ Local Arguments idpath {_ _}.
 Lemma other_adjoint {X Y} (f : X -> Y) (g : Y -> X)
       (p : ∏ y : Y, f (g y) = y)
       (q : ∏ x : X, g (f x) = x)
-      (h : ∏ x : X, ap f (q x) = p (f x)) :
- ∏ y : Y, ap g (p y) = q (g y).
+      (h : ∏ x : X, maponpaths f (q x) = p (f x)) :
+ ∏ y : Y, maponpaths g (p y) = q (g y).
 Proof. intros. apply pathsinv0.
        intermediate_path (
-            !(ap g (p (f (g y))))
-            @' ap g (p (f (g y)))
+            !(maponpaths g (p (f (g y))))
+            @' maponpaths g (p (f (g y)))
             @' q (g y)).
        { rewrite pathsinv0l. reflexivity. }
        intermediate_path (
-            !(ap g (ap f (q (g y))))
-            @' ap g (p (f (g y)))
+            !(maponpaths g (maponpaths f (q (g y))))
+            @' maponpaths g (p (f (g y)))
             @' q (g y)).
-       { maponpaths_pre_post_cat. apply (ap pathsinv0). apply (ap (ap g)).
+       { maponpaths_pre_post_cat. apply (maponpaths pathsinv0). apply (maponpaths (maponpaths g)).
          set (y' := g y). apply pathsinv0. exact (h y'). }
        intermediate_path (
-            !(ap g (ap f (q (g y))))
-            @' ap g (p (f (g y)))
+            !(maponpaths g (maponpaths f (q (g y))))
+            @' maponpaths g (p (f (g y)))
             @' ((!q (g (f (g y))))
                 @' q (g (f (g y)))
                 @' q (g y))).
        { rewrite pathsinv0l. reflexivity. }
        intermediate_path (
-            !(ap g (ap f (q (g y))))
-            @' ap g (p (f (g y)))
+            !(maponpaths g (maponpaths f (q (g y))))
+            @' maponpaths g (p (f (g y)))
             @' ((!q (g (f (g y))))
-                @' (ap g (p (f (g y)))
-                    @' !(ap g (p (f (g y))))
+                @' (maponpaths g (p (f (g y)))
+                    @' !(maponpaths g (p (f (g y))))
                     @' q (g (f (g y))))
                 @' q (g y))).
        { maponpaths_pre_post_cat. apply path_inv_rotate_rr. reflexivity. }
        apply path_inverse_from_right.
        repeat rewrite path_assoc.
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap (funcomp f g) (ap g (p (f (g y))))
-            @' ap g (p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths (funcomp f g) (maponpaths g (p (f (g y))))
+            @' maponpaths g (p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat.
@@ -197,83 +224,83 @@ Proof. intros. apply pathsinv0.
          rewrite (maponpathscomp g (λ x : X, g (f x))) in r.
          rewrite maponpathsidfun in r. exact r. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (ap f (ap g (p (f (g y)))))
-            @' ap g (p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths g (maponpaths f (maponpaths g (p (f (g y)))))
+            @' maponpaths g (p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp f g). reflexivity. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (ap f (ap g (p (f (g y)))) @' p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths g (maponpaths f (maponpaths g (p (f (g y)))) @' p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp0 g).  reflexivity. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (ap (funcomp g f) (p (f (g y))) @' p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths g (maponpaths (funcomp g f) (p (f (g y))) @' p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp g f). reflexivity. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (p (f (g (f (g y)))) @' p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths g (p (f (g (f (g y)))) @' p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp g f).
-         apply (ap (ap g)). generalize (f (g y)); clear y; intro y.
+         apply (maponpaths (maponpaths g)). generalize (f (g y)); clear y; intro y.
          assert (r := maponpaths_fun_fun_natl p p y); simpl in r.
          assert (s := maponpathsidfun (p y)); unfold idfun in s.
          rewrite s in r; clear s. rewrite (maponpathscomp g). exact r. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (p (f (g (f (g y)))))
-            @' ap g (p (f (g y)))
-            @' !(ap g (p (f (g y))))
+            @' maponpaths g (p (f (g (f (g y)))))
+            @' maponpaths g (p (f (g y)))
+            @' !(maponpaths g (p (f (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp0 g). reflexivity. }
        intermediate_path (
-            !(ap g (p y))
-            @' !(ap g (ap f (q (g y))))
+            !(maponpaths g (p y))
+            @' !(maponpaths g (maponpaths f (q (g y))))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (p (f (g (f (g y)))))
+            @' maponpaths g (p (f (g (f (g y)))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. repeat rewrite <- path_assoc.
          rewrite pathsinv0r. rewrite pathscomp0rid. reflexivity. }
        intermediate_path (
-            ap g ((!p y) @' ap f (!q (g y)))
+            maponpaths g ((!p y) @' maponpaths f (!q (g y)))
             @' !(q (g (f (g (f (g y))))))
-            @' ap g (p (f (g (f (g y)))))
+            @' maponpaths g (p (f (g (f (g y)))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. repeat rewrite <- maponpathsinv0.
          rewrite <- (maponpathscomp0 g). reflexivity. }
        intermediate_path (
             !(q (g y))
-            @' ap (funcomp f g) (ap g ((!p y) @' ap f (!q (g y))))
-            @' ap g (p (f (g (f (g y)))))
+            @' maponpaths (funcomp f g) (maponpaths g ((!p y) @' maponpaths f (!q (g y))))
+            @' maponpaths g (p (f (g (f (g y)))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. repeat rewrite maponpathscomp0.
          repeat rewrite <- (maponpathscomp f g).
          repeat rewrite maponpathsinv0. repeat rewrite path_comp_inv_inv.
-         apply (ap pathsinv0).
+         apply (maponpaths pathsinv0).
          assert (r := ! maponpaths_fun_fun_fun_natl q (funcomp f g) q (g y)); simpl in r.
          rewrite maponpathsidfun in r. repeat rewrite <- (maponpathscomp f g) in r.
          unfold funcomp in r; simpl in r. repeat rewrite path_assoc.
@@ -284,23 +311,23 @@ Proof. intros. apply pathsinv0.
          exact r. }
        intermediate_path (
             !(q (g y))
-            @' ap g (ap (funcomp g f) ((!p y) @' ap f (!q (g y))))
-            @' ap g (p (f (g (f (g y)))))
+            @' maponpaths g (maponpaths (funcomp g f) ((!p y) @' maponpaths f (!q (g y))))
+            @' maponpaths g (p (f (g (f (g y)))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp g f).
          rewrite <- (maponpathscomp f g). reflexivity. }
        intermediate_path (
             !(q (g y))
-            @' ap g (ap (funcomp g f) ((!p y) @' ap f (!q (g y)))
+            @' maponpaths g (maponpaths (funcomp g f) ((!p y) @' maponpaths f (!q (g y)))
                   @' p (f (g (f (g y)))))
             @' q (g (f (g y)))
             @' q (g y)).
-       { maponpaths_pre_post_cat. rewrite <- maponpathscomp0. apply (ap (ap g)).
+       { maponpaths_pre_post_cat. rewrite <- maponpathscomp0. apply (maponpaths (maponpaths g)).
          reflexivity. }
        intermediate_path (
             !(q (g y))
-            @' ap g (p y @' ((!p y) @' ap f (!q (g y))))
+            @' maponpaths g (p y @' ((!p y) @' maponpaths f (!q (g y))))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp g f).
@@ -308,25 +335,25 @@ Proof. intros. apply pathsinv0.
          repeat rewrite <- path_assoc. repeat apply path_inv_rotate_ll.
          repeat rewrite path_assoc. repeat apply path_inv_rotate_rr.
          apply pathsinv0. repeat rewrite <- (maponpathscomp0 g).
-         apply (ap (ap g)). rewrite h.
+         apply (maponpaths (maponpaths g)). rewrite h.
          assert (r := ! maponpaths_fun_fun_natl p p (f (g y))); simpl in r.
          rewrite maponpathsidfun in r. unfold funcomp in *; simpl in *.
          repeat rewrite <- (maponpathscomp g f) in r.
          repeat rewrite (path_assoc _ _ (p y)). rewrite r.
-         repeat rewrite <- (path_assoc _ _ (p y)). apply (ap pre_cat). clear r.
+         repeat rewrite <- (path_assoc _ _ (p y)). apply (maponpaths pre_cat). clear r.
          assert (r := maponpaths_fun_fun_natl p p y); simpl in r.
          rewrite maponpathsidfun in r.
          repeat rewrite <- (maponpathscomp g f) in r. exact r. }
        intermediate_path (
             (!q (g y))
-            @' ap g (ap f (!q (g y)))
+            @' maponpaths g (maponpaths f (!q (g y)))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. repeat rewrite <- maponpathsinv0.
-         apply (ap (ap g)). rewrite pathsinv0r. reflexivity. }
+         apply (maponpaths (maponpaths g)). rewrite pathsinv0r. reflexivity. }
        intermediate_path (
             (!q (g y))
-            @' ap (funcomp f g) (!q (g y))
+            @' maponpaths (funcomp f g) (!q (g y))
             @' q (g (f (g y)))
             @' q (g y)).
        { maponpaths_pre_post_cat. rewrite <- (maponpathscomp f g).
