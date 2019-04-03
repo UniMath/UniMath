27cf1ce26f68c8638f1563bf2aa30423b5fdd2a4
dominik-kirst <kirst@ps.uni-saarland.de>
2019-04-03 12:45:15 +0100

    morphism equation and refactoring

--- UniMath/Induction/M/ComputationalM.v
+++ UniMath/Induction/M/ComputationalM.v
@@ -1,13 +1,19 @@
 (** ** Refinement of M-types
 
     M-types can be refined to satisfy the right definitional equalities.
+    This idea is from Felix Rech's Bachelor's thesis.
+
+    Author: Dominik Kirst (@dominik-kirst)
 
  *)
 
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.CategoryTheory.All.
-Require Import UniMath.Induction.All.
+
+Require Import UniMath.Induction.PolynomialFunctors.
+Require Import UniMath.Induction.M.Core.
+Require Import UniMath.Induction.M.Uniqueness.
 
 Section Refinement.
 
@@ -19,64 +25,56 @@ Section Refinement.
   Local Notation F := (polynomial_functor A B).
 
   Variable M0 : coalgebra F.
-  Variable HM0 : is_final M0.
+  Variable finalM0 : is_final M0.
+  Local Notation corecM0 C := (pr11 (finalM0 C)).
 
-  Definition M_carrier := ∑ m : pr1 M0, ∃ C c, pr1 (pr1 (HM0 C)) c = m.
+  (* Refinement of the final coalgebra to computable elements *)
 
-  Definition M_corec (C : coalgebra F) (c : pr1 C) :
-    M_carrier.
+  Definition carrierM := ∑ m : pr1 M0, ∃ C c, corecM0 C c = m.
+
+  (* Definition of the corecursor *)
+
+  Definition corecM (C : coalgebra F) (c : pr1 C) :
+    carrierM.
   Proof.
-    exists (pr1 (pr1 (HM0 C)) c). apply hinhpr. exists C, c. reflexivity.
+    exists (corecM0 C c). apply hinhpr. exists C, c. reflexivity.
   Defined.
 
-  Definition P_help (af : F M_carrier) :
-    F (pr1 M0).
-  Proof.
-    refine (pr1 af,,_).
-    refine (fun x => pr1 (pr2 af x)).
-  Defined.
+  (* Definition of a proposition we factor the computation through *)
 
-  Definition P (m : M_carrier) :=
-    ∑ af : F M_carrier, pr2 M0 (pr1 m) = P_help af.
+  Definition P (m : carrierM) :=
+    ∑ af : F carrierM, pr2 M0 (pr1 m) = # F pr1 af.
 
   Lemma P_isaprop m :
     isaprop (P m).
   Proof.
   Admitted.
 
-  Definition rec' (C : coalgebra F) (c : pr1 C) :
-    F (pr1 M0).
-  Proof.
-    refine (pr1 (pr2 C c),,_).
-    refine (fun x => pr11 (HM0 C) (pr2 (pr2 C c) x)).
-  Defined.
+  (* Now the destructor of M can be defined *)
 
-  Lemma help C c :
-    pr2 M0 ((pr11 (HM0 C)) c) = rec' C c.
-  Proof.
-  Admitted.
-
-  Definition rec (C : coalgebra F) (c : pr1 C) :
-    F M_carrier.
-  Proof.
-    refine (pr1 (pr2 C c),,_).
-    refine (fun x => M_corec C (pr2 (pr2 C c) x)).
-  Defined.
-
-  Definition M_destr' (m : M_carrier) : P m.
+  Definition destrM' (m : carrierM) : P m.
   Proof.
     destruct m as [m0 H]. apply (squash_to_prop H); try apply P_isaprop.
-    intros [C[c <-]]. refine  (rec C c,,_). unfold rec, P_help. cbn.
-    apply help.
+    intros [C[c <-]]. refine  ((# F (corecM C) ∘ (pr2 C)) c,,_). cbn [pr1]. clear H.
+    assert (H : is_coalgebra_homo F (corecM0 C)).
+    - destruct finalM0 as [[G H] H']. apply H.
+    - apply toforallpaths in H. symmetry. apply H.
   Defined.
 
-  Definition M_destr (m : M_carrier) : F M_carrier :=
-    pr1 (M_destr' m).
+  Definition destrM (m : carrierM) : F carrierM :=
+    pr1 (destrM' m).
+
+  (* The destructor satisfies the corecursion equation computationally *)
 
   Lemma corec_computation C c :
-    M_destr (M_corec C c) = rec C c.
+    destrM (corecM C c) = # F (corecM C) (pr2 C c).
   Proof.
     reflexivity.
   Qed.
+
+  (* We pack both components into a coalgebra *)
+
+  Definition M : coalgebra F :=
+    (carrierM,,destrM).
 
 End Refinement.
