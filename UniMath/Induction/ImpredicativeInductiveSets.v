8f1521e83223a479c619fad93e6d05e9ad23847e
sspeight93 <samuel.speight@cs.ox.ac.uk>
2019-04-03 16:58:17 +0100

    yet another test

--- UniMath/Induction/ImpredicativeInductiveSets.v
+++ UniMath/Induction/ImpredicativeInductiveSets.v
@@ -1,6 +1,7 @@
 (** ** Impredicative Construction of Inductive Types that are Sets and Eliminate into Sets *)
 
 (** This is based on Sam Speight's master's thesis *)
+(** test *)
 
 Require Import UniMath.Foundations.Init.
 Require Import UniMath.Foundations.Preamble.
