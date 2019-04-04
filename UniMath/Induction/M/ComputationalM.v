f46e0cf9ba2b3d3d4e2152c410a9f20acbb11a01
dominik-kirst <kirst@ps.uni-saarland.de>
2019-04-04 17:02:27 +0100

    carriers are equal

--- UniMath/Induction/M/ComputationalM.v
+++ UniMath/Induction/M/ComputationalM.v
@@ -9,7 +9,11 @@
 
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
-Require Import UniMath.CategoryTheory.All.
+
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.CategoryTheory.Core.Functors.
+Require Import UniMath.CategoryTheory.FunctorCoalgebras.
+Require Import UniMath.CategoryTheory.categories.Types.
 
 Require Import UniMath.Induction.PolynomialFunctors.
 Require Import UniMath.Induction.M.Core.
@@ -25,12 +29,14 @@ Section Refinement.
   Local Notation F := (polynomial_functor A B).
 
   Variable M0 : coalgebra F.
+  Local Notation carrierM0 := (pr1 M0).
+
   Variable finalM0 : is_final M0.
   Local Notation corecM0 C := (pr11 (finalM0 C)).
 
   (* Refinement of the final coalgebra to computable elements *)
 
-  Definition carrierM := ∑ m : pr1 M0, ∃ C c, corecM0 C c = m.
+  Definition carrierM := ∑ m0 : carrierM0, ∃ C c, corecM0 C c = m0.
 
   (* Definition of the corecursor *)
 
@@ -76,5 +82,104 @@ Section Refinement.
 
   Definition M : coalgebra F :=
     (carrierM,,destrM).
+
+  (* The two carriers are equal *)
+
+  Definition injectM0 m0 :
+    ∃ C c, corecM0 C c = m0.
+  Proof.
+    apply hinhpr. exists M0, m0. destruct finalM0 as [[G H1] H2]. cbn.
+    specialize (H2 (coalgebra_homo_id F M0)). change (pr1 (G,,H1) m0 = m0).
+    pattern (G,,H1). rewrite <- H2. reflexivity.
+  Defined.
+
+  Lemma carriers_weq :
+    carrierM ≃ carrierM0.
+  Proof.
+    apply (weq_iso pr1 (fun m0 => m0,,injectM0 m0)).
+    - intros [m H]. cbn. apply maponpaths, ishinh_irrel.
+    - intros x. cbn. reflexivity.
+  Defined.
+
+  Lemma carriers_eq :
+    carrierM = carrierM0.
+  Proof.
+    apply weqtopaths, carriers_weq.
+  Defined.
+
+  (* M is final *)
+
+  Lemma help_eq1 (m : carrierM0) (H : ∃ C c, corecM0 C c = m) :
+    @paths carrierM (m,, injectM0 m) (m,, H).
+  Proof.
+    apply maponpaths, ishinh_irrel.
+  Qed.
+
+  Lemma finalM :
+    is_final M.
+  Proof.
+    intros C. unshelve refine ((corecM C,,idpath _),,_).
+    intros [h H]. assert (h = corecM C) as ->.
+    - apply funextsec. intros c. unfold corecM. admit.
+    - apply maponpaths.
+  Qed.
+
+  (* The two coalgebras are equial *)
+
+  Lemma eq1 (m0 : carrierM0) :
+    transportf (λ X, X → F X) carriers_eq destrM m0
+    = transportf (λ X, F X) carriers_eq (destrM (transportf (λ X, X) (!carriers_eq) m0)).
+  Proof.
+    destruct carriers_eq. reflexivity.
+  Qed.
+
+  Lemma eq2 (m0 : carrierM0) :
+    transportf (λ X, X) (!carriers_eq) m0 = (m0,,injectM0 m0).
+  Proof.
+  Admitted.
+
+  Definition eq3 (a : A) (f : B a -> carrierM) :
+    transportf (λ X, F X) carriers_eq (a,,f) = (a,,transportf (λ X, B a -> X) carriers_eq f).
+  Proof.
+    destruct carriers_eq. reflexivity.
+  Qed.
+
+  Definition eq4 a f :
+    transportf (λ X, B a → X) carriers_eq f = fun b => pr1 (f b).
+  Proof.
+  Admitted.
+
+  Lemma tpair_eta X (Y : X -> UU) :
+    forall (p : ∑ x, Y x), p = (pr1 p,,pr2 p).
+  Proof.
+    intros []. reflexivity.
+  Qed.
+
+  Lemma algebras_equal :
+    M = M0.
+  Proof.
+    use total2_paths_f.
+    - apply weqtopaths, carriers_weq.
+    - apply funextfun. intros m0.
+      rewrite eq1. rewrite eq2.
+
+      (*replace (destrM (m0,, injectM0 m0)) with
+          (pr1 (pr2 M0 m0),,fun b => (corecM M0) (pr2 (pr2 M0 m0) b)).*)
+
+      pattern (destrM (m0,, injectM0 m0)). rewrite tpair_eta.
+      rewrite eq3. rewrite eq4. cbn.
+  Admitted.
+
+  Definition eq3 (a : A) (f : B a -> carrierM) :
+    transportf (λ X, F X) carriers_eq (a,,f) = (a,,transportf (λ X, pr2 (F X)) carriers_eq f).
+  Proof. induction p; reflexivity. Defined.
+
+  (* Thus M is final *)
+
+  Lemma finalM :
+    is_final M.
+  Proof.
+    rewrite algebras_equal. apply finalM0.
+  Qed.
 
 End Refinement.
