853478b09a7de497d5c5b2950e72591b446d1da5
Maxime Dénès <maxime.denes@inria.fr>
2019-07-15 11:53:00 +0200

    Avoid relying on `Export` bugs
    See https://github.com/coq/coq/issues/10474 and
    https://github.com/coq/coq/issues/10480

--- UniMath/Algebra/ConstructiveStructures.v
+++ UniMath/Algebra/ConstructiveStructures.v
@@ -3,9 +3,9 @@
 
 Unset Kernel Term Sharing.
 
-Require Export UniMath.Algebra.Apartness.
 Require Export UniMath.Algebra.DivisionRig.
 Require Export UniMath.Algebra.Domains_and_Fields.
+Require Export UniMath.Algebra.Apartness.
 
 Require Import UniMath.MoreFoundations.Tactics.
 
