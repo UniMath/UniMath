450291b30ab89d3eed2f85a30c191980bcd91043
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-07-23 12:50:54 +0200

    Initial version of DispPseudofunctor.v.

---
+++ UniMath/CategoryTheory/Bicategories/DisplayedBicats/DispPseudofunctor.v
@@ -0,0 +1,210 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.CategoryTheory.Core.Functors.
+Require Import UniMath.CategoryTheory.DisplayedCats.Auxiliary.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Bicat. Import Bicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Examples.Initial.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Examples.Final.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispBicat. Import DispBicat.Notations.
+Require Import UniMath.CategoryTheory.Bicategories.Bicategories.Univalence.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispInvertibles.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispAdjunctions.
+Require Import UniMath.CategoryTheory.Bicategories.DisplayedBicats.DispUnivalence.
+Require Import UniMath.CategoryTheory.Bicategories.PseudoFunctors.Display.PseudoFunctorBicat.
+Require Import UniMath.CategoryTheory.Bicategories.PseudoFunctors.PseudoFunctor.
+Import PseudoFunctor.Notations.
+
+Local Open Scope cat.
+(* Local Open Scope mor_disp_scope. *)
+
+Section DispPseudofunctor.
+
+Context {B₁ : bicat} (D₁ : disp_bicat B₁)
+        {B₂ : bicat} (D₂ : disp_bicat B₂)
+        (F : psfunctor B₁ B₂).
+
+(** Data *)
+
+Definition disp_psfunctor_data : UU
+  :=
+    ∑ (obFF : ∏ x:B₁, D₁ x → D₂ (F x))
+      (morFF : ∏ (x y : B₁) (f : B₁⟦x,y⟧) (xx : D₁ x) (yy : D₁ y),
+               (xx -->[f] yy) →
+               (obFF x xx -->[#F f] obFF y yy))
+      (cellFF : ∏ (x y : B₁) (f g : B₁⟦x,y⟧) (α : f ==> g) (xx : D₁ x) (yy : D₁ y)
+                  (ff : xx -->[f] yy) (gg : xx -->[g] yy),
+                (ff ==>[α] gg) → (morFF x y f xx yy ff ==>[##F α] morFF x y g xx yy gg))
+      (disp_psfunctor_id : ∏ (x : B₁) (xx : D₁ x),
+                           disp_invertible_2cell
+                             (psfunctor_id F x)
+                             (id_disp (obFF x xx))
+                             (morFF x x (id₁ x) xx xx (id_disp xx))),
+    (∏ (x y z : B₁) (f : x --> y) (g : y --> z)
+       (xx : D₁ x) (yy : D₁ y) (zz : D₁ z)
+       (ff : xx -->[f] yy) (gg : yy -->[g] zz),
+     disp_invertible_2cell
+       (psfunctor_comp F f g)
+       (comp_disp (morFF x y f xx yy ff) (morFF y z g yy zz gg))
+       (morFF x z (f · g) xx zz (comp_disp ff gg))).
+
+Definition disp_psfunctor_ob (FFdata : disp_psfunctor_data)
+           {x : B₁}
+           (xx : D₁ x)
+  : D₂ (F x)
+  := pr1 FFdata x xx.
+
+Coercion disp_psfunctor_ob : disp_psfunctor_data >-> Funclass.
+
+Section Projections.
+
+Variable (FFdata : disp_psfunctor_data).
+
+Definition disp_psfunctor_mor
+           {x y : B₁} {f : B₁⟦x,y⟧} {xx : D₁ x} {yy : D₁ y}
+           (ff : xx -->[f] yy)
+  : FFdata x xx -->[#F f] FFdata y yy
+  := pr12 FFdata x y f xx yy ff.
+
+Definition disp_psfunctor_cell {x y : B₁} {f g : B₁⟦x,y⟧} {α : f ==> g}
+           {xx : D₁ x} {yy : D₁ y}
+           {ff : xx -->[f] yy} {gg : xx -->[g] yy}
+           (αα : ff ==>[α] gg)
+  : disp_psfunctor_mor ff ==>[##F α] disp_psfunctor_mor gg
+  := pr122 FFdata x y f g α xx yy ff gg αα.
+
+Definition disp_psfunctor_id {x : B₁} (xx : D₁ x)
+  : disp_invertible_2cell (psfunctor_id F x)
+                          (id_disp (FFdata x xx))
+                          (disp_psfunctor_mor (id_disp xx))
+  := pr122 (pr2 FFdata) x xx.
+
+Definition disp_psfunctor_comp
+           {x y z : B₁} {f : x --> y} {g : y --> z}
+           {xx : D₁ x} {yy : D₁ y} {zz : D₁ z}
+           (ff : xx -->[f] yy) (gg : yy -->[g] zz)
+  : disp_invertible_2cell (psfunctor_comp F f g)
+                          (comp_disp (disp_psfunctor_mor ff) (disp_psfunctor_mor gg))
+                          (disp_psfunctor_mor (comp_disp ff gg))
+  := pr222 (pr2 FFdata) x y z f g xx yy zz ff gg.
+
+End Projections.
+
+Definition total_psfunctor_data (FFdata : disp_psfunctor_data)
+  : psfunctor_data (total_bicat D₁) (total_bicat D₂).
+Proof.
+  use make_psfunctor_data.
+  - exact (λ x, (F (pr1 x),, FFdata _ (pr2 x))).
+  - exact (λ x y f, (#F (pr1 f) ,, disp_psfunctor_mor FFdata (pr2 f))).
+  - exact (λ x y f g α, (##F (pr1 α),, disp_psfunctor_cell FFdata (pr2 α))).
+  - exact (λ x, iso_in_E_weq _ _ (psfunctor_id F (pr1 x),, disp_psfunctor_id FFdata (pr2 x))).
+  - refine (λ x y z f g, iso_in_E_weq _ _ _).
+    exact (psfunctor_comp F (pr1 f) (pr1 g),, disp_psfunctor_comp FFdata (pr2 f) (pr2 g)).
+Defined.
+
+(** Properties *)
+
+Section DispPseudofunctorLaws.
+
+Variable FFdata : disp_psfunctor_data.
+
+Definition disp_psfunctor_id2_law
+  : UU
+  := ∏ (a b : B₁) (f : a --> b)
+       (aa : D₁ a)
+       (bb : D₁ b)
+       (ff : aa -->[f] bb),
+     disp_psfunctor_cell FFdata (disp_id2 ff) =
+     transportb
+       (λ p : # F f ==> # F f, disp_psfunctor_mor FFdata ff ==>[p] disp_psfunctor_mor FFdata ff)
+       (psfunctor_id2 F f) (disp_id2 (disp_psfunctor_mor FFdata ff)).
+
+Definition disp_psfunctor_vcomp2_law : UU
+  := ∏ (a b : B₁) (f g h : B₁ ⟦a, b⟧) (η : f ==> g) (φ : g ==> h) (aa : D₁ a)
+       (bb : D₁ b) (ff : aa -->[f] bb) (gg : aa -->[g] bb) (hh : aa -->[h] bb)
+       (ηη : ff ==>[η] gg) (φφ : gg ==>[φ] hh),
+     disp_psfunctor_cell FFdata (ηη •• φφ) =
+     transportb
+       (λ p : # F f ==> # F h, disp_psfunctor_mor FFdata ff ==>[ p] disp_psfunctor_mor FFdata hh)
+       (psfunctor_vcomp F η φ) (disp_psfunctor_cell FFdata ηη •• disp_psfunctor_cell FFdata φφ) .
+
+Definition disp_psfunctor_lunitor_law : UU
+  := ∏ (a b : B₁) (f : B₁ ⟦ a, b ⟧) (aa : D₁ a) (bb : D₁ b) (ff : aa -->[ f] bb),
+     disp_lunitor (disp_psfunctor_mor FFdata ff) =
+     transportb
+       (λ p, _ ==>[p] _)
+       (psfunctor_lunitor F f)
+       (((disp_psfunctor_id FFdata aa ▹▹ disp_psfunctor_mor FFdata ff)
+           •• disp_psfunctor_comp FFdata (id_disp aa) ff)
+          •• disp_psfunctor_cell FFdata (disp_lunitor ff)).
+
+Definition disp_psfunctor_runitor_law : UU
+  := ∏ (a b : B₁) (f : B₁ ⟦ a, b ⟧) (aa : D₁ a) (bb : D₁ b) (ff : aa -->[ f] bb),
+     disp_runitor (disp_psfunctor_mor FFdata ff) =
+     transportb
+       (λ p, _ ==>[p] _)
+       (psfunctor_runitor F f)
+       (((disp_psfunctor_mor FFdata ff ◃◃ disp_psfunctor_id FFdata _)
+           •• disp_psfunctor_comp FFdata _ _)
+          •• disp_psfunctor_cell FFdata (disp_runitor ff)).
+
+Definition disp_psfunctor_lassociator_law : UU
+  :=
+    ∏ (a b c d : B₁) (f : B₁ ⟦ a, b ⟧) (g : B₁ ⟦ b, c ⟧) (h : B₁ ⟦ c, d ⟧)
+      (aa : D₁ a) (bb : D₁ b) (cc : D₁ c) (dd : D₁ d) (ff : aa -->[ f] bb) (gg : bb -->[ g] cc)
+      (hh : cc -->[ h] dd),
+    ((disp_psfunctor_mor FFdata ff ◃◃ disp_psfunctor_comp FFdata gg hh)
+       •• disp_psfunctor_comp FFdata ff (gg;; hh))
+      •• disp_psfunctor_cell FFdata (disp_lassociator ff gg hh) =
+    transportb
+      (λ p, _ ==>[p] _)
+      (psfunctor_lassociator F f g h)
+      ((disp_lassociator (disp_psfunctor_mor FFdata ff) (disp_psfunctor_mor FFdata gg)
+                         (disp_psfunctor_mor FFdata hh)
+         •• (disp_psfunctor_comp FFdata ff gg ▹▹ disp_psfunctor_mor FFdata hh))
+        •• disp_psfunctor_comp FFdata (ff;; gg) hh).
+
+Definition disp_psfunctor_lwhisker_law : UU
+  := ∏ (a b c : B₁) (f : B₁ ⟦ a, b ⟧) (g₁ g₂ : B₁ ⟦ b, c ⟧) (η : g₁ ==> g₂)
+       (aa : D₁ a) (bb : D₁ b) (cc : D₁ c) (ff : aa -->[ f] bb) (gg₁ : bb -->[ g₁] cc)
+       (gg₂ : bb -->[ g₂] cc) (ηη : gg₁ ==>[ η] gg₂),
+     disp_psfunctor_comp FFdata ff gg₁ •• disp_psfunctor_cell FFdata (ff ◃◃ ηη) =
+     transportb
+       (λ p, _ ==>[p] _)
+       (psfunctor_lwhisker F f η)
+       ((disp_psfunctor_mor FFdata ff ◃◃ disp_psfunctor_cell FFdata ηη)
+          •• disp_psfunctor_comp FFdata ff gg₂).
+
+Definition disp_psfunctor_rwhisker_law : UU
+  := ∏ (a b c : B₁) (f₁ f₂ : B₁ ⟦ a, b ⟧) (g : B₁ ⟦ b, c ⟧) (η : f₁ ==> f₂)
+       (aa : D₁ a) (bb : D₁ b) (cc : D₁ c)
+       (ff₁ : aa -->[f₁] bb)
+       (ff₂ : aa -->[f₂] bb)
+       (gg : bb -->[g] cc)
+       (ηη : ff₁ ==>[ η] ff₂),
+     disp_psfunctor_comp FFdata _ _ •• disp_psfunctor_cell FFdata (ηη ▹▹ gg) =
+     transportb
+       (λ p, _ ==>[p] _)
+       (psfunctor_rwhisker F g η)
+       ((disp_psfunctor_cell FFdata ηη ▹▹ _)
+          •• disp_psfunctor_comp FFdata _ _).
+
+Definition is_disp_psfunctor : UU
+    := disp_psfunctor_id2_law
+         × disp_psfunctor_vcomp2_law
+         × disp_psfunctor_lunitor_law
+         × disp_psfunctor_runitor_law
+         × disp_psfunctor_lassociator_law
+         × disp_psfunctor_lwhisker_law
+         × disp_psfunctor_rwhisker_law.
+
+End DispPseudofunctorLaws.
+
+(** Disp pseudofunct *)
+
+Definition disp_psfunctor : UU
+  := ∑ FF : disp_psfunctor_data, is_disp_psfunctor FF.
+
+End DispPseudofunctor.
