2d6ef6190635669a1a9e6b5c88cd56c803299ce9
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-25 23:30:57 +0200

    Remove duplicated lemmas
    Remove
    bc_whisker_l_compose
    bc_whisker_r_compose

--- UniMath/Bicategories/Core/EquivToAdjequiv.v
+++ UniMath/Bicategories/Core/EquivToAdjequiv.v
@@ -261,7 +261,7 @@ Section EquivToAdjEquiv.
     rewrite !vassocr in p.
     rewrite !(maponpaths (fun z => _ o (_ o z)) (!(vassocr _ _ _))) in p.
     rewrite rassociator_lassociator, id2_right in p.
-    rewrite <- bc_whisker_l_compose in p.
+    rewrite rwhisker_vcomp in p.
     rewrite <- !vassocr in p.
     pose @inverse_pentagon_5 as q.
     rewrite !lwhisker_hcomp in p.
