f6bcec0d133a5f4680cd4a4d2f611e38ed70f052
Gianluca Amato <gianluca.amato.74@gmail.com>
2020-09-10 21:41:44 +0200

    Fix Equations.v for the multisorted case.

--- UniMath/Algebra/Universal/Equations.v
+++ UniMath/Algebra/Universal/Equations.v
@@ -5,57 +5,69 @@
  *)
 
 Require Import UniMath.Foundations.All.
-Require Import UniMath.Algebra.Universal.
+
+Require Import UniMath.Algebra.Universal.Signatures.
+Require Import UniMath.Algebra.Universal.Algebras.
+Require Import UniMath.Algebra.Universal.FreeAlgebras.
 
 Section Equations.
 
-  Definition equation (sigma : signature) (V: hSet) : UU
-    := vterm sigma V × vterm sigma V.
+  Definition equation_bare (σ : signature) (V: varspec σ): sUU (sorts σ)
+    := λ s: sorts σ, vterm σ V s × vterm σ V s.
 
-  Definition sysequations (sigma : signature) (V: hSet) : UU
-    := ∑ E : hSet, E → equation sigma V.
+  Definition lhs {σ : signature} {V: varspec σ} {s: sorts σ}
+    : equation_bare σ V s → vterm σ V s := pr1.
 
-  Definition eqidx (sigma : signature) (V: hSet) : sysequations sigma V → hSet := pr1.
-  Coercion eqidx : sysequations >-> hSet.
+  Definition rhs {σ : signature} {V: varspec σ} {s: sorts σ}
+    : equation_bare σ V s → vterm σ V s := pr2.
 
-  Context {sigma: signature} {V: hSet}.
+  Definition equation (σ : signature) (V: varspec σ): UU
+    := ∑ s: sorts σ, equation_bare σ V s.
+ 
+  Definition eqsort {σ: signature} {V: varspec σ} (e: equation σ V)
+    : sorts σ := pr1 e.
 
-  Definition lhs : equation sigma V → vterm sigma V := pr1.
-  Definition rhs : equation sigma V → vterm sigma V := pr2.
+  Coercion eqbare {σ: signature} {V: varspec σ} (e: equation σ V)
+    : equation_bare σ V (eqsort e) := pr2 e.
 
-  Definition holds (a:algebra sigma) (e:equation sigma V) : UU
-    := ∏ α, veval a α (lhs e) = veval a α (rhs e).
+  Definition sysequations (σ : signature) (V: varspec σ): UU
+    := ∑ E : hSet, E → equation σ V.
 
-  Definition geteq {sys : sysequations sigma V}
-    : sys → equation sigma V
+  Definition syseqidx (σ : signature) (V: varspec σ): sysequations σ V → hSet := pr1.
+  Coercion syseqidx : sysequations >-> hSet.
+
+  Definition geteq {σ: signature} {V: varspec σ} {sys : sysequations σ V}: sys → equation σ V
     := pr2 sys.
+
+  Definition holds {σ: signature} {V: varspec σ} (a: algebra σ) (e: equation σ V) : UU
+    := ∏ α, veval a α (eqsort e) (lhs e) = veval a α (eqsort e) (rhs e).
 
 End Equations.
 
 Section Varieties.
 
-  Definition eqsignature : UU
-    := ∑ (sigma : signature) (V: hSet), sysequations sigma V.
+  Definition eqsignature: UU 
+    := ∑ (σ : signature) (V: varspec σ), sysequations σ V.
 
-  Definition signature_of_eqsignature : eqsignature → signature := pr1.
-  Coercion signature_of_eqsignature : eqsignature >-> signature.
+  Definition eqsign_signature: eqsignature → signature := pr1.
+  Coercion eqsign_signature : eqsignature >-> signature.
 
-  Definition variables (sigma: eqsignature): hSet := pr1 (pr2 sigma).
+  Definition eqsign_variables (σ: eqsignature): varspec σ := pr1 (pr2 σ).
 
-  Definition eqs (sigma: eqsignature) : sysequations sigma (variables sigma) := pr2 (pr2 sigma).
+  Definition eqs (σ: eqsignature): sysequations σ (eqsign_variables σ) := pr2 (pr2 σ).
 
-  Definition is_eqalgebra {sigma : eqsignature} (a : algebra sigma) : UU
-    := ∏ e : eqs sigma, holds a (geteq e).
+  Definition is_eqalgebra {σ : eqsignature} (a : algebra σ) : UU
+    := ∏ e: eqs σ, holds a (geteq e).
 
-  Definition eqalgebra (sigma : eqsignature) : UU
-    := ∑ a : algebra sigma, is_eqalgebra a.
+  Definition eqalgebra (σ : eqsignature) : UU
+    := ∑ a : algebra σ, is_eqalgebra a.
 
-  Definition algebra_of_eqalgebra {sigma : eqsignature}
-    : eqalgebra sigma -> algebra sigma
+  Definition algebra_of_eqalgebra {σ : eqsignature}
+    : eqalgebra σ -> algebra σ
     := pr1.
   Coercion algebra_of_eqalgebra : eqalgebra >-> algebra.
 
-  Definition eqalgebra_is_eqalgebra {sigma : eqsignature} (a : eqalgebra sigma)
+  Definition eqalgebra_is_eqalgebra {σ : eqsignature} (a : eqalgebra σ)
     : is_eqalgebra a
     := pr2 a.
 
