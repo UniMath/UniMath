3b7808a40878477ae37cc850ec0a96777f2a8638
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-11-20 08:03:47 +0100

    Adapt to fixing dropped implicit arguments in Context.

--- UniMath/CategoryTheory/slicecat.v
+++ UniMath/CategoryTheory/slicecat.v
@@ -843,7 +843,7 @@ Section dependent_product.
 Context (H : ∏ {c c' : C} (g : C⟦c,c'⟧), is_left_adjoint (base_change_functor g)).
 
 Let dependent_product_functor {c c' : C} (g : C⟦c,c'⟧) :
-  functor (C / c) (C / c') := right_adjoint (H c c' g).
+  functor (C / c) (C / c') := right_adjoint (H g).
 
 Let BPC c : BinProducts (C / c) := @BinProducts_slice_precat C hsC PC c.
 
@@ -868,7 +868,7 @@ use tpair.
                            (dependent_product_functor (pr2 Af))).
 + rewrite const_prod_functor1_slicecat.
   apply are_adjoints_functor_composite.
-  - apply (pr2 (H _ _ _)).
+  - apply (pr2 (H _)).
   - apply are_adjoints_slicecat_functor_base_change.
 Defined.
 
