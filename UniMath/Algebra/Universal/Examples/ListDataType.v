42defa9f0e01ca7e93d4d9cb129210e043651040
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2021-01-29 22:31:01 +0100

    Improve Univeral Aglgebra experiments about lists
    Remove the free term algebra for list
    (it reduces trivially to the empty lis)
    
    Add the list algebra structure to usual lists.

--- UniMath/Algebra/Universal/Examples/ListDataType.v
+++ UniMath/Algebra/Universal/Examples/ListDataType.v
@@ -9,28 +9,54 @@ Require Import UniMath.Algebra.Universal.Terms.
 
 Local Open Scope stn.
 
-(** ** Signature for lists *)
-
 (** Indexes for the sorts. *)
-Definition data_sort_idx: ⟦ 2 ⟧ := ●0.
+Definition elem_sort_idx: ⟦ 2 ⟧ := ●0.
 Definition list_sort_idx: ⟦ 2 ⟧ := ●1.
 
-(** Signature with two arities: nil and cons constructors.  *)
+(** ** Signature for lists
+    Two arities: nil and cons constructors.  *)
 Definition list_signature: signature_simple
   := make_signature_simple
       [ (nil ,, list_sort_idx) ;
-        ( [data_sort_idx ; list_sort_idx] ,, list_sort_idx )
+        ( [elem_sort_idx ; list_sort_idx] ,, list_sort_idx )
       ]%list.
 
-Definition data_sort: sorts list_signature := data_sort_idx.
-Definition list_sort: sorts list_signature := list_sort_idx.
-
-Definition nil_op: names list_signature := ●0.
-Definition cons_op: names list_signature := ●1.
+(** Indexes for the constructions. *)
+Definition nil_idx: names list_signature := ●0.
+Definition cons_idx: names list_signature := ●1.
 
 Definition list_algebra (A: hSet) := make_algebra_simple list_signature
   [ A ; listset A ]
   [( λ _, nil ; λ p, cons (pr1 p) (pr12 p) )].
 
-Definition list_nil := build_term_curried nil_op.
-Definition list_const := build_term_curried cons_op.
+Lemma elem_sort_id (A: hSet) : supportset (list_algebra A) elem_sort_idx = A.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma list_sort_id (A: hSet) : supportset (list_algebra A) list_sort_idx = listset A.
+Proof.
+  reflexivity.
+Qed.
+
+Definition list_nil (A: hSet) : listset A := ops (list_algebra A) nil_idx tt.
+
+Lemma list_nil_id (A: hSet) : list_nil A = @nil A.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma list_cons_dom_id (A: hSet)
+  : dom (list_algebra A) cons_idx = (dirprod A (dirprod (listset A) unit)).
+Proof.
+  reflexivity.
+Qed.
+
+Definition list_cons (A: hSet) : A × listset A × unit → listset A
+  := ops (list_algebra A) cons_idx.
+
+Lemma cons_nil_id (A: hSet) (x: A) (l: listset A)
+  : list_cons A (x,, (l,, tt)) = cons x l.
+Proof.
+  reflexivity.
+Qed.
