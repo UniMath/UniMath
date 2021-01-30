c4431553447e7029c60d29b9c9c02c57ef3d4c4f
Gianluca Amato <gianluca.amato.74@gmail.com>
2021-01-30 19:03:20 +0100

    Only require modules which are really needed.

--- UniMath/Algebra/Universal.v
+++ UniMath/Algebra/Universal.v
@@ -1,7 +1,3 @@
 (** * Universal algebra: signatures, algebras and terms *)
 
-Require Export UniMath.Algebra.Universal.Signatures.
-Require Export UniMath.Algebra.Universal.Algebras.
-Require Export UniMath.Algebra.Universal.Terms.
-Require Export UniMath.Algebra.Universal.TermAlgebras.
 Require Export UniMath.Algebra.Universal.FreeAlgebras.
