4f56590b221a387c1b3d0334419feb19480e7e03
Ralph Matthes <ralph.matthes@irit.fr>
2021-06-24 01:46:15 +0200

    the different pentagon laws for strength proved equivalent (rather obvious mathematically)

--- UniMath/CategoryTheory/Monoidal/Strengths.v
+++ UniMath/CategoryTheory/Monoidal/Strengths.v
@@ -86,18 +86,81 @@ Definition strength_pentagon_eq_variant2 (ϛ : strength_nat): UU := ∏ (a : A),
   pr1 ϛ (a, x ⊗ y) · (#F (nat_z_iso_to_trans_inv χ ((a, x), y))) =
   (nat_z_iso_to_trans_inv χ' ((F a, x), y)) · (pr1 ϛ (a, x)) #⊙' (id y) · (pr1 ϛ (a ⊙ x, y)).
 
+Lemma strength_pentagon_eq_tovariant1 (ϛ : strength_nat): strength_pentagon_eq ϛ -> strength_pentagon_eq_variant1 ϛ.
+Proof.
+  intros Heq ? ? ?.
+  red in Heq.
+  apply pathsinv0.
+  unfold nat_z_iso_to_trans_inv; cbn.
+  unfold is_z_isomorphism_mor.
+  do 2 rewrite <- assoc.
+  apply (z_iso_inv_on_right _ _ _ (make_z_iso _ _ (pr2 χ' ((F a, x), y)))).
+  apply pathsinv0.
+  rewrite assoc.
+  cbn.
+  apply Heq.
+Qed.
+
+Lemma strength_pentagon_eq_fromvariant1 (ϛ : strength_nat): strength_pentagon_eq_variant1 ϛ -> strength_pentagon_eq ϛ.
+Proof.
+  intros Heq ? ? ?.
+  red in Heq.
+  unfold nat_z_iso_to_trans_inv in Heq; cbn in Heq.
+  unfold is_z_isomorphism_mor in Heq.
+  apply pathsinv0.
+  apply (z_iso_inv_to_left _ _ _ (make_z_iso _ _ (pr2 χ' ((F a, x), y)))).
+  cbn.
+  apply pathsinv0.
+  do 2 rewrite assoc.
+  apply Heq.
+Qed.
+
+Lemma strength_pentagon_eq_variant1variant2 (ϛ : strength_nat): strength_pentagon_eq_variant1 ϛ -> strength_pentagon_eq_variant2 ϛ.
+Proof.
+  intros Heq ? ? ?.
+  red in Heq.
+  etrans.
+  { unfold nat_z_iso_to_trans_inv.  cbn.
+    apply maponpaths.
+    apply pathsinv0.
+    apply functor_on_inv_from_z_iso'.
+  }
+  apply pathsinv0.
+  apply (z_iso_inv_on_left _ _ _ _ (make_z_iso (# F (pr1 χ ((a, x), y)))
+         (is_z_isomorphism_mor (functor_on_is_z_isomorphism F (pr2 χ ((a, x), y))))
+         (functor_on_is_z_isomorphism F (pr2 χ ((a, x), y))))).
+  apply Heq.
+Qed.
+
+Lemma strength_pentagon_eq_variant2variant1 (ϛ : strength_nat): strength_pentagon_eq_variant2 ϛ -> strength_pentagon_eq_variant1 ϛ.
+Proof.
+  intros Heq ? ? ?.
+  red in Heq.
+  apply pathsinv0.
+  apply (z_iso_inv_to_right _ _ _ _ (make_z_iso (# F (pr1 χ ((a, x), y)))
+         (is_z_isomorphism_mor (functor_on_is_z_isomorphism F (pr2 χ ((a, x), y))))
+         (functor_on_is_z_isomorphism F (pr2 χ ((a, x), y))))).
+  etrans.
+  { apply pathsinv0.
+    apply Heq. }
+  clear Heq.
+  apply maponpaths.
+  apply pathsinv0.
+  apply (functor_on_inv_from_z_iso' _ (pr2 χ ((a, x), y))).
+Qed.
 
 End ActionBasedStrengths_Natural_Transformation.
 
 Definition strength (F : A ⟶ A'): UU := ∑ (ϛ : strength_nat F),
   (strength_triangle_eq F ϛ) × (strength_pentagon_eq F ϛ).
 
+(*
 Definition strength_variant1 (F : A ⟶ A'): UU := ∑ (ϛ : strength_nat F),
   (strength_triangle_eq F ϛ) × (strength_pentagon_eq_variant1 F ϛ).
 
 Definition strength_variant2 (F : A ⟶ A'): UU := ∑ (ϛ : strength_nat F),
   (strength_triangle_eq F ϛ) × (strength_pentagon_eq_variant2 F ϛ).
-
+*)
 
 End ActionBasedStrengths_Definition.
 
@@ -227,6 +290,8 @@ Proof.
     fold ϛ.
     apply idpath.
   - cbn.
+    (* apply strength_pentagon_eq_fromvariant1.
+    apply strength_pentagon_eq_variant2variant1. *)
     red.
     intros v w' w.
     (* massage the goal a bit - easier roads to it seem blocked - in particular, with the change command that does not terminate! *)
