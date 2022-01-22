ba82d4313982425c4a3425485246d516d7a9cf24
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2022-01-22 16:50:55 +0100

    Moved [ActionBasedStrengthOnHomsInBicat] to SubstitutionSystems package.
    Note: alternatively, this file could be split up, with some parts remaining in Bicategories package.

--- UniMath/SubstitutionSystems/ActionBasedStrengthOnHomsInBicat.v
+++ UniMath/SubstitutionSystems/ActionBasedStrengthOnHomsInBicat.v
@@ -1,4 +1,4 @@
-(** Constructs instance of action-based strength for the actions of the endomorphisms by precomposition on fixed hom-categories of a bicategory
+(** Constructs instance of action-based strength for the actions of the endomorphisms by precomposition on fixed hom-categories of a bicategory, and from this, a signature.
 
 Author: Ralph Matthes 2021
 
