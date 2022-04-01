9adc28eace3e3161e70a2c45e38031db9dd67f5f
Tobias Schmude <t.schmude@t-online.de>
2022-04-01 18:07:11 +0200

    problem with transport of pairs in composite_disp_cat

--- UniMath/CategoryTheory/DisplayedCats/MoreFibrations/DisplayedDisplayedCats.v
+++ UniMath/CategoryTheory/DisplayedCats/MoreFibrations/DisplayedDisplayedCats.v
@@ -26,6 +26,35 @@ Local Open Scope type_scope.
 Local Open Scope mor_disp_scope.
 
 
+Definition transportD' {A : UU} (B : A -> UU) (C : (∑ a : A, B a) -> UU)
+           {x1 x2 : A} (p : x1 = x2) (y : B x1) (z : C (x1 ,, y)) :
+  C (x2 ,, (transportf _ p y)).
+Proof.
+  intros.
+  induction p.
+  exact z.
+Defined.
+
+Definition transportf_total2' {A : UU} {B : A -> UU} {C : (∑ a:A, B a) -> UU}
+           {x1 x2 : A} (p : x1 = x2) (y : B x1) (z : C (x1 ,, y)) :
+  transportf (λ x, ∑ y : B x, C (x ,, y)) p (y ,, z) =
+  tpair (λ y, C (x2 ,, y)) (transportf _ p y)
+        (transportD' _ _ p y z).
+Proof.
+  intros.
+  induction p.
+  apply idpath.
+Defined.
+
+Definition transportf_total2'_pr1 {A : UU} {B : A -> UU} {C : (∑ a:A, B a) -> UU}
+           {x1 x2 : A} (p : x1 = x2) (y : B x1) (z : C (x1 ,, y)) :
+  pr1 (transportf (λ x, ∑ y : B x, C (x ,, y)) p (y ,, z)) = transportf _ p y.
+Proof.
+  exact (maponpaths pr1 (transportf_total2' p y z)).
+Defined.
+
+
+
 Definition disp_disp_cat (C : category) : UU
   := ∑ (D : disp_cat C), disp_cat (total_category D).
 
@@ -44,33 +73,45 @@ Proof.
     use tpair.
     + unfold disp_cat_ob_mor.
       use tpair.
-      * intro c.
-        exact (∑ (cc : D c), E (c ,, cc)).
+      * intro x.
+        exact (∑ (xx : D x), E (x ,, xx)).
       * simpl.
-        intros c c' [cc ccc] [cc' ccc'] f.
-        exact (∑ (ff : cc -->[f] cc'), ccc -->[(f ,, ff)] ccc').
+        intros x y [xx xxx] [yy yyy] f.
+        exact (∑ (ff : xx -->[f] yy), xxx -->[(f ,, ff)] yyy).
     + simpl.
       split.
       * simpl.
-        intros c [cc ccc].
-        exists (id_disp cc).
-        exact (id_disp ccc).
+        intros x [xx xxx].
+        exists (id_disp xx).
+        exact (id_disp xxx).
       * simpl.
-        intros c c' c'' f g [cc ccc] [cc' ccc'] [cc'' ccc''] [ff fff] [gg ggg].
+        intros x y z f g [xx xxx] [yy yyy] [zz zzz] [ff fff] [gg ggg].
         exists (comp_disp ff gg).
         exact (comp_disp fff ggg).
-  - split.
+  - simpl.
+    split.
     + simpl.
-      intros c c' f [cc ccc] [cc' ccc'] [ff fff].
-
-
-      admit.
+      intros x y f [xx xxx] [yy yyy] [ff fff].
+      (* eapply transportf_total2'. *)
+      use total2_paths_b.
+      * simpl.
+        eapply pathscomp0.
+        -- apply id_left_disp.
+        -- apply pathsinv0.
+           apply transportf_total2'_pr1.
+      * simpl.
+        eapply pathscomp0.
+        -- exact (id_left_disp fff).
+        --
+           admit.
     + simpl.
       split.
-      *
+      * intros x y f [xx xxx] [yy yyy] [ff fff].
+        set (temp := @id_right_disp _ E _ _ _ _ _ fff).
         admit.
       * split.
-        --
+        -- intros x y z w f g h [xx xxx] [yy yyy] [zz zzz] [ww www] [ff fff] [gg ggg] [hh hhh].
+           set (temp := @assoc_disp _ E _ _ _ _ _ _ _ _ _ _ _ fff ggg hhh).
            admit.
         --
            admit.
