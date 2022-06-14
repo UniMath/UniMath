651645d74f3e45d45afa47e4d7dcb548ee7a1536
Niels <nweide@cs.ru.nl>
2022-06-14 17:10:16 +0200

    Minor fix

--- UniMath/Bicategories/DisplayedBicats/Examples/Prod.v
+++ UniMath/Bicategories/DisplayedBicats/Examples/Prod.v
@@ -10,7 +10,7 @@
  ********************************************************************************* *)
 
 Require Import UniMath.Foundations.All.
-Require Import UniMath.MoreFoundations.PartA.
+Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.DisplayedCats.Core.
 Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
