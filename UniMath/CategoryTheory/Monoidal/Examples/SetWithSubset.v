7750c35036942145facee694e747e7d4cd197587
Kfwullaert <kwullaer@hotmail.com>
2022-07-31 11:17:31 +0200

    Integrated the already existing code on subtypes

--- UniMath/CategoryTheory/Monoidal/Examples/SetWithSubset.v
+++ UniMath/CategoryTheory/Monoidal/Examples/SetWithSubset.v
@@ -20,155 +20,75 @@ Require Import UniMath.CategoryTheory.Monoidal.WhiskeredDisplayedBifunctors.
 Require Import UniMath.CategoryTheory.Monoidal.DisplayedMonoidalWhiskered.
 Require Import UniMath.CategoryTheory.Monoidal.MonoidalSectionsWhiskered.
 
-
 Require Import UniMath.CategoryTheory.categories.HSET.All.
 Require Import UniMath.CategoryTheory.Monoidal.CartesianMonoidalCategoriesWhiskered.
 
-Section Subsets.
+Section Subtype_AUX.
 
-  Definition subset (X : hSet) : UU := X -> hProp.
+  (* We could define the intersection as follows but this makes it more complicated then it should be *)
+  Definition binary_intersection' {X : UU} (U V : hsubtype X) : hsubtype X
+    := subtype_intersection (λ b,  bool_rect (λ _ : bool, hsubtype X) U V b).
 
-  Definition subset_to_elements' {X : hSet} (U : subset X)
-    : UU := ∑ x : X, U x.
-
-  Definition subset_to_elements {X : hSet} (U : subset X) : hSet.
-  Proof.
-    exists (subset_to_elements' U).
-    apply isaset_total2.
-    - apply X.
-    - intro.
-      apply isasetaprop.
-      apply U.
-  Defined.
-
-  Definition subset_to_elements_in_entireset
-             {X : hSet} {U : subset X}
-             (x : subset_to_elements U)
-    : X := pr1 x.
-
-  Definition subset_in_product {X Y : hSet}
-             (U : subset X) (V : subset Y)
-    : subset (setdirprod X Y).
-  Proof.
-    intro xy.
-    exists (U (pr1 xy) × V (pr2 xy)).
-    apply isapropdirprod. apply U. apply V.
-  Defined.
-
-  Definition subset_in_product' {X Y : hSet} (UV : subset (setdirprod X Y))
-    : subset X × subset Y.
-  Proof.
-    split.
-    - intro x.
-      exists (∃ y : Y, UV (x,,y)).
-      apply ishinh.
-    - intro y.
-      exists (∃ x : X, UV (x,,y)).
-      apply ishinh.
-  Defined.
-
-  Definition singleton {X : hSet} (x : X) : subset X.
-  Proof.
-    intro x'.
-    exists (x'=x).
-    apply X.
-  Defined.
-
-  Definition intersection {X : hSet} (U V : subset X) : subset X
+  Definition binary_intersection {X : UU} (U V : hsubtype X) : hsubtype X
     := λ x, U x ∧ V x.
 
-  Lemma intersection_commutative {X : hSet} (U V : subset X)
-    : ∏ x : X, (intersection U V) x -> (intersection V U) x.
+  Lemma binary_intersection_commutative {X : UU} (U V : hsubtype X)
+    : ∏ x : X, (binary_intersection U V) x -> (binary_intersection V U) x.
   Proof.
     intros ? p.
     exact (transportf _ (iscomm_hconj (U x) (V x)) p).
   Qed.
 
-  Definition union_data {X I : hSet} (U : I -> subset X) :  X -> UU
-    := λ x, ∃ i:I, U i x.
-
-  Definition union {X I : hSet} (U : I -> subset X) : subset X.
-  Proof.
-    intro x.
-    exists (union_data U x).
-    apply impred_isaprop ; intro i.
-    apply isapropimpl.
-    apply i.
-  Defined.
-
-  Definition emptysubset (X : hSet) : subset X
+  Definition emptysubtype (X : UU) : hsubtype X
     := λ x, hfalse.
 
-  Definition entiresubset (X : hSet) : subset X
-    := λ x, htrue.
-
-  Definition contained {X : hSet} (U V : subset X) : UU
-    := ∏ x : X, U x -> V x.
-
-  Lemma contained_reflexive {X : hSet} (U : subset X)
-    : contained U U.
-  Proof.
-    intro ; intro;  assumption.
-  Qed.
-
-  Definition contained_trans {X : hSet} {U V W : subset X} (uv : contained U V) (vw : contained V W)
-    : contained U W.
-  Proof.
-    intros x p.
-    apply vw.
-    apply uv.
-    assumption.
-  Qed.
-
-  Definition intersection_contained_l {X : hSet} (U V : subset X)
-    : contained (intersection U V) U.
+  Definition intersection_contained_l {X : UU} (U V : hsubtype X)
+    : subtype_containedIn (binary_intersection U V) U.
   Proof.
     intros ? xinUV.
     apply xinUV.
   Qed.
 
-  Definition intersection_contained_r {X : hSet} (U V : subset X)
-    : contained (intersection U V) V.
+  Definition intersection_contained_r {X : UU} (U V : hsubtype X)
+    : subtype_containedIn (binary_intersection U V) V.
   Proof.
     intros ? xinUV.
     apply xinUV.
   Qed.
 
-  Definition intersection_contained {X : hSet} {U U' V V' : subset X} (uu : contained U U') (vv : contained V V')
-    : contained (intersection U V) (intersection V' U').
+  Definition intersection_contained {X : UU} {U U' V V' : hsubtype X}
+             (uu : subtype_containedIn U U')
+             (vv : subtype_containedIn V V')
+    : subtype_containedIn (binary_intersection U V) (binary_intersection V' U').
   Proof.
     intros x p.
     cbn.
     split.
-    - apply vv.
+    - apply (vv x).
       exact ((intersection_contained_r U V) x p).
-    - apply uu.
+    - apply (uu x).
       exact ((intersection_contained_l U V) x p).
   Qed.
 
-  Lemma isaprop_contained {X : hSet} (U V : subset X)
-    : isaprop (contained U V).
+  Lemma isaprop_subtype_containedIn {X : UU} (U V : hsubtype X)
+    : isaprop (subtype_containedIn U V).
   Proof.
     apply impred_isaprop ; intro.
     apply isapropimpl.
     apply V.
   Qed.
 
-  Definition id (X : hSet) : X -> X := idfun X.
-  Definition comp {X Y Z : hSet} (f : X -> Y) (g : Y -> Z)
-    : X -> Z := funcomp f g.
+  Definition image_hsubtype {X Y : UU} (U : hsubtype X) (f : X → Y)
+    : hsubtype Y := λ y : Y, (∃ x : X, f x = y × U x).
 
-  Definition image_subset {X Y : hSet} (U : subset X) (f : X → Y)
-    : subset Y := λ y : Y, (∃ x : X, f x = y × U x).
-
-  Lemma image_subset_emptysubset {X Y : hSet} (f : X → Y)
-    : image_subset (emptysubset X) f = emptysubset Y.
+  Lemma image_hsubtype_emptyhsubtype {X Y : UU} (f : X → Y)
+    : image_hsubtype (emptysubtype X) f = emptysubtype Y.
   Proof.
     apply funextsec ; intro y.
     apply hPropUnivalence.
     - intro yinfEmpty.
       use (factor_through_squash _ _ yinfEmpty).
-      { apply emptysubset. }
+      { apply emptysubtype. }
       intro x.
       apply (pr22 x).
     - intro yinEmpty.
@@ -176,8 +96,8 @@ Section Subsets.
       exact (yinEmpty).
   Qed.
 
-  Definition image_subset_id {X : hSet} (U : subset X)
-    : image_subset U (id X) = U.
+  Definition image_hsubtype_id {X : UU} (U : hsubtype X)
+    : image_hsubtype U (idfun X) = U.
   Proof.
     apply funextsec ; intro x.
     apply hPropUnivalence.
@@ -197,9 +117,9 @@ Section Subsets.
       exact (x,, idpath x,, xinU).
   Qed.
 
-  Definition image_subset_comp {X Y Z : hSet} (U : subset X)
+  Definition image_hsubtype_comp {X Y Z : UU} (U : hsubtype X)
              (f : X → Y) (g : Y → Z)
-    : image_subset U (comp f g) = image_subset (image_subset U f) g.
+    : image_hsubtype U (funcomp f g) = image_hsubtype (image_hsubtype U f) g.
   Proof.
     apply funextsec ; intro z.
     apply hPropUnivalence.
@@ -223,74 +143,100 @@ Section Subsets.
       exists (pr1 x).
       split.
       + refine (_ @ (pr12 y)).
-        unfold comp.
+        unfold funcomp.
         unfold funcomp.
         apply maponpaths.
         exact (pr12 x).
       + exact (pr22 x).
   Qed.
 
-  Definition subset_preserving {X Y : hSet} (U : subset X) (V : subset Y) (f : X → Y)
-    : UU := contained (image_subset U f) V.
+  Definition hsubtype_preserving {X Y : UU} (U : hsubtype X) (V : hsubtype Y) (f : X → Y)
+    : UU := subtype_containedIn (image_hsubtype U f) V.
 
-  Lemma isaprop_subset_preserving {X Y : hSet} (U : subset X) (V : subset Y) (f : X → Y)
-    : isaprop (subset_preserving U V f).
+  Lemma isaprop_hsubtype_preserving {X Y : UU} (U : hsubtype X) (V : hsubtype Y) (f : X → Y)
+    : isaprop (hsubtype_preserving U V f).
   Proof.
     apply impred_isaprop ; intro.
     apply isapropimpl.
     apply V.
   Qed.
 
-  Lemma id_subset_preserving {X : hSet} (U : subset X) : subset_preserving U U (id X).
+  Lemma id_hsubtype_preserving {X : UU} (U : hsubtype X) : hsubtype_preserving U U (idfun X).
   Proof.
     intros x xinU.
-    rewrite image_subset_id in xinU.
+    rewrite image_hsubtype_id in xinU.
     exact xinU.
   Qed.
 
-  Lemma comp_subset_preserving {X Y Z : hSet}
-             {U : subset X} {V : subset Y} {W : subset Z}
+  Lemma comp_hsubtype_preserving {X Y Z : UU}
+             {U : hsubtype X} {V : hsubtype Y} {W : hsubtype Z}
              {f : X → Y} {g : Y → Z}
-             (fsp : subset_preserving U V f) (gsp : subset_preserving V W g)
-    : subset_preserving U W (comp f g).
+             (fsp : hsubtype_preserving U V f) (gsp : hsubtype_preserving V W g)
+    : hsubtype_preserving U W (funcomp f g).
   Proof.
     intros z zinU.
-    rewrite image_subset_comp in zinU.
-    apply gsp.
-    unfold image_subset.
+    rewrite image_hsubtype_comp in zinU.
+    apply (gsp _).
+    unfold image_hsubtype.
     use (factor_through_squash _ _ zinU).
     { apply ishinh. }
     intro y.
     apply hinhpr.
     exists (pr1 y).
     exists (pr12 y).
-    apply fsp.
+    apply (fsp _).
     exact (pr22 y).
   Qed.
 
-  Lemma empty_subset_preserving {X Y : hSet} (f : X → Y)
-    : subset_preserving (emptysubset X) (emptysubset Y) f.
+  Lemma empty_hsubtype_preserving {X Y : UU} (f : X → Y)
+    : hsubtype_preserving (emptysubtype X) (emptysubtype Y) f.
   Proof.
-    unfold subset_preserving.
-    rewrite image_subset_emptysubset.
-    apply contained_reflexive.
+    unfold hsubtype_preserving.
+    rewrite image_hsubtype_emptyhsubtype.
+    apply subtype_containment_isrefl.
   Qed.
 
-  Lemma entire_subset_preserving {X Y : hSet} (f : X → Y)
-    : subset_preserving (entiresubset X) (entiresubset Y) f.
+  Lemma total_hsubtype_preserving {X Y : UU} (f : X → Y)
+    : hsubtype_preserving (totalsubtype X) (totalsubtype Y) f.
   Proof.
     exact (λ _ _, tt).
   Qed.
 
-End Subsets.
+End Subtype_AUX.
+
+Section SetSubtype.
+
+  Definition setsubtype (X : hSet) : UU := hsubtype X.
+  Identity Coercion setsubtype_to_subtype : setsubtype >-> hsubtype.
+
+  Definition singletonsetsubtype {X : hSet} (x : X) : setsubtype X.
+  Proof.
+    intro x'.
+    exists (x'=x).
+    apply X.
+  Defined.
+
+  Definition emptysetsubtype {X : hSet} : setsubtype X := emptysubtype X.
+
+  Definition setsubtype_in_product {X Y : hSet}
+             (U : hsubtype X) (V : hsubtype Y)
+    : setsubtype (setdirprod X Y) := subtypesdirprod U V.
+
+  Definition union {X I : hSet} (U : I -> setsubtype X) :  setsubtype X := subtype_union U.
+
+  Definition totalsetsubtype (X : hSet) : setsubtype X := totalsubtype X.
+
+  Definition contained {X : hSet} (U V : setsubtype X) : UU := subtype_containedIn U V.
+
+End SetSubtype.
 
 Section SetWithSubset.
 
   Definition SS_disp_cat_ob_mor : disp_cat_ob_mor hset_category.
   Proof.
     use tpair.
-    - exact (λ X, subset X).
-    - exact (λ _ _ U V f, subset_preserving U V f).
+    - exact (λ X, setsubtype X).
+    - exact (λ _ _ U V f, hsubtype_preserving U V f).
   Defined.
 
   Definition SS_disp_cat_data : disp_cat_data hset_category.
@@ -298,57 +244,36 @@ Section SetWithSubset.
     exists SS_disp_cat_ob_mor.
     use tpair.
     - intro ; intro.
-      apply id_subset_preserving.
+      apply id_hsubtype_preserving.
     - intros ? ? ? ? ? ? ? ? fsp gsp.
-      apply (comp_subset_preserving fsp gsp).
+      apply (comp_hsubtype_preserving fsp gsp).
   Defined.
 
   Definition SS_disp_cat_axioms : disp_cat_axioms hset_category SS_disp_cat_data.
   Proof.
-    repeat split; cbn; intros; try (apply proofirrelevance, isaprop_subset_preserving).
-    apply isasetaprop. apply isaprop_subset_preserving.
+    repeat split; cbn; intros; try (apply proofirrelevance, isaprop_hsubtype_preserving).
+    apply isasetaprop. apply isaprop_hsubtype_preserving.
   Defined.
 
   Definition SS_disp_cat : disp_cat hset_category
     := SS_disp_cat_data ,, SS_disp_cat_axioms.
 
-  (* We construct two sections, the empty and entire sections.
-     However, we only have that the entire section is monoidal *)
-  Definition empty_section_data : section_disp_data SS_disp_cat.
+  Definition total_section_data : section_disp_data SS_disp_cat.
   Proof.
-    exists (λ X, emptysubset X).
+    exists (λ X, totalsetsubtype X).
     intro ; intros.
-    apply empty_subset_preserving.
+    apply total_hsubtype_preserving.
   Defined.
 
-  Definition empty_section_axioms : section_disp_axioms empty_section_data.
+  Definition total_section_axioms : section_disp_axioms total_section_data.
   Proof.
     use tpair.
-    - intro ; repeat (apply funextsec ; intro) ; apply isapropempty.
-    - intro ; intros.
-      repeat (apply funextsec ; intro).
-      apply isapropempty.
+    - intro ; repeat (apply funextsec ; intro) ; apply totalsubtype.
+    - intro ; intros ; repeat (apply funextsec ; intro) ; apply totalsubtype.
   Qed.
 
-  Definition empty_section : section_disp SS_disp_cat
-    := empty_section_data ,, empty_section_axioms.
-
-  Definition entire_section_data : section_disp_data SS_disp_cat.
-  Proof.
-    exists (λ X, entiresubset X).
-    intro ; intros.
-    apply entire_subset_preserving.
-  Defined.
-
-  Definition entire_section_axioms : section_disp_axioms entire_section_data.
-  Proof.
-    use tpair.
-    - intro ; repeat (apply funextsec ; intro) ; apply entiresubset.
-    - intro ; intros ; repeat (apply funextsec ; intro) ; apply entiresubset.
-  Qed.
-
-  Definition entire_section : section_disp SS_disp_cat
-    := entire_section_data ,, entire_section_axioms.
+  Definition total_section : section_disp SS_disp_cat
+    := total_section_data ,, total_section_axioms.
 
 End SetWithSubset.
 
@@ -357,18 +282,18 @@ Section SetWithSubsetMonoidal.
   Definition SS_disp_cat_tensor_data
     : disp_bifunctor_data SET_cartesian_monoidal SS_disp_cat SS_disp_cat SS_disp_cat.
   Proof.
-    exists (λ _ _ U V, subset_in_product U V).
+    exists (λ _ _ U V, setsubtype_in_product U V).
     repeat (use tpair).
     - intros X Y1 Y2 g Ux U1 U2 gsp.
       intros xy2 xy1_prop.
       use (factor_through_squash _ _ xy1_prop).
-      { apply subset_in_product. }
+      { apply setsubtype_in_product. }
       intro xy1.
       repeat split.
       + rewrite (! pr12 xy1).
         exact (pr122 xy1).
       + simpl in *.
-        apply gsp.
+        apply (gsp _).
         apply hinhpr.
         exists (pr21 xy1).
         split.
@@ -378,11 +303,11 @@ Section SetWithSubsetMonoidal.
     - intros X1 X2 Y f U1 U2 Uy fsp.
       intros x2y x1y_prop.
       use (factor_through_squash _ _ x1y_prop).
-      { apply subset_in_product. }
+      { apply setsubtype_in_product. }
       intro x1y.
       repeat split.
       + simpl in *.
-        apply fsp.
+        apply (fsp _).
         apply hinhpr.
         exists (pr11 x1y).
         split.
@@ -395,7 +320,7 @@ Section SetWithSubsetMonoidal.
 
   Lemma SS_disp_cat_tensor_laws : is_disp_bifunctor SS_disp_cat_tensor_data.
   Proof.
-    repeat split; red; intros; apply isaprop_subset_preserving.
+    repeat split; red; intros; apply isaprop_hsubtype_preserving.
   Qed.
 
   Definition SS_disp_cat_tensor : disp_tensor SS_disp_cat SET_cartesian_monoidal
@@ -404,7 +329,7 @@ Section SetWithSubsetMonoidal.
   Definition SS_disp_monoidal_data : disp_monoidal_data SS_disp_cat SET_cartesian_monoidal.
   Proof.
     exists (SS_disp_cat_tensor).
-    exists (entiresubset unitHSET).
+    exists (totalsetsubtype unitHSET).
     repeat (use tpair).
     - intros X U x xinU_prop.
       use (factor_through_squash _ _ xinU_prop).
@@ -454,13 +379,13 @@ Section SetWithSubsetMonoidal.
 
   Definition SS_disp_monoidal_laws : disp_monoidal_laws (SS_disp_monoidal_data).
   Proof.
-    repeat split ; try (intro ; intros) ; apply isaprop_subset_preserving.
+    repeat split ; try (intro ; intros) ; apply isaprop_hsubtype_preserving.
   Qed.
 
   Definition SS_disp_monoidal : disp_monoidal SS_disp_cat SET_cartesian_monoidal
     := SS_disp_monoidal_data,, SS_disp_monoidal_laws.
 
-  Definition entire_section_monoidal_data : smonoidal_data SET_cartesian_monoidal SS_disp_monoidal entire_section .
+  Definition entire_section_monoidal_data : smonoidal_data SET_cartesian_monoidal SS_disp_monoidal total_section .
   Proof.
     use tpair.
     - exact (λ _ _ _ _, tt).
@@ -469,21 +394,21 @@ Section SetWithSubsetMonoidal.
 
   Definition entire_section_monoidal_ax : smonoidal_laxlaws _ _ entire_section_monoidal_data.
   Proof.
-    repeat split ; repeat (intro ; intros ; apply isaprop_subset_preserving).
+    repeat split ; repeat (intro ; intros ; apply isaprop_hsubtype_preserving).
   Qed.
 
-  Definition entire_section_monoidal_lax : smonoidal_lax SET_cartesian_monoidal SS_disp_monoidal entire_section
+  Definition entire_section_monoidal_lax : smonoidal_lax SET_cartesian_monoidal SS_disp_monoidal total_section
     := entire_section_monoidal_data,, entire_section_monoidal_ax.
 
-  Definition entire_section_monoidal : smonoidal SET_cartesian_monoidal SS_disp_monoidal entire_section.
+  Definition entire_section_monoidal : smonoidal SET_cartesian_monoidal SS_disp_monoidal total_section.
   Proof.
     exists (entire_section_monoidal_lax).
     use tpair.
     - intros X Y.
-      repeat (use tpair) ; repeat (apply isaprop_subset_preserving).
+      repeat (use tpair) ; repeat (apply isaprop_hsubtype_preserving).
       exists tt.
       exact tt.
-    - repeat (use tpair) ; repeat (apply isaprop_subset_preserving).
+    - repeat (use tpair) ; repeat (apply isaprop_hsubtype_preserving).
       exact (λ _ _, tt).
   Defined.
 
