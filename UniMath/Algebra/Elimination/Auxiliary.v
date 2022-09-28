342078400db9d8b921f17dda76f256a98304f680
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2022-09-28 18:04:19 +0200

    Reorganised Algebra.Elimination.Auxiliary by topic, in preparation for later upstreaming

--- UniMath/Algebra/Elimination/Auxiliary.v
+++ UniMath/Algebra/Elimination/Auxiliary.v
@@ -17,7 +17,9 @@ Require Import UniMath.RealNumbers.Prelim.
 
 (** Results of this file are required for the [Elimination] subpackage but aren’t specifically part of the topic; probably could/should be upstreamed within [UniMath] *)
 
-Section Misc.
+(** * Naturals *)
+(** Lemmas for standard functions on natural numbers *)
+Section Nat.
 
   Lemma min_le_b:
     ∏ a b : (nat), min a b ≤ b.
@@ -59,22 +61,6 @@ Section Misc.
       apply leh.
   Defined.
 
-  Lemma minabstn_to_astn
-    { a b : nat } (i : ⟦ min a b ⟧%stn) : ⟦ a ⟧%stn.
-  Proof.
-    intros.
-    refine (pr1 i,, _).
-    exact (natlthlehtrans _ _ _ (pr2 i) (min_le_a a b)).
-  Defined.
-
-  Lemma minabstn_to_bstn
-    { a b : nat } (i : ⟦ min a b ⟧%stn) : ⟦ b ⟧%stn.
-  Proof.
-    intros.
-    refine (pr1 i,, _).
-    exact (natlthlehtrans _ _ _ (pr2 i) (min_le_b a b)).
-  Defined.
-
   Lemma natminus1lthn
     (n : nat) : n > 0 -> n - 1 < n.
   Proof.
@@ -105,119 +91,7 @@ Section Misc.
     apply isdeceqnat.
   Defined.
 
-  Lemma fldchoice0 {X : fld} (e : X) : coprod (e = 0%ring) (e != 0%ring).
-  Proof.
-    destruct (fldchoice e).
-    2: {left; assumption. }
-    right.
-    unfold multinvpair, invpair in m.
-    destruct m as [m1 [m2 m3]].
-    apply (@ringneq0andmultlinv X e m1).
-    change 1%ring with 1%multmonoid in m3.
-    assert (eq: (e * m1)%ring = 1%ring).
-    { symmetry in m2. apply m3. }
-    rewrite eq; apply nonzeroax.
-  Defined.
-
-  Lemma fldchoice0_left {X : fld} (e : X) (eq : (e = 0)%ring):
-    (fldchoice0 e) = inl eq.
-  Proof.
-    destruct (fldchoice0 _).
-    - apply proofirrelevance.
-      apply isapropcoprod.
-      + use setproperty.
-      + apply isapropneg.
-      + intros; contradiction.
-    - contradiction.
-  Defined.
-
-  Lemma fldchoice0_right {X : fld} (e : X) (neq : (e != 0)%ring):
-    (fldchoice0 e) = inr neq.
-  Proof.
-    destruct (fldchoice0 _).
-    - contradiction.
-    - apply proofirrelevance; apply isapropcoprod.
-      + use setproperty.
-      + apply isapropneg.
-      + intros; contradiction.
-  Defined.
-
-  Lemma fldmultinvlax {X: fld} (e : X) (ne : e != 0%ring) :
-    (fldmultinv e ne * e)%ring = 1%ring.
-  Proof.
-    unfold fldmultinv.
-    unfold fldmultinvpair.
-    destruct (fldchoice _) as [? | contr_eq].
-    2: { apply fromempty.
-         rewrite contr_eq in ne.
-         contradiction. }
-    destruct m as [m1 [m2 m3]].
-    simpl.
-    change (m1 * e)%ring with (m1 * e)%multmonoid.
-    rewrite m2.
-    reflexivity.
-  Defined.
-
-  Lemma fldmultinvrax {X: fld} (e : X) (ne : e != 0%ring) :
-    (e * fldmultinv e ne)%ring = 1%ring.
-  Proof.
-    rewrite ringcomm2; apply fldmultinvlax.
-  Defined.
-
-  Definition fldmultinv' {X : fld} (e : X) : X.
-  Proof.
-    destruct (fldchoice0 e) as [eq0 | neq].
-    - exact 0%ring.
-    - exact (fldmultinv e neq).
-  Defined.
-
-  Lemma fldmultinvlax' {X: fld} (e : X) (ne : e != 0%ring) :
-    (fldmultinv' e * e)%ring = 1%ring.
-  Proof.
-    unfold fldmultinv'.
-    destruct (fldchoice0 _).
-    - contradiction.
-    - apply fldmultinvlax.
-  Defined.
-
-  Lemma fldmultinvrax' {X: fld} (e : X) (ne : e != 0%ring) :
-    (e * fldmultinv' e)%ring = 1%ring.
-  Proof.
-    unfold fldmultinv'.
-    destruct (fldchoice0 _).
-    - contradiction.
-    - apply fldmultinvrax.
-  Defined.
-
-  Lemma fldplusminus
-    {F: fld} (a b : F) : (a + b - b)%ring = a.
-  Proof.
-    replace (a + b - b)%ring with (a + b + (- b))%ring.
-    - rewrite ringassoc1.
-      replace (b + - b)%ring with (b  - b)%ring.
-      + rewrite ringrinvax1. apply (@rigrunax1 F).
-      + reflexivity.
-   - rewrite <- ringcomm1.
-     reflexivity.
-  Defined.
-
-  (** could try to simplify/speed up? *)
-  Lemma hqone_neq_hqzero : 1%hq != 0%hq.
-  Proof.
-    intro contr.
-    assert (contr_hz : intpart 1%hq != intpart 0%hq).
-    { unfold intpart. apply hzone_neg_hzzero. }
-    apply contr_hz.
-    apply maponpaths, contr.
-  Defined.
-
-  Lemma hqplusminus
-    (a b : hq) : (a + b - b)%hq = a.
-  Proof.
-    apply (@fldplusminus hq).
-  Defined.
-
-  Lemma fromnatcontr
+    Lemma fromnatcontr
     {X : UU} (m n : nat) : (m = n) -> (m ≠ n) -> X.
   Proof.
     intros m_eq_n m_neq_n.
@@ -274,9 +148,28 @@ Section Misc.
         apply (fromnatcontr i j i_eq_j i_neq_j).
   Defined.
 
-End Misc.
+End Nat.
 
-Section PrelStn.
+(** * Standard finite sets *)
+(** lemmas for working with [Stn], the standard finite sets *)
+
+Section Stn.
+
+  Lemma minabstn_to_astn
+    { a b : nat } (i : ⟦ min a b ⟧%stn) : ⟦ a ⟧%stn.
+  Proof.
+    intros.
+    refine (pr1 i,, _).
+    exact (natlthlehtrans _ _ _ (pr2 i) (min_le_a a b)).
+  Defined.
+
+  Lemma minabstn_to_bstn
+    { a b : nat } (i : ⟦ min a b ⟧%stn) : ⟦ b ⟧%stn.
+  Proof.
+    intros.
+    refine (pr1 i,, _).
+    exact (natlthlehtrans _ _ _ (pr2 i) (min_le_b a b)).
+  Defined.
 
   Lemma stn_inhabited_implies_succ
     {n:nat} (i : ⟦ n ⟧%stn)
@@ -391,7 +284,6 @@ Section PrelStn.
       apply pathsinv0, maponpaths; assumption.
   Defined.
 
-  (** upstream to stn? *)
   Lemma stn_eq
     {k : nat} (i j : stn k) (eq : pr1 i = pr1 j) : i = j.
   Proof.
@@ -412,57 +304,9 @@ Section PrelStn.
     now apply stn_eq_2.
   Defined.
 
-End PrelStn.
+End Stn.
 
-Section Maybe.
-
-  Lemma isdeceqmaybe
-    {X : UU} (dec : isdeceq X) : isdeceq (maybe X).
-  Proof.
-    apply isdeceqcoprod.
-    - exact dec.
-    - exact isdecequnit.
-  Defined.
-
-  Definition maybe_choice
-    {X : UU} (e : maybe X)
-  : coprod (e != nothing) (e = nothing).
-  Proof.
-    destruct e as [? | u].
-    - apply ii1. apply negpathsii1ii2.
-    - apply ii2. rewrite u. exists.
-  Defined.
-
-  Definition maybe_choice'
-    {X : UU} (e : maybe X)
-  : coprod (X) (e = nothing).
-  Proof.
-  destruct e as [x | u].
-  - apply ii1. exact x.
-  - apply ii2. rewrite u; exists.
-  Defined.
-
-  Definition maybe_stn_choice
-    {X : UU} { n : nat }
-    (e : maybe (⟦ n ⟧)%stn)
-  : coprod (∑ i : ⟦ n ⟧%stn, e = just i) (e = nothing).
-  Proof.
-  destruct e as [i | u].
-  - apply ii1. now exists i.
-  - apply ii2. rewrite u; exists.
-  Defined.
-
-  Definition from_maybe
-    {X : UU} (m : maybe X) (p : m != nothing) : X.
-  Proof.
-    unfold nothing in p.
-     destruct m as [x | u].
-     - exact x.
-     - contradiction p.
-       now rewrite u.
-  Defined.
-
-End Maybe.
+(** Lemmas on the “dual” function on the standard finite sets, reversing the order *)
 
 Section Dual.
 
@@ -652,3 +496,182 @@ Section Dual.
   Defined.
 
 End Dual.
+
+(** * Fields *)
+
+(** Lemmas on fields, and in particular their decidable equality *)
+
+Section Fields.
+
+  Lemma fldchoice0 {X : fld} (e : X) : coprod (e = 0%ring) (e != 0%ring).
+  Proof.
+    destruct (fldchoice e).
+    2: {left; assumption. }
+    right.
+    unfold multinvpair, invpair in m.
+    destruct m as [m1 [m2 m3]].
+    apply (@ringneq0andmultlinv X e m1).
+    change 1%ring with 1%multmonoid in m3.
+    assert (eq: (e * m1)%ring = 1%ring).
+    { symmetry in m2. apply m3. }
+    rewrite eq; apply nonzeroax.
+  Defined.
+
+  Lemma fldchoice0_left {X : fld} (e : X) (eq : (e = 0)%ring):
+    (fldchoice0 e) = inl eq.
+  Proof.
+    destruct (fldchoice0 _).
+    - apply proofirrelevance.
+      apply isapropcoprod.
+      + use setproperty.
+      + apply isapropneg.
+      + intros; contradiction.
+    - contradiction.
+  Defined.
+
+  Lemma fldchoice0_right {X : fld} (e : X) (neq : (e != 0)%ring):
+    (fldchoice0 e) = inr neq.
+  Proof.
+    destruct (fldchoice0 _).
+    - contradiction.
+    - apply proofirrelevance; apply isapropcoprod.
+      + use setproperty.
+      + apply isapropneg.
+      + intros; contradiction.
+  Defined.
+
+  Lemma fldmultinvlax {X: fld} (e : X) (ne : e != 0%ring) :
+    (fldmultinv e ne * e)%ring = 1%ring.
+  Proof.
+    unfold fldmultinv.
+    unfold fldmultinvpair.
+    destruct (fldchoice _) as [? | contr_eq].
+    2: { apply fromempty.
+         rewrite contr_eq in ne.
+         contradiction. }
+    destruct m as [m1 [m2 m3]].
+    simpl.
+    change (m1 * e)%ring with (m1 * e)%multmonoid.
+    rewrite m2.
+    reflexivity.
+  Defined.
+
+  Lemma fldmultinvrax {X: fld} (e : X) (ne : e != 0%ring) :
+    (e * fldmultinv e ne)%ring = 1%ring.
+  Proof.
+    rewrite ringcomm2; apply fldmultinvlax.
+  Defined.
+
+  Definition fldmultinv' {X : fld} (e : X) : X.
+  Proof.
+    destruct (fldchoice0 e) as [eq0 | neq].
+    - exact 0%ring.
+    - exact (fldmultinv e neq).
+  Defined.
+
+  Lemma fldmultinvlax' {X: fld} (e : X) (ne : e != 0%ring) :
+    (fldmultinv' e * e)%ring = 1%ring.
+  Proof.
+    unfold fldmultinv'.
+    destruct (fldchoice0 _).
+    - contradiction.
+    - apply fldmultinvlax.
+  Defined.
+
+  Lemma fldmultinvrax' {X: fld} (e : X) (ne : e != 0%ring) :
+    (e * fldmultinv' e)%ring = 1%ring.
+  Proof.
+    unfold fldmultinv'.
+    destruct (fldchoice0 _).
+    - contradiction.
+    - apply fldmultinvrax.
+  Defined.
+
+  Lemma fldplusminus
+    {F: fld} (a b : F) : (a + b - b)%ring = a.
+  Proof.
+    replace (a + b - b)%ring with (a + b + (- b))%ring.
+    - rewrite ringassoc1.
+      replace (b + - b)%ring with (b  - b)%ring.
+      + rewrite ringrinvax1. apply (@rigrunax1 F).
+      + reflexivity.
+   - rewrite <- ringcomm1.
+     reflexivity.
+  Defined.
+
+End Fields.
+
+(** * Rationals *)
+Section Rationals.
+
+  (* could try to simplify/speed up? perhaps refactor with general injectivity of map from integers to rationals (proved either by this same technique or more directly) *)
+  Lemma hqone_neq_hqzero : 1%hq != 0%hq.
+  Proof.
+    intro contr.
+    assert (contr_hz : intpart 1%hq != intpart 0%hq).
+    { unfold intpart. apply hzone_neg_hzzero. }
+    apply contr_hz.
+    apply maponpaths, contr.
+  Defined.
+
+  Lemma hqplusminus
+    (a b : hq) : (a + b - b)%hq = a.
+  Proof.
+    apply (@fldplusminus hq).
+  Defined.
+
+End Rationals.
+
+(** * Maybe *)
+
+(** Lemmas on the general “maybe” construction *)
+
+Section Maybe.
+
+  Lemma isdeceqmaybe
+    {X : UU} (dec : isdeceq X) : isdeceq (maybe X).
+  Proof.
+    apply isdeceqcoprod.
+    - exact dec.
+    - exact isdecequnit.
+  Defined.
+
+  Definition maybe_choice
+    {X : UU} (e : maybe X)
+  : coprod (e != nothing) (e = nothing).
+  Proof.
+    destruct e as [? | u].
+    - apply ii1. apply negpathsii1ii2.
+    - apply ii2. rewrite u. exists.
+  Defined.
+
+  Definition maybe_choice'
+    {X : UU} (e : maybe X)
+  : coprod (X) (e = nothing).
+  Proof.
+  destruct e as [x | u].
+  - apply ii1. exact x.
+  - apply ii2. rewrite u; exists.
+  Defined.
+
+  Definition maybe_stn_choice
+    {X : UU} { n : nat }
+    (e : maybe (⟦ n ⟧)%stn)
+  : coprod (∑ i : ⟦ n ⟧%stn, e = just i) (e = nothing).
+  Proof.
+  destruct e as [i | u].
+  - apply ii1. now exists i.
+  - apply ii2. rewrite u; exists.
+  Defined.
+
+  Definition from_maybe
+    {X : UU} (m : maybe X) (p : m != nothing) : X.
+  Proof.
+    unfold nothing in p.
+     destruct m as [x | u].
+     - exact x.
+     - contradiction p.
+       now rewrite u.
+  Defined.
+
+End Maybe.
