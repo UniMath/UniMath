2e834e315cd4c142a8ad6e1fa0e11a4529d18d36
Kfwullaert <kwullaer@hotmail.com>
2022-10-04 18:33:02 +0200

    Moved the (co)algebras over endofunctors for precategories into Induction to use them there and adapted the imports in Induction

--- UniMath/Induction/PolynomialFunctors.v
+++ UniMath/Induction/PolynomialFunctors.v
@@ -17,7 +17,8 @@ Require Import UniMath.Foundations.PartA.
 Require Import UniMath.Foundations.UnivalenceAxiom.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
-Require Import UniMath.CategoryTheory.FunctorAlgebras.
+Require Import UniMath.Induction.FunctorAlgebras_legacy.
+Require Import UniMath.Induction.FunctorCoalgebras_legacy.
 Require Import UniMath.CategoryTheory.categories.Type.Core.
 
 Section PolynomialFunctors.
