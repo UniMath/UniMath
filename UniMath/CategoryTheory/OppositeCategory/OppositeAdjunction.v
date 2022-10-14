42b2c4cd1b14d0f6d6835c3980bf668734239f28
Kfwullaert <kwullaer@hotmail.com>
2022-10-14 19:05:34 +0200

    Removed double definition

--- UniMath/CategoryTheory/OppositeCategory/OppositeAdjunction.v
+++ UniMath/CategoryTheory/OppositeCategory/OppositeAdjunction.v
@@ -8,18 +8,7 @@ Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 
 Require Import UniMath.CategoryTheory.Adjunctions.Core.
 
-Local Definition opp_cat (C : category)
-  : category.
-Proof.
-  exists (opp_precat C).
-  apply has_homsets_op.
-Defined.
-
-(* functor_opp is defined as a functor between precategories,
-   so in order to avoid always explicit casting, the following is introduced: *)
-Local Definition functor_op {C D : category} (F : functor C D)
-  : functor (opp_cat C) (opp_cat D)
-  := functor_opp F.
+Require Import UniMath.CategoryTheory.OppositeCategory.Core.
 
 Section OppositeAdjunction.
 
