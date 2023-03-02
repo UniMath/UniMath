5361f5be1443918a41bf30d502718a716f8695de
Tempestas Ludi <tempestasludi@gmail.com>
2023-03-02 16:19:52 +0100

    Reduce the amount of information in Algebraic Theory

--- UniMath/ArnoudsThesis/AlgebraicTheory.v
+++ UniMath/ArnoudsThesis/AlgebraicTheory.v
@@ -14,11 +14,11 @@ Local Open Scope cat.
 Section AlgebraicTheoryData.
 
   Definition algebraic_theory_data := ∑ (T: finite_set_skeleton_category ⟶ HSET),
-    (∏ {n}, stn n → (T n : hSet)) × (∏ {m n}, (T n : hSet) → (stn n → (T m : hSet)) → (T m : hSet)).
+    (T 1 : hSet) × (∏ {m n}, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet)).
 
-  Definition make_algebraic_theory_data (T: finite_set_skeleton_category ⟶ HSET) (proj: ∏ {n}, stn n → (T n : hSet)) (comp: ∏ {m n}, (T n : hSet) → (stn n → (T m : hSet)) → (T m : hSet)) : algebraic_theory_data.
+  Definition make_algebraic_theory_data (T: finite_set_skeleton_category ⟶ HSET) (e : (T 1 : hSet)) (comp: ∏ {m n}, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet)) : algebraic_theory_data.
   Proof.
-    exact (T ,, (proj ,, comp)).
+    exact (T ,, e ,, comp).
   Defined.
 
   Variable T : algebraic_theory_data.
@@ -26,10 +26,15 @@ Section AlgebraicTheoryData.
   Definition f_T (d : algebraic_theory_data) : (finite_set_skeleton_category ⟶ HSET) := pr1 d.
   Coercion f_T : algebraic_theory_data >-> functor.
 
-  Local Definition pr : ∏ {n}, stn n → (T n : hSet) := pr1 (pr2 T).
-  Local Definition comp: ∏ {m n}, (T n : hSet) → (stn n → (T m : hSet)) → (T m : hSet) := pr2 (pr2 T).
+  Definition e : (T 1 : hSet) := pr12 T.
 
-  Local Definition id : (T 1 : hSet) := pr (@firstelement 0).
+  Definition comp: ∏ {m n}, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet) := pr2 (pr2 T).
+
+  Definition pr {n} (i : stn n) : (T n : hSet) := #T (λ (x : stn 1), i) e.
+
+  Notation "f • g" :=
+    (comp f g)
+    (at level 50).
 
   (* Define the associativity property of the algebraic theory *)
   Definition comp_is_assoc : Prop := ∏
@@ -37,47 +42,27 @@ Section AlgebraicTheoryData.
     (f_l : (T l : hSet))
     (f_m : stn l → (T m : hSet))
     (f_n : stn m → (T n : hSet)),
-      (comp (comp f_l f_m) f_n) = (comp f_l (λ t_l, comp (f_m t_l) f_n)).
+      (f_l • f_m) • f_n = f_l • (λ t_l, (f_m t_l) • f_n).
 
   (* Define the unitality property of the algebraic theory *)
   Definition comp_is_unital : Prop := ∏
     (n : nat)
-    (f_n : (T n : hSet)),
-      (comp f_n (λ t, pr t)) = f_n.
+    (f : (T n : hSet)),
+      e • (λ _, f) = f.
 
   (* Define the compatibility of the projection function with composition *)
-  Definition comp_is_compatible_with_projections : Prop := ∏
-    (m n : nat)
-    (l : stn m)
-    (f_n : stn m → (T n : hSet)),
-      (comp (pr l) f_n) = f_n l.
+  Definition comp_identity_projections : Prop := ∏
+    (n : nat)
+    (f : (T n : hSet)),
+      f • (λ i, pr i) = f.
 
   (* Define naturality of the composition in the first argument *)
   Definition comp_is_natural_l : Prop := ∏
-    (s t n : finite_set_skeleton_category)
-    (f: finite_set_skeleton_category⟦s, t⟧)
-    (f_s : (T s : hSet))
-    (f_n : stn s → (T n : hSet)),
-    unit.
-  (* Proof.
-    destruct (natgtb s m) eqn:hm.
-    - apply f_n.
-      use tpair.
-      + exact m.
-      + simpl.
-        rewrite hm.
-        apply idpath.
-    - apply pr.
-      apply firstelement.
-  End. *)
-
-  (* Define naturality of the composition in the second argument *)
-  Definition comp_is_natural_r := ∏
-    (m s t : finite_set_skeleton_category)
-    (f: finite_set_skeleton_category⟦s, t⟧)
-    (f_m : (T m : hSet))
-    (f_s : stn m → (T s : hSet)),
-      ((comp f_m (λ x, #T f (f_s x))) = (#T f) (comp f_m f_s)).
+    (m m' n : finite_set_skeleton_category)
+    (a : finite_set_skeleton_category⟦m, m'⟧)
+    (f : (T m : hSet))
+    (g : stn m' → (T n : hSet)),
+    (#T a f) • g = f • (λ i, g (a i)).
     
 End AlgebraicTheoryData.
 
@@ -86,17 +71,15 @@ Section AlgebraicTheory.
   Definition is_algebraic_theory (T : algebraic_theory_data) :=
       (comp_is_assoc T) ×
       (comp_is_unital T) ×
-      (comp_is_compatible_with_projections T) ×
-      (comp_is_natural_l T) ×
-      (comp_is_natural_r T).
+      (comp_identity_projections T) ×
+      (comp_is_natural_l T).
 
   Definition make_is_algebraic_theory
     {T : algebraic_theory_data}
     (H1 : comp_is_assoc T)
     (H2 : comp_is_unital T)
-    (H3 : comp_is_compatible_with_projections T)
-    (H4 : comp_is_natural_l T)
-    (H5 : comp_is_natural_r T) : is_algebraic_theory T := (H1 ,, H2 ,, H3 ,, H4 ,, H5).
+    (H3 : comp_identity_projections T)
+    (H4 : comp_is_natural_l T) : is_algebraic_theory T := (H1 ,, H2 ,, H3 ,, H4).
 
   Lemma ispredicate_is_algebraic_theory : isPredicate is_algebraic_theory.
   Proof.
@@ -104,7 +87,6 @@ Section AlgebraicTheory.
     repeat apply isapropdirprod;
       repeat (apply impred_isaprop; intros);
       try apply setproperty.
-    exact isapropunit.
   Qed.
 
   Definition algebraic_theory := total2 is_algebraic_theory.
@@ -113,6 +95,36 @@ Section AlgebraicTheory.
 
   Definition algebraic_theory_data_from_algebraic_theory : algebraic_theory -> algebraic_theory_data := pr1.
   Coercion algebraic_theory_data_from_algebraic_theory : algebraic_theory >-> algebraic_theory_data.
+
+  Lemma functor_uses_projections (T : algebraic_theory) (m n : finite_set_skeleton_category) (a : finite_set_skeleton_category⟦m, n⟧) (f : (T m : hSet)) : #T a f = comp T f (λ i, pr T (a i)).
+  Proof.
+    rewrite <- (pr1 (pr222 T) n (#T a f)).
+    apply T.
+  Qed.
+
+  Lemma comp_project_component (T : algebraic_theory) (m n : nat) (i : stn m) (f : (stn m → (T n : hSet))) : comp T (pr T i) f = f i.
+  Proof.
+    unfold pr.
+    rewrite (pr2 (pr222 T)).
+    apply T.
+  Qed.
+
+  (* The composition is natural in the second argument *)
+  Lemma comp_is_natural_r (T : algebraic_theory)
+    (m n n' : finite_set_skeleton_category)
+    (a: finite_set_skeleton_category⟦n, n'⟧)
+    (f : (T m : hSet))
+    (g : stn m → (T n : hSet)) :
+      comp T f (λ i, #T a (g i)) = #T a (comp T f g).
+  Proof.
+    rewrite functor_uses_projections.
+    rewrite (pr12 T).
+    apply maponpaths.
+    apply funextsec2.
+    intros i.
+    rewrite functor_uses_projections.
+    apply idpath.
+  Qed.
 
 End AlgebraicTheory.
 
