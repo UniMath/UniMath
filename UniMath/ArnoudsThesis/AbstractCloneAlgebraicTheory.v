0ab6a67da7a395d80528d67bd42efc173b315807
Tempestas Ludi <tempestasludi@gmail.com>
2023-03-06 07:46:44 +0100

    Split lemmas from definitions, add algebraic_theory_eq

--- UniMath/ArnoudsThesis/AbstractCloneAlgebraicTheory.v
+++ UniMath/ArnoudsThesis/AbstractCloneAlgebraicTheory.v
@@ -15,83 +15,150 @@ Local Open Scope cat.
 
 Section AbstractCloneAlgebraicTheory.
 
-  Definition abstract_clone_from_algebraic_theory : algebraic_theory → abstract_clone.
+  (* The function from algebraic theories to abstract clones *)
+  Definition abstract_clone_data_from_algebraic_theory : algebraic_theory → abstract_clone_data.
   Proof.
     intros T.
-    use make_abstract_clone.
-    - use make_abstract_clone_data.
-      + exact T. 
-      + intros.
-        apply AlgebraicTheory.pr.
-        assumption.
-      + intros m n f g.
-        exact (pr221 T m n f g).
-    - use make_is_abstract_clone. 
-      + intros m n.
-        apply AlgebraicTheory.comp_project_component.
-      + apply (pr1 (pr222 T)).
-      + apply (pr12 T).
+    use make_abstract_clone_data.
+    - exact T. 
+    - intros.
+      apply AlgebraicTheory.pr.
+      assumption.
+    - intros m n f g.
+      exact (pr221 T m n f g).
   Defined.
 
-  Definition abstract_clone_to_functor : abstract_clone → (finite_set_skeleton_category ⟶ HSET).
+  Lemma is_abstract_clone_from_algebraic_theory (T : algebraic_theory) : is_abstract_clone (abstract_clone_data_from_algebraic_theory T).
+  Proof.
+    use make_is_abstract_clone. 
+    - intros m n.
+      apply AlgebraicTheory.comp_project_component.
+    - apply (pr1 (pr222 T)).
+    - apply (pr12 T).
+  Qed.
+
+  Definition abstract_clone_from_algebraic_theory : algebraic_theory → abstract_clone := (λ T, (make_abstract_clone (abstract_clone_data_from_algebraic_theory T) (is_abstract_clone_from_algebraic_theory T))).
+
+  (* The function from abstract clones to functors *)
+  Definition functor_data_from_abstract_clone : abstract_clone → (functor_data finite_set_skeleton_category HSET).
   Proof.
     intros C.
-    use make_functor.
-    - use make_functor_data.
-      + intros.
-        apply C.
-        assumption.
-      + intros m n a f.
-        exact (AbstractClone.reindex a f).
-    - destruct (pr2 C) as [H1 [H2 H3]].
-      split.
-      + intro.
-        apply funextsec2.
-        intro.
-        apply H2.
-      + intro.
-        intros.
-        apply funextsec2.
-        intro.
-        unfold reindex, compose.
-        simpl.
-        rewrite H3.
-        apply maponpaths, funextsec2.
-        intro.
-        symmetry.
-        apply H1.
+    use make_functor_data.
+    - intros.
+      apply C.
+      assumption.
+    - intros m n a f.
+      exact (AbstractClone.reindex a f).
   Defined.
 
-  Definition algebraic_theory_from_abstract_clone : abstract_clone → algebraic_theory.
+  Lemma is_functor_from_abstract_clone (C : abstract_clone) : (is_functor (functor_data_from_abstract_clone C)).
+  Proof.
+    destruct (pr2 C) as [H1 [H2 H3]].
+    split.
+    + intro.
+      apply funextsec2.
+      intro.
+      apply H2.
+    + intro.
+      intros.
+      apply funextsec2.
+      intro.
+      unfold reindex, compose.
+      simpl.
+      unfold reindex.
+      simpl.
+      rewrite H3.
+      apply maponpaths, funextsec2.
+      intro.
+      symmetry.
+      apply H1.
+  Qed.
+
+  Definition functor_from_abstract_clone : abstract_clone → (finite_set_skeleton_category ⟶ HSET) := (λ C, make_functor (functor_data_from_abstract_clone C) (is_functor_from_abstract_clone C)).
+
+  (* The function from abstract clones to algebraic theories *)
+  Definition algebraic_theory_data_from_abstract_clone : abstract_clone → algebraic_theory_data.
   Proof.
     intros C.
-    use make_algebraic_theory.
-    - use make_algebraic_theory_data.
-      + exact (abstract_clone_to_functor C).
-      + exact (pr121 C 1 (firstelement)).
-      + apply (pr221 C).
-    - destruct (pr2 C) as [H1 [H2 H3]].
-      use make_is_algebraic_theory.
-      + apply H3.
-      + intro.
-        intros.
-        apply H1.
-      + intro.
-        intros.
-        rewrite <- H2.
-        apply maponpaths, funextsec2.
-        intro.
-        apply H1.
-      + intro.
-        intros.
-        unfold AlgebraicTheory.comp, AlgebraicTheory.pr.
-        simpl.
-        unfold reindex, comp.
-        rewrite H3.
-        apply maponpaths, funextsec2.
-        intro.
-        apply H1.
+    use make_algebraic_theory_data.
+    - exact (functor_from_abstract_clone C).
+    - exact (pr121 C 1 (firstelement)).
+    - apply (pr221 C).
   Defined.
+
+  Lemma is_algebraic_theory_from_abstract_clone (C : abstract_clone) : is_algebraic_theory (algebraic_theory_data_from_abstract_clone C).
+  Proof.
+    destruct (pr2 C) as [H1 [H2 H3]].
+    use make_is_algebraic_theory.
+    + apply H3.
+    + intro.
+      intros.
+      apply H1.
+    + intro.
+      intros.
+      rewrite <- H2.
+      apply maponpaths, funextsec2.
+      intro.
+      apply H1.
+    + intro.
+      intros.
+      unfold AlgebraicTheory.comp, AlgebraicTheory.pr.
+      simpl.
+      unfold reindex, comp.
+      rewrite H3.
+      apply maponpaths, funextsec2.
+      intro.
+      apply H1.
+  Qed.
+  
+  Definition algebraic_theory_from_abstract_clone : abstract_clone → algebraic_theory := (λ C, make_algebraic_theory (algebraic_theory_data_from_abstract_clone C) (is_algebraic_theory_from_abstract_clone C)).
+
+  (* Prove the weak equality *)
+  Lemma algebraic_theory_eq (X Y : algebraic_theory) : ∏
+    (p : pr11 X = pr11 Y),
+    (transportf (λ T, (T 1 : hSet)) (maponpaths pr1 (maponpaths pr1 p)) (pr121 X) = (pr121 Y)) →
+    (transportf (λ T, ∏ m n, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet)) (maponpaths pr1 (maponpaths pr1 p)) (pr221 X) = (pr221 Y)) →
+    X = Y.
+  Proof.
+    destruct X as [[X [Xe Xcomp]] XH].
+    destruct Y as [[Y [Ye Ycomp]] YH].
+    intros.
+    apply (subtypePairEquality ispredicate_is_algebraic_theory).
+    simpl in p.
+    apply (total2_paths2_f p).
+    induction p.
+    rewrite idpath_transportf.
+    apply pathsdirprod; assumption.
+  Qed.
+
+  Lemma L1 y : pr11 (algebraic_theory_from_abstract_clone (abstract_clone_from_algebraic_theory y)) = pr11 y.
+  Proof.
+    destruct (pr2 y) as [HTassoc [HTunit [HTproj HTnat]]].
+    use (subtypePairEquality' _ (isaprop_is_functor _ _ (pr2 SET) _)).
+    use (total2_paths2_b (idpath _)). 
+    repeat (apply funextsec2; intro).
+    rewrite <- (HTproj _ (transportb _ _ (pr211 (pr1 y)) _ _ _ _)).
+    symmetry.
+    apply y.
+  Qed.
+
+  Lemma L2 y p : transportf (λ T : nat → hSet, T 1 : hSet) (maponpaths pr1 (maponpaths pr1 p)) (pr121 (algebraic_theory_from_abstract_clone (abstract_clone_from_algebraic_theory y))) = pr121 y.
+  Proof.
+    simpl.
+    unfold AlgebraicTheory.pr, e.
+    destruct (pr2 y) as [H1 [H2 [H3 H4]]].
+    assert ((λ (_ : stn 1), firstelement) = identity (1 : finite_set_skeleton_category)).
+    {
+      apply funextsec2.
+      intro i.
+      apply (subtypePairEquality (λ _, (isasetbool _ _))).
+      exact (!(natlth1tois0 _ (pr2 i))).
+    }
+    rewrite X.
+    rewrite (functor_id_id _ _ _ _ _ (idpath _)).
+    unfold identity.
+    simpl.
+  Qed.
 
   Lemma algebraic_theory_weq_abstract_clone : abstract_clone ≃ algebraic_theory.
   Proof.
@@ -100,26 +167,9 @@ Section AbstractCloneAlgebraicTheory.
     repeat use tpair.
     - exact abstract_clone_from_algebraic_theory.
     - intros.
-      apply (subtypePairEquality ispredicate_is_algebraic_theory).
-      use total2_paths2_b.
-      + destruct (pr2 y) as [HTassoc [HTunit [HTproj HTnat]]].
-        use (subtypePairEquality' _ (isaprop_is_functor _ _ (pr2 SET) _)).
-        use (total2_paths2_b (idpath _)). 
-        repeat (apply funextsec2; intro).
-        rewrite <- (HTproj _ (transportb _ _ (pr211 (pr1 y)) _ _ _ _)).
-        symmetry.
-        apply y.
-      + use total2_paths2_b.
-        * 
-          
-          simpl.
-          unfold abstract_clone_from_algebraic_theory.
-          unfold AlgebraicTheory.pr.
-          simpl.
-        simpl.
-        use subtypePairEquality'.
-        use (subtypePairEquality' _ (isaprop_is_functor _ _ (pr2 SET) _)).
-      simpl.
+      use algebraic_theory_eq.
+      + apply L1.
+      + simpl.
   Qed.
 
 End AbstractCloneAlgebraicTheory.
