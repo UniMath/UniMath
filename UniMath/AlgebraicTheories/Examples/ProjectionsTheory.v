fa8f3d000d7f3f136c354bbd17a65dfc569e0953
Tempestas Ludi <tempestasludi@gmail.com>
2023-04-12 15:12:17 +0200

    Show that any set is an algebra for the projections theory

--- UniMath/AlgebraicTheories/Examples/ProjectionsTheory.v
+++ UniMath/AlgebraicTheories/Examples/ProjectionsTheory.v
@@ -6,6 +6,7 @@ Require Import UniMath.Combinatorics.StandardFiniteSets.
 Require Import UniMath.AlgebraicTheories.AlgebraicBases.
 Require Import UniMath.AlgebraicTheories.AlgebraicTheories.
 Require Import UniMath.AlgebraicTheories.AbstractClones.AbstractClones.
+Require Import UniMath.AlgebraicTheories.AbstractClones.AbstractCloneAlgebras.
 Require Import UniMath.AlgebraicTheories.AbstractCloneAlgebraicTheory.
 
 (* Construct an algebraic theory as an abstract clone *)
@@ -13,7 +14,7 @@ Definition projections_clone_data
   : abstract_clone_data
   := make_abstract_clone_data
     (make_algebraic_base
-      (λ n, (stn n ,, isasetstn n))
+      stnset
       (λ _ _ f g, g f))
     (λ _ i, i).
 
@@ -23,6 +24,26 @@ Proof.
     repeat intro.
 Qed.
 
+Definition projections_clone
+  : abstract_clone
+  := make_abstract_clone _ projections_is_clone.
+
+
 Definition projections_theory
   : algebraic_theory
-  := algebraic_theory_weq_abstract_clone (make_abstract_clone _ projections_is_clone).
+  := algebraic_theory_weq_abstract_clone projections_clone.
+
+Definition projections_clone_algebra_data (A : hSet)
+  : abstract_clone_algebra_data projections_clone
+  := make_abstract_clone_algebra_data A (λ _ (i : projections_clone _) f, f i).
+
+Lemma projections_clone_algebra_is_algebra (A : hSet)
+  : is_abstract_clone_algebra (projections_clone_algebra_data A).
+Proof.
+  use make_is_abstract_clone_algebra;
+    now repeat intro.
+Qed.
+
+Definition projections_clone_algebra (A : hSet)
+  : abstract_clone_algebra projections_clone
+  := make_abstract_clone_algebra _ (projections_clone_algebra_is_algebra A).
