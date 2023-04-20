8a5523ee4575a80f3d8909824143266da66f9b60
Tempestas Ludi <tempestasludi@gmail.com>
2023-04-20 16:43:40 +0200

    Add a less categorical way of defining an algebraic theory

--- UniMath/AlgebraicTheories/AlgebraicTheoryCategory.v
+++ UniMath/AlgebraicTheories/AlgebraicTheoryCategory.v
@@ -203,3 +203,143 @@ Definition make_algebraic_theory
   : algebraic_theory
   := T ,, H.
 
+(* Definining an algebraic theory in another way *)
+Section AlgebraicTheory'.
+
+  Definition algebraic_theory_data' : UU
+    := ∑ (T : nat → hSet), (∏ n, stn n → T n) × (∏ m n, T m → (stn m → T n) → T n).
+
+  Definition make_algebraic_theory_data'
+    (T : nat → hSet)
+    (pr : ∏ n, stn n → T n)
+    (comp : ∏ m n, T m → (stn m → T n) → T n)
+    : algebraic_theory_data'
+    := (T ,, pr ,, comp).
+
+  Local Definition pr' {T : algebraic_theory_data'} {n : nat} : stn n → pr1 T n := pr12 T n.
+  Local Definition comp' {T : algebraic_theory_data'} {m n : nat} : pr1 T m → (stn m → pr1 T n) → pr1 T n := pr22 T m n.
+  Local Notation "f • g" := (comp' f g).
+
+  Local Definition comp_projects_component' (T : algebraic_theory_data') : UU
+    := ∏ m n i (f : stn m → pr1 T n), (pr' i) • f = f i.
+
+  Local Definition comp_identity_projections' (T : algebraic_theory_data') : UU
+    := ∏ n (f : pr1 T n), f • (λ i, pr' i) = f.
+
+  Local Definition comp_is_assoc' (T : algebraic_theory_data') : UU
+    := ∏ l m n f g h, @comp' T m n (comp' (m := l) f g) h = f • (λ t, (g t) • h).
+
+  Definition is_algebraic_theory' (T : algebraic_theory_data') : UU :=
+    comp_projects_component' T ×
+    comp_identity_projections' T ×
+    comp_is_assoc' T.
+
+  Definition make_is_algebraic_theory'
+    (T : algebraic_theory_data')
+    (H1 : comp_projects_component' T)
+    (H' : comp_identity_projections' T)
+    (H3 : comp_is_assoc' T)
+    : is_algebraic_theory' T
+    := (H1 ,, H' ,, H3).
+
+  Local Definition theory_comp_projects_component'
+    {T : algebraic_theory_data'}
+    (H : is_algebraic_theory' T)
+    : comp_projects_component' T
+    := pr1 H.
+
+  Local Definition theory_comp_identity_projections'
+    {T : algebraic_theory_data'}
+    (H : is_algebraic_theory' T)
+    : comp_identity_projections' T
+    := pr12 H.
+
+  Local Definition theory_comp_is_assoc'
+    {T : algebraic_theory_data'}
+    (H : is_algebraic_theory' T)
+    : comp_is_assoc' T
+    := pr22 H.
+
+  Definition algebraic_theory'_to_functor_data
+    (T : algebraic_theory_data')
+    (H : is_algebraic_theory' T)
+    : functor_data finite_set_skeleton_category HSET.
+  Proof.
+    use make_functor_data.
+    - exact (pr1 T).
+    - exact (λ _ _ a f, f • (λ i, pr' (a i))).
+  Defined.
+
+  Lemma algebraic_theory'_to_is_functor
+    (T : algebraic_theory_data')
+    (H : is_algebraic_theory' T)
+    : is_functor (algebraic_theory'_to_functor_data T H).
+  Proof.
+    use tpair.
+    - intro.
+      apply funextfun.
+      intro.
+      apply (theory_comp_identity_projections' H).
+    - do 5 intro.
+      apply funextfun.
+      intro.
+      cbn.
+      rewrite (theory_comp_is_assoc' H).
+      apply maponpaths.
+      apply funextfun.
+      intro.
+      now rewrite (theory_comp_projects_component' H).
+  Qed.
+
+  Definition algebraic_theory'_to_algebraic_theory_data
+    (T : algebraic_theory_data')
+    (H : is_algebraic_theory' T)
+    : algebraic_theory_data.
+  Proof.
+    use make_algebraic_theory_data.
+    - use (make_functor _ (algebraic_theory'_to_is_functor T H)).
+    - exact (pr' firstelement).
+    - intros m n.
+      exact (comp').
+  Defined.
+
+  Lemma algebraic_theory'_to_is_algebraic_theory
+    (T : algebraic_theory_data')
+    (H : is_algebraic_theory' T)
+    : is_algebraic_theory (algebraic_theory'_to_algebraic_theory_data T H).
+  Proof.
+    use make_is_algebraic_theory.
+    - exact (theory_comp_is_assoc' H).
+    - do 2 intro.
+      apply (theory_comp_projects_component' H).
+    - do 2 intro.
+      rewrite <- (theory_comp_identity_projections' H).
+      apply maponpaths, funextfun.
+      intro.
+      apply (theory_comp_projects_component' H).
+    - do 6 intro.
+      simpl.
+      rewrite (theory_comp_is_assoc' H).
+      apply maponpaths, funextfun.
+      intro.
+      apply (theory_comp_projects_component' H).
+  Defined.
+
+  Definition make_algebraic_theory'
+    (T : algebraic_theory_data')
+    (H : is_algebraic_theory' T)
+    : algebraic_theory
+    := make_algebraic_theory _ (algebraic_theory'_to_is_algebraic_theory T H).
+
+End AlgebraicTheory'.
+
+(* Constructors for the algebraic theory morphism type *)
+
+
+(* Defininig an algebraic theory morphism in another way *)
+
+
+
+(* Accessors for the properties of an algebraic theory *)
+
+(* Accessors for the properties of an algebraic theory morphism *)
