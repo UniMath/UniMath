1180b3f7a3cb23e67f53f3ca159c2ac2741d5aff
Tempestas Ludi <tempestasludi@gmail.com>
2023-04-25 11:30:50 +0200

    Update the one point theory example

--- UniMath/AlgebraicTheories/Examples/OnePointTheory.v
+++ UniMath/AlgebraicTheories/Examples/OnePointTheory.v
@@ -2,6 +2,7 @@ Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
 Require Import UniMath.Combinatorics.Vectors.
+Require Import UniMath.CategoryTheory.Core.Functors.
 
 Require Import UniMath.AlgebraicTheories.AlgebraicTheories.
 Require Import UniMath.AlgebraicTheories.AlgebraicTheories2.
@@ -9,16 +10,16 @@ Require Import UniMath.AlgebraicTheories.AlgebraicTheoryAlgebras.
 
 Local Open Scope vec_scope.
 
-Definition one_point_clone_data
-  : algebraic_theory_data
-  := make_algebraic_theory_data
+Definition one_point_theory'_data
+  : algebraic_theory'_data
+  := make_algebraic_theory'_data
       (λ _, (unit ,, isasetunit))
       (λ _ _, tt)
       (λ _ _ _ _, tt).
 
-Lemma one_point_is_clone : is_algebraic_theory one_point_clone_data.
+Lemma one_point_is_theory' : is_algebraic_theory' one_point_theory'_data.
 Proof.
-  use make_is_algebraic_theory.
+  use make_is_algebraic_theory'.
   - intros m n i f.
     now induction (f i).
   - intros n f.
@@ -28,16 +29,16 @@ Qed.
 
 Definition one_point_theory
   : algebraic_theory
-  := make_algebraic_theory' _ one_point_is_clone.
+  := make_algebraic_theory' _ one_point_is_theory'.
 
 Lemma one_point_clone_algebra_is_trivial
-  : ∏ (A : algebraic_theory_algebra one_point_clone), A ≃ unit.
+  : ∏ (A : algebraic_theory_algebra one_point_theory), A ≃ unit.
 Proof.
   intro A.
   apply weqcontrtounit.
   use tpair.
-  - use (action (tt : one_point_clone 0) (weqvecfun 0 vnil)).
+  - use (action (tt : (one_point_theory 0 : hSet)) (weqvecfun 0 vnil)).
   - intro a.
-    rewrite <- (algebraic_theory_algebra_action_projects_component _ _ (make_stn 1 0 (idpath _)) (λ _, a) : _ = a).
+    rewrite <- (algebraic_theory_algebra_projects_component _ _ (make_stn 1 0 (idpath _)) (λ _, a) : _ = a).
     exact (lift_constant_action _ _ _).
 Qed.
