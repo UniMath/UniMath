d4156453b97569c35494eb493c350558246b7f1d
Tempestas Ludi <tempestasludi@gmail.com>
2023-04-25 11:58:09 +0200

    Add algebraic theory algebra morphisms

---
+++ UniMath/AlgebraicTheories/AlgebraicTheoryAlgebraMorphisms.v
@@ -0,0 +1,36 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.Foundations.All.
+Require Import UniMath.Combinatorics.StandardFiniteSets.
+
+Require Import UniMath.AlgebraicTheories.AlgebraicTheories.
+Require Import UniMath.AlgebraicTheories.AlgebraicTheoryAlgebras.
+
+Definition preserves_action
+  {T : algebraic_theory_data}
+  {A A' : algebraic_theory_algebra_data T}
+  (F : A → A')
+  : UU
+  := ∏ n f (a : stn n → A), F (action f a) = action f (λ i, F (a i)).
+
+Definition algebraic_theory_algebra_morphism
+  {T : algebraic_theory_data}
+  (A A' : algebraic_theory_algebra_data T)
+  : UU
+  := ∑ (F : A → A'), preserves_action F.
+
+Definition algebraic_theory_algebra_morphism_to_function
+  {T : algebraic_theory_data}
+  {A A' : algebraic_theory_algebra_data T}
+  (F : algebraic_theory_algebra_morphism A A')
+  : A → A'
+  := pr1 F.
+
+Coercion algebraic_theory_algebra_morphism_to_function :
+  algebraic_theory_algebra_morphism >-> Funclass.
+
+Definition algebraic_theory_algebra_morphism_preserves_action
+  {T : algebraic_theory_data}
+  {A A' : algebraic_theory_algebra_data T}
+  (F : algebraic_theory_algebra_morphism A A')
+  : preserves_action F
+  := pr2 F.
