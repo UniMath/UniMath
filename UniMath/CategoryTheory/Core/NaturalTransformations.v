d3225d7e244ae6e573a0c08aaf2fc7483dd76911
EwenBC <134510945+EwenBC@users.noreply.github.com>
2023-05-30 15:59:52 +0200

    Update NaturalTransformations.v

--- UniMath/CategoryTheory/Core/NaturalTransformations.v
+++ UniMath/CategoryTheory/Core/NaturalTransformations.v
@@ -444,7 +444,7 @@ Definition is_nat_z_iso_id {C D : precategory} {F G : C ⟶ D} (eq : F = G) (ν 
   ∏ (c : C), nat_comp_to_endo eq (nat_z_iso_to_trans ν c) = identity (F c).
 
 
-Lemma is_nat_z_iso_nat_trans_id {C D : precategory} (F : C ⟶ D): is_nat_z_iso (nat_trans_id F).
+ Lemma is_nat_z_iso_nat_trans_id {C D : precategory} (F :functor_data C D): is_nat_z_iso (nat_trans_id F).
 Proof.
   intro c.
   exists (identity (F c)).
