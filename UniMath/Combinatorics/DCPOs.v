b6886d5bf96dfa0833616d644f66d7b2de8db4f5
nmvdw <nnmvdw@gmail.com>
2023-06-01 13:02:16 +0200

    Coproducts of DCPOs
    Co-authored-by: Dan Frumin <dan@groupoid.moe>

--- UniMath/Combinatorics/DCPOs.v
+++ UniMath/Combinatorics/DCPOs.v
@@ -4,6 +4,7 @@
 Require Export UniMath.Combinatorics.DCPOs.DirectedSets.
 Require Export UniMath.Combinatorics.DCPOs.DCPOBasics.
 Require Export UniMath.Combinatorics.DCPOs.ScottContinuous.
+Require Export UniMath.Combinatorics.DCPOs.DCPOSum.
 Require Export UniMath.Combinatorics.DCPOs.LeastFixpoint.
 Require Export UniMath.Combinatorics.DCPOs.Constructions.
 Require Export UniMath.Combinatorics.DCPOs.FubiniTheorem.
