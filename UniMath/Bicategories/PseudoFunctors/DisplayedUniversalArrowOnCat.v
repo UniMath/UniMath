899ebef81c06e91530ff44e375c1265a67bef1b4
Kfwullaert <kwullaer@hotmail.com>
2023-06-05 16:26:53 +0200

    Added proofs that weak equivalences preserve and reflect terminal objects and added Rezk completion for terminal object (in terms of universal arrows)

--- UniMath/Bicategories/PseudoFunctors/DisplayedUniversalArrowOnCat.v
+++ UniMath/Bicategories/PseudoFunctors/DisplayedUniversalArrowOnCat.v
@@ -25,6 +25,8 @@ Require Import UniMath.CategoryTheory.DisplayedCats.Equivalences.
 Require Import UniMath.CategoryTheory.DisplayedCats.TotalAdjunction.
 
 Require Import UniMath.CategoryTheory.WeakEquivalences.
+Require Import UniMath.CategoryTheory.limits.terminal.
+Require Import UniMath.CategoryTheory.limits.Preservation.
 
 Require Import UniMath.Bicategories.Core.Bicat. Import Bicat.Notations.
 Require Import UniMath.Bicategories.Core.Invertible_2cells.
@@ -52,6 +54,8 @@ Require Import UniMath.Bicategories.PseudoFunctors.DisplayedUniversalArrow.
 
 Require Import UniMath.Bicategories.PseudoFunctors.Examples.BicatOfCatToUnivCat.
 Require Import UniMath.Bicategories.DisplayedBicats.Examples.DispBicatOnCatToUniv.
+
+Require Import UniMath.Bicategories.DisplayedBicats.Examples.CategoriesWithStructure.
 
 Local Open Scope cat.
 
@@ -151,34 +155,74 @@ Section BicatOfCategoriesWithTerminalHasRezkCompletion.
   Let UnivCat := bicat_of_univ_cats.
   Let Cat := bicat_of_cats.
   Let R := univ_cats_to_cats.
+  Context (LUR : left_universal_arrow R).
+  Let η := (pr12 LUR).
+  Context (η_weak_equiv : ∏ C : category, is_weak_equiv (η C)).
 
-  (* Provided by Niels PR *)
-  (* Let D := disp_bicat_have_terminal_obj. *)
+  Let D := disp_bicat_have_terminal_obj.
 
-  (* Require Import UniMath.Bicategories.DisplayedBicats.Examples.Sub1Cell.
-  Search "disp_subbicat". *)
-
-  Context (D : disp_bicat Cat)
-    (HD : disp_2cells_iscontr D)
-    (LUR : left_universal_arrow R).
+  Lemma HD : disp_2cells_iscontr D.
+  Proof.
+    intro ; intros.
+    exists (tt,,tt).
+    intro.
+    use total2_paths_f ; apply iscontrunit.
+  Qed.
 
   Let RR := (disp_psfunctor_on_cat_to_univ_cat D (disp_2cells_isaprop_from_disp_2cells_iscontr _ HD)).
+
+  Lemma isaprop_isTerminal
+    {C : category} (x : C)
+    : isaprop (isTerminal _ x).
+  Proof.
+    apply impred_isaprop ; intro
+    ; apply isapropiscontr.
+  Qed.
 
   Definition cat_with_terminal_obj_has_RezkCompletion
     : disp_left_universal_arrow LUR RR.
   Proof.
-    (*
-      apply weak_equiv_preserves_terminal.
-      apply weak_equiv_preserves_chosen_terminal.
-      apply weak_equiv_preserves_chosen_terminal_eq.
-    *)
     use make_disp_left_universal_arrow_if_contr_CAT_from_weak_equiv.
-    - intro C.
-      admit.
-    - intros C1 C2 C2_univ F Fw.
-      admit.
-    - admit.
-    - admit.
-  Admitted.
+    - exact η_weak_equiv.
+    - intros C1 C2 C2_univ F Fw C1_term.
+      refine (_ ,, tt).
+      use (factor_through_squash _ _ (pr1 C1_term)).
+      { apply isapropishinh. }
+      clear C1_term.
+      intro C1_term.
+      apply hinhpr.
+      set (t := weak_equiv_preserves_chosen_terminal _ Fw C1_term).
+      exact (F C1_term ,, t).
+    - intros C C1_term.
+      refine (tt ,, _).
+      use (factor_through_squash _ _ (pr1 C1_term)).
+      { apply Preservation.isaprop_preserves_terminal. }
+      intro C1_term'.
+      use weak_equiv_preserves_terminal.
+      apply η_weak_equiv.
+    - intros C1 C2 C3 F G H α C1_term C2_term C3_term Gw.
+      simpl.
+      intros [t Fpterm].
+      exists tt.
+      intros x2 x2_is_term.
+
+      set (x1 := eso_from_weak_equiv _ Gw x2).
+      use (factor_through_squash _ _ x1).
+      { apply isaprop_isTerminal. }
+      clear x1.
+      intros [x1 i].
+
+      intro y3.
+      use (iscontrweqb' (Y :=  (pr1 C3⟦y3, H(G x1)⟧))).
+      + use (iscontrweqb' (Y := (pr1 C3⟦y3, F x1⟧))).
+        * use (Fpterm _ _).
+          use (weak_equiv_reflects_terminal _ Gw).
+          exact (iso_to_Terminal (_,, x2_is_term) _ (z_iso_inv i)).
+        * apply z_iso_comp_left_weq.
+          exact (_ ,, pr2 α x1).
+      + apply z_iso_comp_left_weq.
+        apply functor_on_z_iso.
+        exact (z_iso_inv i).
+  Defined.
 
 End BicatOfCategoriesWithTerminalHasRezkCompletion.
--- UniMath/CategoryTheory/WeakEquivalences.v
+++ UniMath/CategoryTheory/WeakEquivalences.v
@@ -8,6 +8,11 @@ Require Import UniMath.CategoryTheory.Core.Univalence.
 Require Import UniMath.CategoryTheory.limits.terminal.
 Require Import UniMath.CategoryTheory.limits.Preservation.
 
+Require Import UniMath.CategoryTheory.FunctorCategory.
+Require Import UniMath.CategoryTheory.catiso.
+Require Import UniMath.CategoryTheory.whiskering.
+Require Import UniMath.CategoryTheory.PrecompEquivalence.
+
 Local Open Scope cat.
 
 Section WeakEquivalences.
@@ -70,22 +75,39 @@ Section WeakEquivalences.
                _ (ff_from_weak_equiv _ Iw)).
   Qed.
 
-  (* Definition two_out_of_three_CAT
-    {∏ }
-
-  Lemma two_out_of_three_weak_equivalence
-    : two_out_of_three is_weak_equiv.
-  Proof.
-    split.
-    - apply two_out_of_three_eso.
-    - apply two_out_of_three_ff.
-  Qed. *)
-
   Definition weak_equiv
     (C D : category) : UU
     := ∑ H : C ⟶ D, is_weak_equiv H.
 
 End WeakEquivalences.
+
+Section WeakEquivalenceInducesIsoOnUnivalentFunctorCategories.
+
+  Context {C D : category}
+    (H : C ⟶ D)
+    (Hw : is_weak_equiv H).
+
+  Definition precomp_is_iso
+    : ∏ E : univalent_category, is_catiso (pre_composition_functor _ _ E H).
+  Proof.
+    intro E.
+    transparent assert (a : (Core.adj_equivalence_of_cats (pre_composition_functor _ _ (pr1 E) H))). {
+      apply precomp_adjoint_equivalence ; apply Hw.
+    }
+
+    use (pr2 (adj_equivalence_of_cats_to_cat_iso a _ _))
+    ; apply is_univalent_functor_category ; apply E.
+  Defined.
+
+  Definition precomp_is_equality
+    : ∏ E : univalent_category,  [D, E] = [C, E].
+  Proof.
+    intro E.
+    apply catiso_to_category_path.
+    exact (_ ,, precomp_is_iso E).
+  Defined.
+
+End WeakEquivalenceInducesIsoOnUnivalentFunctorCategories.
 
 Section WeakEquivalencePreservations.
 
@@ -137,3 +159,17 @@ Section WeakEquivalencePreservations.
   Qed.
 
 End WeakEquivalencePreservations.
+
+Section WeakEquivalenceReflections.
+
+  Lemma weak_equiv_reflects_terminal
+    {C D : category} (F : C ⟶ D)
+    : is_weak_equiv F
+      → ∏ c : C, isTerminal _ (F c) → isTerminal _ c.
+  Proof.
+    intros Fw c Fc_term c'.
+    apply (iscontrweqb' (Fc_term (F c'))).
+    apply ((_ ,, ff_from_weak_equiv _ Fw _ _))%weq.
+  Qed.
+
+End WeakEquivalenceReflections.
