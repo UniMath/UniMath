3a08d3e8247cc1e410e70c0750ef21d52799661b
nmvdw <nnmvdw@gmail.com>
2023-06-25 21:47:50 +0200

    Update exported files

--- UniMath/Combinatorics/DCPOs.v
+++ UniMath/Combinatorics/DCPOs.v
@@ -11,13 +11,13 @@ Require Export UniMath.Combinatorics.DCPOs.Basis.Basis.
 Require Export UniMath.Combinatorics.DCPOs.Basis.CompactBasis.
 Require Export UniMath.Combinatorics.DCPOs.Core.ScottTopology.
 Require Export UniMath.Combinatorics.DCPOs.Core.IntrinsicApartness.
-Require Export UniMath.Combinatorics.DCPOs.LeastFixpoint.
 Require Export UniMath.Combinatorics.DCPOs.Elements.Sharp.
 Require Export UniMath.Combinatorics.DCPOs.Elements.Maximal.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Unit.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Propositions.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Discrete.
 Require Export UniMath.Combinatorics.DCPOs.Examples.SubDCPO.
+Require Export UniMath.Combinatorics.DCPOs.Examples.Fixpoints.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Equalizers.
 Require Export UniMath.Combinatorics.DCPOs.Examples.BinaryProducts.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Products.
@@ -27,3 +27,4 @@ Require Export UniMath.Combinatorics.DCPOs.Examples.IdealCompletion.
 Require Export UniMath.Combinatorics.DCPOs.Core.FubiniTheorem.
 Require Export UniMath.Combinatorics.DCPOs.Core.CoordinateContinuity.
 Require Export UniMath.Combinatorics.DCPOs.Examples.Exponentials.
+Require Export UniMath.Combinatorics.DCPOs.FixpointTheorems.LeastFixpoint.
