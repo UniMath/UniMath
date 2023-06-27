94bbbeed843c13ed07104d51355507196aa4f97d
Tempestas Ludi <tempestasludi@gmail.com>
2023-06-27 09:36:54 +0200

    Add support for coproducts

--- UniMath/AlgebraicTheories/Tactics.v
+++ UniMath/AlgebraicTheories/Tactics.v
@@ -26,11 +26,13 @@ Local Ltac convert_to_hlevel :=
 (* Reduce the different possible constructions *)
 Local Ltac progress_hlevel :=
   match goal with
-  | [ |- isofhlevel _ (_ × _) ] => apply isofhleveldirprod
-  | [ |- isofhlevel _ (_ → _) ] => apply impredfun
-  | [ |- isofhlevel _ (∑ _, _) ] => (apply isofhleveltotal2; [ | intro ])
-  | [ |- isofhlevel _ (∏ _, _) ] => (apply impred; intro)
   | [ |- isofhlevel ?n (_ = _) ] => refine ((_ : isofhlevel (S n) _) _ _)
+  | [ |- isofhlevel 1 (_ ⨿ _) ] => refine (isapropcoprod _ _ (_ : isofhlevel _ _) (_ : isofhlevel _ _) _)
+  | [ |- isofhlevel (S (S _)) (_ ⨿ _) ] => apply isofhlevelssncoprod
+  | [ |- isofhlevel _ (_ × _) ] => apply isofhleveldirprod
+  | [ |- isofhlevel _ (∑ _, _) ] => (apply isofhleveltotal2; [ | intro ])
+  | [ |- isofhlevel _ (_ → _) ] => apply impredfun
+  | [ |- isofhlevel _ (∏ _, _) ] => (apply impred; intro)
   end.
 
 Local Ltac finish_contr :=
@@ -54,11 +56,12 @@ Local Ltac finish :=
   end.
 
 Ltac unfold_hlevel_expression :=
+  refine (_ : isofhlevel _ (_ = _)) ||
+  refine (_ : isofhlevel (S _) (_ ⨿ _)) ||
   refine (_ : isofhlevel _ (_ × _)) ||
-  refine (_ : isofhlevel _ (_ → _)) ||
   refine (_ : isofhlevel _ (∑ _, _)) ||
-  refine (_ : isofhlevel _ (∏ tmp, _)) ||
-  refine (_ : isofhlevel _ (_ = _)).
+  refine (_ : isofhlevel _ (_ → _)) ||
+  refine (_ : isofhlevel _ (∏ tmp, _)).
 
 (* Reduce a goal about hlevels to its components *)
 Ltac prove_hlevel' n :=
