90b2d501a6c7ed53572d6eb27eceeb8ef129a1d5
Kfwullaert <kwullaer@hotmail.com>
2023-07-23 12:07:46 +0200

    Moved transportation lemmas (independent of the symmetry or braiding) into a separate file

--- UniMath/CategoryTheory/Monoidal/Displayed/Symmetric.v
+++ UniMath/CategoryTheory/Monoidal/Displayed/Symmetric.v
@@ -11,20 +11,23 @@ Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.Core.Isos.
 Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
-Require Import UniMath.CategoryTheory.Monoidal.WhiskeredBifunctors.
-Require Import UniMath.CategoryTheory.Monoidal.Categories.
-Require Import UniMath.CategoryTheory.Monoidal.Displayed.WhiskeredDisplayedBifunctors.
+
 Require Import UniMath.CategoryTheory.DisplayedCats.Core.
 Require Import UniMath.CategoryTheory.DisplayedCats.Functors.
 Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
 Require Import UniMath.CategoryTheory.DisplayedCats.Isos.
 Require Import UniMath.CategoryTheory.DisplayedCats.NaturalTransformations.
+Require Import UniMath.CategoryTheory.DisplayedCats.Total.
 
+Require Import UniMath.CategoryTheory.Monoidal.WhiskeredBifunctors.
+Require Import UniMath.CategoryTheory.Monoidal.Categories.
+Require Import UniMath.CategoryTheory.Monoidal.Displayed.WhiskeredDisplayedBifunctors.
 Require Import UniMath.CategoryTheory.Monoidal.Displayed.Monoidal.
+Require Import UniMath.CategoryTheory.Monoidal.Displayed.TotalMonoidal.
+
+Require Import UniMath.CategoryTheory.Monoidal.Displayed.TransportLemmas.
 Require Import UniMath.CategoryTheory.Monoidal.Structure.Symmetric.
 
-Require Import UniMath.CategoryTheory.DisplayedCats.Total.
-Require Import UniMath.CategoryTheory.Monoidal.Displayed.TotalMonoidal.
 
 Local Open Scope cat.
 Local Open Scope mor_disp_scope.
@@ -33,109 +36,6 @@ Import BifunctorNotations.
 Import MonoidalNotations.
 Import DisplayedBifunctorNotations.
 Import DisplayedMonoidalNotations.
-
-Lemma disp_tensor_distributes_over_transportb_right
-  {C : category}
-  {D : disp_cat C}
-  {M : monoidal C}
-  (DM : disp_monoidal D M)
-  {x y z : C}
-  {f g : y --> z}
-  (p : f = g)
-  (xx : D x)
-  {yy : D y}
-  {zz : D z}
-  (ff : yy -->[f] zz)
-  (gg : yy -->[g] zz)
-  : ff = transportb _ p gg
-    -> xx ⊗⊗^{DM}_{l} transportb _ p ff
-    = transportb _ (maponpaths  (leftwhiskering_on_morphisms M x y z) p) (xx ⊗⊗^{DM}_{l} gg).
-Proof.
-  induction p.
-  intro q.
-  cbn.
-  apply maponpaths.
-  exact q.
-Qed.
-
-Lemma disp_tensor_distributes_over_transportb_left
-  {C : category}
-  {D : disp_cat C}
-  {M : monoidal C}
-  (DM : disp_monoidal D M)
-  {x y z : C}
-  {f g : y --> z}
-  (p : f = g)
-  (xx : D x)
-  {yy : D y}
-  {zz : D z}
-  (ff : yy -->[f] zz)
-  (gg : yy -->[g] zz)
-  : ff = transportb _ p gg
-    -> transportb _ p ff ⊗⊗^{DM}_{r} xx
-    = transportb _ (maponpaths  (rightwhiskering_on_morphisms M _ _ _) p) (gg ⊗⊗^{DM}_{r} xx).
-Proof.
-  induction p.
-  intro q.
-  cbn.
-  apply maponpaths.
-  exact q.
-Qed.
-
-Lemma disp_tensor_with_id_left
-  {C : category}
-  {D : disp_cat C}
-  {M : monoidal C}
-  (DM : disp_monoidal D M)
-  {x y z : C}
-  {f : z --> y}
-  {g : y --> z}
-  (p : g · f = identity y)
-  (xx : D x)
-  (yy : D y)
-  : xx ⊗⊗^{DM}_{l} transportb (mor_disp _ _) p (id_disp yy)
-    = transportb _ (maponpaths (λ i, x ⊗^{M}_{l} i) p @ bifunctor_leftid M x y) (id_disp (xx ⊗⊗_{DM} yy)).
-Proof.
-  set (pp := disp_tensor_distributes_over_transportb_right DM p xx (transportb _ p (id_disp yy)) (id_disp yy) (idpath _)).
-
-  rewrite <- transport_b_b.
-  etrans.
-  2: {
-    apply maponpaths.
-    apply (disp_bifunctor_leftid DM _ _ xx yy).
-  }
-  refine (_ @ pp).
-  apply maponpaths.
-  now rewrite transportb_const.
-Qed.
-
-Lemma disp_tensor_with_id_right
-  {C : category}
-  {D : disp_cat C}
-  {M : monoidal C}
-  (DM : disp_monoidal D M)
-  {x y z : C}
-  {f : z --> y}
-  {g : y --> z}
-  (p : g · f = identity y)
-  (xx : D x)
-  (yy : D y)
-  : transportb (mor_disp _ _) p (id_disp yy)  ⊗⊗^{DM}_{r} xx
-    = transportb _ (maponpaths (λ i, i ⊗^{M}_{r} x) p @ bifunctor_rightid M x y) (id_disp (yy ⊗⊗_{DM} _)).
-Proof.
-  Proof.
-  set (pp := disp_tensor_distributes_over_transportb_left DM p xx (transportb _ p (id_disp yy)) (id_disp yy) (idpath _)).
-
-  rewrite <- transport_b_b.
-  etrans.
-  2: {
-    apply maponpaths.
-    apply (disp_bifunctor_rightid DM _ _ _ _).
-  }
-  refine (_ @ pp).
-  apply maponpaths.
-  now rewrite transportb_const.
-Qed.
 
 Definition disp_z_iso_inv_on_left
   {C : category}
@@ -228,59 +128,6 @@ Proof.
   apply pathsinv0, id_right_disp_var.
 Qed.
 
-Lemma disp_tensor_with_id_on_right
-  {C : category} {D : disp_cat C}
-  {M : monoidal C} {DM : disp_monoidal D M}
-  {x y z : C}
-  {f : C⟦x, y⟧}
-  {xx : D x} {yy : D y} {zz : D z}
-  (ff : xx -->[f] yy)
-  : ff ⊗⊗^{ DM} id_disp zz
-    = transportb _ (when_bifunctor_becomes_rightwhiskering M z f) (ff ⊗⊗^{ DM}_{r} zz).
-Proof.
-  unfold dispfunctoronmorphisms1.
-  etrans. {
-    apply maponpaths.
-    apply disp_bifunctor_leftid.
-  }
-  etrans. {
-    apply mor_disp_transportf_prewhisker.
-  }
-  use transportf_transpose_left.
-  rewrite transport_b_b.
-  etrans. {
-    apply id_right_disp.
-  }
-  apply maponpaths_2.
-  apply homset_property.
-Qed.
-
-Lemma disp_tensor_with_id_on_left
-  {C : category} {D : disp_cat C}
-  {M : monoidal C} {DM : disp_monoidal D M}
-  {x y z : C}
-  {f : C⟦x, y⟧}
-  {xx : D x} {yy : D y} {zz : D z}
-  (ff : xx -->[f] yy)
-  : id_disp zz ⊗⊗^{ DM} ff
-    = transportb _ (when_bifunctor_becomes_leftwhiskering M z f) (zz ⊗⊗^{ DM}_{l} ff).
-Proof.
-  unfold dispfunctoronmorphisms1.
-  etrans. {
-    apply maponpaths_2.
-    apply disp_bifunctor_rightid.
-  }
-  etrans. {
-    apply mor_disp_transportf_postwhisker.
-  }
-  use transportf_transpose_left.
-  rewrite transport_b_b.
-  etrans. {
-    apply id_left_disp.
-  }
-  apply maponpaths_2.
-  apply homset_property.
-Qed.
 
 Local Lemma specialized_transport_lemma
   {C : category} {D : disp_cat C}
@@ -820,45 +667,45 @@ Section DisplayedSymmetric.
     : disp_braiding_law_naturality_left DM c.
   Proof.
     intro ; intros.
-      use precomp_disp_id_left_inj.
-      set (q := transportb_transpose_right (p_nat _ _ _ _ _ _ xx xx yy1 yy2 (id_disp xx) gg)).
-      unfold dispfunctoronmorphisms1 in q.
-      set (tq := assoc_disp_var (id_disp xx ⊗⊗^{ DM}_{r} yy1) (xx ⊗⊗^{ DM}_{l} gg) (c x y2 xx yy2)) in q.
-      set (q' := transportb_transpose_right (! tq @ q)).
-      rewrite disp_bifunctor_rightid in q'.
-      unfold transportb in q'.
-      rewrite (mor_disp_transportf_postwhisker _ (id_disp (xx ⊗⊗_{ DM} yy1))) in q'.
-      set (q'' := transportb_transpose_right q').
-      refine (_ @ ! q'').
-      clear q'' q' tq q.
-      unfold transportb.
-      rewrite ! transport_f_f.
-      etrans.
-      2: {
-        apply maponpaths.
-        apply pathsinv0, assoc_disp.
-      }
-      unfold transportb.
-      rewrite transport_f_f.
-      rewrite mor_disp_transportf_prewhisker.
-      use transportf_transpose_right.
-      unfold transportb.
-      rewrite transport_f_f.
-      rewrite id_left_disp.
-      unfold transportb.
-      rewrite transport_f_f.
-      rewrite disp_bifunctor_leftid.
-      unfold transportb.
-      rewrite ! mor_disp_transportf_prewhisker.
-      use transportf_transpose_right.
-      unfold transportb.
-      rewrite transport_f_f.
-      rewrite id_right_disp.
-      use transportf_transpose_right.
-      unfold transportb.
-      rewrite transport_f_f.
-      use transportf_set.
-      apply homset_property.
+    use precomp_disp_id_left_inj.
+    set (q := transportb_transpose_right (p_nat _ _ _ _ _ _ xx xx yy1 yy2 (id_disp xx) gg)).
+    unfold dispfunctoronmorphisms1 in q.
+    set (tq := assoc_disp_var (id_disp xx ⊗⊗^{ DM}_{r} yy1) (xx ⊗⊗^{ DM}_{l} gg) (c x y2 xx yy2)) in q.
+    set (q' := transportb_transpose_right (! tq @ q)).
+    rewrite disp_bifunctor_rightid in q'.
+    unfold transportb in q'.
+    rewrite (mor_disp_transportf_postwhisker _ (id_disp (xx ⊗⊗_{ DM} yy1))) in q'.
+    set (q'' := transportb_transpose_right q').
+    refine (_ @ ! q'').
+    clear q'' q' tq q.
+    unfold transportb.
+    rewrite ! transport_f_f.
+    etrans.
+    2: {
+      apply maponpaths.
+      apply pathsinv0, assoc_disp.
+    }
+    unfold transportb.
+    rewrite transport_f_f.
+    rewrite mor_disp_transportf_prewhisker.
+    use transportf_transpose_right.
+    unfold transportb.
+    rewrite transport_f_f.
+    rewrite id_left_disp.
+    unfold transportb.
+    rewrite transport_f_f.
+    rewrite disp_bifunctor_leftid.
+    unfold transportb.
+    rewrite ! mor_disp_transportf_prewhisker.
+    use transportf_transpose_right.
+    unfold transportb.
+    rewrite transport_f_f.
+    rewrite id_right_disp.
+    use transportf_transpose_right.
+    unfold transportb.
+    rewrite transport_f_f.
+    use transportf_set.
+    apply homset_property.
   Qed.
 
   Lemma make_disp_symm_braiding_nat_right
