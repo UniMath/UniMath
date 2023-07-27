5c97568bfe0fdacc902156875292084982877ce4
Tempestas Ludi <tempestasludi@gmail.com>
2023-07-27 13:55:44 +0200

    Show that the category of algebraic theories has all limits

--- UniMath/AlgebraicTheories/AlgebraicTheoryCategory.v
+++ UniMath/AlgebraicTheories/AlgebraicTheoryCategory.v
@@ -1,4 +1,4 @@
-(* Defines the univalent category of algebraic theories. *)
+(* Defines the univalent category of algebraic theories and shows that it has all limits *)
 
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
@@ -7,14 +7,19 @@ Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.Core.NaturalTransformations.
 Require Import UniMath.CategoryTheory.Core.Univalence.
 Require Import UniMath.CategoryTheory.Core.Isos.
-Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.categories.HSET.Core.
+Require Import UniMath.CategoryTheory.categories.HSET.Limits.
+Require Import UniMath.CategoryTheory.categories.HSET.Univalence.
 Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
-Require Import UniMath.CategoryTheory.DisplayedCats.Total.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
 Require Import UniMath.CategoryTheory.DisplayedCats.Fiber.
+Require Import UniMath.CategoryTheory.DisplayedCats.Limits.
+Require Import UniMath.CategoryTheory.DisplayedCats.Total.
 Require Import UniMath.CategoryTheory.DisplayedCats.Univalence.
 Require Import UniMath.CategoryTheory.FunctorCategory.
-Require Import UniMath.CategoryTheory.categories.HSET.Core.
-Require Import UniMath.CategoryTheory.categories.HSET.Univalence.
+Require Import UniMath.CategoryTheory.limits.products.
+Require Import UniMath.CategoryTheory.limits.graphs.limits.
+Require Import UniMath.CategoryTheory.limits.graphs.colimits.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
 
 Require Import UniMath.AlgebraicTheories.AlgebraicTheories.
@@ -24,10 +29,25 @@ Require Import UniMath.AlgebraicTheories.FiniteSetSkeleton.
 Local Open Scope cat.
 Local Open Scope algebraic_theories.
 
+(* Base category of functors from the finite set skeleton to HSET *)
 Definition base_functor_category
   : category
   := [finite_set_skeleton_category, HSET].
 
+Lemma is_univalent_base_functor_category
+  : is_univalent base_functor_category.
+Proof.
+  apply is_univalent_functor_category.
+  apply is_univalent_HSET.
+Qed.
+
+Definition limits_base_functor_category
+  : Lims base_functor_category.
+Proof.
+  apply LimsFunctorCategory, LimsHSET.
+Defined.
+
+(* The category of functors T, together with a point in T 1 *)
 Definition pointed_functor_disp_cat
   : disp_cat base_functor_category.
 Proof.
@@ -38,46 +58,15 @@ Proof.
     exact ((F : base_nat_trans _ _) _ Tdata = T'data).
   - abstract (intros; apply setproperty).
   - now intros.
-  - intros T T' T'' e e' e'' F F' HF HF'.
-    now rewrite (!HF'), (!HF).
+  - abstract (
+    intros T T' T'' e e' e'' F F' HF HF';
+    now rewrite (!HF'), (!HF)
+  ).
 Defined.
 
-Definition pointed_functor_cat
-  : category
-  := total_category pointed_functor_disp_cat.
-
-Definition algebraic_theory_data_disp_cat
-  : disp_cat pointed_functor_cat.
+Lemma is_univalent_disp_pointed_functor_disp_cat
+  : is_univalent_disp pointed_functor_disp_cat.
 Proof.
-  use disp_struct.
-  - exact (λ (T : pointed_functor), ∏ m n, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet)).
-  - exact (λ T T' Tdata T'data (F : pointed_functor_morphism T T'),
-      ∏ m n f g, (F _ (Tdata m n f g)) = T'data m n (F _ f) (λ i, F _ (g i))).
-  - intros.
-    repeat (apply impred_isaprop; intro).
-    apply setproperty.
-  - now intros.
-  - intros T T' T'' Tdata T'data T''data F F' Fdata F'data m n f g.
-    cbn.
-    now rewrite Fdata, F'data.
-Defined.
-
-Definition algebraic_theory_data_cat
-  : category
-  := total_category algebraic_theory_data_disp_cat.
-
-Definition algebraic_theory_disp_cat
-  : disp_cat algebraic_theory_data_cat
-  := disp_full_sub algebraic_theory_data_cat is_algebraic_theory.
-
-Definition algebraic_theory_cat
-  : category
-  := total_category algebraic_theory_disp_cat.
-
-Lemma is_univalent_pointed_functor_cat
-  : is_univalent pointed_functor_cat.
-Proof.
-  apply (is_univalent_total_category (is_univalent_functor_category _ _ is_univalent_HSET)).
   apply is_univalent_disp_iff_fibers_are_univalent.
   do 3 intro.
   use isweq_iso.
@@ -89,10 +78,105 @@ Proof.
     apply setproperty.
 Qed.
 
-Lemma is_univalent_algebraic_theory_data_cat
-  : is_univalent algebraic_theory_data_cat.
+Section Limits.
+  Context (D := pointed_functor_disp_cat).
+  Context {J : graph}.
+  Context (d : diagram J (total_category D)).
+  Context (L := limits_base_functor_category J (mapdiagram (pr1_category _) d)).
+
+  Definition tip_pointed_functor_disp_cat
+    : D (lim L).
+  Proof.
+    use tpair.
+    - exact (λ i, pr2 (dob d i)).
+    - exact (λ _ _ e, pr2 (dmor d e)).
+  Defined.
+
+  Lemma cone_pointed_functor_disp_cat
+    (j : vertex J)
+    : tip_pointed_functor_disp_cat -->[limOut L j] pr2 (dob d j).
+  Proof.
+    easy.
+  Qed.
+
+  Lemma uniqueness_pointed_functor_disp_cat
+    (d' : D (lim L))
+    (cone_out : ∏ j, d' -->[limOut L j] (pr2 (dob d j)))
+    : d' = tip_pointed_functor_disp_cat.
+  Proof.
+    apply subtypePairEquality.
+    - intro.
+      repeat (apply impred_isaprop; intro).
+      apply setproperty.
+    - apply funextsec.
+      exact cone_out.
+  Qed.
+
+  Lemma is_limit_pointed_functor_disp_cat
+    (d' : total_category D)
+    (cone_out : ∏ u, d' --> (dob d u))
+    (is_cone : ∏ u v e, cone_out u · (dmor d e) = cone_out v)
+    : pr2 d' -->[limArrow L _ (make_cone (d := (mapdiagram (pr1_category D) d)) _ (λ u v e, (maponpaths pr1 (is_cone u v e))))] tip_pointed_functor_disp_cat.
+  Proof.
+    apply subtypePairEquality.
+    - intro.
+      repeat (apply impred_isaprop; intro).
+      apply setproperty.
+    - apply funextsec.
+      intro i.
+      exact (pr2 (cone_out i)).
+  Qed.
+
+End Limits.
+
+Definition creates_limits_pointed_functor_disp_cat
+  {J : graph}
+  (d : diagram J _)
+  : creates_limit pointed_functor_disp_cat d (limits_base_functor_category _ _)
+  := creates_limit_disp_struct _
+    (tip_pointed_functor_disp_cat _)
+    (cone_pointed_functor_disp_cat _)
+    (uniqueness_pointed_functor_disp_cat _)
+    (is_limit_pointed_functor_disp_cat _).
+
+Definition pointed_functor_cat
+  : category
+  := total_category pointed_functor_disp_cat.
+
+Lemma is_univalent_pointed_functor_cat
+  : is_univalent pointed_functor_cat.
 Proof.
-  apply (is_univalent_total_category is_univalent_pointed_functor_cat).
+  apply is_univalent_total_category.
+  - exact is_univalent_base_functor_category.
+  - exact is_univalent_disp_pointed_functor_disp_cat.
+Qed.
+
+Definition limits_pointed_functor_cat
+  : Lims pointed_functor_cat
+  := λ _ _, (total_limit _ (limits_base_functor_category _ _) (creates_limits_pointed_functor_disp_cat _)).
+
+(* The category of algebraic theory data *)
+Definition algebraic_theory_data_disp_cat
+  : disp_cat pointed_functor_cat.
+Proof.
+  use disp_struct.
+  - exact (λ (T : pointed_functor), ∏ m n, (T m : hSet) → (stn m → (T n : hSet)) → (T n : hSet)).
+  - exact (λ T T' Tdata T'data (F : pointed_functor_morphism T T'),
+      ∏ m n f g, (F _ (Tdata m n f g)) = T'data m n (F _ f) (λ i, F _ (g i))).
+  - abstract (
+    intros;
+    repeat (apply impred_isaprop; intro);
+    apply setproperty).
+  - abstract now intros.
+  - abstract (
+    intros T T' T'' Tdata T'data T''data F F' Fdata F'data m n f g;
+    cbn;
+    now rewrite Fdata, F'data).
+Defined.
+
+Lemma is_univalent_disp_algebraic_theory_data_disp_cat
+  : is_univalent_disp algebraic_theory_data_disp_cat.
+Proof.
   apply is_univalent_disp_iff_fibers_are_univalent.
   intros T comp comp'.
   use isweq_iso.
@@ -109,13 +193,141 @@ Proof.
     apply setproperty.
 Qed.
 
-Lemma is_univalent_algebraic_theory_cat
-  : is_univalent algebraic_theory_cat.
+Section Limits.
+  Context (D := algebraic_theory_data_disp_cat).
+  Context {J : graph}.
+  Context (d : diagram J (total_category D)).
+  Context (L := limits_pointed_functor_cat J (mapdiagram (pr1_category _) d)).
+
+  Definition tip_algebraic_theory_data_disp_cat
+    : D (lim L).
+  Proof.
+    intros m n f g.
+    use tpair.
+    - exact (λ i, (pr1 f) i • (λ j, pr1 (g j) i)).
+    - abstract (
+        refine (λ u v e, pr2 (dmor d e) _ _ _ _ @ _);
+        refine (maponpaths (λ x, x • _) _ @ maponpaths _ _);
+        [ exact (pr2 f u v e)
+        | apply funextfun;
+          intro;
+          exact (pr2 (g _) u v e) ]
+      ).
+  Defined.
+
+  Lemma cone_algebraic_theory_data_disp_cat
+    (j : vertex J)
+    : tip_algebraic_theory_data_disp_cat -->[limOut L j] pr2 (dob d j).
+  Proof.
+    easy.
+  Qed.
+
+  Lemma uniqueness_algebraic_theory_data_disp_cat
+    (d' : D (lim L))
+    (cone_out : ∏ j, d' -->[limOut L j] (pr2 (dob d j)))
+    : d' = tip_algebraic_theory_data_disp_cat.
+  Proof.
+    do 4 (apply funextsec; intro).
+    apply subtypePairEquality.
+    - intro.
+      repeat (apply impred_isaprop; intro).
+      apply setproperty.
+    - apply funextsec.
+      intro.
+      exact (cone_out _ _ _ _ _).
+  Qed.
+
+  Lemma is_limit_algebraic_theory_data_disp_cat
+    (d' : total_category D)
+    (cone_out : ∏ u, d' --> (dob d u))
+    (is_cone : ∏ u v e, cone_out u · (dmor d e) = cone_out v)
+    : pr2 d' -->[limArrow L _ (make_cone (d := (mapdiagram (pr1_category D) d)) _ (λ u v e, (maponpaths pr1 (is_cone u v e))))] tip_algebraic_theory_data_disp_cat.
+  Proof.
+    intros m n f g.
+    apply subtypePairEquality.
+    - intro.
+      repeat (apply impred_isaprop; intro).
+      exact (setproperty _ _ _).
+    - apply funextsec.
+      intro i.
+      exact (pr2 (cone_out i) m n f g).
+  Qed.
+
+End Limits.
+
+Definition creates_limits_algebraic_theory_data_disp_cat
+  {J : graph}
+  (d : diagram J _)
+  : creates_limit algebraic_theory_data_disp_cat d (limits_pointed_functor_cat _ _)
+  := creates_limit_disp_struct _
+    (tip_algebraic_theory_data_disp_cat _)
+    (cone_algebraic_theory_data_disp_cat _)
+    (uniqueness_algebraic_theory_data_disp_cat _)
+    (is_limit_algebraic_theory_data_disp_cat _).
+
+Definition algebraic_theory_data_cat
+  : category
+  := total_category algebraic_theory_data_disp_cat.
+
+Lemma is_univalent_algebraic_theory_data_cat
+  : is_univalent algebraic_theory_data_cat.
 Proof.
-  apply (is_univalent_total_category is_univalent_algebraic_theory_data_cat).
+  apply is_univalent_total_category.
+  - exact is_univalent_pointed_functor_cat.
+  - exact is_univalent_disp_algebraic_theory_data_disp_cat.
+Qed.
+
+Definition limits_algebraic_theory_data_cat
+  : Lims algebraic_theory_data_cat
+  := λ _ _, (total_limit _ (limits_pointed_functor_cat _ _) (creates_limits_algebraic_theory_data_disp_cat _)).
+
+(* The category of algebraic theories *)
+Definition algebraic_theory_disp_cat
+  : disp_cat algebraic_theory_data_cat
+  := disp_full_sub algebraic_theory_data_cat is_algebraic_theory.
+
+Lemma is_univalent_disp_algebraic_theory_disp_cat
+  : is_univalent_disp algebraic_theory_disp_cat.
+Proof.
   apply disp_full_sub_univalent.
   exact isaprop_is_algebraic_theory.
 Qed.
+
+Definition creates_limits_algebraic_theory_disp_cat
+  {J : graph}
+  (d : diagram J _)
+  : creates_limit algebraic_theory_disp_cat d (limits_algebraic_theory_data_cat _ _).
+Proof.
+  apply creates_limit_disp_full_sub.
+  - apply isaprop_is_algebraic_theory.
+  - use make_is_algebraic_theory;
+      repeat intro;
+      (use total2_paths_f;
+      [ apply funextsec;
+        intro
+      | do 3 (apply impred_isaprop; intro);
+        apply setproperty ]).
+    + apply algebraic_theory_comp_is_assoc.
+    + apply (algebraic_theory_comp_is_unital _ _ (pr1 f _)).
+    + apply algebraic_theory_comp_identity_projections.
+    + apply algebraic_theory_comp_is_natural_l.
+Defined.
+
+Definition algebraic_theory_cat
+  : category
+  := total_category algebraic_theory_disp_cat.
+
+Lemma is_univalent_algebraic_theory_cat
+  : is_univalent algebraic_theory_cat.
+Proof.
+  apply is_univalent_total_category.
+  - exact is_univalent_algebraic_theory_data_cat.
+  - exact is_univalent_disp_algebraic_theory_disp_cat.
+Qed.
+
+Definition products_algebraic_theory_cat
+  : Lims algebraic_theory_cat
+  := λ _ _, total_limit _ (limits_algebraic_theory_data_cat _ _) (creates_limits_algebraic_theory_disp_cat _).
 
 Definition algebraic_theory_univalent_category
   : univalent_category
--- UniMath/CategoryTheory/DisplayedCats/Limits.v
+++ UniMath/CategoryTheory/DisplayedCats/Limits.v
@@ -3,14 +3,19 @@ Limits
 *)
 
 Require Import UniMath.Foundations.Sets.
+Require Import UniMath.MoreFoundations.PartA.
+Require Import UniMath.MoreFoundations.Notations.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Isos.
 Require Import UniMath.CategoryTheory.Core.Functors.
 Require Import UniMath.CategoryTheory.limits.graphs.colimits.
 Require Import UniMath.CategoryTheory.limits.graphs.limits.
+Require Import UniMath.CategoryTheory.limits.products.
+Require Import UniMath.CategoryTheory.limits.equalizers.
 Local Open Scope cat.
 
 Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.DisplayedCats.Fiber.
 Require Import UniMath.CategoryTheory.DisplayedCats.Total.
 Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
 Require Import UniMath.CategoryTheory.DisplayedCats.Fibrations.
@@ -24,48 +29,160 @@ Section Creates_Limits.
 Definition creates_limit
   {C : category}
   (D : disp_cat C)
-  {J : graph} (F : diagram J (total_category D))
-  {x : C} (L : cone (mapdiagram (pr1_category D) F)  x)
-  (isL : isLimCone _ x L) : UU
+  {J : graph}
+  (F : diagram J (total_category D))
+  (L : LimCone (mapdiagram (pr1_category D) F)) : UU
 :=
-  ∑ (CC : iscontr
-      ( ∑ (d : D x)
-          (δ : ∏ j : vertex J, d -->[coneOut L j] (pr2 (dob F j))),
-          forms_cone(c:=(x,,d)) F (λ j, (coneOut L j ,, δ j))))
-  , isLimCone _ _ (make_cone _ (pr2 (pr2 (iscontrpr1 CC)))).
+  ∑ (CC :
+      ( ∃! (d : D (pr11 L))
+          (δ : ∏ j : vertex J, d -->[limOut L j] (pr2 (dob F j))),
+          forms_cone(c:=((pr11 L),,d)) F (λ j, (limOut L j ,, δ j))))
+  , isLimCone _ _ (make_cone _ (pr2 (pr2 (pr1 CC)))).
 
-Definition creates_limits {C : category} (D : disp_cat C) : UU
-:=
-  ∏ (J : graph) (F : diagram J (total_category D))
-    (x : C) (L : cone (mapdiagram (pr1_category D) F)  x)
-    (isL : isLimCone _ x L),
-  creates_limit _ _ _ isL.
+Definition creates_limit_disp_struct
+  {C : category}
+  {P H Hprop Hid Hcomp}
+  (D := disp_struct C P H Hprop Hid Hcomp)
+  {J : graph}
+  {F : diagram J (total_category D)}
+  (L : LimCone (mapdiagram (pr1_category D) F))
+  (d : D (pr11 L))
+  (cone : ∏ j : vertex J, d -->[limOut L j] (pr2 (dob F j)))
+  (d_is_unique : ∏ d', (∏ j : vertex J, d' -->[limOut L j] (pr2 (dob F j))) → d' = d)
+  (is_limit : ∏ (d' : total_category D) (cone_out : ∏ u, d' --> (dob F u)) (is_cone : ∏ u v e, cone_out u · (dmor F e) = cone_out v), pr2 d' -->[limArrow L _ (make_cone (d := (mapdiagram (pr1_category D) F)) _ (λ u v e, (maponpaths pr1 (is_cone u v e))))] d)
+  : creates_limit D F L.
+Proof.
+  use (((d ,, (cone ,, _)) ,, _) ,, (λ d' cone', (_ ,, _) ,, _)).
+  - abstract (
+      do 3 intro;
+      apply (subtypePairEquality (λ _, Hprop _ _ _ _ _));
+      apply (limOutCommutes L)
+    ).
+  - abstract (
+      intro t;
+      apply subtypePairEquality;
+      [ intro;
+        use (isaprop_total2 (_ ,, _) (λ _, _ ,, _));
+          repeat (apply impred_isaprop; intro);
+        [ apply Hprop
+        | apply (homset_property (total_category _)) ]
+      | apply d_is_unique;
+        exact (pr12 t) ]
+    ).
+  - exact (_ ,, is_limit d' (pr1 cone') (pr2 cone')).
+  - abstract (
+      intro u;
+      apply (subtypePairEquality (λ _, Hprop _ _ _ _ _));
+      exact (limArrowCommutes L _ _ _)
+    ).
+  - abstract (
+      intro t;
+      repeat apply subtypePairEquality;
+      [ intro;
+        apply impred_isaprop;
+        intro;
+        apply (homset_property (total_category _))
+      | simpl;
+        intro;
+        apply Hprop
+      | apply (limArrowUnique L);
+        intro;
+        exact (maponpaths pr1 (pr2 t u)) ]
+    ).
+Defined.
+
+Definition creates_limit_disp_full_sub
+  {C : category}
+  {P : C → UU}
+  (D := disp_full_sub C P)
+  (H : ∏ c, isaprop (P c))
+  {J : graph}
+  {F : diagram J (total_category D)}
+  (L : LimCone (mapdiagram (pr1_category D) F))
+  (d : P (pr11 L))
+  : creates_limit D F L.
+Proof.
+  use (((d ,, (_ ,, _)) ,, _) ,, (λ d' cone', (_ ,, _) ,, _));
+    simpl.
+  - abstract exact (λ _, tt).
+  - abstract (
+      do 3 intro;
+      apply subtypePairEquality';
+      [ apply (limOutCommutes L)
+      | apply isapropunit ]
+    ).
+  - abstract (
+      intro;
+      apply subtypePairEquality';
+      [ apply H
+      | use (isaprop_total2 (_ ,, _) (λ _, _ ,, _));
+        [ apply impred_isaprop;
+          intro;
+          apply isapropunit
+        | do 3 (apply impred_isaprop; intro);
+          simpl;
+          apply isaset_dirprod;
+          [ apply homset_property
+          | apply isasetunit] ] ]
+    ).
+  - use (_ ,, tt).
+    apply limArrow.
+    exact (mapcone (pr1_category D) F cone').
+  - abstract (
+      intro;
+      apply subtypePairEquality';
+      [ apply (limArrowCommutes L)
+      | apply isapropunit ]
+    ).
+  - abstract (
+      intro;
+      repeat apply subtypePairEquality';
+      [ apply limArrowUnique;
+        intro;
+        exact (maponpaths pr1 (pr2 t u))
+      | abstract apply isapropunit
+      | apply impred_isaprop;
+        intro;
+        apply (homset_property (total_category _)) ]
+    ).
+Defined.
+
+Definition creates_limits {C : category} (D : disp_cat C) : UU := ∏
+  (J : graph)
+  (F : diagram J (total_category D))
+  (L : LimCone (mapdiagram (pr1_category D) F) ),
+  creates_limit _ _ L.
 
 End Creates_Limits.
+
+Definition total_limit
+  {C : category}
+  (D : disp_cat C)
+  {J : graph}
+  {F : diagram J (total_category D)}
+  (L : LimCone (mapdiagram (pr1_category D) F))
+  (H : creates_limit _ _ L)
+  : LimCone F
+  := make_LimCone _ _ _ (pr2 H).
+
+Definition total_limits
+  {C : category}
+  (D : disp_cat C)
+  (J : graph)
+  (H : creates_limits D)
+  (X : Lims_of_shape J C)
+  : Lims_of_shape J (total_category D)
+  := λ d, total_limit _ (X _) (H _ _ _).
 
 Section creates_preserves.
 
 Context {C : category}
         (D : disp_cat C)
-        (H : creates_limits D)
         (J : graph)
+        (H : creates_limits D)
         (X : Lims_of_shape J C).
 
 Notation π := (pr1_category D).
-
-Definition total_limits : Lims_of_shape J (total_category D).
-Proof.
-  intro d.
-  set (πd := mapdiagram π d).
-  set (LL := X πd).
-  set (L := pr1 LL).
-  set (c := pr1 L).
-  set (isL := pr2 LL). cbn in isL.
-  set (XR := H _ d _ _ isL).
-  unfold creates_limit in XR.
-  cbn.
-  use (make_LimCone _ _ _ (pr2 XR)).
-Defined.
 
 Lemma pr1_preserves_limit (d : diagram J (total_category D))
   (x : total_category D) (CC : cone d x) : preserves_limit π _ x CC.
@@ -76,8 +193,8 @@ Proof.
   - apply X.
   - match goal with |[ |- is_z_isomorphism ?foo ] => set (T:= foo) end.
     destruct X as [[a L] isL]. cbn in isL.
+    set (tL := H _ _ (make_LimCone _ _ _ isL)).
     clear XR.
-    set (tL := H _ _ _ _ isL).
     unfold creates_limit in tL.
     set (RR := pr1 tL).
     set (RT1 := pr2 tL).
