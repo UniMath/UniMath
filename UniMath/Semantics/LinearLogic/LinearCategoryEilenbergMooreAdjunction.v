23f93c1252812ce7b0e0193f0fa0a8ff8ac3cba2
Kfwullaert <kwullaer@hotmail.com>
2023-08-25 14:09:08 +0200

    Forgot to push a file

---
+++ UniMath/Semantics/LinearLogic/LinearCategoryEilenbergMooreAdjunction.v
@@ -0,0 +1,136 @@
+(*
+In this file, we show how any linear category induces a linear non linear model.
+This boils down to proving how the eilenberg moore category of the (symmetric monoidal) comonad, given by a linear category, is cartesian monoidal.
+*)
+
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Core.Prelude.
+Require Import UniMath.CategoryTheory.Adjunctions.Core.
+Require Import UniMath.CategoryTheory.whiskering.
+Require Import UniMath.CategoryTheory.Monads.Comonads.
+Require Import UniMath.CategoryTheory.Monoidal.WhiskeredBifunctors.
+Require Import UniMath.CategoryTheory.Monoidal.Categories.
+Require Import UniMath.CategoryTheory.Monoidal.Functors.
+Require Import UniMath.CategoryTheory.Monoidal.Adjunctions.
+Require Import UniMath.CategoryTheory.Monoidal.FunctorCategories.
+Require Import UniMath.CategoryTheory.Monoidal.Structure.Cartesian.
+Require Import UniMath.CategoryTheory.Monoidal.Structure.Symmetric.
+Require Import UniMath.CategoryTheory.Monoidal.Structure.Closed.
+Require Import UniMath.CategoryTheory.Monoidal.Structure.SymmetricDiagonal.
+
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.DisplayedCats.Total.
+Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
+Require Import UniMath.CategoryTheory.categories.Dialgebras.
+
+Require Import UniMath.CategoryTheory.Monoidal.Displayed.WhiskeredDisplayedBifunctors.
+Require Import UniMath.CategoryTheory.Monoidal.Displayed.Monoidal.
+
+Require Import UniMath.CategoryTheory.categories.CoEilenbergMoore.
+Require Import UniMath.CategoryTheory.Monoidal.Examples.MonoidalDialgebras.
+Require Import UniMath.CategoryTheory.Monoidal.Examples.SymmetricMonoidalCoEilenbergMoore.
+
+Require Import UniMath.Semantics.LinearLogic.LinearCategory.
+
+Import MonoidalNotations.
+
+Local Open Scope cat.
+Local Open Scope moncat.
+
+Section CofreeAdjunction.
+
+  Context (L : linear_category).
+
+  Definition eilenberg_moore_cofree
+    :  pr1 L ⟶ co_eilenberg_moore_cat (linear_category_bang L).
+  Proof.
+    use functor_to_co_eilenberg_moore_cat.
+    - apply (linear_category_bang L).
+    - use nat_trans_comp.
+      2: { apply nat_trans_functor_id_left. }
+      exact (δ (linear_category_bang L)).
+    - abstract (
+          intro x;
+          refine (_ @ Comonad_law1 (T := linear_category_bang L) x);
+          refine (assoc' _ _ _ @ _);
+          apply id_left).
+    - abstract (
+          intro x;
+          cbn;
+          rewrite id_left;
+          exact (Comonad_law3 (T := linear_category_bang L) x)).
+  Defined.
+
+  (* Lemma test_ob (x : L)
+    : pr11 (eilenberg_moore_cofree x) = linear_category_bang L x.
+  Proof.
+    apply idpath.
+  Qed.
+
+  Lemma test_mor (x : L)
+    : pr21 (eilenberg_moore_cofree x) =  δ (linear_category_bang L) x.
+  Proof.
+    apply id_left.
+  Qed. *)
+
+  Local Definition eilenberg_moore_forget
+    : full_subcat (dialgebra (functor_identity (pr1 L)) (linear_category_bang L))
+        mon_cat_co_eilenberg_moore_extra_condition ⟶ L.
+    (* : co_eilenberg_moore_cat (linear_category_bang L) ⟶ L. *)
+  Proof.
+    exact (functor_composite (pr1_category _) (pr1_category _)).
+  Defined.
+
+  Local Definition eilenberg_moore_adj_unit
+    : functor_identity
+        (full_subcat (dialgebra (functor_identity (pr1 L)) (linear_category_bang L))
+           mon_cat_co_eilenberg_moore_extra_condition) ⟹
+        eilenberg_moore_forget ∙ eilenberg_moore_cofree.
+  Proof.
+    use make_nat_trans.
+    - intro x.
+      use make_mor_co_eilenberg_moore.
+      + exact (pr21 x).
+      + abstract (
+            refine (! pr22 x @ _);
+            apply maponpaths,
+              pathsinv0,
+              id_left).
+    - abstract (
+          intros x y f;
+          use eq_mor_co_eilenberg_moore;
+          exact (! pr21 f)).
+  Defined.
+
+  Lemma eilenberg_moore_cmd_form_adj
+    :  form_adjunction'
+         (eilenberg_moore_forget,,
+            eilenberg_moore_cofree,,
+            eilenberg_moore_adj_unit,,
+            ε (linear_category_bang L)).
+  Proof.
+    split.
+    - exact (λ x, pr12 x).
+    - intro x.
+      use eq_mor_co_eilenberg_moore.
+      cbn.
+      refine (assoc' _ _ _ @ _).
+      refine (id_left _ @ _).
+      exact (Comonad_law2 (T := linear_category_bang L) x).
+  Qed.
+
+  Definition eilenberg_moore_cmd_adj
+    : adjunction
+    (full_subcat (dialgebra (functor_identity (pr1 L)) (linear_category_bang L))
+       mon_cat_co_eilenberg_moore_extra_condition) L.
+  Proof.
+    use make_adjunction.
+    - simple refine (_ ,, _ ,, _ ,, _).
+      + exact eilenberg_moore_forget.
+      + exact eilenberg_moore_cofree.
+      + exact eilenberg_moore_adj_unit.
+      + exact (ε (linear_category_bang L)).
+    - exact eilenberg_moore_cmd_form_adj.
+  Defined.
+
+End CofreeAdjunction.
