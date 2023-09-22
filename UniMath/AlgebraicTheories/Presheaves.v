c1eae5ac2b129f9e83208ce68fa97eb73eb3eee3
Tempestas Ludi <tempestasludi@gmail.com>
2023-09-22 15:46:22 +0200

    Add an alternative definition of defining presheaves

--- UniMath/AlgebraicTheories/Presheaves.v
+++ UniMath/AlgebraicTheories/Presheaves.v
@@ -16,7 +16,7 @@ Local Open Scope algebraic_theories.
 
 (* The presheaf data type *)
 Definition presheaf_data (T : algebraic_theory_data) : UU
-  := ∑ P: (finite_set_skeleton_category ⟶ HSET),
+  := ∑ P : (finite_set_skeleton_category ⟶ HSET),
     ∏ m n, (P m : hSet) → (stn m → (T n : hSet)) → (P n : hSet).
 
 Coercion presheaf_data_to_functor {T : algebraic_theory_data} (P : presheaf_data T)
@@ -98,12 +98,30 @@ Qed.
 Definition presheaf_morphism {T : algebraic_theory_data} (P P' : presheaf_data T) : UU
   := ∑ (F: P ⟹ P') (HF: ∏ m n a f, F n (action a f) = action (F m a) f), unit.
 
-Definition presheaf_morphism_to_nat_trans
-  {T : algebraic_theory}
-  {P P' : presheaf T}
+Coercion presheaf_morphism_to_nat_trans
+  {T : algebraic_theory_data}
+  {P P' : presheaf_data T}
   (f : presheaf_morphism P P')
   : nat_trans P P'
   := pr1 f.
+
+Lemma presheaf_morphism_eq
+  {T : algebraic_theory_data}
+  {P P' : presheaf_data T}
+  (F F' : presheaf_morphism P P')
+  (H : ∏ n, F n = F' n)
+  : F = F'.
+Proof.
+  apply subtypePairEquality.
+  - intro.
+    use isapropdirprod.
+    + do 4 (apply impred_isaprop; intro).
+      apply setproperty.
+    + exact isapropunit.
+  - apply nat_trans_eq.
+    + apply homset_property.
+    + exact H.
+Qed.
 
 Definition theory_presheaf (T : algebraic_theory)
   : presheaf T.
@@ -118,6 +136,119 @@ Proof.
     + exact (algebraic_theory_comp_identity_projections T).
     + exact (algebraic_theory_comp_is_natural_l T).
 Defined.
+
+Definition presheaf_data' (T : algebraic_theory_data) : UU
+  := ∑ (P : nat → hSet),
+    ∏ m n, P m → (stn m → (T n : hSet)) → P n.
+
+Definition presheaf_data'_to_function
+  {T : algebraic_theory_data}
+  (P : presheaf_data' T)
+  : nat → hSet
+  := pr1 P.
+
+Coercion presheaf_data'_to_function : presheaf_data' >-> Funclass.
+
+Definition action'
+  {T : algebraic_theory_data}
+  {P : presheaf_data' T}
+  {m n : nat}
+  (f : P m)
+  (g : stn m → (T n : hSet))
+  : P n
+  := pr2 P m n f g.
+
+Definition make_presheaf_data'
+  {T : algebraic_theory_data}
+  (P : nat → hSet)
+  (action : ∏ m n, P m → (stn m → (T n : hSet)) → P n)
+  : presheaf_data' T
+  := P ,, action.
+
+Definition is_assoc' {T : algebraic_theory_data} (P : presheaf_data' T) : UU
+  := ∏ l m n (a : P l) f (g : stn m → (T n : hSet)), action' (action' a f) g = action' a (λ i, (f i) • g).
+
+Definition identity_projections' {T : algebraic_theory_data} (P : presheaf_data' T) : UU
+  := ∏ n (a : P n), action' a pr = a.
+
+Definition is_presheaf' {T : algebraic_theory_data}
+  (P : presheaf_data' T)
+  := is_assoc' P ×
+    identity_projections' P.
+
+Definition make_is_presheaf'
+  {T : algebraic_theory_data}
+  {P : presheaf_data' T}
+  (H1 : is_assoc' P)
+  (H2 : identity_projections' P)
+  : is_presheaf' P
+  := H1 ,, H2.
+
+Section MakePresheaf'.
+
+  Context {T : algebraic_theory}.
+  Context (P : presheaf_data' T).
+  Context (H : is_presheaf' P).
+
+  Definition presheaf'_to_functor_data
+    : functor_data finite_set_skeleton_category HSET.
+  Proof.
+    use make_functor_data.
+    - exact P.
+    - intros n n' a f.
+      exact (action' f (λ i, pr (a i))).
+  Defined.
+
+  Lemma presheaf'_to_is_functor
+    : is_functor presheaf'_to_functor_data.
+  Proof.
+    use tpair.
+    - intro n.
+      use funextfun.
+      intro t.
+      apply (pr2 H).
+    - intros l m n f g.
+      use funextfun.
+      intro h.
+      refine (_ @ !pr1 H _ _ _ _ _ _).
+      simpl.
+      apply maponpaths.
+      apply funextfun.
+      intro i.
+      symmetry.
+      apply algebraic_theory_comp_projects_component.
+  Qed.
+
+  Definition presheaf'_to_presheaf_data
+    : presheaf_data T.
+  Proof.
+    use make_presheaf_data.
+    - use make_functor.
+      + exact presheaf'_to_functor_data.
+      + exact presheaf'_to_is_functor.
+    - exact (pr2 P).
+  Defined.
+
+  Lemma presheaf'_to_is_presheaf
+    : is_presheaf presheaf'_to_presheaf_data.
+  Proof.
+    use make_is_presheaf.
+    - exact (pr1 H).
+    - exact (pr2 H).
+    - intros n n' n'' a f g.
+      simpl.
+      rewrite (pr1 H).
+      apply maponpaths.
+      apply funextfun.
+      intro.
+      apply algebraic_theory_comp_projects_component.
+  Qed.
+
+  Definition make_presheaf'
+    : presheaf T
+    := make_presheaf _ presheaf'_to_is_presheaf.
+
+End MakePresheaf'.
 
 (* Definition yoneda_lemma
   (T : algebraic_theory)
