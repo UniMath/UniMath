aed1e3a636a9722bd9dd672d27bdc59ce890da37
Niels van der Weide <nnmvdw@gmail.com>
2023-10-21 19:39:54 +0200

    Orthogonal factorization systems in bicategories (#1794)
    This PR cleans the file on eso 1-cells in bicategories. It does so by:
    - defining the notion of orthogonality for 1-cells of bicategory (this
    means that certain lifting properties are satisfied)
    - refactor the eso file by basing it on orthogonality
    - defining the notion of orthogonal factorization systems in
    bicategories
    - constructing the (eso, ff) factorization system for 1-types and for
    univalent categories
    
    I am planning to also construct an orthogonal factorization system for
    enriched categories (using eso and fully faithful functors), but that's
    for a separate PR. From this factorization system, we directly get that
    functors that are essentially surjective and fully faithful, also are
    adjoint equivalences.

--- UniMath/Bicategories/Morphisms/Examples/EsosInBicatOfUnivCats.v
+++ UniMath/Bicategories/Morphisms/Examples/EsosInBicatOfUnivCats.v
@@ -651,12 +651,12 @@ Definition eso_ff_factorization_bicat_of_univ_cats
   : eso_ff_factorization bicat_of_univ_cats.
 Proof.
   intros C₁ C₂ F.
-  refine (univalent_image F ,, sub_precategory_inclusion _ _ ,, functor_full_img _ ,, _).
+  refine (univalent_image F ,, functor_full_img _ ,, sub_precategory_inclusion _ _ ,, _).
   simple refine (_ ,, _ ,, _).
-  - use cat_fully_faithful_is_fully_faithful_1cell.
-    apply fully_faithful_sub_precategory_inclusion.
   - use essentially_surjective_is_eso.
     apply functor_full_img_essentially_surjective.
+  - use cat_fully_faithful_is_fully_faithful_1cell.
+    apply fully_faithful_sub_precategory_inclusion.
   - use nat_z_iso_to_invertible_2cell.
     exact (full_image_inclusion_commute_nat_iso F).
 Defined.
