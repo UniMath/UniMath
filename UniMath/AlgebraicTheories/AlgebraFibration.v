bbeb3bc2b9c981479b78c23b1ae17667183c56cf
Tempestas Ludi <tempestasludi@gmail.com>
2023-10-24 08:33:41 +0200

    Resolve review of AlgebraFibration

--- UniMath/AlgebraicTheories/AlgebraFibration.v
+++ UniMath/AlgebraicTheories/AlgebraFibration.v
@@ -6,7 +6,7 @@
   over algebraic theories.
 
   Contents
-  1. A cleaving [algebra_cleaving]
+  1. The cleaving [algebra_cleaving]
   2. The fibration [algebra_fibration]
 
  **************************************************************************************************)
@@ -30,112 +30,96 @@ Require Import UniMath.AlgebraicTheories.Algebras.
 Local Open Scope cat.
 Local Open Scope mor_disp.
 
-(** * 1. A cleaving *)
+(** * 1. The cleaving *)
 
-Definition algebra_cleaving_algebra_data
-  {T T' : algebraic_theory}
-  (F : algebraic_theory_morphism T' T)
-  (A : algebra T)
-  : algebra_data T'.
-Proof.
-  use tpair.
-  - exact A.
-  - intros n f a.
-    exact (action (F _ f) a).
-Defined.
+Section Cleaving.
 
-Lemma algebra_cleaving_is_algebra
-  {T T' : algebraic_theory}
-  (F : algebraic_theory_morphism T' T)
-  (A : algebra T)
-  : is_algebra (algebra_cleaving_algebra_data F A).
-Proof.
-  repeat split.
-  - do 5 intro.
-    simpl.
-    rewrite (algebraic_theory_morphism_preserves_composition F).
-    apply algebra_is_assoc.
-  - intro.
-    rewrite <- algebra_is_unital.
-    simpl.
-    apply (maponpaths (λ i, _ _ i _)).
-    apply algebraic_theory_morphism_preserves_id_pr.
-  - do 5 intro.
-    simpl.
-    rewrite (maponpaths (λ x, x _) (nat_trans_ax (F : algebraic_theory_morphism _ _) _ _ _
-      : (λ x, _ _ (# (T' : algebraic_theory) _ _)) = _)).
-    apply algebra_is_natural.
-Qed.
+  Context {T T' : algebraic_theory}.
+  Context (F : algebraic_theory_morphism T' T).
+  Context (A : algebra T).
 
-Definition algebra_cleaving_algebra
-  {T T' : algebraic_theory}
-  (F : algebraic_theory_morphism T' T)
-  (A : algebra T)
-  : algebra T'
-  := make_algebra _ (algebra_cleaving_is_algebra F A).
+  Definition algebra_cleaving_algebra_data
+    : algebra_data T'.
+  Proof.
+    use tpair.
+    - exact A.
+    - intros n f a.
+      exact (action (F _ f) a).
+  Defined.
 
-Definition algebra_cleaving_morphism
-  {T T' : algebraic_theory}
-  (F : algebraic_theory_morphism T' T)
-  (A : algebra T)
-  : (algebra_cleaving_algebra F A : algebra_disp_cat _) -->[F] A.
-Proof.
-  use (idfun _ ,, _ ,, tt).
-  abstract now do 3 intro.
-Defined.
+  Lemma algebra_cleaving_is_algebra
+    : is_algebra algebra_cleaving_algebra_data.
+  Proof.
+    repeat split.
+    - do 5 intro.
+      simpl.
+      rewrite (algebraic_theory_morphism_preserves_composition F).
+      apply algebra_is_assoc.
+    - intro.
+      rewrite <- algebra_is_unital.
+      simpl.
+      apply (maponpaths (λ i, _ _ i _)).
+      apply algebraic_theory_morphism_preserves_id_pr.
+    - do 5 intro.
+      simpl.
+      rewrite (maponpaths (λ x, x _) (nat_trans_ax (F : algebraic_theory_morphism _ _) _ _ _
+        : (λ x, _ _ (# (T' : algebraic_theory) _ _)) = _)).
+      apply algebra_is_natural.
+  Qed.
 
-Definition algebra_cleaving_induced_morphism
-  {T T' T'' : algebraic_theory}
-  {A : algebra T}
-  {A'' : algebra T''}
-  (F : algebraic_theory_morphism T' T)
-  (F' : algebraic_theory_morphism T'' T')
-  (G' : (A'' : algebra_disp_cat _) -->[(F' : algebraic_theory_cat⟦_, _⟧) · F] A)
-  : (A'' : algebra_disp_cat _) -->[F'] algebra_cleaving_algebra F A.
-Proof.
-  use (pr1 G' ,, _ ,, tt).
-  abstract (do 3 intro; now rewrite (pr12 G')).
-Defined.
+  Definition algebra_cleaving_algebra
+    : algebra T'
+    := make_algebra _ algebra_cleaving_is_algebra.
 
-Definition algebra_lift
-  {T T' T'' : algebraic_theory}
-  {A : algebra T}
-  {A'' : algebra T''}
-  (F : algebraic_theory_morphism T' T)
-  (F' : algebraic_theory_morphism T'' T')
-  (G' : (A'' : algebra_disp_cat _) -->[(F' : algebraic_theory_cat⟦_, _⟧) · F] A)
-  : ∑ gg, (gg ;; algebra_cleaving_morphism F A) = G'.
-Proof.
-  exists (algebra_cleaving_induced_morphism F F' G').
-  apply displayed_algebra_morphism_eq.
-  exact (comp_disp_cartesian _ _ _ _).
-Defined.
+  Definition algebra_cleaving_morphism
+    : (algebra_cleaving_algebra : algebra_disp_cat _) -->[F] A.
+  Proof.
+    use (idfun _ ,, _ ,, tt).
+    abstract now do 3 intro.
+  Defined.
 
-Lemma algebra_lift_is_unique
-  {T T' T'' : algebraic_theory}
-  {A : algebra T}
-  {A'' : algebra T''}
-  (F : algebraic_theory_morphism T' T)
-  (F' : algebraic_theory_morphism T'' T')
-  (G' : (A'' : algebra_disp_cat _) -->[(F' : algebraic_theory_cat⟦_, _⟧) · F] A)
-  : ∏ t : (∑ gg, (gg ;; algebra_cleaving_morphism F A) = G'), t = (algebra_lift F F' G').
-Proof.
-  intro t.
-  apply subtypePath.
-  {
-    intro.
-    apply homsets_disp.
-  }
-  apply displayed_algebra_morphism_eq.
-  exact (!comp_disp_cartesian _ _ (pr11 t) _ @ maponpaths _ (pr2 t)).
-Qed.
+  Section Lift.
 
-Definition algebra_cleaving_is_cartesian
-  {T T' : algebraic_theory}
-  (F : algebraic_theory_morphism T' T)
-  (A : algebra T)
-  : is_cartesian (algebra_cleaving_morphism F A)
-  := (λ _ F' _ G', algebra_lift F F' G' ,, algebra_lift_is_unique F F' G').
+    Context {T'' : algebraic_theory}.
+    Context {A'' : algebra T''}.
+    Context (F' : algebraic_theory_morphism T'' T').
+    Context (G' : (A'' : algebra_disp_cat _) -->[(F' : algebraic_theory_cat⟦_, _⟧) · F] A).
+
+    Definition algebra_cleaving_induced_morphism
+      : (A'' : algebra_disp_cat _) -->[F'] algebra_cleaving_algebra.
+    Proof.
+      use (pr1 G' ,, _ ,, tt).
+      abstract (do 3 intro; now rewrite (pr12 G')).
+    Defined.
+
+    Definition algebra_lift
+      : ∑ gg, (gg ;; algebra_cleaving_morphism) = G'.
+    Proof.
+      exists algebra_cleaving_induced_morphism.
+      apply displayed_algebra_morphism_eq.
+      exact (comp_disp_cartesian _ _ _ _).
+    Defined.
+
+    Lemma algebra_lift_is_unique
+      : ∏ t : (∑ gg, (gg ;; algebra_cleaving_morphism) = G'), t = algebra_lift.
+    Proof.
+      intro t.
+      apply subtypePath.
+      {
+        intro.
+        apply homsets_disp.
+      }
+      apply displayed_algebra_morphism_eq.
+      exact (!comp_disp_cartesian _ _ (pr11 t) _ @ maponpaths _ (pr2 t)).
+    Qed.
+
+  End Lift.
+
+  Definition algebra_cleaving_is_cartesian
+    : is_cartesian algebra_cleaving_morphism
+    := (λ _ F' _ G', algebra_lift F' G' ,, algebra_lift_is_unique F' G').
+
+End Cleaving.
 
 Definition algebra_cleaving
   : cleaving algebra_disp_cat
