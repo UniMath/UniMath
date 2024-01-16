9bd95b6bcb5f01c5107c35e06b264b51823f0716
Tempestas Ludi <tempestasludi@gmail.com>
2024-01-16 16:22:47 +0100

    Generalize the construction of free_monoid_theory, and change the weq equivalence between monoids and algebras into an equivalence of categories instead

--- UniMath/AlgebraicTheories/Examples/FreeMonoidTheory.v
+++ UniMath/AlgebraicTheories/Examples/FreeMonoidTheory.v
@@ -7,16 +7,22 @@
 
   Contents
   1. The definition of the algebraic theory [free_monoid_theory]
-  2. The equivalence between algebras and monoids [monoid_weq_free_monoid_theory_algebra]
+  2. The functor from monoids to algebras [monoid_to_algebra]
+  3. Constructing a monoid from an algebra [free_monoid_theory_algebra_to_monoid]
+  4. The equivalence of categories between algebras and monoids [monoid_algebra_equivalence]
 
-  **************************************************************************************************)
+ **************************************************************************************************)
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 Require Export UniMath.Algebra.BinaryOperations.
 Require Import UniMath.Algebra.Free_Monoids_and_Groups.
 Require Import UniMath.Algebra.IteratedBinaryOperations.
 Require Import UniMath.Algebra.Monoids.
+Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
+Require Import UniMath.CategoryTheory.Core.Isos.
+Require Import UniMath.CategoryTheory.Equivalences.Core.
+Require Import UniMath.CategoryTheory.categories.monoids.
 Require Import UniMath.Combinatorics.Lists.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
 Require Import UniMath.Combinatorics.Vectors.
@@ -24,41 +30,38 @@ Require Import UniMath.Combinatorics.Vectors.
 Require Import UniMath.AlgebraicTheories.AlgebraicTheoryCategoryCore.
 Require Import UniMath.AlgebraicTheories.AlgebraicTheories.
 Require Import UniMath.AlgebraicTheories.Algebras.
+Require Import UniMath.AlgebraicTheories.AlgebraCategoryCore.
+Require Import UniMath.AlgebraicTheories.AlgebraCategory.
+Require Import UniMath.AlgebraicTheories.AlgebraMorphisms.
+Require Import UniMath.AlgebraicTheories.Examples.FreeObjectTheory.
 
 Local Open Scope algebraic_theories.
 Local Open Scope vec_scope.
+Local Open Scope cat.
 
 (** * 1. The definition of the algebraic theory *)
 
-Definition free_monoid_theory_data : algebraic_theory_data.
-Proof.
-  use make_algebraic_theory_data.
-  - intro n.
-    use tpair.
-    * exact (free_monoid (stnset n)).
-    * apply isasetmonoid.
-  - intros n i.
-    exact (free_monoid_unit (i : stnset n)).
-  - intros m n f g.
-    exact (free_monoid_extend (λ i : stnset m, g i) f).
-Defined.
-
-Lemma free_monoid_is_theory : is_algebraic_theory free_monoid_theory_data.
-Proof.
-  use make_is_algebraic_theory.
-  - intros l m n f_l f_m f_n.
-    exact (maponpaths (λ x, pr1monoidfun _ _ x f_l)
-      (free_monoid_extend_funcomp2 (X := stnset l) f_m f_n)).
-  - now do 4 intro.
-  - intros n f.
-    apply (free_monoid_extend_comp (idmonoidiso (free_monoid (stnset n)))).
-Qed.
-
 Definition free_monoid_theory
   : algebraic_theory
-  := make_algebraic_theory _ free_monoid_is_theory.
+  := free_object_theory monoid_free_forgetful_adjunction.
 
-(** * 2. The equivalence between algebras and monoids *)
+(** * 2. The functor from monoids to algebras *)
+
+Definition monoid_to_algebra
+  : monoid_category ⟶ algebra_cat free_monoid_theory
+  := free_object_algebra_functor monoid_free_forgetful_adjunction.
+
+(** * 3. Constructing a monoid from an algebra *)
+
+Definition op_el
+  : free_monoid_theory 2
+  := op
+    (free_monoid_unit (X := stnset _) (make_stn 2 0 (idpath _)))
+    (free_monoid_unit (X := stnset _) (make_stn 2 1 (idpath _))).
+
+Definition unit_el
+  : free_monoid_theory 0
+  := unel _.
 
 Definition free_monoid_theory_algebra_to_setwithbinop
   (A : algebra free_monoid_theory)
@@ -67,11 +70,7 @@ Proof.
   use make_setwithbinop.
   - exact A.
   - intros a b.
-    refine (action _ _).
-    + exact (op
-      (free_monoid_unit (make_stn 2 0 (idpath _) : stnset _))
-      (free_monoid_unit (make_stn 2 1 (idpath _) : stnset _))).
-    + exact (weqvecfun _ [(a ; b)]).
+    exact (action op_el (weqvecfun _ [(a ; b)])).
 Defined.
 
 Local Lemma move_action_through_weqvecfun
@@ -90,10 +89,8 @@ Lemma free_monoid_theory_algebra_to_setwithbinop_op_is_assoc
 Proof.
   intros a b c.
   pose (f := weqvecfun _ [(a ; b ; c)]).
-  pose (Hf := λ i Hi,
-    !(pr_action _ (make_stn 3 i Hi) f)).
-  do 4 rewrite (idpath _ : op (X := (free_monoid_theory_algebra_to_setwithbinop A)) _ _
-    = action _ _).
+  pose (Hf := λ i Hi, !(pr_action _ (make_stn 3 i Hi) f)).
+  cbn -[weqvecfun action].
   rewrite (Hf 0 (idpath true) : a = _),
     (Hf 1 (idpath true) : b = _),
     (Hf 2 (idpath true) : c = _).
@@ -102,7 +99,7 @@ Qed.
 
 Definition free_monoid_theory_algebra_to_unit (A : algebra free_monoid_theory)
   : A
-  := action (T := free_monoid_theory) (unel _) (weqvecfun _ [()]).
+  := action unit_el (weqvecfun _ [()]).
 
 Lemma free_monoid_theory_algebra_to_isunit (A : algebra free_monoid_theory)
   : isunit
@@ -111,9 +108,8 @@ Lemma free_monoid_theory_algebra_to_isunit (A : algebra free_monoid_theory)
 Proof.
   split;
     intro x;
-    now rewrite (idpath _ : op (X := free_monoid_theory_algebra_to_setwithbinop A) _ _
-        = action (T := free_monoid_theory) _ _),
-      <- (pr_action _ (make_stn 1 0 (idpath _)) (λ _, x)),
+    cbn -[weqvecfun action];
+    now rewrite <- (pr_action _ (make_stn 1 0 (idpath _)) (λ _, x)),
       <- (lift_constant_action 1 _ (λ _, x) : _ = free_monoid_theory_algebra_to_unit A),
       (move_action_through_weqvecfun (λ _, x)),
       <- comp_action.
@@ -135,71 +131,67 @@ Definition free_monoid_theory_algebra_to_monoid (A : algebra free_monoid_theory)
   := free_monoid_theory_algebra_to_setwithbinop A ,,
     free_monoid_theory_algebra_to_setwithbinop_op_is_monoidop A.
 
-Definition monoid_to_free_monoid_theory_algebra_data (M : monoid)
-  : algebra_data free_monoid_theory.
+(** * 4. The equivalence *)
+
+Definition fully_faithful_monoid_to_algebra
+  : fully_faithful monoid_to_algebra.
 Proof.
-  use make_algebra_data.
-  - exact M.
-  - intros n f m.
-    exact (free_monoid_extend (X := stnset n) m f).
+  intros M M'.
+  use isweq_iso.
+  - refine (λ (f : algebra_morphism _ _), _).
+    exists f.
+    abstract (
+      use make_ismonoidfun;
+      [ exact (λ m m', mor_action f op_el (weqvecfun _ [(m ; m')]))
+      | exact (mor_action f unit_el (weqvecfun _ [()])) ]
+    ).
+  - abstract (
+      intro f;
+      apply subtypePath;
+      [ intro;
+        apply isapropdirprod;
+        repeat (apply impred_isaprop; intro);
+        apply setproperty
+      | apply idpath ]
+    ).
+  - abstract (
+      intro f;
+      now apply algebra_morphism_eq
+    ).
 Defined.
 
-Lemma monoid_to_is_free_monoid_theory_algebra (M : monoid)
-  : is_algebra (monoid_to_free_monoid_theory_algebra_data M).
-Proof.
-  use make_is_algebra.
-  - intros m n f g a.
-    unfold AlgebraCategory.comp_action_ax.
-    now rewrite (idpath _ : action f (λ _, action _ a)
-      = free_monoid_extend (X := stnset m) (λ _, free_monoid_extend (X := stnset n) _ _) _),
-      <- free_monoid_extend_funcomp2.
-  - now do 3 intro.
-Qed.
-
-Definition monoid_to_free_monoid_theory_algebra (M : monoid)
-  : algebra free_monoid_theory
-  := make_algebra _ (monoid_to_is_free_monoid_theory_algebra M).
-
-Lemma monoid_to_free_monoid_theory_algebra_and_back (M : monoid)
-  : free_monoid_theory_algebra_to_monoid (monoid_to_free_monoid_theory_algebra M) = M.
-Proof.
-  apply subtypePath.
-  {
-    intro.
-    apply isapropismonoidop.
-  }
-  apply idpath.
-Qed.
-
-Lemma free_monoid_theory_algebra_to_monoid_and_back
+Lemma mor_action_ax_identity
   (A : algebra free_monoid_theory)
-  : monoid_to_free_monoid_theory_algebra (free_monoid_theory_algebra_to_monoid A) = A.
+  (n : nat)
+  (f : free_monoid_theory n)
+  (a : stn n → (monoid_to_algebra (free_monoid_theory_algebra_to_monoid A) : algebra _))
+  : mor_action_ax (identity _) (idfun A)
+    (@action free_monoid_theory
+      (monoid_to_algebra (free_monoid_theory_algebra_to_monoid A) : algebra _))
+    (@action free_monoid_theory A) n f a.
 Proof.
-  use algebra_eq.
-  - apply idpath.
-  - intros n f.
-    rewrite idpath_transportf.
-    apply funextfun.
-    intro a.
-    pose (A' := monoid_to_free_monoid_theory_algebra (free_monoid_theory_algebra_to_monoid A)).
-    apply (list_ind (λ x, action (A := A') x a = action (A := A) x a)).
-    + exact (!(lift_constant_action (A := A) _ (unel _) a)).
-    + intros i xs Haction.
-      rewrite (idpath _ : cons i xs = (op
-        (free_monoid_unit (make_stn 2 0 (idpath true) : stnset _))
-        (free_monoid_unit (make_stn 2 1 (idpath true) : stnset _))
-      : free_monoid_theory 2) • (weqvecfun _ [((free_monoid_unit i) ; xs)])).
-      do 2 rewrite comp_action.
-      now rewrite <- (move_action_through_weqvecfun (A := A) a),
-        <- Haction,
-        (pr_action A _ _ :
-          action (free_monoid_unit i : free_monoid_theory n) _ = _).
+  pose (A' := monoid_to_algebra (free_monoid_theory_algebra_to_monoid A) : algebra _).
+  apply (list_ind (λ x, action (A := A') x a = action (A := A) x a)).
+  - exact (!(lift_constant_action (A := A) _ (unel _) a)).
+  - intros i xs Haction.
+    refine (comp_action A' op_el (weqvecfun _ [(free_monoid_unit i ; xs)]) a @ !_).
+    refine (comp_action A op_el (weqvecfun _ [(free_monoid_unit i ; xs)]) a @ !_).
+    now rewrite <- (move_action_through_weqvecfun (A := A) a),
+      <- Haction,
+      (pr_action A _ _ :
+        action (free_monoid_unit i : free_monoid_theory n) _ = _).
 Qed.
 
-Definition monoid_weq_free_monoid_theory_algebra
-  : monoid ≃ (algebra free_monoid_theory)
-  := weq_iso
-    monoid_to_free_monoid_theory_algebra
-    free_monoid_theory_algebra_to_monoid
-    monoid_to_free_monoid_theory_algebra_and_back
-    free_monoid_theory_algebra_to_monoid_and_back.
+Definition monoid_algebra_equivalence
+  : adj_equivalence_of_cats monoid_to_algebra.
+Proof.
+  apply rad_equivalence_of_cats.
+  - apply monoid_category_is_univalent.
+  - apply fully_faithful_monoid_to_algebra.
+  - intro A.
+    apply hinhpr.
+    exists (free_monoid_theory_algebra_to_monoid A).
+    use make_algebra_z_iso.
+    + apply identity_z_iso.
+    + apply mor_action_ax_identity.
+Defined.
