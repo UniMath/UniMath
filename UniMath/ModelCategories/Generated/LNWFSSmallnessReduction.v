5dfd2db3b679d6d469e93dd2c26f1fd234589af5
Dennis <randomspam1906@gmail.com>
2024-03-05 17:53:04 +0100

    only 1st and 2nd

--- UniMath/ModelCategories/Generated/LNWFSSmallnessReduction.v
+++ UniMath/ModelCategories/Generated/LNWFSSmallnessReduction.v
@@ -673,7 +673,7 @@ Opaque CL.
 Local Lemma Ff_lt_preserves_colim_impl_LNWFS_lt_preserves_colim_mor_disp_pr1_category_tensor_commutes :
   z_iso_mor Ffiso = pr1 LNWFSarr.
 Proof.
-  (* use (colimArrowUnique' FfCCbase).
+  use (colimArrowUnique' FfCCbase).
   intro v.
   etrans. apply (colimArrowCommutes FfCCbase).
   apply pathsinv0.
@@ -683,8 +683,8 @@ Proof.
   use subtypePath; [intro; apply isapropdirprod; apply homset_property|].
   etrans. use pr1_transportf_const.
   etrans. apply (colimArrowCommutes (CCFf_pt_ob1 CC Ldbase f)).
-  reflexivity. *)
-Admitted.
+  reflexivity.
+Qed.
 
 End ProjectCoconeComm.
 
@@ -725,7 +725,7 @@ Local Lemma Ff_lt_preserves_colim_impl_LNWFS_lt_preserves_colim_inv_in_precat :
     (pr1 (colimArrow LNWFSCC _ (mapcocone (monoidal_left_tensor (L : LNWFS_mon)) d (colimCocone CL))))
     (pr1 base_mor).
 Proof.
-  split.
+  (* split.
   - etrans; [|exact (pr12 base_mor)].
     apply (cancel_postcomposition _ _ (pr1 base_mor)).
     use (colimArrowUnique' FfCCbase).
@@ -741,8 +741,8 @@ Proof.
     etrans. exact (colimArrowCommutes FfCCbase _ _ v).
     apply pathsinv0.
     etrans. apply (colimArrowCommutes FfCCbase).
-    reflexivity.
-Qed.
+    reflexivity. *)
+Admitted.
 
 End InvInPrecat.
 
