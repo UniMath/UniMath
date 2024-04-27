21eae97c7df245c5989ac585e365a3f947ddf4e5
Tempestas Ludi <tempestasludi@gmail.com>
2024-04-27 19:44:59 +0200

    Renamed "RingedSpace" to "PresheafedSpace", tinker a bit

--- UniMath/Test/PresheafedSpace.v
+++ UniMath/Test/PresheafedSpace.v
@@ -73,86 +73,24 @@ Section Sheaves.
     (F : presheafed_space_hom X X')
     : presheafed_space_comp F (presheafed_space_id X') = F.
   Proof.
-    unfold presheafed_space_comp.
-    simpl.
     use presheafed_space_hom_eq.
     - apply (id_right (C := top_cat)).
     - apply nat_trans_eq_alt.
       intro U.
+      refine (maponpaths (λ x, _ · (_ · x) · _) (id_left _) @ _).
       simpl.
-      repeat rewrite assoc.
-      refine (maponpaths (λ x, x · _ · _) (id_right _) @ _).
-      repeat rewrite assoc'.
-      refine (_ @ id_left _).
       rewrite assoc.
-      refine (maponpaths (λ x, _ · x) _ @ _).
-      Search (idtomor _ _).
-      + refine (maponpaths (λ x, _ · x U) (!eq_idtoiso_idtomor _ _ _) @ _).
+      refine (maponpaths (λ x, _ · x · _) (functor_comp _ _ _) @ _).
       rewrite assoc.
-      rewrite assoc.
-      refine (maponpaths (λ x, x · _) (_ : # (pr2 X' :  _ ⟶ _) (identity U) = identity ((pr2 X' : _ ⟶ _) U)) @ _).
-      refine (maponpaths (λ x, x · _ · _) _ @ _).
-
-      unfold continuous_open_preimage.
-
-      cbn.
-      refine (maponpaths (λ x, (_ · x) · _) _ @ _).
-      {
-        cbn.
-        unfold functor_on_morphisms.
-        match goal with
-        | [ |- _ ?a ?b _ = _ ] => pose (U0 := a); pose (U1 := b)
+      refine (maponpaths (λ x, _ · x · _) _ @ _).
+      + match goal with
+        | [ |- # _ (identity ?a') = _ ] => pose (a := a')
         end.
-        pose (
-          H0 := subtypePath (λ _, propproperty _) (idpath _ : pr1 (continuous_open_preimage (pr1 F) U) = pr1 U0)
-        ).
-        pose (
-          H1 := subtypePath (λ _, propproperty _) (idpath _ : pr1 (continuous_open_preimage (pr1 F) U) = pr1 U1)
-        ).
-        unfold U0, U1 in H0, H1.
-        refine (transportf (λ x, _ x _ _) H0 (transportf (λ x, _ x _ _) H1 _)).
-        rewrite X0.
-        refine (maponpaths (λ (x : topological_space_category _), pr21 (pr2 X : _ ⟶ _) x _ _) _ @ _).
-        rewrite (_ : )
-        assert ((continuous_open_preimage (pr1 F)
-     (continuous_open_preimage
-        ((λ x : pr11 X', x),,
-         (λ (x0 : pr11 X') (x1 : hsubtype (pr11 X'))
-          (X0 : ishinh_UU (∑ O : Open, O x0 × (∏ x : pr11 X', O x → x1 x))), X0)) U)) = (continuous_open_preimage (pr1 F) U)).
-        cbn.
-        unfold continuous_open_preimage.
-        cbn.
-        unfold continuous_function_is_continuous.
-        exact (functor_id (pr2 X) _).
-        now cbn.
-      }
-      apply id_right.
-      refine (maponpaths (λ x, (x · _) · _) _ @ _).
-      + exact (functor_id _ _ U).
-      rewrite (functor_id (pr2 X') U).
-      match goal with
-      | [ |- nat_trans_comp _ _ _ ?a _ = _ ] => pose a
-      end.
-      simpl.
-      simpl in p.
-
-      simpl.
-      assert (continuous_to_functor (identity (pr1 X' : top_cat)) = functor_identity _).
-      {
-        refine (isotoid [_, _] _ _).
-        - apply is_univalent_functor_category.
-          apply is_univalent_topological_space_category.
-        - apply z_iso_from_nat_z_iso.
-          apply identity_to_functor_is_identity.
-      }
-      simpl in X0.
-      symmetry in X0.
-      induction X0.
-      rewrite X0.
-      epose (isotoid _ _ (z_iso_from_nat_z_iso _ _)).
+        epose (H := functor_id (pr2 X) a).
+        exact H.
   Qed.
 
-  Definition space_with_local_data_disp_cat
+  (* Definition space_with_local_data_disp_cat
     : disp_cat (total_category disp_top).
   Proof.
     use tpair.
@@ -188,6 +126,6 @@ Section Sheaves.
         unfold transportb.
 
         Search (transportb ()).
-  Defined.
+  Defined. *)
 
 End Sheaves.
