a9a1c03a3920f51cd60820e0bb14674b06648824
nmvdw <nnmvdw@gmail.com>
2024-05-04 12:15:53 +0200

    Fix import file

--- UniMath/CategoryTheory/EnrichedCats/Profunctors/Composition.v
+++ UniMath/CategoryTheory/EnrichedCats/Profunctors/Composition.v
@@ -1,4 +1,4 @@
-Require Import UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.CompositionProf.
-Require Import UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Unitors.
-Require Import UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Associators.
-Require Import UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Whiskering.
+Require Export UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.CompositionProf.
+Require Export UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Unitors.
+Require Export UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Associators.
+Require Export UniMath.CategoryTheory.EnrichedCats.Profunctors.Composition.Whiskering.
