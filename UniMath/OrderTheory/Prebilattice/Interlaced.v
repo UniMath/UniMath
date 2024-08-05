f3c300735e8117e2ae3b5a10a45129ada5ceb755
Georgios V. Pitsiladis <research@gpits.me>
2024-08-05 15:22:48 +0300

    Add basic definitions and results regarding pre-bilattices.
    Pre-bilattices are structures comprising two lattices (over the same set).
    
    First, this commit defines pre-bilattices, bounded prebilattices,
    and the displayed category of pre-bilattices.
    
    Moreover, it defines two special cases:
    interlaced and distributive pre-bilattices.
    
    Also, it defines the construction of product pre-bilattices
    and proves that product pre-bilattices are interlaced
    and that every interlaced pre-bilattice is isomoprhic to a product one
    (representation theorem).
    
    The pre-bilattice FOUR is included as an example of a pre-bilattice.

---
+++ UniMath/OrderTheory/Prebilattice/Interlaced.v
@@ -0,0 +1,245 @@
+(** Interlaced pre-bilattice *)
+(** Georgios V. Pitsiladis, Aug. 2024 - *)
+
+(**
+ In interlaced pre-bilattices, the two lattices
+ are linked via monotonicity of the operators of each lattice
+ with respect to the other lattice.
+**)
+Require Import UniMath.OrderTheory.Prebilattice.Prebilattice.
+Require Import UniMath.OrderTheory.Lattice.Lattice.
+Require Import UniMath.Foundations.Sets.
+Require Import UniMath.MoreFoundations.Subtypes.
+Require Import UniMath.Algebra.BinaryOperations.
+
+Section definition .
+  Definition interlacing {X : hSet} (op : binop X) (R : hrel X) :=
+    ∏ x y z : X, R x y -> R (op x z) (op y z).
+
+  Lemma isaprop_interlacing {X : hSet} (op : binop X) (R : hrel X) : isaprop (interlacing op R).
+  Proof.
+    do 4 (use impred; intro).
+    use propproperty.
+  Defined.
+
+  Definition is_interlaced {X : hSet} (b : prebilattice X) : UU :=
+    interlacing (consensus b) (tle b)
+                ×
+                interlacing (gullibility b) (tle b)
+                ×
+                interlacing (meet b) (kle b)
+                ×
+                interlacing (join b) (kle b).
+
+  Lemma isaprop_is_interlaced {X : hSet} {b : prebilattice X} : isaprop (is_interlaced b).
+  Proof.
+    do 3 (try use (isapropdirprod)); use isaprop_interlacing.
+  Defined.
+
+  Definition interlaced_prebilattice (X : hSet) :=
+    ∑ b : prebilattice X,  is_interlaced b.
+
+  Definition make_interlaced_prebilattice {X : hSet} {b : prebilattice X} (is : is_interlaced b) : interlaced_prebilattice X := b,,is.
+
+  Definition interlaced_prebilattice_to_prebilattice {X : hSet} (b: interlaced_prebilattice X) : prebilattice X := pr1 b.
+  Coercion interlaced_prebilattice_to_prebilattice : interlaced_prebilattice >-> prebilattice .
+End definition.
+
+Section equalities.
+  Definition interlaced_prebilattice_eq {X : hSet} (b1 : interlaced_prebilattice X) (b2 : interlaced_prebilattice X) (m : meet b1 ~ meet b2) (j : join b1 ~ join b2) (c : consensus b1 ~ consensus b2) (g : gullibility b1 ~ gullibility b2): b1 = b2.
+  Proof.
+    use total2_paths_f.
+    - use (prebilattice_eq b1 b2 m j c g).
+    - apply isaprop_is_interlaced.
+  Defined.
+
+  Definition interlaced_prebilattice_transportf {X1 X2 : hSet} (b1 : interlaced_prebilattice X1) (b2 : interlaced_prebilattice X2) (p : X1 = X2)
+             (m : meet (transportf interlaced_prebilattice p b1) ~ meet b2)
+             (j : join (transportf interlaced_prebilattice p b1) ~ join b2)
+             (c : consensus (transportf interlaced_prebilattice p b1) ~ consensus b2)
+             (g : gullibility (transportf interlaced_prebilattice p b1) ~ gullibility b2)
+    : transportf interlaced_prebilattice p b1 = b2.
+  Proof.
+    use total2_paths_f.
+    - unfold interlaced_prebilattice.
+      rewrite (pr1_transportf p b1).
+      use prebilattice_transportf; [set (i := m) | set (i := j) | set (i := c) | set (i := g)];
+        use (homotcomp _ i);
+        induction p;
+        rewrite idpath_transportf, idpath_transportf;
+        use homotrefl.
+    - apply isaprop_is_interlaced.
+  Defined.
+End equalities.
+
+Section properties.
+  Lemma interlacing_consensus_t {X : hSet} (b : interlaced_prebilattice X) : interlacing (consensus b) (tle b).
+  Proof .
+    exact (pr12 b) .
+  Defined.
+  Lemma interlacing_gullibility_t {X : hSet} (b : interlaced_prebilattice X) : interlacing (gullibility b) (tle b).
+  Proof.
+    exact (pr122 b) .
+  Defined.
+  Lemma interlacing_meet_k {X : hSet} (b : interlaced_prebilattice X) :  interlacing (meet b) (kle b) .
+  Proof.
+    exact (pr1 (pr2 (pr2 (pr2 b)))) .
+  Defined.
+  Lemma interlacing_join_k {X : hSet} (b : interlaced_prebilattice X) :  interlacing (join b) (kle b) .
+  Proof.
+    exact (pr2 (pr2 (pr2 (pr2 b)))) .
+  Defined.
+
+  Definition double_interlacing {X : hSet} {op : binop X} {R : hrel X} (i : interlacing op R) (a : istrans R) (c : iscomm op) {x y z w : X} (p : R x y) (q : R z w) : R (op x z) (op y w).
+  Proof.
+    use (a (op x z) (op y z)).
+    - use i. exact p.
+    - rewrite (c y z), (c y w). use i . exact q.
+  Defined.
+
+  Lemma double_interlacing_gullibility_t {X : hSet} {b : interlaced_prebilattice X} {x y z w : X} (p : tle b x y) (q : tle b z w) : tle b (gullibility b x z) (gullibility b y w) .
+  Proof.
+    exact (double_interlacing (interlacing_gullibility_t _) (istrans_Lle (tlattice _)) (iscomm_Lmax (klattice _)) p q) .
+  Defined.
+  Lemma double_interlacing_consensus_t {X : hSet} {b : interlaced_prebilattice X} {x y z w : X} (p : tle b x y) (q : tle b z w) : tle b (consensus b x z) (consensus b y w) .
+  Proof.
+    exact (double_interlacing (interlacing_consensus_t _) (istrans_Lle (tlattice _)) (iscomm_Lmin (klattice _)) p q) .
+  Defined.
+  Lemma double_interlacing_meet_k {X : hSet} {b : interlaced_prebilattice X} {x y z w : X} (p : kle b x y) (q : kle b z w) : kle b (meet b x z) (meet b y w) .
+  Proof.
+    exact (double_interlacing (interlacing_meet_k _) (istrans_Lle (klattice _)) (iscomm_Lmin (tlattice _)) p q).
+  Defined.
+  Lemma double_interlacing_join_k {X : hSet} {b : interlaced_prebilattice X} {x y z w : X} (p : kle b x y) (q : kle b z w) : kle b (join b x z) (join b y w) .
+  Proof.
+    exact (double_interlacing (interlacing_join_k _) (istrans_Lle (klattice _)) (iscomm_Lmax (tlattice _)) p q).
+  Defined.
+
+  Lemma top_interlacing_gullibility_t {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : tle b x z) (q : tle b y z) : tle b (gullibility b x y) z.
+  Proof.
+    rewrite <- (Lmax_id (klattice b) z).
+    use (double_interlacing_gullibility_t p q).
+  Defined.
+  Lemma top_interlacing_consensus_t {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : tle b x z) (q : tle b y z) : tle b (consensus b x y) z.
+  Proof.
+    rewrite <- (Lmin_id (klattice b) z).
+    use (double_interlacing_consensus_t p q).
+  Defined.
+  Lemma top_interlacing_join_k {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : kle b x z) (q : kle b y z) : kle b (join b x y) z.
+  Proof.
+    rewrite <- (Lmax_id (tlattice b) z).
+    use (double_interlacing_join_k p q).
+  Defined.
+  Lemma top_interlacing_meet_k {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : kle b x z) (q : kle b y z) : kle b (meet b x y) z.
+  Proof.
+    rewrite <- (Lmin_id (tlattice b) z).
+    use (double_interlacing_meet_k p q).
+  Defined.
+
+  Lemma bottom_interlacing_join_k {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : kle b z x) (q : kle b z y) : kle b z (join b x y).
+  Proof.
+    rewrite <- (Lmax_id (tlattice b) z).
+    use (double_interlacing_join_k p q).
+  Defined.
+  Lemma bottom_interlacing_meet_k {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : kle b z x) (q : kle b z y) : kle b z (meet b x y).
+  Proof.
+    rewrite <- (Lmin_id (tlattice b) z).
+    use (double_interlacing_meet_k p q).
+  Defined.
+  Lemma bottom_interlacing_gullibility_t {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : tle b z x) (q : tle b z y) : tle b z (gullibility b x y).
+  Proof.
+    rewrite <- (Lmax_id (klattice b) z).
+    use (double_interlacing_gullibility_t p q).
+  Defined.
+  Lemma bottom_interlacing_consensus_t {X : hSet} {b : interlaced_prebilattice X} {x y z : X} (p : tle b z x) (q : tle b z y) : tle b z (consensus b x y).
+  Proof.
+    rewrite <- (Lmin_id (klattice b) z).
+    use (double_interlacing_consensus_t p q).
+  Defined.
+
+  Lemma dual_prebilattice_is_interlaced {X : hSet} (b : interlaced_prebilattice X) : is_interlaced (dual_prebilattice b).
+  Proof.
+    destruct b as [? [? [? [? ?]]]] . do 3 (try split); assumption .
+  Defined.
+
+  Lemma opp_prebilattice_is_interlaced {X : hSet} (b : interlaced_prebilattice X) : is_interlaced (opp_prebilattice b).
+  Proof.
+    do 3 (try split); intros ? ? ? H;
+    [set (i := (interlacing_gullibility_t b)) | set (i := interlacing_consensus_t b) | set (i := interlacing_join_k b) | set (i := interlacing_meet_k b)];
+    use (Lmax_le_eq_l _ _ _ (i _ _ _ (Lmax_le_eq_l _ _ _ H))).
+  Defined.
+
+  Lemma t_opp_prebilattice_is_interlaced {X : hSet} (b : interlaced_prebilattice X) : is_interlaced (t_opp_prebilattice b).
+  Proof.
+    do 3 (try split); intros ? ? ? H.
+    - use (Lmax_le_eq_l _ _ _ (interlacing_consensus_t _ _ _ _ (Lmax_le_eq_l _ _ _ H))).
+    - use (Lmax_le_eq_l _ _ _ (interlacing_gullibility_t _ _ _ _ (Lmax_le_eq_l _ _ _ H))).
+    - use (interlacing_join_k _ _ _ _ H).
+    - use (interlacing_meet_k _ _ _ _ H).
+  Defined.
+
+  Lemma k_opp_prebilattice_is_interlaced {X : hSet} (b : interlaced_prebilattice X) : is_interlaced (k_opp_prebilattice b).
+  Proof.
+    destruct (t_opp_prebilattice_is_interlaced (make_interlaced_prebilattice (dual_prebilattice_is_interlaced b))) as [? [? [? ?]]].
+    do 3 (try split); assumption.
+  Defined.
+
+  Lemma interlaced_leqt_leqk_leqkmeet {X : hSet} : ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, tle b x r × kle b r y) -> kle b x (meet b y x).
+  Proof.
+    intros ? x y [? [p1 p2]].
+    set (w := (meet _ y x)); rewrite <- (Lmin_le_eq_r _ _ _ p1).
+    use (interlacing_meet_k _ _ _ _ p2).
+  Defined.
+
+  Lemma interlaced_leqk_leqt_leqtconsensus {X:hSet}: ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, kle b x r × tle b r y) -> tle b x (consensus b y x).
+  Proof.
+    intro b'.
+    use (interlaced_leqt_leqk_leqkmeet (make_interlaced_prebilattice (dual_prebilattice_is_interlaced b'))).
+  Defined.
+
+  Lemma interlaced_leqk_geqt_geqtconsensus {X:hSet}: ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, kle b x r × tle b y r) -> tle b (consensus b y x) x.
+  Proof.
+    intros ? x y [? [p1 p2]].
+    set (w := (consensus _ y x)); rewrite <- (Lmin_le_eq_r _ _ _ p1).
+    use (interlacing_consensus_t _ _ _ _ p2).
+  Defined.
+
+  Lemma interlaced_leqt_leqk_leqtconsensus {X : hSet} : ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, tle b x r × kle b r y) -> tle b x (consensus b y x).
+  Proof.
+    intros b' ? ? [? [p1 p2]].
+    set (pp := interlaced_leqt_leqk_leqkmeet _ _ _ (_,,p1,,p2)).
+    rewrite (iscomm_meet) in pp.
+    use (interlaced_leqk_leqt_leqtconsensus _ _ _ (_,, pp,, Lmin_le_r (tlattice b') _ _)).
+  Defined.
+
+  Lemma interlaced_geqt_leqk_geqtconsensus {X : hSet} : ∏ (b : interlaced_prebilattice X) (x y : X), (∑ r : X, tle b r x × kle b r y) -> tle b (consensus b y x) x.
+  Proof.
+    intros b x y [r [p1 p2]].
+    assert (p1' :  tle (make_interlaced_prebilattice (t_opp_prebilattice_is_interlaced b)) x r).
+    {
+      rewrite <- p1, iscomm_Lmin.
+      use Lmax_absorb.
+    }
+    set (t := consensus b y x).
+    assert (p : Lmax (tlattice b) t x = x).
+    {
+      rewrite iscomm_Lmax.
+      exact (interlaced_leqt_leqk_leqtconsensus (make_interlaced_prebilattice (t_opp_prebilattice_is_interlaced b)) x y (r,,p1',,p2)).
+    }
+    rewrite <- p.
+    use Lmin_absorb.
+  Defined.
+
+  Lemma interlaced_geqt_geqk_geqkjoin {X: hSet} :  ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, tle b r x × kle b y r) -> kle b (join b y x) x .
+  Proof.
+    intros b' ? ? [? [p1 p2]].
+    set (bop := make_interlaced_prebilattice (opp_prebilattice_is_interlaced b')).
+    set (H := interlaced_leqt_leqk_leqkmeet bop _ _ (_,,(Lmax_le_eq_l _ _ _ p1),, (Lmax_le_eq_l _ _ _ p2))).
+    use (Lmax_le_eq_l _ _ _ H).
+  Defined.
+
+  Lemma interlaced_leqk_leqt_leqkmeet {X : hSet} :  ∏ (b : interlaced_prebilattice X) (x y : X) , (∑ r : X, kle b x r × tle b r y) -> kle b x (meet b y x) .
+  Proof .
+    intro b'.
+    use (interlaced_leqt_leqk_leqtconsensus (make_interlaced_prebilattice (dual_prebilattice_is_interlaced b'))).
+  Defined.
+End properties.
---
+++ UniMath/OrderTheory/Prebilattice/PrebilatticeDisplayedCat.v
@@ -0,0 +1,106 @@
+(** Displayed category of pre-bilattices *)
+(** Georgios V. Pitsiladis, Aug. 2024 - *)
+Require Import UniMath.OrderTheory.Prebilattice.Prebilattice.
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.DisplayedCats.SIP.
+Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
+Require Import UniMath.CategoryTheory.Categories.HSET.Core.
+Require Import UniMath.CategoryTheory.Core.Univalence.
+Require Import UniMath.CategoryTheory.DisplayedCats.Total.
+Require Import UniMath.Foundations.Sets.
+Require Import UniMath.MoreFoundations.Notations.
+Require Import UniMath.MoreFoundations.Tactics.
+
+Definition respects_prebilattice_structure {X1 : hSet} {X2 : hSet} (b1 : prebilattice X1) (b2 : prebilattice X2) (f : X1 -> X2) : UU
+:= ∀ x y,
+  make_hProp _ (
+               isapropdirprod _ _
+                              (setproperty _  (meet b2 (f x) (f y)) (f (meet b1 x y)))
+                              (isapropdirprod _ _ (setproperty _ (join b2 (f x) (f y)) (f (join b1 x y)))
+                                              (isapropdirprod _ _
+                                                              (setproperty _  (consensus b2 (f x) (f y)) (f (consensus b1 x y))  )
+                                                              (setproperty _  (gullibility b2 (f x) (f y)) (f (gullibility b1 x y)))))
+             ).
+
+Lemma weqinv_respects_prebilattice_structure {X1 : hSet} {X2 : hSet} (b1 : prebilattice X1) (b2 : prebilattice X2) (f : weq X1 X2) (p : respects_prebilattice_structure b1 b2 f) : respects_prebilattice_structure b2 b1 (invmap f).
+Proof.
+intros x y.
+do 3 (try use make_dirprod);
+rewrite <- (homotweqinvweq f x), <- (homotweqinvweq f y);
+[
+  rewrite (pr1 (p (invmap f x) (invmap f y))) |
+  rewrite (pr12 (p (invmap f x) (invmap f y))) |
+  rewrite (pr122 (p (invmap f x) (invmap f y))) |
+  rewrite (pr222 (p (invmap f x) (invmap f y)))
+];
+now rewrite homotweqinvweq,  homotweqinvweq, homotinvweqweq.
+Defined.
+
+Definition prebilattice_disp_category : category.
+Proof.
+use (@total_category hset_category).
+use disp_cat_from_SIP_data.
+- intro X.
+  exact (prebilattice X).
+- intros X1 X2 b1 b2 f.
+  use (respects_prebilattice_structure b1 b2 f).
+- intros X1 X2 b1 b2 f p1 p2.
+  use impred_prop.
+- now intros X1 b1 x y.
+- intros X1 X2 X3 b1 b2 b3 f g p1 p2 x y.
+  cbn.
+  do 3 (try use make_dirprod).
+  -- now rewrite (pr1 (p2 (f x) (f y))), (pr1 (p1 x y)).
+  -- now rewrite (pr12 (p2 (f x) (f y))), (pr12 (p1 x y)).
+  -- now rewrite (pr122 (p2 (f x) (f y))), (pr122 (p1 x y)).
+  -- now rewrite (pr222 (p2 (f x) (f y))), (pr222 (p1 x y)).
+Defined.
+
+Lemma is_univalent_prebilattice_disp_category : is_univalent (prebilattice_disp_category) .
+Proof.
+use SIP.
+- use Univalence.is_univalent_HSET.
+- use isaset_prebilattice.
+- intros X b1 b2 p1 p2.
+  use prebilattice_eq; intro x; use weqfunextsec; intro y.
+  -- exact (pr1 (p2 x y)).
+  -- exact (pr12 (p2 x y)).
+  -- exact (pr122 (p2 x y)).
+  -- exact (pr222 (p2 x y)).
+Defined.
+
+Definition prebilattice_iso {X1 : hSet} {X2 : hSet} (b1 : prebilattice X1) (b2 : prebilattice X2) (f : weq X1 X2) (p : respects_prebilattice_structure b1 b2 f) : @Isos.iso prebilattice_disp_category (X1,,b1) (X2,,b2).
+Proof.
+set( f' := (pr1weq f,, p) : (prebilattice_disp_category ⟦ X1,, b1, X2,, b2 ⟧)%cat).
+set (p_inv := weqinv_respects_prebilattice_structure b1 b2 f p).
+exists f'.
+use Isos.is_iso_from_is_z_iso.
+exists (invmap f,, p_inv).
+use make_dirprod.
+- use total2_paths_f.
+  -- use weqfunextsec.
+     use homotinvweqweq.
+  -- use weqfunextsec.
+     intro x.
+     use weqfunextsec.
+     intro y.
+     do 3 (try use dirprod_paths); use PartA.Unnamed_thm.
+- use total2_paths_f.
+  -- use weqfunextsec.
+     use homotweqinvweq.
+  -- use weqfunextsec.
+     intro x.
+     use weqfunextsec.
+     intro y.
+     do 3 (try use dirprod_paths); use PartA.Unnamed_thm.
+Defined.
+
+Definition prebilattice_transportf_iso {X1 : hSet} {X2 : hSet} (b1 : prebilattice X1) (b2 : prebilattice X2) (f : weq X1 X2) (p : respects_prebilattice_structure b1 b2 f) : ∑ p, transportf prebilattice p b1 = b2.
+Proof.
+  set (I := Isos.iso_to_z_iso (prebilattice_iso b1 b2 f p)).
+  set (i := fiber_paths (isotoid prebilattice_disp_category is_univalent_prebilattice_disp_category I)) .
+  (* cbn in i. (* in order to see what path to use in the next line *)  *)
+  exists ((base_paths (X1,, b1) (X2,, b2) (isotoid prebilattice_disp_category is_univalent_prebilattice_disp_category I))).
+  exact i.
+Defined.
