1e15ac0083b7e1515d8ee005dea7ca5771fe7551
Tempestas Ludi <tempestasludi@gmail.com>
2024-08-19 13:36:52 +0200

    Move stuff in AlgebraicGeometry/Topology to Topology/Topology

--- UniMath/Topology/Topology.v
+++ UniMath/Topology/Topology.v
@@ -11,6 +11,10 @@ Require Import UniMath.Algebra.Groups.
 
 Local Open Scope subtype.
 
+Declare Scope open_scope.
+Delimit Scope open_scope with open.
+Open Scope open.
+
 Section Open.
 
   Context {X : UU}.
@@ -200,6 +204,64 @@ Section Topology_pty.
   Qed.
 
 End Topology_pty.
+
+Section OpenOperations.
+
+  Context {X : TopologicalSpace}.
+
+  Definition union_open_hsubtype (A : hsubtype Open) : hsubtype X :=
+    union (λ U, ∃ H : isOpen U, A (U ,, H)).
+
+  Lemma is_open_union_open_hsubtype (A : hsubtype Open) : isOpen (union_open_hsubtype A).
+  Proof.
+    apply isOpen_union. intros U HU.
+    use (hinhuniv _ HU); intros H. exact (pr1 H).
+  Qed.
+
+  Definition union_open (A : hsubtype Open) : Open :=
+    union_open_hsubtype A ,, is_open_union_open_hsubtype A.
+
+  Lemma contained_in_union_open
+    {A : hsubtype (@Open X)}
+    (U : A)
+    : pr1 U ⊆ union_open A.
+  Proof.
+    intros x Hx. apply hinhpr. exists (pr1 U). use make_dirprod.
+    - apply hinhpr. exists (pr21 U). exact (pr2 U).
+    - exact Hx.
+  Qed.
+
+  Lemma hexists_open_neighborhood
+    {A : hsubtype (@Open X)}
+    (p : carrier (union_open A)):
+    ∃ U : A, pr1 U (pr1 p).
+  Proof.
+    induction p as [p Hp]. simpl in Hp.
+    use (hinhuniv _ Hp); intro Hu.
+    induction Hu as [u Hu]. induction Hu as [Hu Hup].
+    use (hinhfun _ Hu); intro H.
+    induction H as [H HUu].
+    exists (make_carrier _ _ HUu). exact Hup.
+  Qed.
+
+  Definition binary_intersection_open {X : TopologicalSpace} (u v : @Open X) : Open :=
+    (λ x : X, u x ∧ v x) ,, isOpen_and _ _ (pr2 u) (pr2 v).
+
+  Definition intersection_contained1
+    (u v : @Open X)
+    : binary_intersection_open u v ⊆ u
+    := λ _ Hx, pr1 Hx.
+
+  Definition intersection_contained2
+    (u v : @Open X)
+    : binary_intersection_open u v ⊆ v
+    := λ _ Hx, pr2 Hx.
+
+End OpenOperations.
+
+Notation "⋃ S" := (union_open S) (at level 100, no associativity) : open_scope.
+
+Notation "u ∩ v" := (binary_intersection_open u v)  (at level 40, left associativity) : open_scope.
 
 (** ** Neighborhood *)
 
