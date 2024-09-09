9c690edd2e95e10cb610a9ab2164769a8cca3a61
Tempestas Ludi <tempestasludi@gmail.com>
2024-09-09 15:53:31 +0200

    Create the index file Constructions

---
+++ UniMath/CategoryTheory/DisplayedCats/Constructions.v
@@ -0,0 +1,12 @@
+(**************************************************************************************************
+
+  Constructions involving displayed categories
+
+  This is an index file for the folder Constructions, mostly for backwards compatibility.
+
+ **************************************************************************************************)
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.CategoryWithStructure.
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.DisplayedFunctorCat.
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.FullSubcategory.
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.FunctorLift.
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.Product.
