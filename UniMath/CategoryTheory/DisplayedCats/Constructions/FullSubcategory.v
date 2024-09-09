e776b9da5c0624e86bc68150c532ad432aa5b670
Tempestas Ludi <tempestasludi@gmail.com>
2024-09-09 16:31:12 +0200

    Add a doccomment to FullSubcategory

--- UniMath/CategoryTheory/DisplayedCats/Constructions/FullSubcategory.v
+++ UniMath/CategoryTheory/DisplayedCats/Constructions/FullSubcategory.v
@@ -1,9 +1,19 @@
+(**************************************************************************************************
 
-(** Full subcategory as total category of a displayed category
+  The displayed construction of a full subcategory
 
-Partial contents:
-*)
+  If we construct a displayed category where the structure on the morphisms is the unit type, and
+  the structure on the objects is a proposition, then the resulting total category is a full
+  subcategory of the base category.
+  Because both the morphisms and the equalities of the objects in the resulting subcategory are
+  inherited from the base category, the resulting category is univalent if the base category is.
 
+  Contents
+  1. The construction of the full subcategory as a displayed category [disp_full_sub]
+  2. Univalence [disp_full_sub_univalent]
+  3. Shortcuts for just the resulting total category [full_subcat] [is_univalent_full_subcat]
+
+ **************************************************************************************************)
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.CategoryTheory.Core.Categories.
@@ -16,9 +26,7 @@ Require Import UniMath.CategoryTheory.DisplayedCats.Univalence.
 
 Local Open Scope cat.
 
-(** * Full subcategories *)
-
-Section full_subcat.
+(** * 1. The construction of the full subcategory as a displayed category *)
 
 Definition disp_full_sub_ob_mor (C : precategory_ob_mor) (P : C → UU)
   : disp_cat_ob_mor C
@@ -48,6 +56,8 @@ Proof.
   - apply disp_full_sub_locally_prop.
 Defined.
 
+(** * 2. Univalence *)
+
 Lemma disp_full_sub_univalent (C : category) (P : C → UU) :
   (∏ x : C, isaprop (P x)) →
   is_univalent_disp (disp_full_sub C P).
@@ -61,6 +71,8 @@ Proof.
   - intro. apply (@isaprop_is_z_iso_disp _ (disp_full_sub C P)).
 Defined.
 
+(** * 3. Shortcuts for just the resulting total category *)
+
 Definition full_subcat (C : category) (P : C → UU) : category := total_category (disp_full_sub C P).
 
 Definition is_univalent_full_subcat (C : category) (univC : is_univalent C) (P : C → UU) :
@@ -71,5 +83,3 @@ Proof.
   - exact univC.
   - exact (disp_full_sub_univalent _ _ H).
 Defined.
-
-End full_subcat.
