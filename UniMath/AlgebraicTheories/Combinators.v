cca842f887c2c9c0f5559f9dbee639e75fb8cfa6
Arnoud van der Leer <tempestasludi@gmail.com>
2025-02-22 15:19:47 +0100

    Show the equivalence between the presheaves on the monoid L1 and the Lawvere theory of an algebraic theory (#1993)

--- UniMath/AlgebraicTheories/Combinators.v
+++ UniMath/AlgebraicTheories/Combinators.v
@@ -436,6 +436,39 @@ Proof.
     exact (maponpaths (λ x, ((_ ∘ x) ∘ _)) (var_subst _ _ _)).
 Qed.
 
+Lemma subst_is_compose
+  (L : lambda_theory)
+  (Lβ : has_β L)
+  (A B : L 0)
+  : appx A • (λ _, appx B) = appx (A ∘ B).
+Proof.
+  refine '(maponpaths (λ x, x • _) (appx_to_app _) @ _).
+  refine '(maponpaths (λ x, _ • (λ _, x)) (appx_to_app _) @ _).
+  refine '(subst_app _ _ _ _ @ _).
+  refine '(maponpaths (λ x, (app x _)) (subst_inflate _ _ _) @ _).
+  refine '(maponpaths (λ x, (app _ x)) (var_subst _ _ _) @ !_).
+  refine '(appx_to_app _ @ _).
+  refine '(maponpaths (λ x, (app x _)) (inflate_compose _ _ _) @ _).
+  refine '(app_compose _ Lβ _ _ _ @ _).
+  apply (maponpaths (λ x, app (A • x) _)).
+  apply funextfun.
+  intro i.
+  apply fromempty.
+  apply (negstn0 i).
+Qed.
+
+Lemma compose_is_subst
+  (L : lambda_theory)
+  (Lβ : has_β L)
+  (A B : L 1)
+  : abs A ∘ abs B = abs (A • (λ _, B)).
+Proof.
+  refine '(_ @ maponpaths (λ x, abs (x • _)) (Lβ _ _)).
+  refine '(_ @ maponpaths (λ x, abs (_ • (λ _, x))) (Lβ _ _)).
+  refine '(_ @ !maponpaths _ (subst_is_compose _ Lβ _ _)).
+  exact (maponpaths _ (!Lβ _ _)).
+Qed.
+
 (** * 3. Pair *)
 
 Definition pair
