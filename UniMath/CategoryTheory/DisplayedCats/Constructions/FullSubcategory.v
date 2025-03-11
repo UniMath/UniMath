06ef7783fb3f97672bc5e2595fff2931a0295537
Tempestas Ludi <tempestasludi@gmail.com>
2025-03-11 16:25:14 +0100

    Fix a problem that was caused by moving material around

--- UniMath/CategoryTheory/DisplayedCats/Constructions/FullSubcategory.v
+++ UniMath/CategoryTheory/DisplayedCats/Constructions/FullSubcategory.v
@@ -19,11 +19,10 @@
  **************************************************************************************************)
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.MoreFoundations.All.
-Require Import UniMath.CategoryTheory.Core.Categories.
-Require Import UniMath.CategoryTheory.Core.Functors.
-Require Import UniMath.CategoryTheory.Core.Univalence.
+Require Import UniMath.CategoryTheory.Core.Prelude.
 
 Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.CategoryTheory.DisplayedCats.Functors.
 Require Import UniMath.CategoryTheory.DisplayedCats.Total.
 Require Import UniMath.CategoryTheory.DisplayedCats.Isos.
 Require Import UniMath.CategoryTheory.DisplayedCats.Univalence.
